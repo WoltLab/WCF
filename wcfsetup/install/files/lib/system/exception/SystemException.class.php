1ed41239009d2ed7f7dc77dfaa3a8578348eaff5
kurtextrem <kurtextrem@gmail.com>
2011-07-19 17:35:04 +0200

    8 Tab indent

--- wcfsetup/install/files/lib/system/exception/SystemException.class.php
+++ wcfsetup/install/files/lib/system/exception/SystemException.class.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace wcf\system\exception;
-
 use wcf\util\StringUtil;
 
 /**
@@ -15,7 +13,6 @@ use wcf\util\StringUtil;
  * @category 	Community Framework
  */
 class SystemException extends \Exception implements PrintableException {
-
 	/**
 	 * error description
 	 * @var string
@@ -139,7 +136,7 @@ class SystemException extends \Exception implements PrintableException {
 							<b>php version:</b> <?php echo StringUtil::encodeHTML(phpversion()); ?><br>
 							<b>wcf version:</b> <?php echo WCF_VERSION; ?><br>
 							<b>date:</b> <?php echo gmdate('r'); ?><br>
-							<b>request:</b> <?php if (isset($_SERVER['REQUEST_URI'])) echo StringUtil::encodeHTML($_SERVER['REQUEST_URI']); ?><br>
+							<b>request:</b> <?php if (isset($_SERVER['REQUEST_URI']))  echo StringUtil::encodeHTML($_SERVER['REQUEST_URI']); ?><br>
 							<b>referer:</b> <?php if (isset($_SERVER['HTTP_REFERER'])) echo StringUtil::encodeHTML($_SERVER['HTTP_REFERER']); ?><br>
 						</p>
 
