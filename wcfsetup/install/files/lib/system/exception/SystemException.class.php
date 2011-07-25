952539b1589b1cbd79d720d5eb6aaca95aca1626
Alexander Ebert <ebert@woltlab.com>
2011-07-25 16:35:20 +0200

    Merge pull request #37 from kurtextrem/master

--- wcfsetup/install/files/lib/system/exception/SystemException.class.php
+++ wcfsetup/install/files/lib/system/exception/SystemException.class.php
@@ -4,7 +4,7 @@ use wcf\util\StringUtil;
 
 /**
  * A SystemException is thrown when an unexpected error occurs.
- * 
+ *
  * @author	Marcel Werk
  * @copyright	2001-2011 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
@@ -33,10 +33,10 @@ class SystemException extends \Exception implements IPrintableException {
 	
 	/**
 	 * Creates a new SystemException.
-	 * 
+	 *
 	 * @param	string		$message	error message
 	 * @param	integer		$code		error code
-	 * @param	string		$description	description of the error	
+	 * @param	string		$description	description of the error
 	 */
 	public function __construct($message = '', $code = 0, $description = '') {
 		parent::__construct($message, $code);
@@ -45,7 +45,7 @@ class SystemException extends \Exception implements IPrintableException {
 	
 	/**
 	 * Returns the description of this exception.
-	 * 
+	 *
 	 * @return 	string
 	 */
 	public function getDescription() {
@@ -54,7 +54,7 @@ class SystemException extends \Exception implements IPrintableException {
 	
 	/**
 	 * Removes database password from stack trace.
-	 * @see Exception::getTraceAsString()
+	 * @see	\Exception::getTraceAsString()
 	 */
 	public function __getTraceAsString() {
 		$string = preg_replace('/Database->__construct\(.*\)/', 'Database->__construct(...)', $this->getTraceAsString());
@@ -71,78 +71,84 @@ class SystemException extends \Exception implements IPrintableException {
 		
 		// print report
 		echo '<?xml version="1.0" encoding="UTF-8"?>';
+		
 		?>
-		
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" xml:lang="en">
-<head>
-<title>Fatal error: <?php echo StringUtil::encodeHTML($this->getMessage()); ?></title>
-<style type="text/css">
-/*<![CDATA[*/
-.systemException {
-	border: 1px outset lightgrey;
-	padding: 3px;
-	background-color: lightgrey;
-	text-align: left;
-	overflow: auto;
-	font-family: Verdana, Helvetica, sans-serif;
-	font-size: .8em;
-}
-.systemException div {
-	border: 1px inset lightgrey;
-	padding: 4px;
-}
-.systemException h1 {
-	background-color: #154268;
-	padding: 4px;
-	color: #fff;
-	margin: 0 0 3px 0;
-	font-size: 1.15em;
-}
-.systemException h2 {
-	font-size: 1.1em;
-	margin-bottom: 0;
-}
-.systemException pre, .systemException p {
-	margin: 0;
-}
-.systemException pre {
-	font-size: .85em;
-	font-family: "Courier New";
-}
-/*]]>*/
-</style>
-</head>
-<body>
-	<div class="systemException">
-		<h1>Fatal error: <?php echo StringUtil::encodeHTML($this->getMessage()); ?></h1>
-	
-		<div>
-			<p><?php echo $this->getDescription(); ?></p>
-			<?php if ($this->getCode()) { ?><p>You get more information about the problem in the official WoltLab knowledge base: <a href="http://www.woltlab.com/help/?code=<?php echo intval($this->getCode()); ?>">http://www.woltlab.com/help/?code=<?php echo intval($this->getCode()); ?></a></p><?php } ?>
-			
-			<h2>Information:</h2>
-			<p>
-				<b>error message:</b> <?php echo StringUtil::encodeHTML($this->getMessage()); ?><br />
-				<b>error code:</b> <?php echo intval($this->getCode()); ?><br />				
-				<?php echo $this->information; ?>
-				<b>file:</b> <?php echo StringUtil::encodeHTML($this->getFile()); ?> (<?php echo $this->getLine(); ?>)<br />
-				<b>php version:</b> <?php echo StringUtil::encodeHTML(phpversion()); ?><br />
-				<b>wcf version:</b> <?php echo WCF_VERSION; ?><br />
-				<b>date:</b> <?php echo gmdate('r'); ?><br />
-				<b>request:</b> <?php if (isset($_SERVER['REQUEST_URI'])) echo StringUtil::encodeHTML($_SERVER['REQUEST_URI']); ?><br />
-				<b>referer:</b> <?php if (isset($_SERVER['HTTP_REFERER'])) echo StringUtil::encodeHTML($_SERVER['HTTP_REFERER']); ?><br />
-			</p>
-			
-			<h2>Stacktrace:</h2>
-			<pre><?php echo StringUtil::encodeHTML($this->__getTraceAsString()); ?></pre>
-		</div>
-		
-		<?php echo $this->functions; ?>
-	</div>
-</body>
-</html>
 
-<?php
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<title>Fatal error: <?php echo StringUtil::encodeHTML($this->getMessage()); ?></title>
+				<style>
+					.systemException {
+						border: 1px outset lightgrey;
+						padding: 3px;
+						background-color: lightgrey;
+						text-align: left;
+						overflow: auto;
+						font-family: Verdana, Helvetica, sans-serif;
+						font-size: .8em;
+					}
+					.systemException div {
+						border: 1px inset lightgrey;
+						padding: 4px;
+					}
+					.systemException h1 {
+						background-color: #154268;
+						padding: 4px;
+						color: #fff;
+						margin: 0 0 3px 0;
+						font-size: 1.15em;
+						word-wrap: break-word;
+					}
+					.systemException h2 {
+						font-size: 1.1em;
+						margin-bottom: 0;
+					}
+					.systemException pre, .systemException p {
+						margin: 0;
+					}
+					.systemException pre {
+						font-size: .85em;
+						font-family: "Courier New";
+						text-overflow: ellipsis;
+						overflow: hidden;
+					}
+					.systemException pre:hover{
+						overflow: auto;
+						text-overflow: clip;
+					}
+				</style>
+			</head>
+			<body>
+				<div class="systemException">
+					<h1>Fatal error: <?php echo StringUtil::encodeHTML($this->getMessage()); ?></h1>
+
+					<div>
+						<p><?php echo $this->getDescription(); ?></p>
+						<?php if ($this->getCode()) { ?><p>You get more information about the problem in the official WoltLab knowledge base: <a href="http://www.woltlab.com/help/?code=<?php echo intval($this->getCode()); ?>">http://www.woltlab.com/help/?code=<?php echo intval($this->getCode()); ?></a></p><?php } ?>
+
+						<h2>Information:</h2>
+						<p>
+							<b>error message:</b> <?php echo StringUtil::encodeHTML($this->getMessage()); ?><br>
+							<b>error code:</b> <?php echo intval($this->getCode()); ?><br>
+							<?php echo $this->information; ?>
+							<b>file:</b> <?php echo StringUtil::encodeHTML($this->getFile()); ?> (<?php echo $this->getLine(); ?>)<br>
+							<b>php version:</b> <?php echo StringUtil::encodeHTML(phpversion()); ?><br>
+							<b>wcf version:</b> <?php echo WCF_VERSION; ?><br>
+							<b>date:</b> <?php echo gmdate('r'); ?><br>
+							<b>request:</b> <?php if (isset($_SERVER['REQUEST_URI']))  echo StringUtil::encodeHTML($_SERVER['REQUEST_URI']); ?><br>
+							<b>referer:</b> <?php if (isset($_SERVER['HTTP_REFERER'])) echo StringUtil::encodeHTML($_SERVER['HTTP_REFERER']); ?><br>
+						</p>
+
+						<h2>Stacktrace:</h2>
+						<pre><?php echo StringUtil::encodeHTML($this->__getTraceAsString()); ?></pre>
+					</div>
+
+				<?php echo $this->functions; ?>
+				</div>
+			</body>
+		</html>
+
+		<?php
 	}
 }
