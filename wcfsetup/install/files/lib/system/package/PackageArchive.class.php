da89969e9b4a8bae1da1a2e8bf5a9135588129e0
Marcel Werk <burntime@woltlab.com>
2011-08-05 19:48:14 +0200

    Fixed some package update errors (update is still not working)

--- wcfsetup/install/files/lib/system/package/PackageArchive.class.php
+++ wcfsetup/install/files/lib/system/package/PackageArchive.class.php
@@ -361,7 +361,7 @@ class PackageArchive {
 		
 		if ($this->package != null) {
 			$validFromVersion = null;
-			foreach ($this->update as $fromVersion => $update) {
+			foreach ($this->instructions['update'] as $fromVersion => $update) {
 				if (Package::checkFromversion($this->package->packageVersion, $fromVersion)) {
 					$validFromVersion = $fromVersion;
 					break;
