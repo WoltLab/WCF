b68f0af4378647df83e713f2ad15288b6db39695
Matthias Schmidt <gravatronics@live.com>
2011-08-05 11:33:12 +0200

    Fixed 2 errors that prevented the installation of standalone application
    `PackageInstallationNodeBuilder::buildPackageNode()` didn't handle
    non-existing `packageDescription` and `authorURL` tags (which may be
    omitted according to the `package.xsd` file) so that they were set to
    null which caused problems later when
    `PackageInstallationDispatcher::installPackage()` is called and tries to
    save that node data since the database table columns
    `packageDescription` and `authorURL` may not be null.  Also the
    `SQLPackageInstallationPlugin::install()` method called
    `Package::getAbbreviation()` the wrong way.

--- wcfsetup/install/files/lib/system/package/PackageInstallationNodeBuilder.class.php
+++ wcfsetup/install/files/lib/system/package/PackageInstallationNodeBuilder.class.php
@@ -319,13 +319,13 @@ class PackageInstallationNodeBuilder {
 				'package' => $this->installation->getArchive()->getPackageInfo('name'),
 				'packageName' => $this->installation->getArchive()->getPackageInfo('packageName'),
 				'instanceNo' => $instanceNo,
-				'packageDescription' => $this->installation->getArchive()->getPackageInfo('packageDescription'),
+				'packageDescription' => $this->installation->getArchive()->getPackageInfo('packageDescription') !== null ? $this->installation->getArchive()->getPackageInfo('packageDescription') : '',
 				'packageVersion' => $this->installation->getArchive()->getPackageInfo('version'),
 				'packageDate' => $this->installation->getArchive()->getPackageInfo('date'),
 				'packageURL' => $this->installation->getArchive()->getPackageInfo('packageURL'),
 				'standalone' => $this->installation->getArchive()->getPackageInfo('standalone'),
 				'author' => $this->installation->getArchive()->getAuthorInfo('author'),
-				'authorURL' => $this->installation->getArchive()->getAuthorInfo('authorURL'),
+				'authorURL' => $this->installation->getArchive()->getAuthorInfo('authorURL') !== null ? $this->installation->getArchive()->getAuthorInfo('authorURL') : '',
 				'installDate' => TIME_NOW,
 				'updateDate' => TIME_NOW
 			))
