21894db28e54e5b87e1481d32fad69b136d9657d
Matthias Schmidt <gravatronics@live.com>
2011-08-15 16:48:38 +0200

    Fixed namespace

--- wcfsetup/install/files/lib/system/option/user/group/InfiniteIntegerUserGroupOptionType.class.php
+++ wcfsetup/install/files/lib/system/option/user/group/InfiniteIntegerUserGroupOptionType.class.php
@@ -1,5 +1,5 @@
 <?php
-namespace wcf\system\option\userGroup;
+namespace wcf\system\option\user\group;
 
 /**
  * InfiniteIntegerUserGroupOptionType is an implementation of IUserGroupOptionType
@@ -11,12 +11,12 @@ namespace wcf\system\option\userGroup;
  * @copyright	2001-2011 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	com.woltlab.wcf
- * @subpackage	system.option.userGroup
+ * @subpackage	system.option.user.group
  * @category 	Community Framework
  */
 class InfiniteIntegerUserGroupOptionType extends IntegerUserGroupOptionType {
 	/**
-	 * @see wcf\system\option\userGroup\IUserGroupOptionType::merge()
+	 * @see wcf\system\option\user\group\IUserGroupOptionType::merge()
 	 */
 	public function merge(array $values) {
 		if (in_array(-1, $values)) return -1;
--- wcfsetup/install/files/lib/system/option/user/group/InfiniteInverseIntegerUserGroupOptionType.class.php
+++ wcfsetup/install/files/lib/system/option/user/group/InfiniteInverseIntegerUserGroupOptionType.class.php
@@ -1,5 +1,5 @@
 <?php
-namespace wcf\system\option\userGroup;
+namespace wcf\system\option\user\group;
 
 /**
  * InfiniteInverseIntegerUserGroupOptionType is an implementation of IUserGroupOptionType
@@ -10,12 +10,12 @@ namespace wcf\system\option\userGroup;
  * @copyright	2001-2011 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	com.woltlab.wcf
- * @subpackage	system.option.userGroup
+ * @subpackage	system.option.user.group
  * @category 	Community Framework
  */
 class InfiniteInverseIntegerUserGroupOptionType extends InverseIntegerUserGroupOptionType {
 	/**
-	 * @see wcf\system\option\userGroup\IUserGroupOptionType::merge()
+	 * @see wcf\system\option\user\group\IUserGroupOptionType::merge()
 	 */
 	public function merge(array $values) {
 		foreach ($values as $key => $value) {
