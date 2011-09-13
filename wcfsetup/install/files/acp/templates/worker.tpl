532f117328b472af2b686c01d389e34a5f369aa4
Alexander Ebert <ebert@woltlab.com>
2011-09-13 21:06:34 +0200

    Implemented worker system based upon AJAX dialogs.
    Reworked the previous worker system relying on the old-style WCF 1.1 worker system as used by the installation itself. Right now the system is pretty unstable and I'm not sure wether the loop calculation is right.

---
+++ wcfsetup/install/files/acp/templates/worker.tpl
@@ -0,0 +1,14 @@
+<div id="workerContainer">
+	<header class="mainHeading">
+		<img src="{@RELATIVE_WCF_DIR}icon/workerL.png" alt="" />
+		<hgroup>
+			<h1>Aufgaben werden ausgeführt &hellip;</h1>
+			<h2>Aktueller Schritt: <span id="workerAction">{lang}wcf.package.installation.step.prepare{/lang}</span></h2>
+			<p><progress id="workerProgress" value="0" max="100" style="width: 200px;">0%</progress></p>
+		</hgroup>
+	</header>
+	
+	<div id="workerInnerContentContainer" class="border content" style="display: none;">
+		<div id="workerInnerContent" class="container-1"></div>
+	</div>
+</div>
---
+++ wcfsetup/install/files/lib/system/worker/IWorker.class.php
@@ -0,0 +1,53 @@
+<?php
+namespace wcf\system\worker;
+
+/**
+ * Basic interface for workers.
+ * 
+ * @author	Alexander Ebert
+ * @copyright	2001-2011 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf
+ * @subpackage	system.worker
+ * @category 	Community Framework
+ */
+interface IWorker {
+	/**
+	 * Creates a new worker object with additional parameters
+	 * 
+	 * @param	array		$parameters
+	 */
+	public function __construct(array $parameters);
+	
+	/**
+	 * Sets current loop count.
+	 * 
+	 * @param	integer		$loopCount
+	 */
+	public function setLoopCount($loopCount);
+	
+	/**
+	 * Gets current process, integer between 0 and 100. If the progress
+	 * hits 100 the worker will terminate.
+	 * 
+	 * @return	integer
+	 */
+	public function getProgress();
+	
+	/**
+	 * Executes worker action.
+	 */
+	public function execute();
+	
+	/**
+	 * Returns parameters previously given within __construct().
+	 * 
+	 * @return	array
+	 */
+	public function getParameters();
+	
+	/**
+	 * Validates parameters.
+	 */
+	public function validate();
+}
