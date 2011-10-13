37fe79756eb890f873a28d95dfc26405cc1da649
Marcel Werk <burntime@woltlab.com>
2011-10-13 17:38:36 +0200

    Added interface for breadcrumb provider

---
+++ wcfsetup/install/files/lib/system/breadcrumb/IBreadcrumbProvider.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace wcf\system\breadcrumb;
+
+/**
+ * Interface for breadcrumb provider.
+ *
+ * @author	Marcel Werk
+ * @copyright	2001-2011 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf
+ * @subpackage	system.breadcrumb
+ * @category 	Community Framework
+ */
+interface IBreadcrumbProvider {
+	/**
+	 * Returns a Breadcrumb object.
+	 * 
+	 * @return	wcf\system\breadcrumb\Breadcrumb
+	 */
+	public function getBreadcrumb();
+}
