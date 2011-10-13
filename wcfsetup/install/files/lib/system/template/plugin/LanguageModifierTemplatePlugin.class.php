bce0caa2e26601468d611c4bade6c4dc2f5ed15b
Marcel Werk <burntime@woltlab.com>
2011-10-13 19:26:02 +0200

    Added language modifier

---
+++ wcfsetup/install/files/lib/system/template/plugin/LanguageModifierTemplatePlugin.class.php
@@ -0,0 +1,26 @@
+<?php
+namespace wcf\system\template\plugin;
+use wcf\system\template\TemplateEngine;
+use wcf\system\WCF;
+
+/**
+ * The 'language' modifier returns dynamic language variables.
+ * 
+ * Usage:
+ * {$string|language}
+ *
+ * @author 	Marcel Werk
+ * @copyright	2001-2011 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf
+ * @subpackage	system.template.plugin
+ * @category 	Community Framework
+ */
+class LanguageModifierTemplatePlugin implements IModifierTemplatePlugin {
+	/**
+	 * @see wcf\system\template\IModifierTemplatePlugin::execute()
+	 */
+	public function execute($tagArgs, TemplateEngine $tplObj) {
+		return WCF::getLanguage()->getDynamicVariable($tagArgs[0]);
+	}
+}
