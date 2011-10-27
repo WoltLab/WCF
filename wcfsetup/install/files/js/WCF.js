e5b8376f2f73947e3c7145b830847c646dc6f55f
Luzifr <szekely@woltlab.com>
2011-10-27 12:21:23 +0200

    Overlay dialogues
    Re-implemented the centering script due to CSS changes; optimized
    overlay position & look.

--- wcfsetup/install/files/js/WCF.js
+++ wcfsetup/install/files/js/WCF.js
@@ -2803,6 +2803,11 @@ $.widget('ui.wcfDialog', $.ui.dialog, {
 		this.options.width = 'auto';
 		
 		$.ui.dialog.prototype._init.apply(this, arguments);
+				
+		// center dialog on resize
+		$(window).resize($.proxy(function() {
+			this.option('position', 'center');
+		}, this));
 	}
 });
 
@@ -2872,6 +2877,11 @@ $.widget('ui.wcfAJAXDialog', $.ui.dialog, {
 				this.element.parent('.ui-dialog').find('a.ui-dialog-titlebar-close').empty().remove();
 			}
 		}
+		
+		// center dialog on window resize
+		$(window).resize($.proxy(function() {
+			this.option('position', 'center');
+		}, this));
 	},
 	
 	/**
