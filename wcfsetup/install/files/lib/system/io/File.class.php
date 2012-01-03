3cb09b2c5e414c79db1bceef1e6168c0e8a59b6c
H3llGhost <h3llghost@gmx.de>
2012-01-03 16:36:54 +0100

    File.class.php: Added Handler for stream functions
    RemoteFile.class.php:
    fsockopen doesn't support stream context, replaced
    with stream_socket_client

--- wcfsetup/install/files/lib/system/io/File.class.php
+++ wcfsetup/install/files/lib/system/io/File.class.php
@@ -4,18 +4,18 @@ use wcf\system\exception\SystemException;
 
 /**
  * The File class handles all file operations.
- * 
+ *
  * Example:
  * using php functions:
  * $fp = fopen('filename', 'wb');
  * fwrite($fp, '...');
  * fclose($fp);
- * 
+ *
  * using this class:
  * $file = new File('filename');
  * $file->write('...');
  * $file->close();
- * 
+ *
  * @author	Marcel Werk
  * @copyright	2001-2011 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
@@ -29,16 +29,16 @@ class File {
 	 * @var resource
 	 */
 	protected $resource = null;
-	
+
 	/**
 	 * filename
 	 * @var string
 	 */
 	protected $filename = '';
-	
+
 	/**
 	 * Opens a new file.
-	 * 
+	 *
 	 * @param 	string		$filename
 	 * @param 	string		$mode
 	 * @param	array		$options
@@ -56,22 +56,26 @@ class File {
 			throw new SystemException('Can not open file ' . $filename);
 		}
 	}
-	
+
 	/**
 	 * Calls the specified function on the open file.
 	 * Do not call this function directly. Use $file->write('') instead.
-	 * 
+	 *
 	 * @param 	string		$function
 	 * @param 	array		$arguments
 	 */
 	public function __call($function, $arguments) {
 		if (function_exists('f' . $function)) {
 			array_unshift($arguments, $this->resource);
-	       		return call_user_func_array('f' . $function, $arguments);
+			return call_user_func_array('f' . $function, $arguments);
+		}
+		else if (strpos($function, 'stream') == 0) {
+			array_unshift($arguments, $this->resource);
+			return call_user_func_array($function, $arguments);
 		}
 		else if (function_exists($function)) {
 			array_unshift($arguments, $this->filename);
-	       		return call_user_func_array($function, $arguments);
+			return call_user_func_array($function, $arguments);
 		}
 		else {
 			throw new SystemException('Can not call file method ' . $function);
--- wcfsetup/install/files/lib/system/io/RemoteFile.class.php
+++ wcfsetup/install/files/lib/system/io/RemoteFile.class.php
@@ -4,7 +4,7 @@ use wcf\system\exception\SystemException;
 
 /**
  * The RemoteFile class opens a connection to a remote host as a file.
- * 
+ *
  * @author	Marcel Werk
  * @copyright	2001-2011 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
@@ -14,64 +14,76 @@ use wcf\system\exception\SystemException;
  */
 class RemoteFile extends File {
 	/**
+	 * socket transport protocol see http://www.php.net/manual/en/transports.php
+	 * @var string
+	 */
+	protected $protocol = '';
+	
+	/**
 	 * host address
 	 * @var string
 	 */
 	protected $host = '';
-	
+
 	/**
 	 * port
 	 * @var integer
 	 */
 	protected $port = 0;
-	
+
 	/**
 	 * error number
 	 * @var integer
 	 */
 	protected $errorNumber = 0;
-	
+
 	/**
 	 * error description
 	 * @var string
 	 */
 	protected $errorDesc = '';
-	
+
 	/**
 	 * Opens a new connection to a remote host.
-	 * 
+	 *
+	 * @param	string		$protocol
 	 * @param 	string		$host
 	 * @param 	integer		$port
-	 * @param 	integer		$timeout
+	 * @param 	integer		$connectionTimeout
 	 * @param	array		$options
+	 * @param	integer		$flags
 	 */
-	public function __construct($host, $port, $timeout = 30, $options = array()) {
+	public function __construct($protocol, $host, $port, $connectionTimeout = 30, $options = array(), $flags = STREAM_CLIENT_CONNECT) {
+		$this->protocol = $protocol;
 		$this->host = $host;
 		$this->port = $port;
+		$this->remoteSocket = $this->protocol.'://'.$this->host.':'.$this->port;
+
 		if (count($options)) {
 			$context = stream_context_create($options);
-			$this->resource = fsockopen($host, $port, $this->errorNumber, $this->errorDesc, $timeout, $context);
+			$this->resource = stream_socket_client($this->remoteSocket, $this->errorNumber, $this->errorDesc, $connectionTimeout, $flags, $context);
 		}
 		else {
-			$this->resource = fsockopen($host, $port, $this->errorNumber, $this->errorDesc, $timeout);
+			$this->resource = stream_socket_client($this->remoteSocket, $this->errorNumber, $this->errorDesc, $connectionTimeout);
 		}
+		
 		if ($this->resource === false) {
-			throw new SystemException('Can not connect to ' . $host);
+			throw new SystemException('Can not connect to ' . $host, 14000);
 		}
 	}
-	
+
 	/**
 	 * Returns the error number of the last error.
-	 * 
+	 *
 	 * @return 	integer
 	 */
 	public function getErrorNumber() {
 		return $this->errorNumber;
 	}
-	
+
 	/**
 	 * Returns the error description of the last error.
-	 * 
+	 *
 	 * @return	string
 	 */
 	public function getErrorDesc() {
