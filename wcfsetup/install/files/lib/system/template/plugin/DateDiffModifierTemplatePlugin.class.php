506966c4f030bee5067d2c5eeec1edbfa0cc8e92
Tim Düsterhus <duesterhus@woltlab.com>
2012-11-01 18:15:32 +0100

    DateUtil::formatInterval now supports dates in the future

--- wcfsetup/install/files/lib/system/template/plugin/DateDiffModifierTemplatePlugin.class.php
+++ wcfsetup/install/files/lib/system/template/plugin/DateDiffModifierTemplatePlugin.class.php
@@ -33,8 +33,8 @@ class DateDiffModifierTemplatePlugin implements IModifierTemplatePlugin {
 			$fullInterval = $tagArgs[2];
 		}
 		
-		$startTime = DateUtil::getDateTimeByTimestamp(min($tagArgs[0], $tagArgs[1]));
-		$endTime = DateUtil::getDateTimeByTimestamp(max($tagArgs[0], $tagArgs[1]));
+		$startTime = DateUtil::getDateTimeByTimestamp($tagArgs[1]);
+		$endTime = DateUtil::getDateTimeByTimestamp($tagArgs[0]);
 		
 		return DateUtil::formatInterval($endTime->diff($startTime), $fullInterval);
 	}
