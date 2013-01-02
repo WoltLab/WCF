6c80f0dfb3ec67d2285ba4625a2341a12261fad5
Alexander Ebert <ebert@woltlab.com>
2013-01-02 01:01:31 +0100

    Merged com.woltlab.wcf.acp.style

---
+++ wcfsetup/install/files/acp/templates/styleList.tpl
@@ -0,0 +1,95 @@
+{include file='header' pageTitle='wcf.acp.style.list'}
+
+<script type="text/javascript" src="{@$__wcf->getPath()}acp/js/WCF.ACP.Style.js"></script>
+<script type="text/javascript">
+	//<![CDATA[
+	$(function() {
+		new WCF.Action.Toggle('wcf\\data\\style\\StyleAction', $('.buttonList'));
+		new WCF.ACP.Style.List();
+	});
+	//]]>
+</script>
+
+<header class="boxHeadline">
+	<hgroup>
+		<h1>{lang}wcf.acp.style.list{/lang}</h1>
+	</hgroup>
+</header>
+
+<div class="contentNavigation">
+	{pages print=true assign=pagesLinks controller="StyleList" link="pageNo=%d"}
+	
+	{hascontent}
+		<nav>
+			<ul>
+				{content}
+					{if $__wcf->session->getPermission('admin.style.canAddStyle')}
+						<li><a href="{link controller='StyleAdd'}{/link}" title="{lang}wcf.acp.menu.link.style.add{/lang}" class="button"><img src="{@$__wcf->getPath()}icon/add.svg" alt="" class="icon24" /> <span>{lang}wcf.acp.menu.link.style.add{/lang}</span></a></li>
+						<li><a href="{link controller='StyleImport'}{/link}" title="{lang}wcf.acp.menu.link.style.import{/lang}" class="button"><img src="{@$__wcf->getPath()}icon/upload.svg" alt="" class="icon24" /> <span>{lang}wcf.acp.menu.link.style.import{/lang}</span></a></li>
+					{/if}
+					
+					{event name='largeButtons'}
+				{/content}
+			</ul>
+		</nav>
+	{/hascontent}
+</div>
+
+<div class="container marginTop">
+	<ol class="containerList styleList">
+		{foreach from=$objects item=style}
+			<li>
+				<div class="box64">
+					<span class="framed"><img src="{@$style->getPreviewImage()}" alt="" /></span>
+					<div class="details">
+						<hgroup class="containerHeadline">
+							<h1><a href="{link controller='StyleEdit' id=$style->styleID}{/link}">{$style->styleName}</a></h1>
+							{if $style->styleDescription}<h2>{$style->styleDescription}</h2>{/if}
+						</hgroup>
+						<ul class="buttonList" data-style-id="{@$style->styleID}">
+							<li><a href="{link controller='StyleEdit' id=$style->styleID}{/link}" title="{lang}wcf.global.button.edit{/lang}" class="jsTooltip"><img src="{@$__wcf->getPath()}icon/edit.svg" class="icon16" alt="" /></a></li>
+							{if !$style->isDefault}
+								<li><img src="{@$__wcf->getPath()}icon/{if $style->disabled}disabled{else}enabled{/if}.svg" title="{lang}wcf.global.button.{if $style->disabled}enable{else}disable{/if}{/lang}" alt="" class="icon16 jsToggleButton jsTooltip" data-object-id="{@$style->styleID}" /></li>
+								<li><a title="{lang}wcf.acp.style.button.setAsDefault{/lang}" class="jsSetAsDefault jsTooltip"><img src="{@$__wcf->getPath()}icon/default.svg" class="icon16 jsTooltip" alt="" /></a></li>
+								<li><a title="{lang}wcf.global.button.delete{/lang}" class="jsDelete jsTooltip" data-confirm-message="{lang}wcf.acp.style.delete.confirmMessage{/lang}"><img src="{@$__wcf->getPath()}icon/delete.svg" class="icon16" alt="" /></a></li>
+							{/if}
+						</ul>
+						<dl class="plain inlineDataList">
+							<dt>{lang}wcf.acp.style.users{/lang}</dt>
+							<dd>{#$style->users}</dd>
+						</dl>
+						<dl class="plain inlineDataList">
+							<dt>{lang}wcf.acp.style.styleVersion{/lang}</dt>
+							<dd>{$style->styleVersion} ({$style->styleDate})</dd>
+						</dl>
+						<dl class="plain inlineDataList">
+							<dt>{lang}wcf.acp.style.authorName{/lang}</dt>
+							<dd>{if $style->authorURL}<a href="{@$__wcf->getPath()}acp/dereferrer.php?url={$style->authorURL}">{$style->authorName}</a>{else}{$style->authorName}{/if}</dd>
+						</dl>
+					</div>
+				</div>
+			</li>
+		{/foreach}
+	</ol>
+</div>
+
+<div class="contentNavigation">
+	{@$pagesLinks}
+	
+	{hascontent}
+		<nav>
+			<ul>
+				{content}
+					{if $__wcf->session->getPermission('admin.style.canAddStyle')}
+						<li><a href="{link controller='StyleAdd'}{/link}" title="{lang}wcf.acp.menu.link.style.add{/lang}" class="button"><img src="{@$__wcf->getPath()}icon/add.svg" alt="" class="icon24" /> <span>{lang}wcf.acp.menu.link.style.add{/lang}</span></a></li>
+						<li><a href="{link controller='StyleImport'}{/link}" title="{lang}wcf.acp.menu.link.style.import{/lang}" class="button"><img src="{@$__wcf->getPath()}icon/upload.svg" alt="" class="icon24" /> <span>{lang}wcf.acp.menu.link.style.import{/lang}</span></a></li>
+					{/if}
+					
+					{event name='largeButtons'}
+				{/content}
+			</ul>
+		</nav>
+	{/hascontent}
+</div>
+
+{include file='footer'}
---
+++ wcfsetup/install/files/lib/acp/form/StyleExportForm.class.php
@@ -0,0 +1,197 @@
+<?php
+namespace wcf\acp\form;
+use wcf\data\package\Package;
+use wcf\data\style\Style;
+use wcf\data\style\StyleEditor;
+use wcf\form\AbstractForm;
+use wcf\system\exception\IllegalLinkException;
+use wcf\system\exception\UserInputException;
+use wcf\system\WCF;
+use wcf\util\StringUtil;
+
+/**
+ * Shows the style export form.
+ * 
+ * @author	Alexander Ebert
+ * @copyright	2001-2013 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf
+ * @subpackage	acp.form
+ * @category	Community Framework
+ */
+class StyleExportForm extends AbstractForm {
+	/**
+	 * @see	wcf\acp\form\ACPForm::$activeMenuItem
+	 */
+	public $activeMenuItem = 'wcf.acp.menu.link.style';
+	
+	/**
+	 * true, if style has custom icons
+	 * @var	boolean
+	 */
+	public $canExportIcons = false;
+	
+	/**
+	 * true, if style has custom images
+	 * @var	boolean
+	 */
+	public $canExportImages = false;
+	
+	/**
+	 * true, if style has custom templates
+	 * @var	boolean
+	 */
+	public $canExportTemplates = false;
+	
+	/**
+	 * export style as installable package
+	 * @var	boolean
+	 */
+	public $exportAsPackage = false;
+	
+	/**
+	 * true, if icons should be exported
+	 * @var	boolean
+	 */
+	public $exportIcons = false;
+	
+	/**
+	 * true, if images should be exported
+	 * @var	boolean
+	 */
+	public $exportImages = false;
+	
+	/**
+	 * true, if templates should be exported
+	 * @var	boolean
+	 */
+	public $exportTemplates = false;
+	
+	/**
+	 * @see	wcf\page\AbstractPage::$neededPermissions
+	 */
+	public $neededPermissions = array('admin.style.canEditStyle');
+	
+	/**
+	 * package identifier
+	 * @var	string
+	 */
+	public $packageName = '';
+	
+	/**
+	 * style object
+	 * @var	wcf\data\style\Style
+	 */
+	public $style = null;
+	
+	/**
+	 * style id
+	 * @var	integer
+	 */
+	public $styleID = 0;
+	
+	/**
+	 * @see	wcf\page\IPage::readParameters()
+	 */
+	public function readParameters() {
+		parent::readParameters();
+		
+		if (isset($_REQUEST['id'])) $this->styleID = intval($_REQUEST['id']);
+		$this->style = new Style($this->styleID);
+		if (!$this->style->styleID) {
+			throw new IllegalLinkException();
+		}
+		
+		if ($this->style->iconPath && $this->style->iconPath != 'icon/') $this->canExportIcons = true;
+		if ($this->style->imagePath && $this->style->imagePath != 'images/') $this->canExportImages = true;
+		if ($this->style->templateGroupID) $this->canExportTemplates = true;
+	}
+	
+	/**
+	 * @see	wcf\form\IForm::readFormParameters()
+	 */
+	public function readFormParameters() {
+		parent::readFormParameters();
+		
+		if ($this->canExportIcons && isset($_POST['exportIcons'])) $this->exportIcons = true;
+		if ($this->canExportImages && isset($_POST['exportImages'])) $this->exportImages = true;
+		if ($this->canExportTemplates && isset($_POST['exportTemplates'])) $this->exportTemplates = true;
+		
+		if (isset($_POST['exportAsPackage'])) {
+			$this->exportAsPackage = true;
+			
+			if (isset($_POST['packageName'])) $this->packageName = StringUtil::trim($_POST['packageName']);
+		}
+	}
+	
+	/**
+	 * @see	wcf\form\IForm::validate()
+	 */
+	public function validate() {
+		parent::validate();
+		
+		if ($this->exportAsPackage) {
+			if (empty($this->packageName)) {
+				throw new UserInputException('packageName');
+			}
+			
+			if (!Package::isValidPackageName($this->packageName)) {
+				throw new UserInputException('packageName', 'notValid');
+			}
+			
+			// 3rd party packages may never have com.woltlab.* as name
+			if (strpos($this->packageName, 'com.woltlab.') === 0) {
+				throw new UserInputException('packageName', 'reserved');
+			}
+		}
+	}
+	
+	/**
+	 * @see	wcf\form\IForm::save()
+	 */
+	public function save() {
+		parent::save();
+		
+		// get style filename
+		$filename = str_replace(' ', '-', preg_replace('/[^a-z0-9 _-]/', '', StringUtil::toLowerCase($this->style->styleName)));
+		
+		// send headers
+		header('Content-Type: application/x-gzip; charset=utf-8');
+		
+		if ($this->exportAsPackage) {
+			header('Content-Disposition: attachment; filename="'.$this->packageName.'.tar.gz"');
+		}
+		else {
+			header('Content-Disposition: attachment; filename="'.$filename.'-style.tgz"');
+		}
+		
+		// export style
+		$styleEditor = new StyleEditor($this->style);
+		$styleEditor->export($this->exportTemplates, $this->exportImages, $this->exportIcons, $this->packageName);
+		
+		// call saved event
+		$this->saved();
+		
+		exit;
+	}
+	
+	/**
+	 * @see	wcf\form\IForm::assignVariables()
+	 */
+	public function assignVariables() {
+		parent::assignVariables();
+		
+		WCF::getTPL()->assign(array(
+			'canExportIcons' => $this->canExportIcons,
+			'canExportImages' => $this->canExportImages,
+			'canExportTemplates' => $this->canExportTemplates,
+			'exportAsPackage' => $this->exportAsPackage,
+			'exportIcons' => $this->exportIcons,
+			'exportImages' => $this->exportImages,
+			'exportTemplates' => $this->exportTemplates,
+			'packageName' => $this->packageName,
+			'style' => $this->style,
+			'styleID' => $this->styleID
+		));
+	}
+}
