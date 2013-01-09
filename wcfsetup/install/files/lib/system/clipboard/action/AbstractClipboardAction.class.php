106a5ea4f317ff075bf37be2ebdc81a8026a2ac3
Matthias Schmidt <gravatronics@live.com>
2013-01-09 11:31:05 +0100

    Adds default implementation AbstractClipboardAction::execute()

--- wcfsetup/install/files/lib/system/clipboard/action/AbstractClipboardAction.class.php
+++ wcfsetup/install/files/lib/system/clipboard/action/AbstractClipboardAction.class.php
@@ -1,5 +1,7 @@
 <?php
 namespace wcf\system\clipboard\action;
+use wcf\system\clipboard\ClipboardEditorItem;
+use wcf\system\exception\SystemException;
 
 /**
  * Abstract implementation of a clipboard action handler.
@@ -12,6 +14,54 @@ namespace wcf\system\clipboard\action;
  * @category	Community Framework
  */
 abstract class AbstractClipboardAction implements IClipboardAction {
+	/**
+	 * list of the clipboard actions which are executed by the action class
+	 * @var	array<string>
+	 */
+	protected $actionClassActions = array();
+	
+	/**
+	 * relevant database objects
+	 * @var	array<wcf\data\DatabaseObject>
+	 */
+	protected $objects = array();
+	
+	/**
+	 * list of the supported clipboard actions
+	 * @var	array<string>
+	 */
+	protected $supportedActions = array();
+	
+	/**
+	 * @see	wcf\system\clipboard\action\IClipboardAction::execute()
+	 */
+	public function execute(array $objects, $actionName) {
+		if (!in_array($actionName, $this->supportedActions)) {
+			throw new SystemException("Unknown clipboard action '".$actionName."'");
+		}
+		
+		$this->objects = $objects;
+		
+		$item = new ClipboardEditorItem();
+		$item->setName($this->getTypeName().'.'.$actionName);
+		if (in_array($actionName, $this->actionClassActions)) {
+			$item->addParameter('actionName', $actionName);
+			$item->addParameter('className', $this->getClassName());
+		}
+		
+		$methodName = 'validate'.ucfirst($actionName);
+		if (method_exists($this, $methodName)) {
+			$objectIDs = $this->$methodName();
+			if (empty($objectIDs)) {
+				return null;
+			}
+			
+			$item->addParameter('objectIDs', $objectIDs);
+		}
+		
+		return $item;
+	}
+	
 	/**
 	 * @see	wcf\system\clipboard\action\IClipboardAction::filterObjects()
 	 */
