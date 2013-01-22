735b2f5219d1757b321ce8d6de45ef76290fe90e
Matthias Schmidt <gravatronics@live.com>
2013-01-22 17:27:34 +0100

    Removes obsolete code

--- wcfsetup/install/files/lib/data/style/ActiveStyle.class.php
+++ wcfsetup/install/files/lib/data/style/ActiveStyle.class.php
@@ -1,15 +1,13 @@
 <?php
 namespace wcf\data\style;
-use wcf\data\DatabaseObject;
 use wcf\data\DatabaseObjectDecorator;
-use wcf\system\cache\CacheHandler;
 use wcf\system\WCF;
 
 /**
  * Represents the active user style.
  * 
  * @author	Marcel Werk
- * @copyright	2001-2012 WoltLab GmbH
+ * @copyright	2001-2013 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	com.woltlab.wcf
  * @subpackage	data.style
@@ -20,13 +18,6 @@ class ActiveStyle extends DatabaseObjectDecorator {
 	 * @see	wcf\data\DatabaseObjectDecorator::$baseClass
 	 */
 	protected static $baseClass = 'wcf\data\style\Style';
-	
-	/**
-	 * @see	wcf\data\DatabaseObjectDecorator::__construct()
-	 */
-	public function __construct(DatabaseObject $object) {
-		parent::__construct($object);
-	}
 	
 	/**
 	 * Returns full path to specified image.
