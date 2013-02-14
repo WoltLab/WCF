77350ee4ea4ce92d7168344f51ad974c4578bf3b
Alexander Ebert <ebert@woltlab.com>
2013-02-14 21:49:37 +0100

    Added support for braindead IPv6 notation

--- wcfsetup/install/files/lib/system/cache/source/MemcachedCacheSource.class.php
+++ wcfsetup/install/files/lib/system/cache/source/MemcachedCacheSource.class.php
@@ -42,7 +42,7 @@ class MemcachedCacheSource implements ICacheSource {
 		$tmp = explode("\n", StringUtil::unifyNewlines(CACHE_SOURCE_MEMCACHED_HOST));
 		$servers = array();
 		$defaultWeight = floor(100 / count($tmp));
-		$regex = new Regex('^\[([a-z0-9\:]+)\](?::([0-9]{1,5}))?(?::([0-9]{1,3}))?$', Regex::CASE_INSENSITIVE);
+		$regex = new Regex('^\[([a-z0-9\:\.]+)\](?::([0-9]{1,5}))?(?::([0-9]{1,3}))?$', Regex::CASE_INSENSITIVE);
 		
 		foreach ($tmp as $server) {
 			$server = StringUtil::trim($server);
