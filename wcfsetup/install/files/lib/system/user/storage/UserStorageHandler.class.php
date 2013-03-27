79da2f1df6fc999c8dee5f595b3a7b6f3bea517b
Marcel Werk <burntime@woltlab.com>
2013-03-27 19:09:31 +0100

    Renamed method

--- wcfsetup/install/files/lib/system/user/storage/UserStorageHandler.class.php
+++ wcfsetup/install/files/lib/system/user/storage/UserStorageHandler.class.php
@@ -189,7 +189,7 @@ class UserStorageHandler extends SingletonFactory {
 	/**
 	 * Removes the entire user storage data.
 	 */
-	public function nukeStorage() {
+	public function clear() {
 		$this->resetFields = $this->updateFields = array();
 		
 		$sql = "DELETE FROM	wcf".WCF_N."_user_storage";
