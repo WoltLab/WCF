7e57d1130004361507ea09f9b7268929df3b1f20
Alexander Ebert <ebert@woltlab.com>
2013-05-29 15:25:40 +0200

    Preparing release 2.0.0 Beta 1

--- com.woltlab.wcf/update_post_b1.sql
+++
@@ -1,5 +0,0 @@
-CREATE UNIQUE INDEX applicationTemplate ON wcf1_acp_template (application, templateName);
-
-CREATE UNIQUE INDEX applicationFile ON wcf1_package_installation_file_log (application, filename);
-
-CREATE UNIQUE INDEX applicationTemplate ON wcf1_template (application, templateGroupID, templateName);
