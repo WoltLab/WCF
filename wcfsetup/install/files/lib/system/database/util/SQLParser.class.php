b2f79fc585d32f39aba7075e30cbe41d0f1f5b6b
Alexander Ebert <ebert@woltlab.com>
2013-06-03 00:22:33 +0200

    Improved logging for SQLParser

--- wcfsetup/install/files/lib/system/database/util/SQLParser.class.php
+++ wcfsetup/install/files/lib/system/database/util/SQLParser.class.php
@@ -30,12 +30,14 @@ class SQLParser {
 	 */
 	public function __construct($queries) {
 		// delete comments
+		$start = microtime(true);
 		$queries = preg_replace("~('[^'\\\\]*(?:\\\\.[^'\\\\]*)*')|(?:(?:--|#)[^\n]*|/\*.*?\*/)~s", '$1', $queries);
 		
 		// split queries by semicolon
 		if (preg_match_all("~(?:[^;']+(?:'[^'\\\\]*(?:\\\\.[^'\\\\]*)*')*)*(?=;|\$)~s", $queries, $matches)) {
 			$this->queryArray = ArrayUtil::trim($matches[0]);
 		}
+		$GLOBALS['__db']['__construct'] = round(microtime(true) - $start, 3);
 	}
 	
 	/**
@@ -66,6 +68,7 @@ class SQLParser {
 			case 'CREATE TABLE':
 				// get table name
 				if (preg_match('~^CREATE\s+TABLE\s+(\w+)\s*\(~i', $query, $match)) {
+					$startG = microtime(true);
 					$tableName = $match[1];
 					$columns = $indices = array();
 					
@@ -118,6 +121,7 @@ class SQLParser {
 					$this->executeCreateTableStatement($tableName, $columns, $indices);
 					$GLOBALS['__db']['tableCount']++;
 					$GLOBALS['__db']['table'] += round(microtime(true) - $s, 3);
+					$GLOBALS['__db']['tableGlobal'] += round(microtime(true) - $startG, 3);
 				}
 			break;
 			
@@ -128,7 +132,10 @@ class SQLParser {
 				}
 				// add foreign key
 				else if (preg_match('~^ALTER\s+TABLE\s+(\w+)\s+ADD\s+FOREIGN KEY\s+(?:(\w+)\s*)?\((\s*\w+\s*(?:,\s*\w+\s*)*)\)\s+REFERENCES\s+(\w+)\s+\((\s*\w+\s*(?:,\s*\w+\s*)*)\)(?:\s+ON\s+(UPDATE|DELETE)\s+(CASCADE|SET NULL|NO ACTION))?~is', $query, $match)) {
+					$s = microtime(true);
 					$this->executeAddForeignKeyStatement($match[1], ($match[2] ?: self::getGenericIndexName($match[1], $match[3], 'fk')), array('columns' => $match[3], 'referencedTable' => $match[4], 'referencedColumns' => $match[5], 'operation' => $match[6], 'action' => $match[7]));
+					$GLOBALS['__db']['keyCount']++;
+					$GLOBALS['__db']['key'] += round(microtime(true) - $s, 3);
 				}
 				// add/change column
 				else if (preg_match("~^ALTER\s+TABLE\s+(\w+)\s+(?:(ADD)\s+(?:COLUMN\s+)?|(CHANGE)\s+(?:COLUMN\s+)?(\w+)\s+)(\w+)\s+(\w+)(?:\s*\((\s*(?:\d+(?:\s*,\s*\d+)?|'[^']*'(?:\s*,\s*'[^']*')*))\))?(?:\s+UNSIGNED)?(?:\s+(NOT NULL|NULL))?(?:\s+DEFAULT\s+(-?\d+.\d+|-?\d+|NULL|'[^'\\\\]*(?:\\\\.[^'\\\\]*)*'))?(?:\s+(AUTO_INCREMENT))?(?:\s+(UNIQUE|PRIMARY)(?: KEY)?)?~is", $query, $match)) {
