4e8e52a9d50a9d4a927212ea8a9a7c3892bbbabd
Alexander Ebert <ebert@woltlab.com>
2013-06-21 22:02:39 +0200

    Fixed SQL update

--- com.woltlab.wcf/update_b4.sql
+++ com.woltlab.wcf/update_b4.sql
@@ -1,8 +1,12 @@
 /* 40381db */
-ALTER TABLE wcf1_search_index DROP KEY (objectTypeID, objectID);
-ALTER TABLE wcf1_search_index ADD UNIQUE KEY (objectTypeID, objectID, languageID);
+ALTER TABLE wcf1_search_index DROP INDEX objectTypeID;
+ALTER TABLE wcf1_search_index ADD UNIQUE KEY objectTypeID (objectTypeID, objectID, languageID);
 
 /* 8ce85e3 */
 INSERT INTO wcf1_style_variable (variableName, defaultValue) VALUES ('wcfButtonBorderRadius', '15px');
 INSERT INTO wcf1_style_variable (variableName, defaultValue) VALUES ('wcfSmallButtonBorderRadius', '3px');
 INSERT INTO wcf1_style_variable (variableName, defaultValue) VALUES ('wcfInputBorderRadius', '0');
+
+/* 133a9cf */
+INSERT INTO wcf1_style_variable (variableName, defaultValue) VALUES ('wcfTextShadowLightColor', 'rgba(255, 255, 255, .8)');
+INSERT INTO wcf1_style_variable (variableName, defaultValue) VALUES ('wcfTextShadowDarkColor', 'rgba(0, 0, 0, .8)');
