3f32e7add010ef9dba853768e74bf26bc7f98c2a
NurPech <nurpech@nurpech.de>
2013-07-14 02:28:39 +0200

    added support for additional data in data export
    see:
    http://beta.woltlab.com/index.php/Thread/2370-eigene-Einstellungen-im-Importer/?postID=22098#post22098
    
    Signed-off-by: NurPech <nurpech@nurpech.de>

--- wcfsetup/install/files/lib/system/exporter/AbstractExporter.class.php
+++ wcfsetup/install/files/lib/system/exporter/AbstractExporter.class.php
@@ -16,121 +16,128 @@ use wcf\util\FileUtil;
  * @category	Community Framework
  */
 abstract class AbstractExporter implements IExporter {
-	/**
-	 * database host name
-	 * @var string
-	 */
-	protected $databaseHost = '';
-	
-	/**
-	 * database username
-	 * @var string
-	 */
-	protected $databaseUser = '';
-	
-	/**
-	 * database password
-	 * @var string
-	 */
-	protected $databasePassword = '';
-	
-	/**
-	 * database name
-	 * @var string
-	 */
-	protected $databaseName = '';
-	
-	/**
-	 * table prefix
-	 * @var string
-	 */
-	protected $databasePrefix = '';
-	
-	/**
-	 * file system path
-	 * @var string
-	 */
-	protected $fileSystemPath = '';
-	
-	/**
-	 * database connection
-	 * @var wcf\system\database\Database
-	 */
-	protected $database = null;
-	
-	/**
-	 * object type => method names
-	 * @var array
-	 */
-	protected $methods = array();
-	
-	/**
-	 * limits for items per run
-	 * @var array<integer>
-	 */
-	protected $limits = array();
-	
-	/**
-	 * default limit for items per run
-	 * @var integer
-	 */
-	protected $defaultLimit = 1000;
-	
-	/**
-	 * @see wcf\system\exporter\IExporter::setData()
-	 */
-	public function setData($databaseHost, $databaseUser, $databasePassword, $databaseName, $databasePrefix, $fileSystemPath) {
-		$this->databaseHost = $databaseHost;
-		$this->databaseUser = $databaseUser;
-		$this->databasePassword = $databasePassword;
-		$this->databaseName = $databaseName;
-		$this->databasePrefix = $databasePrefix;
-		$this->fileSystemPath = ($fileSystemPath ? FileUtil::addTrailingSlash($fileSystemPath) : '');
-	}
-	
-	/**
-	 * @see wcf\system\exporter\IExporter::init()
-	 */
-	public function init() {
-		$this->database = new MySQLDatabase($this->databaseHost, $this->databaseUser, $this->databasePassword, $this->databaseName, 0);
-	}
-	
-	/**
-	 * @see wcf\system\exporter\IExporter::validateDatabaseAccess()
-	 */
-	public function validateDatabaseAccess() {
-		$this->init();
-	}
-	
-	/**
-	 * @see wcf\system\exporter\IExporter::getDefaultDatabasePrefix()
-	 */
-	public function getDefaultDatabasePrefix() {
-		return '';
-	}
-	
-	/**
-	 * @see wcf\system\exporter\IExporter::countLoops()
-	 */
-	public function countLoops($objectType) {
-		if (!isset($this->methods[$objectType]) || !method_exists($this, 'count'.$this->methods[$objectType])) {
-			throw new SystemException("unknown object type '".$objectType."' given");
-		}
-		
-		$count = call_user_func(array($this, 'count'.$this->methods[$objectType]));
-		$limit = (isset($this->limits[$objectType]) ? $this->limits[$objectType] : $this->defaultLimit);
-		return ceil($count / $limit);
-	}
-	
-	/**
-	 * @see wcf\system\exporter\IExporter::exportData()
-	 */
-	public function exportData($objectType, $loopCount = 0) {
-		if (!isset($this->methods[$objectType]) || !method_exists($this, 'export'.$this->methods[$objectType])) {
-			throw new SystemException("unknown object type '".$objectType."' given");
-		}
-		
-		$limit = (isset($this->limits[$objectType]) ? $this->limits[$objectType] : $this->defaultLimit);
-		call_user_func(array($this, 'export'.$this->methods[$objectType]), $loopCount * $limit, $limit);
-	}
+    /**
+     * additional data
+     * @var array
+     */
+    public $additionalData = array();
+
+    /**
+     * database host name
+     * @var string
+     */
+    protected $databaseHost = '';
+
+    /**
+     * database username
+     * @var string
+     */
+    protected $databaseUser = '';
+
+    /**
+     * database password
+     * @var string
+     */
+    protected $databasePassword = '';
+
+    /**
+     * database name
+     * @var string
+     */
+    protected $databaseName = '';
+
+    /**
+     * table prefix
+     * @var string
+     */
+    protected $databasePrefix = '';
+
+    /**
+     * file system path
+     * @var string
+     */
+    protected $fileSystemPath = '';
+
+    /**
+     * database connection
+     * @var wcf\system\database\Database
+     */
+    protected $database = null;
+
+    /**
+     * object type => method names
+     * @var array
+     */
+    protected $methods = array();
+
+    /**
+     * limits for items per run
+     * @var array<integer>
+     */
+    protected $limits = array();
+
+    /**
+     * default limit for items per run
+     * @var integer
+     */
+    protected $defaultLimit = 1000;
+
+    /**
+     * @see wcf\system\exporter\IExporter::setData()
+     */
+    public function setData($databaseHost, $databaseUser, $databasePassword, $databaseName, $databasePrefix, $fileSystemPath, $additionalData) {
+        $this->databaseHost = $databaseHost;
+        $this->databaseUser = $databaseUser;
+        $this->databasePassword = $databasePassword;
+        $this->databaseName = $databaseName;
+        $this->databasePrefix = $databasePrefix;
+        $this->fileSystemPath = ($fileSystemPath ? FileUtil::addTrailingSlash($fileSystemPath) : '');
+        $this->additionalData = $additionalData;
+    }
+
+    /**
+     * @see wcf\system\exporter\IExporter::init()
+     */
+    public function init() {
+        $this->database = new MySQLDatabase($this->databaseHost, $this->databaseUser, $this->databasePassword, $this->databaseName, 0);
+    }
+
+    /**
+     * @see wcf\system\exporter\IExporter::validateDatabaseAccess()
+     */
+    public function validateDatabaseAccess() {
+        $this->init();
+    }
+
+    /**
+     * @see wcf\system\exporter\IExporter::getDefaultDatabasePrefix()
+     */
+    public function getDefaultDatabasePrefix() {
+        return '';
+    }
+
+    /**
+     * @see wcf\system\exporter\IExporter::countLoops()
+     */
+    public function countLoops($objectType) {
+        if (!isset($this->methods[$objectType]) || !method_exists($this, 'count'.$this->methods[$objectType])) {
+            throw new SystemException("unknown object type '".$objectType."' given");
+        }
+
+        $count = call_user_func(array($this, 'count'.$this->methods[$objectType]));
+        $limit = (isset($this->limits[$objectType]) ? $this->limits[$objectType] : $this->defaultLimit);
+        return ceil($count / $limit);
+    }
+
+    /**
+     * @see wcf\system\exporter\IExporter::exportData()
+     */
+    public function exportData($objectType, $loopCount = 0) {
+        if (!isset($this->methods[$objectType]) || !method_exists($this, 'export'.$this->methods[$objectType])) {
+            throw new SystemException("unknown object type '".$objectType."' given");
+        }
+
+        $limit = (isset($this->limits[$objectType]) ? $this->limits[$objectType] : $this->defaultLimit);
+        call_user_func(array($this, 'export'.$this->methods[$objectType]), $loopCount * $limit, $limit);
+    }
 }
