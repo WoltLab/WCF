fe0f49963165b63ac50699aefea5adf2844db401
NurPech <nurpech@nurpech.de>
2013-07-14 02:51:53 +0200

    added support for additional data in data exporter

--- wcfsetup/install/files/lib/system/exporter/AbstractExporter.class.php
+++ wcfsetup/install/files/lib/system/exporter/AbstractExporter.class.php
@@ -17,98 +17,105 @@ use wcf\util\FileUtil;
  */
 abstract class AbstractExporter implements IExporter {
 	/**
+	 * additional data
+	 * @var array
+	 */
+	public $additionalData = array();
+
+	/**
 	 * database host name
 	 * @var string
 	 */
 	protected $databaseHost = '';
-	
+
 	/**
 	 * database username
 	 * @var string
 	 */
 	protected $databaseUser = '';
-	
+
 	/**
 	 * database password
 	 * @var string
 	 */
 	protected $databasePassword = '';
-	
+
 	/**
 	 * database name
 	 * @var string
 	 */
 	protected $databaseName = '';
-	
+
 	/**
 	 * table prefix
 	 * @var string
 	 */
 	protected $databasePrefix = '';
-	
+
 	/**
 	 * file system path
 	 * @var string
 	 */
 	protected $fileSystemPath = '';
-	
+
 	/**
 	 * database connection
 	 * @var wcf\system\database\Database
 	 */
 	protected $database = null;
-	
+
 	/**
 	 * object type => method names
 	 * @var array
 	 */
 	protected $methods = array();
-	
+
 	/**
 	 * limits for items per run
 	 * @var array<integer>
 	 */
 	protected $limits = array();
-	
+
 	/**
 	 * default limit for items per run
 	 * @var integer
 	 */
 	protected $defaultLimit = 1000;
-	
+
 	/**
 	 * @see wcf\system\exporter\IExporter::setData()
 	 */
-	public function setData($databaseHost, $databaseUser, $databasePassword, $databaseName, $databasePrefix, $fileSystemPath) {
+	public function setData($databaseHost, $databaseUser, $databasePassword, $databaseName, $databasePrefix, $fileSystemPath, $additionalData) {
 		$this->databaseHost = $databaseHost;
 		$this->databaseUser = $databaseUser;
 		$this->databasePassword = $databasePassword;
 		$this->databaseName = $databaseName;
 		$this->databasePrefix = $databasePrefix;
 		$this->fileSystemPath = ($fileSystemPath ? FileUtil::addTrailingSlash($fileSystemPath) : '');
+		$this->additionalData = $additionalData;
 	}
-	
+
 	/**
 	 * @see wcf\system\exporter\IExporter::init()
 	 */
 	public function init() {
 		$this->database = new MySQLDatabase($this->databaseHost, $this->databaseUser, $this->databasePassword, $this->databaseName, 0);
 	}
-	
+
 	/**
 	 * @see wcf\system\exporter\IExporter::validateDatabaseAccess()
 	 */
 	public function validateDatabaseAccess() {
 		$this->init();
 	}
-	
+
 	/**
 	 * @see wcf\system\exporter\IExporter::getDefaultDatabasePrefix()
 	 */
 	public function getDefaultDatabasePrefix() {
 		return '';
 	}
-	
+
 	/**
 	 * @see wcf\system\exporter\IExporter::countLoops()
 	 */
@@ -116,12 +123,12 @@ abstract class AbstractExporter implements IExporter {
 		if (!isset($this->methods[$objectType]) || !method_exists($this, 'count'.$this->methods[$objectType])) {
 			throw new SystemException("unknown object type '".$objectType."' given");
 		}
-		
+
 		$count = call_user_func(array($this, 'count'.$this->methods[$objectType]));
 		$limit = (isset($this->limits[$objectType]) ? $this->limits[$objectType] : $this->defaultLimit);
 		return ceil($count / $limit);
 	}
-	
+
 	/**
 	 * @see wcf\system\exporter\IExporter::exportData()
 	 */
@@ -129,7 +136,7 @@ abstract class AbstractExporter implements IExporter {
 		if (!isset($this->methods[$objectType]) || !method_exists($this, 'export'.$this->methods[$objectType])) {
 			throw new SystemException("unknown object type '".$objectType."' given");
 		}
-		
+
 		$limit = (isset($this->limits[$objectType]) ? $this->limits[$objectType] : $this->defaultLimit);
 		call_user_func(array($this, 'export'.$this->methods[$objectType]), $loopCount * $limit, $limit);
 	}
