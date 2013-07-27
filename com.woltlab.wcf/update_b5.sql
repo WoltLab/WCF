5cbf5fd9ac4382a467d351133899c11a1a41ef3e
Alexander Ebert <ebert@woltlab.com>
2013-07-27 16:54:44 +0200

    Added missing import mapping in update Beta 5

--- com.woltlab.wcf/update_b5.sql
+++ com.woltlab.wcf/update_b5.sql
@@ -9,3 +9,13 @@ ALTER TABLE wcf1_user_activity_point ADD items INT(10) NOT NULL DEFAULT '0';
 ALTER TABLE wcf1_package_installation_queue DROP COLUMN confirmInstallation;
 ALTER TABLE wcf1_package_installation_queue DROP COLUMN packageType;
 ALTER TABLE wcf1_package_installation_queue ADD isApplication TINYINT(1) NOT NULL DEFAULT '0';
+
+DROP TABLE IF EXISTS wcf1_import_mapping;
+CREATE TABLE wcf1_import_mapping (
+	objectTypeID INT(10) NOT NULL,
+	oldID VARCHAR(255) NOT NULL,
+	newID INT(10) NOT NULL,
+	UNIQUE KEY (objectTypeID, oldID)
+);
+
+ALTER TABLE wcf1_import_mapping ADD FOREIGN KEY (objectTypeID) REFERENCES wcf1_object_type (objectTypeID) ON DELETE CASCADE;
