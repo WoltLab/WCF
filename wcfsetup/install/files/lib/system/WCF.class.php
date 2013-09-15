f605b5061b2ab2853e2d08679a3bffd3f2b4ddbd
Alexander Ebert <ebert@woltlab.com>
2013-09-15 20:01:39 +0200

    Preparing update 2.0.0 Beta 9

--- wcfsetup/install/files/lib/system/WCF.class.php
+++ wcfsetup/install/files/lib/system/WCF.class.php
@@ -36,7 +36,7 @@ if (!@ini_get('date.timezone')) {
 }
 
 // define current wcf version
-define('WCF_VERSION', '2.0.0 Beta 8 (Maelstrom)');
+define('WCF_VERSION', '2.0.0 Beta 9 (Maelstrom)');
 
 // define current unix timestamp
 define('TIME_NOW', time());
