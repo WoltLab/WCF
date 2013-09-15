7facdd14842631e298a36150e83769deadc53887
Alexander Ebert <ebert@woltlab.com>
2013-09-16 00:43:07 +0200

    Fixed SQL update

--- com.woltlab.wcf/update_b9.sql
+++ com.woltlab.wcf/update_b9.sql
@@ -3,7 +3,7 @@ ALTER TABLE wcf1_comment DROP COLUMN lastResponseIDs;
 ALTER TABLE wcf1_comment ADD COLUMN responseIDs VARCHAR(255) NOT NULL DEFAULT '';
 
 /* 28283ef */
-ALTER TABLE wcf1_style_variable SET defaultValue = '1.7rem' WHERE variableName = 'wcfHeadlineFontSize';
-ALTER TABLE wcf1_style_variable SET defaultValue = '1.4rem' WHERE variableName = 'wcfSubHeadlineFontSize';
-ALTER TABLE wcf1_style_variable SET defaultValue = '1.2rem' WHERE variableName = 'wcfTitleFontSize';
-ALTER TABLE wcf1_style_variable SET defaultValue = '.85rem' WHERE variableName = 'wcfSmallFontSize';
+UPDATE wcf1_style_variable SET defaultValue = '1.7rem' WHERE variableName = 'wcfHeadlineFontSize';
+UPDATE wcf1_style_variable SET defaultValue = '1.4rem' WHERE variableName = 'wcfSubHeadlineFontSize';
+UPDATE wcf1_style_variable SET defaultValue = '1.2rem' WHERE variableName = 'wcfTitleFontSize';
+UPDATE wcf1_style_variable SET defaultValue = '.85rem' WHERE variableName = 'wcfSmallFontSize';
