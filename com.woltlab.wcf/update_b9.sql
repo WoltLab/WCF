573d0777efba540925408149ad5004825fb1f31d
Alexander Ebert <ebert@woltlab.com>
2013-09-15 23:04:28 +0200

    Added missing SQL updates

--- com.woltlab.wcf/update_b9.sql
+++ com.woltlab.wcf/update_b9.sql
@@ -1,3 +1,9 @@
 /* c12b1bc */
 ALTER TABLE wcf1_comment DROP COLUMN lastResponseIDs;
 ALTER TABLE wcf1_comment ADD COLUMN responseIDs VARCHAR(255) NOT NULL DEFAULT '';
+
+/* 28283ef */
+ALTER TABLE wcf1_style_variable SET defaultValue = '1.7rem' WHERE variableName = 'wcfHeadlineFontSize';
+ALTER TABLE wcf1_style_variable SET defaultValue = '1.4rem' WHERE variableName = 'wcfSubHeadlineFontSize';
+ALTER TABLE wcf1_style_variable SET defaultValue = '1.2rem' WHERE variableName = 'wcfTitleFontSize';
+ALTER TABLE wcf1_style_variable SET defaultValue = '.85rem' WHERE variableName = 'wcfSmallFontSize';
