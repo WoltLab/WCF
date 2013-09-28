dc0cc4fcd324d0906cc99ca5db0d06925bae21f3
Alexander Ebert <ebert@woltlab.com>
2013-09-28 22:42:08 +0200

    Preparing release 2.0.0 Beta 10

--- com.woltlab.wcf/update_b10.sql
+++ com.woltlab.wcf/update_b10.sql
@@ -2,3 +2,12 @@
 DELETE FROM wcf1_user_storage;
 
 ALTER TABLE wcf1_user_storage CHANGE fieldValue fieldValue MEDIUMTEXT;
+
+/* ec09840 */
+DROP TABLE IF EXISTS wcf1_cli_history;
+CREATE TABLE wcf1_cli_history (
+	historyItem INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	userID INT(10) NOT NULL,
+	command VARCHAR(255) NOT NULL,
+	KEY (userID)
+);
