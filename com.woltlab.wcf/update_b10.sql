36ec55938381d65d74d6cbe1929fa21af5539886
Alexander Ebert <ebert@woltlab.com>
2013-09-29 19:10:25 +0200

    Fixed a few issues during update

--- com.woltlab.wcf/update_b10.sql
+++ com.woltlab.wcf/update_b10.sql
@@ -4,7 +4,6 @@ DELETE FROM wcf1_user_storage;
 ALTER TABLE wcf1_user_storage CHANGE fieldValue fieldValue MEDIUMTEXT;
 
 /* ec09840 */
-DROP TABLE IF EXISTS wcf1_cli_history;
 CREATE TABLE wcf1_cli_history (
 	historyItem INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	userID INT(10) NOT NULL,
--- wcfsetup/install/files/lib/system/package/PackageInstallationNodeBuilder.class.php
+++ wcfsetup/install/files/lib/system/package/PackageInstallationNodeBuilder.class.php
@@ -552,7 +552,11 @@ class PackageInstallationNodeBuilder {
 		
 		$this->emptyNode = true;
 		$instructions = ($this->installation->getAction() == 'install') ? $this->installation->getArchive()->getInstallInstructions() : $this->installation->getArchive()->getUpdateInstructions();
+		$count = count($instructions);
+		$i = 0;
 		foreach ($instructions as $pip) {
+			$i++;
+			
 			if (isset($pip['attributes']['run']) && ($pip['attributes']['run'] == 'standalone')) {
 				// move into a new node unless current one is empty
 				if (!$this->emptyNode) {
@@ -567,12 +571,14 @@ class PackageInstallationNodeBuilder {
 					'sequenceNo' => $this->sequenceNo
 				);
 				
-				// create a new node for following PIPs
-				$this->parentNode = $this->node;
-				$this->node = $this->getToken();
-				$this->sequenceNo = 0;
-				
-				$this->emptyNode = true;
+				// create a new node for following PIPs, unless it is the last one
+				if ($i < $count) {
+					$this->parentNode = $this->node;
+					$this->node = $this->getToken();
+					$this->sequenceNo = 0;
+					
+					$this->emptyNode = true;
+				}
 			}
 			else {
 				$this->sequenceNo++;
