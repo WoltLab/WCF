fccbdeaf03556192d3fd9f6e0962663fd1c9fad6
Tim Düsterhus <duesterhus@woltlab.com>
2013-11-10 15:34:22 +0100

    Add "commands" cli command
    See #1561

---
+++ wcfsetup/install/files/lib/system/cli/command/CommandsCLICommand.class.php
@@ -0,0 +1,30 @@
+<?php
+namespace wcf\system\cli\command;
+use wcf\system\CLIWCF;
+use wcf\util\CLIUtil;
+
+/**
+ * Lists available commands.
+ * 
+ * @author	Tim Duesterhus
+ * @copyright	2001-2013 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf
+ * @subpackage	system.cli.command
+ * @category	Community Framework
+ */
+class CommandsCLICommand implements ICLICommand {
+	/**
+	 * @see	\wcf\system\cli\command\ICLICommand::execute()
+	 */
+	public function execute(array $parameters) {
+		CLIWCF::getReader()->println(CLIUtil::generateList(array_keys(CLICommandHandler::getCommands())));
+	}
+	
+	/**
+	 * @see	\wcf\system\cli\command\ICLICommand::canAccess()
+	 */
+	public function canAccess() {
+		return true;
+	}
+}
