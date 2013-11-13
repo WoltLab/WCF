295d811f8f44e380ccc99653d7abed3999d31616
Marcel Werk <burntime@woltlab.com>
2013-11-13 12:56:57 +0100

    Added importer for smiley categories

---
+++ wcfsetup/install/files/lib/system/importer/SmileyCategoryImporter.class.php
@@ -0,0 +1,28 @@
+<?php
+namespace wcf\system\importer;
+use wcf\data\object\type\ObjectTypeCache;
+
+/**
+ * Imports smiley categories.
+ * 
+ * @author	Marcel Werk
+ * @copyright	2001-2013 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf
+ * @subpackage	system.importer
+ * @category	Community Framework
+ */
+class SmileyCategoryImporter extends AbstractCategoryImporter {
+	/**
+	 * @see	\wcf\system\importer\AbstractCommentImporter::$objectTypeName
+	 */
+	protected $objectTypeName = 'com.woltlab.wcf.smiley.category';
+	
+	/**
+	 * Creates a new SmileyCategoryImporter object.
+	 */
+	public function __construct() {
+		$objectType = ObjectTypeCache::getInstance()->getObjectTypeByName('com.woltlab.wcf.category', 'com.woltlab.wcf.bbcode.smiley');
+		$this->objectTypeID = $objectType->objectTypeID;
+	}
+}
