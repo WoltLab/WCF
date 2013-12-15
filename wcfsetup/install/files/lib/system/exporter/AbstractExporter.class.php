53812f25a76cd5f55dab529f63c1c52451963d7f
Alexander Ebert <ebert@woltlab.com>
2013-12-15 16:37:53 +0100

    Lame code sniff

--- wcfsetup/install/files/lib/system/exporter/AbstractExporter.class.php
+++ wcfsetup/install/files/lib/system/exporter/AbstractExporter.class.php
@@ -106,7 +106,8 @@ abstract class AbstractExporter implements IExporter {
 	public function init() {
 		$host = $this->databaseHost;
 		$port = 0;
-		if (preg_match('^~([0-9.]+):([0-9]{1,5})~$', $host, $matches)) { // simple check, does not care for valid ip addresses
+		if (preg_match('^~([0-9.]+):([0-9]{1,5})~$', $host, $matches)) {
+			// simple check, does not care for valid ip addresses
 			$host = $matches[1];
 			$port = $matches[2];
 		}
