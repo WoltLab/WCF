f07097c02792a68090067c2b292e1fe101032120
Alexander Ebert <ebert@woltlab.com>
2013-12-29 12:22:18 +0100

    Removed $this->literalStack (not used anymore)
    Fixes #1633

--- wcfsetup/install/files/lib/system/template/TemplateScriptingCompiler.class.php
+++ wcfsetup/install/files/lib/system/template/TemplateScriptingCompiler.class.php
@@ -204,7 +204,6 @@ class TemplateScriptingCompiler {
 				'autoloadPlugins' => $this->autoloadPlugins,
 				'currentIdentifier' => $this->currentIdentifier,
 				'currentLineNo' => $this->currentLineNo,
-				'literalStack' => $this->literalStack,
 				'stringStack' => $this->stringStack,
 				'tagStack' => $this->tagStack
 			);
@@ -214,7 +213,7 @@ class TemplateScriptingCompiler {
 		}
 		
 		// reset vars
-		$this->autoloadPlugins = $this->tagStack = $this->stringStack = $this->literalStack = array();
+		$this->autoloadPlugins = $this->tagStack = $this->stringStack = array();
 		$this->currentIdentifier = $identifier;
 		$this->currentLineNo = 1;
 		
@@ -286,7 +285,6 @@ class TemplateScriptingCompiler {
 			$this->autoloadPlugins = $previousData['autoloadPlugins'];
 			$this->currentIdentifier = $previousData['currentIdentifier'];
 			$this->currentLineNo = $previousData['currentLineNo'];
-			$this->literalStack = $previousData['literalStack'];
 			$this->stringStack = $previousData['stringStack'];
 			$this->tagStack = $previousData['tagStack'];
 		}
