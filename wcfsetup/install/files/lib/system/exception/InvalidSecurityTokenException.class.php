aeb4221fbe06919ed2ca4585f92b7e75231a6484
Alexander Ebert <ebert@woltlab.com>
2014-02-12 21:57:12 +0100

    Added missing class

---
+++ wcfsetup/install/files/lib/system/exception/InvalidSecurityTokenException.class.php
@@ -0,0 +1,22 @@
+<?php
+namespace wcf\system\exception;
+use wcf\system\WCF;
+
+/**
+ * An InvalidSecurityTokenException is thrown when the security token does not match the active session.
+ * 
+ * @author	Alexander Ebert
+ * @copyright	2001-2014 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf
+ * @subpackage	system.exception
+ * @category	Community Framework
+ */
+class InvalidSecurityTokenException extends UserException {
+	/**
+	 * Creates a new InvalidSecurityTokenException object.
+	 */
+	public function __construct() {
+		parent::__construct(WCF::getLanguage()->get('wcf.ajax.error.sessionExpired'));
+	}
+}
