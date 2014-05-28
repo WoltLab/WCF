13da9b8593942e8c20467aabaae5a18827c038b6
Matthias Schmidt <gravatronics@live.com>
2014-05-28 21:39:40 +0200

    Add missing IContentCondition file

---
+++ wcfsetup/install/files/lib/system/condition/IContentCondition.class.php
@@ -0,0 +1,26 @@
+<?php
+namespace wcf\system\condition;
+use wcf\data\condition\Condition;
+
+/**
+ * Every implementation for content conditions needs to implements this interface.
+ * 
+ * @author	Matthias Schmidt
+ * @copyright	2001-2014 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf
+ * @subpackage	system.condition
+ * @category	Community Framework
+ */
+interface IContentCondition extends ICondition {
+	/**
+	 * Returns true if content with the given condition will be shown.
+	 * 
+	 * All necessary data to check the condition needs to be globally available
+	 * like the active user object via WCF::getUser().
+	 * 
+	 * @param	\wcf\data\condition\Condition	$condition
+	 * @return	boolean
+	 */
+	public function showContent(Condition $condition);
+}
