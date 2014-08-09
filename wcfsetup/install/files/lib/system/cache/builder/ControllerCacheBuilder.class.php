3e72c4c871af937f955c45f220ec6a83007eeb8b
Alexander Ebert <ebert@woltlab.com>
2014-08-09 12:00:53 +0200

    <3 travis

--- wcfsetup/install/files/lib/system/cache/builder/ControllerCacheBuilder.class.php
+++ wcfsetup/install/files/lib/system/cache/builder/ControllerCacheBuilder.class.php
@@ -51,7 +51,7 @@ class ControllerCacheBuilder extends AbstractCacheBuilder {
 		$controllers = array();
 		$path .= $type . '/';
 		
-		foreach(glob($path . '*' . ucfirst($type) . '.class.php') as $file) {
+		foreach (glob($path . '*' . ucfirst($type) . '.class.php') as $file) {
 			$file = basename($file);
 			if (preg_match('~^([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)(Action|Form|Page)\.class\.php$~', $file, $match)) {
 				if ($match[1] === 'I') {
