7a699097be0cbf22371f34e7bd00fcae79835240
Alexander Ebert <ebert@woltlab.com>
2014-08-19 20:49:04 +0200

    Added whitespace

--- wcfsetup/install/files/lib/core.functions.php
+++ wcfsetup/install/files/lib/core.functions.php
@@ -28,7 +28,7 @@ if (PHP_EOL == "\r\n") {
 	if (!isset($_SERVER['DOCUMENT_ROOT']) && isset($_SERVER['SCRIPT_FILENAME'])) {
 		$_SERVER['DOCUMENT_ROOT'] = str_replace( '\\', '/', substr($_SERVER['SCRIPT_FILENAME'], 0, 0 - strlen($_SERVER['PHP_SELF'])));
 	}
-	if(!isset($_SERVER['DOCUMENT_ROOT']) && isset($_SERVER['PATH_TRANSLATED'])) {
+	if (!isset($_SERVER['DOCUMENT_ROOT']) && isset($_SERVER['PATH_TRANSLATED'])) {
 		$_SERVER['DOCUMENT_ROOT'] = str_replace( '\\', '/', substr(str_replace('\\\\', '\\', $_SERVER['PATH_TRANSLATED']), 0, 0 - strlen($_SERVER['PHP_SELF'])));
 	}
 	
