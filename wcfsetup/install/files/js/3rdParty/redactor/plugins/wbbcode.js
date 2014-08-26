f21167b671c39ce98fe54d79b0f9a9eef0140f54
Alexander Ebert <ebert@woltlab.com>
2014-08-26 20:18:55 +0200

    Ensure that a smiley is always followed by a whitespace

--- wcfsetup/install/files/js/3rdParty/redactor/plugins/wbbcode.js
+++ wcfsetup/install/files/js/3rdParty/redactor/plugins/wbbcode.js
@@ -279,8 +279,8 @@ RedactorPlugins.wbbcode = {
 		html = html.replace(/<\/sup>/gi, '[/sup]');
 		
 		// smileys
-		html = html.replace(/<img [^>]*?alt="([^"]+?)" class="smiley".*?>/gi, '$1'); // firefox
-		html = html.replace(/<img [^>]*?class="smiley" alt="([^"]+?)".*?>/gi, '$1'); // chrome, ie
+		html = html.replace(/<img [^>]*?alt="([^"]+?)" class="smiley".*?> ?/gi, '$1 '); // firefox
+		html = html.replace(/<img [^>]*?class="smiley" alt="([^"]+?)".*?> ?/gi, '$1 '); // chrome, ie
 		
 		// attachments
 		html = html.replace(/<img [^>]*?class="redactorEmbeddedAttachment" data-attachment-id="(\d+)".*?>/gi, '[attach=$1][/attach]');
