1c1d5904b3f36bf611f8f087498b3fc4667fd3a0
Alexander Ebert <ebert@woltlab.com>
2014-11-10 23:44:58 +0100

    Improved update

--- com.woltlab.wcf/update_2.1.0_alpha_1.sql
+++ com.woltlab.wcf/update_2.1.0_alpha_1.sql
@@ -211,6 +211,7 @@ ALTER TABLE wcf1_user_notification ADD guestTimesTriggered INT(10) NOT NULL DEFA
 ALTER TABLE wcf1_user_notification ADD userID INT(10) NOT NULL;
 ALTER TABLE wcf1_user_notification ADD mailNotified TINYINT(1) NOT NULL DEFAULT 0;
 ALTER TABLE wcf1_user_notification ADD confirmed TINYINT(1) NOT NULL DEFAULT 0;
+ALTER TABLE wcf1_user_notification ADD baseObjectID INT(10) NOT NULL DEFAULT 0;
 ALTER TABLE wcf1_user_notification ADD KEY (userID, eventID, objectID, confirmed);
 
 ALTER TABLE wcf1_user_notification_to_user DROP mailNotified;
