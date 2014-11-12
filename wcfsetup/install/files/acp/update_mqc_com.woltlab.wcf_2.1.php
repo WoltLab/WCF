4735e21ee8ae7345c262490d5d6a42a7e55f8a5b
Alexander Ebert <ebert@woltlab.com>
2014-11-12 02:25:25 +0100

    Properly handling legacy comments in moderation queue

---
+++ wcfsetup/install/files/acp/update_mqc_com.woltlab.wcf_2.1.php
@@ -0,0 +1,46 @@
+<?php
+use wcf\system\WCF;
+
+/**
+ * Updates the wcf1_moderation_queue by converting the 'comment' field into a wcf1_comment object.
+ * 
+ * @author	Alexander Ebert
+ * @copyright	2001-2014 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf
+ * @category	Community Framework
+ */
+
+$sql = "SELECT	objectTypeID
+	FROM	wcf".WCF_N."_objectType
+	WHERE	objectType = ?
+		AND definitionID = (
+			SELECT	definitionID
+			FROM	wcf".WCF_N."_object_type_definition
+			WHERE	definitionName = ?
+		)";
+$statement = WCF::getDB()->prepareStatement($sql);
+$statement->execute(array(
+	'com.woltlab.wcf.moderation.queue',
+	'com.woltlab.wcf.comment.commentableContent'
+));
+$row = $statement->fetchArray();
+
+// create comments for non-empty 'comment' fields
+$sql = "INSERT INTO	wcf".WCF_N."_comment
+			(objectTypeID, objectID, time, userID, username, message)
+	SELECT		".$row['objectTypeID'].", queueID, ".TIME_NOW.", NULL, 'Unknown', comment
+	FROM		wcf".WCF_N."_moderation_queue
+	WHERE		comment <> ''";
+$statement = WCF::getDB()->prepareStatement($sql);
+$statement->execute();
+
+// set comment counter
+$sql = "UPDATE	wcf".WCF_N."_moderation_queue
+	SET	comments = 1
+	WHERE	comment <> ''";
+$statement = WCF::getDB()->prepareStatement($sql);
+$statement->execute();
+
+// drop comment column
+WCF::getDB()->getEditor()->dropColumn('wcf'.WCF_N.'_moderation_queue', 'comment');
