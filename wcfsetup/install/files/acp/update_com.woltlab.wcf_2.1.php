a8363f05fd87461c465f46ce90231501839e79b9
Alexander Ebert <ebert@woltlab.com>
2014-11-14 15:07:13 +0100

    Fixed update script

--- wcfsetup/install/files/acp/update_com.woltlab.wcf_2.1.php
+++ wcfsetup/install/files/acp/update_com.woltlab.wcf_2.1.php
@@ -13,6 +13,7 @@ OptionEditor::updateAll(array(
 	'cache_source_type' => (CACHE_SOURCE_TYPE == 'no' ? 'disk' : CACHE_SOURCE_TYPE), 
 	'last_update_time' => TIME_NOW,
 	'url_legacy_mode' => 1,
+	'url_to_lowercase' => 0,
 	// the line below equals \wcf\util\StringUtil::getUUID(), but since we have to do it in one step, the "old" class exists in memory
 	'wcf_uuid' => sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535))
 ));
