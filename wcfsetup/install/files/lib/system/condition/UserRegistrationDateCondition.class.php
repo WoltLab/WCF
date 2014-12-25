f044bea64072e388e0354ad6c2077259657228b3
Matthias Schmidt <gravatronics@live.com>
2014-12-25 13:33:49 +0100

    Unify errorField in UserRegistrationDateCondition

--- wcfsetup/install/files/lib/system/condition/UserRegistrationDateCondition.class.php
+++ wcfsetup/install/files/lib/system/condition/UserRegistrationDateCondition.class.php
@@ -131,7 +131,7 @@ HTML;
 			if ($registrationDateStart === false) {
 				$this->errorMessage = 'wcf.user.condition.registrationDate.error.startNotValid';
 				
-				throw new UserInputException('registrationDateStart', 'startNotValid');
+				throw new UserInputException('registrationDate', 'startNotValid');
 			}
 		}
 		if (strlen($this->registrationDateEnd)) {
@@ -139,14 +139,14 @@ HTML;
 			if ($registrationDateEnd === false) {
 				$this->errorMessage = 'wcf.user.condition.registrationDate.error.endNotValid';
 				
-				throw new UserInputException('registrationDateEnd', 'endNotValid');
+				throw new UserInputException('registrationDate', 'endNotValid');
 			}
 		}
 		
 		if ($registrationDateEnd !== null && $registrationDateStart !== null && $registrationDateEnd < $registrationDateStart) {
 			$this->errorMessage = 'wcf.user.condition.registrationDate.error.endBeforeStart';
 			
-			throw new UserInputException('registrationDateEnd', 'endBeforeStart');
+			throw new UserInputException('registrationDate', 'endBeforeStart');
 		}
 	}
 	
