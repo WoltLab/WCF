c62b6994d53f75f3fcd72a75fed65dcb17ee1e65
Alexander Ebert <ebert@woltlab.com>
2015-03-22 18:14:05 +0100

    Preparing internal release 2.1.2

---
+++ com.woltlab.wcf/update_212.sql
@@ -0,0 +1,1 @@
+UPDATE wcf1_bbcode_media_provider SET html = '<iframe style="max-width:100%;" width="560" height="315" src="https://www.youtube.com/embed/{$ID}?wmode=transparent{$start}" allowfullscreen></iframe>' WHERE title = 'YouTube';
