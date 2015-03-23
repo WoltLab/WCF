8c51706e6883ca1e074086661e7b8e3ce95eb0ca
Alexander Ebert <ebert@woltlab.com>
2015-03-23 10:59:28 +0100

    Fixed bad redirects

--- wcfsetup/install/files/lib/system/request/RequestHandler.class.php
+++ wcfsetup/install/files/lib/system/request/RequestHandler.class.php
@@ -263,7 +263,7 @@ class RequestHandler extends SingletonFactory {
 		
 		// append the remaining query parameters
 		foreach ($_GET as $key => $value) {
-			if (!empty($value)) {
+			if (!empty($value) && $key != 'controller') {
 				$routeData[$key] = $value;
 			}
 		}
