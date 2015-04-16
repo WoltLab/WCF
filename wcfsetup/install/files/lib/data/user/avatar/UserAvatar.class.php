87e93fdc8d54ffc20835e5727baae3d694634a5e
Matthias Schmidt <gravatronics@live.com>
2015-04-16 19:00:26 +0200

    Fix displaying retina avatars smaller than 128px

--- wcfsetup/install/files/lib/data/user/avatar/UserAvatar.class.php
+++ wcfsetup/install/files/lib/data/user/avatar/UserAvatar.class.php
@@ -130,7 +130,9 @@ class UserAvatar extends DatabaseObject implements IUserAvatar {
 			
 			case 64:
 			case 96:
-				$retinaSize = 128;
+				if ($this->size >= 128 && $this->height >= 128) {
+					$retinaSize = 128;
+				}
 			break;
 		}
 		
