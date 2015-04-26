1af618a49adf052a4fa901051c364e31dd4e00ac
Alexander Ebert <ebert@woltlab.com>
2015-04-26 17:04:26 +0200

    Reverted some changes

--- wcfsetup/install/files/js/3rdParty/redactor/plugins/wbbcode.js
+++ wcfsetup/install/files/js/3rdParty/redactor/plugins/wbbcode.js
@@ -958,19 +958,9 @@ RedactorPlugins.wbbcode = function() {
 			}
 			
 			// [*]
-			var $hasListItems = false;
 			data = data.replace(/\[\*\]([\s\S]*?)(?=\[\*\]|\[\/list\])/gi, function(match, content) {
-				$hasListItems = true;
-				
 				return '<li>' + $.trim(content) + '</li>';
 			});
-			
-			// fix expanded formatting for lists
-			if ($hasListItems) {
-				data = data.replace(/(<p style="[^"]+">)(\[list[^\]]*\])?<li>/g, '$2<li>$1');
-				data = data.replace(/(<p style="[^"]+">)<\/li><li>/g, '</li><li>$1');
-				data = data.replace(/(<\/li>\[\/list\])<\/p>/g, '$1');
-			}
 			
 			// fix superflous newlines with nested lists
 			data = data.replace(/\n*(\[list\]<\/li>)/g, '$1');
@@ -1421,7 +1411,6 @@ RedactorPlugins.wbbcode = function() {
 						+ '</div>';
 					}).bind(this));
 					
-					//data = data.replace(new RegExp('(?:<p>)?(@@' + $cachedCode.key + '@@)(?:<\/p>)?', 'g'), '$1');
 					data = data.replace($regex, $value);
 				}
 			}
