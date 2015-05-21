625136ac2e609575c849bfa86723626711b2d54a
Matthias Schmidt <gravatronics@live.com>
2015-05-21 23:14:09 +0200

    Add missing trait files

---
+++ wcfsetup/install/files/lib/data/TDatabaseObjectPermissions.class.php
@@ -0,0 +1,36 @@
+<?php
+namespace wcf\data;
+use wcf\system\WCF;
+
+/**
+ * Provides a method for validating database object permissions.
+ * 
+ * @author	Matthias Schmidt
+ * @copyright	2001-2015 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf
+ * @subpackage	data
+ * @category	Community Framework
+ */
+trait TDatabaseObjectPermissions {
+	/**
+	 * Returns true if the active user has at least one permission required
+	 * by this object.
+	 * 
+	 * @return	boolean
+	 */
+	public function validatePermissions() {
+		if ($this->permissions) {
+			$permissions = explode(',', $this->permissions);
+			foreach ($permissions as $permission) {
+				if (WCF::getSession()->getPermission($permission)) {
+					return true;
+				}
+			}
+			
+			return false;
+		}
+		
+		return true;
+	}
+}
