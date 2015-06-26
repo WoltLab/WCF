0795d1801cb9a7dbdd929d52e50046a7a7e0efdd
Alexander Ebert <ebert@woltlab.com>
2015-06-26 14:58:26 +0200

    Fixed build config and moved inline script into require.linearExecution.js

---
+++ wcfsetup/install/files/js/require.linearExecution.js
@@ -0,0 +1,32 @@
+(function(window) {
+	var orgRequire = window.require;
+	var queue = [];
+	var counter = 0;
+	
+	window.require = function(dependencies, callback) {
+		if (!Array.isArray(dependencies)) {
+			return orgRequire.apply(window, arguments);
+		}
+		
+		var i = counter++;
+		queue.push(i);
+		
+		orgRequire(dependencies, function() {
+			var args = arguments;
+			
+			queue[queue.indexOf(i)] = function() { callback.apply(window, args); };
+			
+			executeCallbacks();
+		});
+	};
+	
+	function executeCallbacks() {
+		while (queue.length) {
+			if (typeof queue[0] !== 'function') {
+				break;
+			}
+			
+			queue.shift()();
+		}
+	};
+})(window);
