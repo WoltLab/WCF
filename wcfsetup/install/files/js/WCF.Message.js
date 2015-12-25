f579491540031487c360d61ef3dfcbc6727dec24
Sascha Greuel <SoftCreatR@users.noreply.github.com>
2015-12-25 20:10:15 +0100

    Improved display of Facebook shares

--- wcfsetup/install/files/js/WCF.Message.js
+++ wcfsetup/install/files/js/WCF.Message.js
@@ -2948,12 +2948,12 @@ WCF.Message.Share.Page = Class.extend({
 	},
 	
 	/**
-	 * Fetches number of Facebook likes.
+	 * Fetches number of Facebook shares.
 	 */
 	_fetchFacebook: function() {
-		this._fetchCount('https://graph.facebook.com/?id={pageURL}', $.proxy(function(data) {
-			if (data.shares) {
-				this._ui.facebook.children('span.badge').show().text(data.shares);
+		this._fetchCount('https://graph.facebook.com/fql?q=SELECT%20share_count%20FROM%20link_stat%20WHERE%20url=%27{pageURL}%27', $.proxy(function(data) {
+			if (data.data.share_count) {
+				this._ui.facebook.children('span.badge').show().text(data.data.share_count);
 			}
 		}, this));
 	},
