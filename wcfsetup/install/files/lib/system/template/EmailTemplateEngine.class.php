156214016abd723dc6cd9096d76e300efdb715c7
Tim Düsterhus <duesterhus@woltlab.com>
2016-05-30 16:04:59 +0200

    Add EmailTemplateEngine

---
+++ wcfsetup/install/files/lib/system/template/EmailTemplateEngine.class.php
@@ -0,0 +1,45 @@
+<?php
+namespace wcf\system\template;
+use wcf\system\WCF;
+
+/**
+ * Loads and displays templates in emails.
+ * 
+ * @author	Tim Duesterhus
+ * @copyright	2001-2016 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf
+ * @subpackage	system.template
+ * @category	Community Framework
+ */
+class EmailTemplateEngine extends TemplateEngine {
+	/**
+	 * @inheritDoc
+	 */
+	protected $environment = 'email';
+	
+	/**
+	 * @inheritDoc
+	 */
+	protected function init() {
+		parent::init();
+		
+		$sql = "SELECT	templateGroupID
+			FROM	wcf".WCF_N."_template_group
+			WHERE	templateGroupFolderName = ?";
+		$statement = WCF::getDB()->prepareStatement($sql);
+		$statement->execute(['_wcf_email/']);
+		
+		parent::setTemplateGroupID($statement->fetchSingleColumn());
+	}
+	
+	/**
+	 * This method always throws, because changing the template
+	 * group is not supported.
+	 * 
+	 * @throws \BadMethodCallException
+	 */
+	public function setTemplateGroupID($templateGroupID) {
+		throw new \BadMethodCallException("You may not change the template group of the email template engine");
+	}
+}
