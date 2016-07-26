77e06c6ca2489aa3d3f6ca45b139b03ed54baaae
Marcel Werk <burntime@woltlab.com>
2016-07-26 17:37:05 +0200

    Improved box handler

--- wcfsetup/install/files/lib/data/box/content/BoxContentList.class.php
+++ wcfsetup/install/files/lib/data/box/content/BoxContentList.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace wcf\data\box\content;
 use wcf\data\DatabaseObjectList;
+use wcf\data\media\ViewableMediaList;
 
 /**
  * Represents a list of box content.
@@ -21,4 +22,50 @@ class BoxContentList extends DatabaseObjectList {
 	 * @inheritDoc
 	 */
 	public $className = BoxContent::class;
+	
+	/**
+	 * enables/disables the loading of box content images
+	 * @var	boolean
+	 */
+	protected $imageLoading = false;
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function readObjects() {
+		parent::readObjects();
+		
+		if ($this->imageLoading) {
+			$imageIDs = [];
+			foreach ($this->getObjects() as $boxContent) {
+				if ($boxContent->imageID) {
+					$imageIDs[] = $boxContent->imageID;
+				}
+			}
+			
+			// cache images
+			if (!empty($imageIDs)) {
+				$mediaList = new ViewableMediaList();
+				$mediaList->setObjectIDs($imageIDs);
+				$mediaList->readObjects();
+				$images = $mediaList->getObjects();
+				
+				foreach ($this->getObjects() as $boxContent) {
+					if ($boxContent->imageID && isset($images[$boxContent->imageID])) {
+						$boxContent->setImage($images[$boxContent->imageID]);
+					}
+				}
+			}
+		}
+	}
+	
+	
+	/**
+	 * Enables/disables the loading of box content images.
+	 *
+	 * @param	boolean		$enable
+	 */
+	public function enableImageLoading($enable = true) {
+		$this->imageLoading = $enable;
+	}
 }
