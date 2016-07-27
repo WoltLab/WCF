81ce19cc438b9132ea091b95630fb99ad710f97e
utime77 <wbbtime@me.com>
2016-07-27 23:06:29 +0200

    Create InstagramUserOptionOutput.class.php
    Instagram is an important social network nowadays.

---
+++ wcfsetup/install/files/lib/system/option/user/InstagramUserOptionOutput.class.php
@@ -0,0 +1,27 @@
+<?php
+namespace wcf\system\option\user;
+use wcf\data\user\option\UserOption;
+use wcf\data\user\User;
+use wcf\util\StringUtil;
+
+/**
+ * User option output implementation for the output of an instagram user profile.
+ * 
+ * @author      
+ * @copyright   2001-2016 WoltLab GmbH
+ * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package     WoltLabSuite\Core\System\Option\User
+ */
+class InstagramUserOptionOutput implements IUserOptionOutput {
+	/**
+	 * @see	\wcf\system\option\user\IUserOptionOutput::getOutput()
+	 */
+	public function getOutput(User $user, UserOption $option, $value) {
+		if (empty($value)) return '';
+		
+		$url = StringUtil::encodeHTML('http://instagram.com/'.$value);
+		$value = StringUtil::encodeHTML($value);
+		
+		return '<a href="'.$url.'" class="externalURL"'.(EXTERNAL_LINK_REL_NOFOLLOW ? ' rel="nofollow"' : '').(EXTERNAL_LINK_TARGET_BLANK ? ' target="_blank"' : '').'>'.$value.'</a>';
+	}
+}
