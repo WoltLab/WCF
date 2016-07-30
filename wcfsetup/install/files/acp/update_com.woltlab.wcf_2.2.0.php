c23634e7f8d3f53a99152e6b67d4ef5991a78736
Tim Düsterhus <duesterhus@woltlab.com>
2016-07-30 14:33:28 +0200

    Update ToDo list for upgrade to WSC 3

--- wcfsetup/install/files/acp/update_com.woltlab.wcf_2.2.0.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// TODO:
-// 1. update label's show order
-// 2. If a template group uses '_wcf_email' as the folder: Move it!
---
+++ wcfsetup/install/files/acp/update_com.woltlab.wcf_3.0.0.php
@@ -0,0 +1,10 @@
+<?php
+
+// TODO:
+// 1. update label's show order
+// 2. If a template group uses '_wcf_email' as the folder: Move it!
+// see: https://github.com/WoltLab/WCF/commit/76eee28f8502fec923fe1d374cf008ab42f23a28
+// 3. Fill in the mail_* constants with something sane.
+// see: https://github.com/WoltLab/WCF/commit/e1b6d09d705d6c153612bb36c70efbdb3d42871e
+// 4. Generate signature_secret
+// see: https://github.com/WoltLab/WCF/commit/9fe8b50cc6011628e2ac72d6e80c9561312503f4
