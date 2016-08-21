109e70e761a6bb4a22992896f6f2e8ef729cf99d
Matthias Schmidt <gravatronics@live.com>
2016-08-21 15:40:18 +0200

    Add check if any users are affected in UserGroupAssignmentCronjob

--- wcfsetup/install/files/lib/system/cronjob/UserGroupAssignmentCronjob.class.php
+++ wcfsetup/install/files/lib/system/cronjob/UserGroupAssignmentCronjob.class.php
@@ -9,7 +9,7 @@ use wcf\system\user\group\assignment\UserGroupAssignmentHandler;
  * Executes automatic user group assignments.
  * 
  * @author	Matthias Schmidt
- * @copyright	2001-2015 WoltLab GmbH
+ * @copyright	2001-2016 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	com.woltlab.wcf
  * @subpackage	system.cronjob
@@ -33,12 +33,14 @@ class UserGroupAssignmentCronjob extends AbstractCronjob {
 		}
 		
 		foreach ($usersToGroup as $groupID => $users) {
-			$userAction = new UserAction(array_unique($users), 'addToGroups', array(
-				'addDefaultGroups' => false,
-				'deleteOldGroups' => false,
-				'groups' => array($groupID)
-			));
-			$userAction->executeAction();
+			if (!empty($users)) {
+				$userAction = new UserAction(array_unique($users), 'addToGroups', array(
+					'addDefaultGroups' => false,
+					'deleteOldGroups' => false,
+					'groups' => array($groupID)
+				));
+				$userAction->executeAction();
+			}
 		}
 	}
 }
