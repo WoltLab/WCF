0a6276f31a80c78e4b10c4c26ecd47ce82aab1cb
Alexander Ebert <ebert@woltlab.com>
2016-08-24 22:50:50 +0200

    bbcodes return an encoded marker

--- wcfsetup/install/files/lib/system/html/node/AbstractHtmlNodeProcessor.class.php
+++ wcfsetup/install/files/lib/system/html/node/AbstractHtmlNodeProcessor.class.php
@@ -81,7 +81,12 @@ abstract class AbstractHtmlNodeProcessor implements IHtmlNodeProcessor {
 			$obj = $data['object'];
 			$string = $obj->replaceTag($data['data']);
 			$html = preg_replace_callback('~<wcfNode-' . $data['identifier'] . '>(?P<content>[\s\S]*)</wcfNode-' . $data['identifier'] . '>~', function($matches) use ($string) {
-				$string = str_replace('<!-- META_CODE_INNER_CONTENT -->', $matches['content'], $string);
+				if (mb_strpos($string, '<!-- META_CODE_INNER_CONTENT -->') !== false) {
+					return str_replace('<!-- META_CODE_INNER_CONTENT -->', $matches['content'], $string);
+				}
+				else if (mb_strpos($string, '&lt;!-- META_CODE_INNER_CONTENT --&gt;') !== false) {
+					return str_replace('&lt;!-- META_CODE_INNER_CONTENT --&gt;', $matches['content'], $string);
+				}
 				
 				return $string;
 			}, $html);
