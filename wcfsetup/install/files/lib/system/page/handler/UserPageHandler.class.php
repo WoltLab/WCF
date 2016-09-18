29340381b38610d6d0de9983b993ed7db0898c42
Matthias Schmidt <gravatronics@live.com>
2016-09-18 13:10:09 +0200

    Add TUserOnlineLocationPageHandler

--- wcfsetup/install/files/lib/system/page/handler/UserPageHandler.class.php
+++ wcfsetup/install/files/lib/system/page/handler/UserPageHandler.class.php
@@ -1,13 +1,9 @@
 <?php
 namespace wcf\system\page\handler;
-use wcf\data\page\Page;
-use wcf\data\user\online\UserOnline;
-use wcf\system\cache\runtime\UserRuntimeCache;
-use wcf\system\WCF;
 
 /**
  * Menu page handler for the user profile page.
- *
+ * 
  * @author	Matthias Schmidt
  * @copyright	2001-2016 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
@@ -15,30 +11,5 @@ use wcf\system\WCF;
  * @since	3.0
  */
 class UserPageHandler extends AbstractMenuPageHandler implements IOnlineLocationPageHandler {
-	use TOnlineLocationPageHandler;
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function getOnlineLocation(Page $page, UserOnline $user) {
-		if ($user->pageObjectID === null) {
-			return '';
-		}
-		
-		$visitedUser = UserRuntimeCache::getInstance()->getObject($user->pageObjectID);
-		if ($visitedUser === null) {
-			return '';
-		}
-		
-		return WCF::getLanguage()->getDynamicVariable('wcf.page.onlineLocation.'.$page->identifier, ['user' => $visitedUser]);
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function prepareOnlineLocation(Page $page, UserOnline $user) {
-		if ($user->pageObjectID !== null) {
-			UserRuntimeCache::getInstance()->cacheObjectID($user->pageObjectID);
-		}
-	}
+	use TUserOnlineLocationPageHandler;
 }
