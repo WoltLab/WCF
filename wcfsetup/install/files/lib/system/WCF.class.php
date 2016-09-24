870245634fd0f5abdb361583cd52a1a851fa6fcc
Alexander Ebert <ebert@woltlab.com>
2016-09-24 20:06:32 +0200

    Updated version number

--- wcfsetup/install/files/lib/system/WCF.class.php
+++ wcfsetup/install/files/lib/system/WCF.class.php
@@ -47,7 +47,7 @@ if (!@ini_get('date.timezone')) {
 }
 
 // define current woltlab suite version
-define('WCF_VERSION', '3.0.0 Alpha 1');
+define('WCF_VERSION', '3.0.0 Beta 1');
 
 // define current unix timestamp
 define('TIME_NOW', time());
