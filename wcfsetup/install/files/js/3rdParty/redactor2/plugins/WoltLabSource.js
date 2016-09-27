12c322aa6bdd2f2f48af264c67f0fee912b49eb0
Alexander Ebert <ebert@woltlab.com>
2016-09-27 14:02:22 +0200

    Added source formatting

--- wcfsetup/install/files/js/3rdParty/redactor2/plugins/WoltLabSource.js
+++ wcfsetup/install/files/js/3rdParty/redactor2/plugins/WoltLabSource.js
@@ -31,6 +31,8 @@ $.Redactor.prototype.WoltLabSource = function() {
 				textarea.style.setProperty('height', Math.ceil(height) + 'px', '');
 				textarea.style.setProperty('display', 'block', '');
 				
+				textarea.value = this.WoltLabSource.format(textarea.value);
+				
 				textarea.selectionStart = textarea.selectionEnd = textarea.value.length;
 			}).bind(this);
 			
@@ -44,6 +46,26 @@ $.Redactor.prototype.WoltLabSource = function() {
 		
 		isActive: function () {
 			return (this.$editor[0].style.getPropertyValue('display') === 'none');
+		},
+		
+		format: function (html) {
+			var blockTags = this.block.tags.join('|').toLowerCase();
+			blockTags += '|ul|ol|li';
+			
+			html = html.replace(new RegExp('<(' + blockTags + ')([^>]*)>\s*', 'g'), '<$1$2>\n\t');
+			html = html.replace(new RegExp('\s*</(' + blockTags + ')>', 'g'), '\n</$1>');
+			
+			html = html.replace(/\s*<li>/g, '\n<li>');
+			html = html.replace(/<\/li>\s*/g, '</li>\n');
+			html = html.replace(/<li>([\s\S]*?)<\/li>/g, function(match, content) {
+				var parts = content.trim().split('\n');
+				return '<li>\n\t' + parts.join('\n\t') + '\n</li>';
+			});
+			
+			html = html.replace(/\s*<\/p>\s*/g, '\n</p>\n');
+			html = html.replace(/<p>\s*<br>\s*<\/p>/g, '<p><br></p>');
+			
+			return html.replace(/^\s+/, '');
 		}
 	};
 };
