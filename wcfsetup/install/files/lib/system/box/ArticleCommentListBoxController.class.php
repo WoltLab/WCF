0943780a1c054ad75baa8f1773f364d0447f0c2e
Matthias Schmidt <gravatronics@live.com>
2016-10-06 17:24:03 +0200

    Add box controller for article comments

---
+++ wcfsetup/install/files/lib/system/box/ArticleCommentListBoxController.class.php
@@ -0,0 +1,45 @@
+<?php
+namespace wcf\system\box;
+use wcf\data\article\Article;
+use wcf\data\article\category\ArticleCategory;
+use wcf\data\comment\ViewableCommentList;
+use wcf\system\language\LanguageFactory;
+use wcf\system\WCF;
+
+/**
+ * Box controller implementation for a list of article comments.
+ * 
+ * @author	Matthias Schmidt
+ * @copyright	2001-2016 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	WoltLabSuite\Core\System\Box
+ */
+class ArticleCommentListBoxController extends AbstractCommentListBoxController {
+	/**
+	 * @inheritDoc
+	 */
+	protected $objectTypeName = 'com.woltlab.wcf.articleComment';
+	
+	/**
+	 * @inheritDoc
+	 */
+	protected function applyObjectTypeFilters(ViewableCommentList $commentList) {
+		$accessibleCategoryIDs = ArticleCategory::getAccessibleCategoryIDs();
+		if (!empty($accessibleCategoryIDs)) {
+			$commentList->sqlJoins .= ' INNER JOIN wcf' . WCF_N . '_article_content article_content ON (article_content.articleContentID = comment.objectID)';
+			$commentList->sqlJoins .= ' INNER JOIN wcf' . WCF_N . '_article article ON (article.articleID = article_content.articleID)';
+			$commentList->sqlSelects = 'article_content.title';
+			
+			$commentList->getConditionBuilder()->add('article.categoryID IN (?)', [$accessibleCategoryIDs]);
+			$commentList->getConditionBuilder()->add('article.publicationStatus = ?', [Article::PUBLISHED]);
+			
+			// apply language filter
+			if (LanguageFactory::getInstance()->multilingualismEnabled() && !empty(WCF::getUser()->getLanguageIDs())) {
+				$commentList->getConditionBuilder()->add('(article_content.languageID IN (?) OR article_content.languageID IS NULL)', [WCF::getUser()->getLanguageIDs()]);
+			}
+		}
+		else {
+			$commentList->getConditionBuilder()->add('0 = 1');
+		}
+	}
+}
