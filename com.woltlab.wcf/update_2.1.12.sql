32c31af3e7c9282a29df7f5bbc7047644072a35a
Alexander Ebert <ebert@woltlab.com>
2016-10-18 12:41:14 +0200

    Preparing release 2.1.12

---
+++ com.woltlab.wcf/update_2.1.12.sql
@@ -0,0 +1,5 @@
+/* a61d3877562d5ca0e16deb73fa0eb4a2608e5d5c */
+DELETE FROM wcf1_bbcode_media_provider WHERE title = 'MyVideo';
+
+/* 6259fd829bc984d24ce74b91bae6ce546f233311 */
+ALTER TABLE wcf1_user ADD KEY email (email);
