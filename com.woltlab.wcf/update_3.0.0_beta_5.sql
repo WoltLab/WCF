12d970f039e5e3d0373f2056f47020f4ab2300c6
Alexander Ebert <ebert@woltlab.com>
2016-11-22 17:58:52 +0100

    Fixed SQL update

--- com.woltlab.wcf/update_3.0.0_beta_5.sql
+++ com.woltlab.wcf/update_3.0.0_beta_5.sql
@@ -1,2 +1,2 @@
 /* 1fee22a07a7ed2e759bef5a3fb87476576508b60 */
-ALTER TABLE wcf1_style_variable SET defaultValue = '1025px' WHERE variableName = 'wcfLayoutMinWidth';
+UPDATE wcf1_style_variable SET defaultValue = '1025px' WHERE variableName = 'wcfLayoutMinWidth';
