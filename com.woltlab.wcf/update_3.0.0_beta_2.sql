588ff917bac29767f2c51cd3fbf7021212acee5f
Alexander Ebert <ebert@woltlab.com>
2016-11-22 15:36:50 +0100

    Preparing release 3.0.0 Beta 5

--- com.woltlab.wcf/update_3.0.0_beta_2.sql
+++
@@ -1,8 +0,0 @@
-/* 1535ab4ef15edc761a02e91e25e28e866716c0b5 */
-ALTER TABLE wcf1_application DROP COLUMN cookiePath;
-
-/* d3925b67b9eba7eaef81887a0cf772dcfc5baa15 */
-UPDATE wcf1_style_variable SET defaultValue = 'rgba(217, 220, 222, 1)' WHERE variableName = 'wcfFooterCopyrightLink';
-
-/* 640a8bea2af6fd577932e9a083fc800c7d1a1190 */
-DELETE FROM wcf1_style_variable WHERE variableName IN ('wcfButtonBorder', 'wcfButtonBorderActive', 'wcfButtonDisabledBorder', 'wcfButtonPrimaryBorder', 'wcfButtonPrimaryBorderActive', 'wcfDropdownBorder');
