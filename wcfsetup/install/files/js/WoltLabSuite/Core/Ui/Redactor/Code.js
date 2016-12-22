e712d872ac219cd12ce1e7686f20242e0985146e
Alexander Ebert <ebert@woltlab.com>
2016-12-22 14:12:13 +0100

    Prevent superfluous `<br>` in `<pre>`

--- wcfsetup/install/files/js/WoltLabSuite/Core/Ui/Redactor/Code.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Ui/Redactor/Code.js
@@ -53,6 +53,11 @@ define(['EventHandler', 'EventKey', 'Language', 'StringUtil', 'Dom/Util', 'Ui/Di
 			
 			var pre = this._editor.selection.block();
 			if (pre && pre.nodeName === 'PRE') {
+				if (pre.childElementCount === 1 && pre.children[0].nodeName === 'BR') {
+					// drop superfluous linebreak
+					pre.removeChild(pre.children[0]);
+				}
+				
 				this._setTitle(pre);
 				
 				pre.addEventListener(WCF_CLICK_EVENT, this._callbackEdit);
