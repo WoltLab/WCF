5c8049ebd39015774227cc481a7dca08c27430d5
Matthias Schmidt <gravatronics@live.com>
2016-12-24 10:02:32 +0100

    Exclude menu boxes in box search

--- wcfsetup/install/files/lib/system/search/acp/BoxACPSearchResultProvider.class.php
+++ wcfsetup/install/files/lib/system/search/acp/BoxACPSearchResultProvider.class.php
@@ -24,6 +24,7 @@ class BoxACPSearchResultProvider implements IACPSearchResultProvider {
 		$results = [];
 		
 		$boxList = new BoxList();
+		$boxList->getConditionBuilder()->add('box.boxType <> ?', ['menu']);
 		$boxList->getConditionBuilder()->add('box.name LIKE ?', ['%'.$query.'%']);
 		$boxList->sqlLimit = 10;
 		$boxList->sqlOrderBy = 'box.name';
