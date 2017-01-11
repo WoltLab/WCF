ce55a5294dcce2fcc562ebf75eef1fecf66f0a8b
Alexander Ebert <ebert@woltlab.com>
2017-01-11 12:26:06 +0100

    Added work-around for upgrade 2.1 -> 3.0

---
+++ wcfsetup/install/files/acp/update_com.woltlab.wcf_3.0_appConfig.php
@@ -0,0 +1,10 @@
+<?php
+use wcf\data\package\Package;
+
+/**
+ * @author	Alexander Ebert
+ * @copyright	2001-2017 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	WoltLabSuite\Core
+ */
+Package::writeConfigFile(1);
---
+++ wcfsetup/install/files/acp/update_com.woltlab.wcf_3.0_noop.php
@@ -0,0 +1,2 @@
+<?php
+/* DO NOT REMOVE THIS FILE! -- As strange as it looks, this is a necessary work-around */
