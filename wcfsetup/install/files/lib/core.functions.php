416c2b5117374dfbbb075196014a081adb38854b
Alexander Ebert <ebert@woltlab.com>
2017-02-17 15:33:42 +0100

    Global gzip compression breaks output buffering

--- wcfsetup/install/files/lib/core.functions.php
+++ wcfsetup/install/files/lib/core.functions.php
@@ -83,6 +83,11 @@ namespace {
 			}
 		}
 	}
+	
+	// setting global gzip compression breaks output buffering
+	if (@ini_get('zlib.output_compression')) {
+		@ini_set('zlib.output_compression', 0);
+	}
 }
 
 // @codingStandardsIgnoreStart
