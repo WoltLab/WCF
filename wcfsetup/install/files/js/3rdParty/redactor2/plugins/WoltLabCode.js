398389a3fc28db7569f80bfd08b39bf52e4b68ba
Alexander Ebert <ebert@woltlab.com>
2017-02-22 15:04:49 +0100

    Properly resetting editor toolbar

--- wcfsetup/install/files/js/3rdParty/redactor2/plugins/WoltLabCode.js
+++ wcfsetup/install/files/js/3rdParty/redactor2/plugins/WoltLabCode.js
@@ -13,6 +13,16 @@ $.Redactor.prototype.WoltLabCode = function() {
 				
 				WCF.System.Event.fireEvent('com.woltlab.wcf.redactor2', 'codeStart_' + this.$element[0].id);
 			}).bind(this);
+			
+			var mpSet = this.code.set;
+			this.code.set = (function(html, options) {
+				mpSet.call(this, html, options);
+				
+				if (this.utils.isEmpty()) {
+					// reset all buttons
+					this.observe.toolbar();
+				}
+			}).bind(this);
 		}
 	};
 };
