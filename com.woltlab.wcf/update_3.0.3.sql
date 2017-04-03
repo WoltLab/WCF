db9ffd297d18ee6020f7ac2d21a49a7a5e41326a
Alexander Ebert <ebert@woltlab.com>
2017-04-03 11:45:23 +0200

    Preparing release 3.0.4

--- com.woltlab.wcf/update_3.0.3.sql
+++
@@ -1,2 +0,0 @@
-/* 453b84d553254088b2d123d8905931f4d3593c70 */
-UPDATE wcf1_style_variable SET defaultValue = 'rgba(255, 255, 255, 1)' WHERE variableName = 'wcfHeaderLink';
--- wcfsetup/install/files/lib/system/WCF.class.php
+++ wcfsetup/install/files/lib/system/WCF.class.php
@@ -46,7 +46,7 @@ if (!@ini_get('date.timezone')) {
 }
 
 // define current woltlab suite version
-define('WCF_VERSION', '3.0.3');
+define('WCF_VERSION', '3.0.4');
 
 // define current unix timestamp
 define('TIME_NOW', time());
