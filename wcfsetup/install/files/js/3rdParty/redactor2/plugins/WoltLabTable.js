abb354c9acac65c99ed499f7aa9b768c4cf30cff
Alexander Ebert <ebert@woltlab.com>
2017-05-20 14:40:48 +0200

    Fixed table selection and erroneous whitespaces

--- wcfsetup/install/files/js/3rdParty/redactor2/plugins/WoltLabTable.js
+++ wcfsetup/install/files/js/3rdParty/redactor2/plugins/WoltLabTable.js
@@ -5,10 +5,21 @@ $.Redactor.prototype.WoltLabTable = function() {
 		init: function() {
 			this.WoltLabEvent.register('insertedTable', (function() {
 				window.setTimeout((function () {
-					var node = this.selection.block() || this.selection.current();
-					if (node.nodeName === 'TBODY') {
+					var table = this.selection.block() || this.selection.current();
+					if (table.nodeName === 'TBODY') table = table.parentNode;
+					if (table.nodeName === 'TABLE') {
+						// remove whitespaces directly inside the table element
+						var node, textNodes = [];
+						for (var i = 0, length = table.childNodes.length; i < length; i++) {
+							node = table.childNodes[i];
+							if (node.nodeType === Node.TEXT_NODE && node.textContent.length > 0) {
+								textNodes.push(node);
+							}
+						}
+						textNodes.forEach(elRemove);
+						
 						// find first <td> and set the caret inside it
-						var td = elBySel('td', node);
+						var td = elBySel('td', table);
 						if (td) {
 							this.caret.end(td);
 						}
