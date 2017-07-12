7194c45fecf0ddbb3a77d9e27270f52f344dc0ff
jens1o <jenshausdorf@gmail.com>
2017-07-12 16:24:32 +0200

    http => https in social networks links

--- wcfsetup/install/files/lib/system/option/user/FacebookUserOptionOutput.class.php
+++ wcfsetup/install/files/lib/system/option/user/FacebookUserOptionOutput.class.php
@@ -19,7 +19,7 @@ class FacebookUserOptionOutput implements IUserOptionOutput {
 	public function getOutput(User $user, UserOption $option, $value) {
 		if (empty($value)) return '';
 		
-		$url = StringUtil::encodeHTML('http://www.facebook.com/'.$value);
+		$url = StringUtil::encodeHTML('https://www.facebook.com/'.$value);
 		$value = StringUtil::encodeHTML($value);
 		
 		return '<a href="'.$url.'" class="externalURL"'.((EXTERNAL_LINK_REL_NOFOLLOW || EXTERNAL_LINK_TARGET_BLANK) ? (' rel="'.(EXTERNAL_LINK_REL_NOFOLLOW ? 'nofollow' : '').((EXTERNAL_LINK_REL_NOFOLLOW && EXTERNAL_LINK_TARGET_BLANK) ? ' ' : '').(EXTERNAL_LINK_TARGET_BLANK ? 'noopener noreferrer' : '').'"') : '').(EXTERNAL_LINK_TARGET_BLANK ? ' target="_blank"' : '').'>'.$value.'</a>';
