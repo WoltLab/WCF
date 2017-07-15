7c930597d6fcd680b6bfdcdf2841a86036a17681
Joshua Rüsweg <josh@bastelstu.be>
2017-07-15 22:50:24 +0200

    Fix $databaseTableIndexName for UserTrophy
    See #2315

--- wcfsetup/install/files/lib/data/user/trophy/UserTrophy.class.php
+++ wcfsetup/install/files/lib/data/user/trophy/UserTrophy.class.php
@@ -25,9 +25,14 @@ use wcf\util\StringUtil;
  * @property-read	integer		$userID				user id
  * @property-read	integer		$time				the time when the trophy was rewarded
  * @property-read	string		$description			the custom trophy description
- * @property-read	string		$useCustomDescription		`1`, iif the trophy use a custom description
+ * @property-read	string		$useCustomDescription		`1`, if the trophy use a custom description
  */
 class UserTrophy extends DatabaseObject {
+	/**
+	 * @inheritDoc
+	 */
+	protected static $databaseTableIndexName = 'userTrophyID';
+	
 	/**
 	 * The description text replacements. 
 	 * @var string[]
