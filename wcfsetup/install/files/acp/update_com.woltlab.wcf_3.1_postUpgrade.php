485df5c928f8a3bbfe87bad92f58c10c73e93e7d
Alexander Ebert <ebert@woltlab.com>
2017-09-07 14:42:18 +0200

    Force disable the HTML bbcode on upgrade
    Fixes #2388

---
+++ wcfsetup/install/files/acp/update_com.woltlab.wcf_3.1_postUpgrade.php
@@ -0,0 +1,39 @@
+<?php
+use wcf\system\WCF;
+
+/**
+ * @author	Alexander Ebert
+ * @copyright	2001-2017 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	WoltLabSuite\Core
+ */
+// force disable of HTML bbcode for all groups
+$sql = "UPDATE  wcf".WCF_N."_user_group_option_value
+	SET     optionValue = ?
+	WHERE   groupID = ?
+		AND optionID = ?";
+$updateStatement = WCF::getDB()->prepareStatement($sql);
+
+$sql = "SELECT  *
+	FROM    wcf".WCF_N."_user_group_option_value
+	WHERE   optionID IN (
+			SELECT  optionID
+			FROM    wcf".WCF_N."_user_group_option
+			WHERE   optionType = ?
+		)";
+$statement = WCF::getDB()->prepareStatement($sql);
+$statement->execute(['BBCodeSelect']);
+
+WCF::getDB()->beginTransaction();
+while ($row = $statement->fetchArray()) {
+	$value = $row['optionValue'];
+	if (!empty($value)) $value .= ',';
+	$value .= 'html';
+	
+	$updateStatement->execute([
+		$value,
+		$row['groupID'],
+		$row['optionID']
+	]);
+}
+WCF::getDB()->commitTransaction();
