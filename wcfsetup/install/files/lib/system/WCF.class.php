7511311cd928eda662114f69cf145f0d82e3bb44
Alexander Ebert <ebert@woltlab.com>
2017-09-08 18:18:03 +0200

    Preparing release 3.0.9

--- wcfsetup/install/files/lib/system/WCF.class.php
+++ wcfsetup/install/files/lib/system/WCF.class.php
@@ -47,7 +47,7 @@ if (!@ini_get('date.timezone')) {
 }
 
 // define current woltlab suite version
-define('WCF_VERSION', '3.0.8');
+define('WCF_VERSION', '3.0.9');
 
 // define current unix timestamp
 define('TIME_NOW', time());
