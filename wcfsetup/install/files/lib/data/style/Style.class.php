e9dfcb4b108673034d954bdc01ca824bd0b36a9b
Alexander Ebert <ebert@woltlab.com>
2017-10-20 13:36:18 +0200

    Added a proxy script to allow CORS access for `manifest.json`

--- wcfsetup/install/files/lib/data/style/Style.class.php
+++ wcfsetup/install/files/lib/data/style/Style.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace wcf\data\style;
 use wcf\data\DatabaseObject;
+use wcf\system\application\ApplicationHandler;
 use wcf\system\WCF;
 
 /**
@@ -152,6 +153,12 @@ class Style extends DatabaseObject {
 	}
 	
 	protected function getFaviconPath($filename, $absolutePath = true) {
+		if ($filename === 'manifest.json') {
+			if (ApplicationHandler::getInstance()->getActiveApplication()->domainName !== ApplicationHandler::getInstance()->getApplicationByID(1)->domainName) {
+				return WCF::getPath() . 'images/favicon/corsProxy.php?type=manifest' . ($this->hasFavicon ? '&amp;styleID=' . $this->styleID : '');
+			}
+		}
+		
 		$path = 'images/favicon/'. ($this->hasFavicon ? $this->styleID : 'default') . ".{$filename}";
 		if ($absolutePath) {
 			return WCF::getPath() . $path;
