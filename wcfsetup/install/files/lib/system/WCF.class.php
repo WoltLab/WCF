69a42d2c0cec4c931fafe4eb58ec267ca130e057
Alexander Ebert <ebert@woltlab.com>
2017-10-24 12:32:42 +0200

    Preparing release 3.1.0 Beta 2

--- wcfsetup/install/files/lib/system/WCF.class.php
+++ wcfsetup/install/files/lib/system/WCF.class.php
@@ -47,7 +47,7 @@ if (!@ini_get('date.timezone')) {
 }
 
 // define current woltlab suite version
-define('WCF_VERSION', '3.1.0 Beta 1');
+define('WCF_VERSION', '3.1.0 Beta 2');
 
 // define current API version
 define('WSC_API_VERSION', 2018);
