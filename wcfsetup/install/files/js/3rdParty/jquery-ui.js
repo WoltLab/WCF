c488a43077c3e8949bb6aca7b363df8b63c857fa
Alexander Ebert <ebert@woltlab.com>
2017-11-14 22:00:41 +0100

    Revert performance nightmare in jQuery UI Sortable
    Partially reverts the changes done in
    jquery/jquery-ui@90c27b4b5130fff9abbfea671c193c59e4bf8850
    
    Decreases the initialization time for 1.3k sortable lists/items from ~2
    minutes down to 2.8 seconds

--- wcfsetup/install/files/js/3rdParty/jquery-ui.js
+++ wcfsetup/install/files/js/3rdParty/jquery-ui.js
@@ -5019,15 +5019,12 @@ var widgetsSortable = $.widget( "ui.sortable", $.ui.mouse, {
 	},
 
 	_setHandleClassName: function() {
-		var that = this;
 		this._removeClass( this.element.find( ".ui-sortable-handle" ), "ui-sortable-handle" );
 		$.each( this.items, function() {
-			that._addClass(
-				this.instance.options.handle ?
-					this.item.find( this.instance.options.handle ) :
-					this.item,
-				"ui-sortable-handle"
-			);
+			(this.instance.options.handle
+				? this.item.find( this.instance.options.handle )
+				: this.item
+			).addClass('ui-sortable-handle');
 		} );
 	},
 
