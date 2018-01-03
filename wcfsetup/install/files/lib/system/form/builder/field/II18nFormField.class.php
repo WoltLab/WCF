d983c4039e193dd8c6d3ce59d663fd41e7f48016
Matthias Schmidt <gravatronics@live.com>
2018-01-03 13:11:35 +0100

    Add more methods to `II18nFormField`
    See #2509

--- wcfsetup/install/files/lib/system/form/builder/field/II18nFormField.class.php
+++ wcfsetup/install/files/lib/system/form/builder/field/II18nFormField.class.php
@@ -12,6 +12,22 @@ namespace wcf\system\form\builder\field;
  */
 interface II18nFormField extends IFormField {
 	/**
+	 * Returns `true` if the current field value is a i18n value and returns `false`
+	 * otherwise or if no value has been set.
+	 * 
+	 * @return	bool
+	 */
+	public function hasI18nValues();
+	
+	/**
+	 * Returns `true` if the current field value is a plain value and returns `false`
+	 * otherwise or if no value has been set.
+	 *
+	 * @return	bool
+	 */
+	public function hasPlainValue();
+	
+	/**
 	 * Sets whether this field is supports i18n input and returns this field.
 	 * 
 	 * @param	bool		$i18n		determined if field is supports i18n input
@@ -22,10 +38,31 @@ interface II18nFormField extends IFormField {
 	public function i18n($i18n = true);
 	
 	/**
+	 * Sets whether this field's value must be i18n input and returns this field.
+	 * 
+	 * If this method sets that the field's value must be i18n input, it also must
+	 * ensure that i18n support is enabled.
+	 * 
+	 * @param	bool		$i18nRequired		determined if field value must be i18n input
+	 * @return	static					this field
+	 *
+	 * @throws	\InvalidArgumentException		if the given value is no bool
+	 */
+	public function i18nRequired($i18nRequired = true);
+	
+	/**
 	 * Returns `true` if this field supports i18n input and returns `false` otherwise.
 	 * By default, fields do not support i18n input.
 	 * 
 	 * @return	bool
 	 */
 	public function isI18n();
+	
+	/**
+	 * Returns `true` if this field's value must be i18n input and returns `false` otherwise.
+	 * By default, fields do not support i18n input.
+	 * 
+	 * @return	bool
+	 */
+	public function isI18nRequired();
 }
