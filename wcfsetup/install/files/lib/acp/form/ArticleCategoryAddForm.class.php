68ef3543db10017662134671918cff4ad6ae89a5
Alexander Ebert <ebert@woltlab.com>
2018-07-09 17:37:53 +0200

    Custom sort order for articles per category
    See #2592

--- wcfsetup/install/files/lib/acp/form/ArticleCategoryAddForm.class.php
+++ wcfsetup/install/files/lib/acp/form/ArticleCategoryAddForm.class.php
@@ -1,15 +1,18 @@
 <?php
 declare(strict_types=1);
 namespace wcf\acp\form;
+use wcf\system\exception\UserInputException;
+use wcf\system\WCF;
+use wcf\util\StringUtil;
 
 /**
  * Shows the article category add form.
  *
- * @author	Marcel Werk
- * @copyright	2001-2018 WoltLab GmbH
- * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package	WoltLabSuite\Core\Acp\Form
- * @since	3.0
+ * @author      Marcel Werk
+ * @copyright   2001-2018 WoltLab GmbH
+ * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package     WoltLabSuite\Core\Acp\Form
+ * @since       3.0
  */
 class ArticleCategoryAddForm extends AbstractCategoryAddForm {
 	/**
@@ -26,4 +29,76 @@ class ArticleCategoryAddForm extends AbstractCategoryAddForm {
 	 * @inheritDoc
 	 */
 	public $neededModules = ['MODULE_ARTICLE'];
+	
+	/**
+	 * @var string[]
+	 * @since 3.2
+	 */
+	public $availableSortFields = [
+		'publicationDate',
+		'title'
+	];
+	
+	/**
+	 * @var string
+	 * @since 3.2
+	 */
+	public $sortField = 'publicationDate';
+	
+	/**
+	 * @var string
+	 * @since 3.2
+	 */
+	public $sortOrder = 'DESC';
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function readParameters() {
+		parent::readParameters();
+		
+		if (isset($_POST['sortField'])) $this->sortField = StringUtil::trim($_POST['sortField']);
+		if (isset($_POST['sortOrder'])) $this->sortOrder = StringUtil::trim($_POST['sortOrder']);
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function validate() {
+		parent::validate();
+		
+		if (!in_array($this->sortField, $this->availableSortFields)) {
+			throw new UserInputException('sortField');
+		}
+		
+		if ($this->sortOrder !== 'ASC' && $this->sortOrder !== 'DESC') {
+			throw new UserInputException('sortOrder');
+		}
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function save() {
+		$this->additionalData['sortField'] = $this->sortField;
+		$this->additionalData['sortOrder'] = $this->sortOrder;
+		
+		parent::save();
+		
+		$this->sortField = 'publicationDate';
+		$this->sortOrder = 'DESC';
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function assignVariables() {
+		parent::assignVariables();
+		
+		WCF::getTPL()->assign([
+			'availableSortFields' => $this->availableSortFields,
+			'sortField' => $this->sortField,
+			'sortOrder' => $this->sortOrder
+		]);
+	}
 }
