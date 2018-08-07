a34c622cd6524b578866537effbe0c79b1a0957d
Joshua Rüsweg <josh@bastelstu.be>
2018-08-07 12:40:17 +0200

    Fix typo See #2508

--- wcfsetup/install/files/lib/data/reaction/ReactionAction.class.php
+++ wcfsetup/install/files/lib/data/reaction/ReactionAction.class.php
@@ -264,7 +264,7 @@ class ReactionAction extends AbstractDatabaseObjectAction {
 		]);
 		$row = $statement->fetchArray();
 		
-		// no (dis-)likes at all
+		// no reactions at all
 		if ($row === false) {
 			return;
 		}
