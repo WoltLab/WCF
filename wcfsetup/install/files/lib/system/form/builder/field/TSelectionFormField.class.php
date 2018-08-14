855dba8af4282581bc76127c211f279e5dff5f71
Matthias Schmidt <gravatronics@live.com>
2018-08-14 19:49:39 +0200

    Make selection form field labels' treatment as language items optional
    See #2509

--- wcfsetup/install/files/lib/system/form/builder/field/TSelectionFormField.class.php
+++ wcfsetup/install/files/lib/system/form/builder/field/TSelectionFormField.class.php
@@ -108,14 +108,15 @@ trait TSelectionFormField {
 	 * and the this field is nullable, then the save value of that key is `null`
 	 * instead of the given empty value.
 	 * 
-	 * @param	array|callable		$options	selectable options or callable returning the options
-	 * @param	bool			$nestedOptions
-	 * @return	static					this field
+	 * @param	array|callable		$options		selectable options or callable returning the options
+	 * @param	bool			$nestedOptions		is `true` if the passed options are nested options
+	 * @param	bool			$labelLanguageItems	is `true` if the labels should be treated as language items if possible
+	 * @return	static						this field
 	 * 
-	 * @throws	\InvalidArgumentException		if given options are no array or callable or otherwise invalid
-	 * @throws	\UnexpectedValueException		if callable does not return an array
+	 * @throws	\InvalidArgumentException	if given options are no array or callable or otherwise invalid
+	 * @throws	\UnexpectedValueException	if callable does not return an array
 	 */
-	public function options($options, $nestedOptions = false) {
+	public function options($options, $nestedOptions = false, $labelLanguageItems = true) {
 		if ($nestedOptions) {
 			if (!is_array($options) && !is_callable($options)) {
 				throw new \InvalidArgumentException("The given nested options are neither an array nor a callable, " . gettype($options) . " given.");
@@ -137,7 +138,7 @@ trait TSelectionFormField {
 				throw new \UnexpectedValueException("The options callable is expected to return an array or an instance of '" . DatabaseObjectList::class . "', " . gettype($options) . " returned.");
 			}
 			
-			return $this->options($options, $nestedOptions);
+			return $this->options($options, $nestedOptions, $labelLanguageItems);
 		}
 		else if ($options instanceof DatabaseObjectList) {
 			// automatically read objects
@@ -186,7 +187,7 @@ trait TSelectionFormField {
 				}
 				
 				// resolve language item for label
-				if (preg_match('~^([a-zA-Z0-9-_]+\.){2,}[a-zA-Z0-9-_]+$~', (string) $option['label'])) {
+				if ($labelLanguageItems && preg_match('~^([a-zA-Z0-9-_]+\.){2,}[a-zA-Z0-9-_]+$~', (string) $option['label'])) {
 					$option['label'] = WCF::getLanguage()->getDynamicVariable($option['label']);
 				}
 				
@@ -231,7 +232,7 @@ trait TSelectionFormField {
 				}
 				
 				// resolve language item for label
-				if (preg_match('~^([a-zA-Z0-9-_]+\.){2,}[a-zA-Z0-9-_]+$~', (string) $label)) {
+				if ($labelLanguageItems && preg_match('~^([a-zA-Z0-9-_]+\.){2,}[a-zA-Z0-9-_]+$~', (string) $label)) {
 					$label = WCF::getLanguage()->getDynamicVariable($label);
 				}
 				
