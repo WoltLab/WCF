6107cf4db939c0b6a94763d13e419ce183f9b85a
Jens Hausdorf <hello@jens-hausdorf.de>
2018-08-18 15:09:27 +0200

    Fix some typos (#2740)

--- wcfsetup/install/files/lib/system/acl/ACLHandler.class.php
+++ wcfsetup/install/files/lib/system/acl/ACLHandler.class.php
@@ -323,7 +323,7 @@ class ACLHandler extends SingletonFactory {
 				$data[$type][$objectID] = [];
 			}
 			
-			// build JS-compilant structure
+			// build JS-compliant structure
 			$data[$type] = [
 				'label' => [],
 				'option' => $data[$type][$objectID]
