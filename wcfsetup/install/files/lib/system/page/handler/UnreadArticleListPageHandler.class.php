4e975b2aecefc1e10dcadb113bfb5fdc7f0da7dd
Joshua Rüsweg <josh@bastelstu.be>
2018-08-29 21:12:26 +0200

    Add unread article page feature See #2623

---
+++ wcfsetup/install/files/lib/system/page/handler/UnreadArticleListPageHandler.class.php
@@ -0,0 +1,30 @@
+<?php
+namespace wcf\system\page\handler;
+use wcf\data\article\ViewableArticle;
+
+/**
+ * Page handler implementation for the page showing the list of unread articles.
+ *
+ * @author	Joshua Ruesweg
+ * @copyright	2001-2018 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	WoltLabSuite\Core\System\Page\Handler
+ * @since	3.2
+ */
+class UnreadArticleListPageHandler extends AbstractMenuPageHandler {
+	/** @noinspection PhpMissingParentCallCommonInspection */
+	/**
+	 * @inheritDoc
+	 */
+	public function getOutstandingItemCount(/** @noinspection PhpUnusedParameterInspection */$objectID = null) {
+		return ViewableArticle::getUnreadArticles();
+	}
+	
+	/** @noinspection PhpMissingParentCallCommonInspection */
+	/**
+	 * @inheritDoc
+	 */
+	public function isVisible(/** @noinspection PhpUnusedParameterInspection */$objectID = null) {
+		return ARTICLE_ENABLE_VISIT_TRACKING && !empty(ViewableArticle::getUnreadArticles());
+	}
+}
