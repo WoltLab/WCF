28097364b9342855da4c73ff6690c1059f93109c
Joshua Rüsweg <josh@bastelstu.be>
2018-11-11 15:36:44 +0100

    Add comment user content provider See #2716

---
+++ wcfsetup/install/files/lib/system/user/content/provider/CommentUserContentProvider.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace wcf\system\user\content\provider;
+use wcf\data\comment\Comment;
+
+/**
+ * User content provider for comments.
+ *
+ * @author	Joshua Ruesweg
+ * @copyright	2001-2018 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	WoltLabSuite\Core\System\User\Content\Provider
+ * @since	3.2
+ */
+class CommentUserContentProvider extends AbstractDatabaseUserContentProvider {
+	/**
+	 * @inheritdoc
+	 */
+	public static function getDatabaseObjectClass() {
+		return Comment::class;
+	}
+}
