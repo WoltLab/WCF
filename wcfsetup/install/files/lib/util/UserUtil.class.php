7aff29c45fc1fbeda9d72b336504a7bce4a9c08e
Tim Düsterhus <duesterhus@woltlab.com>
2018-11-26 14:07:49 +0100

    Add missing return in UserUtil::usesMobileBrowser()

--- wcfsetup/install/files/lib/util/UserUtil.class.php
+++ wcfsetup/install/files/lib/util/UserUtil.class.php
@@ -126,7 +126,7 @@ final class UserUtil {
 	 */
 	public static function usesMobileBrowser() {
 		$userAgent = self::getUserAgent();
-		if (!$userAgent) false;
+		if (!$userAgent) return false;
 		
 		if (preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $userAgent)) {
 			return true;
