8a3caaf31b161e41ba37c905dd96542862b422b1
Alexander Ebert <ebert@woltlab.com>
2018-12-06 16:27:29 +0100

    Put the Google Fonts name in quotes to avoid parsing
    Replaces d2c3d9659e44dab360eaa505ec92984d0ea0cf81

--- wcfsetup/install/files/lib/system/style/StyleCompiler.class.php
+++ wcfsetup/install/files/lib/system/style/StyleCompiler.class.php
@@ -252,7 +252,11 @@ class StyleCompiler extends SingletonFactory {
 		
 		$variables['wcfFontFamily'] = $variables['wcfFontFamilyFallback'];
 		if (!empty($variables['wcfFontFamilyGoogle'])) {
-			$variables['wcfFontFamily'] = '"' . $variables['wcfFontFamilyGoogle'] . '", ' . $variables['wcfFontFamily'];
+			// The SCSS parser attempts to evaluate the variables, causing issues with font names that
+			// include logical operators such as "And" or "Or".
+			$variables['wcfFontFamilyGoogle'] = '"' . $variables['wcfFontFamilyGoogle'] . '"';
+			
+			$variables['wcfFontFamily'] = $variables['wcfFontFamilyGoogle'] . ', ' . $variables['wcfFontFamily'];
 		}
 		
 		// add options as SCSS variables
