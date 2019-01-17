628c780f45682e103b86801309d721cdc092a8a3
Tim Düsterhus <duesterhus@woltlab.com>
2019-01-17 13:41:09 +0100

    Fail IEventListener as deprecated

--- wcfsetup/install/files/lib/system/event/IEventListener.class.php
+++ wcfsetup/install/files/lib/system/event/IEventListener.class.php
@@ -1,15 +1,16 @@
 <?php
 namespace wcf\system\event;
 
+$_ = \wcf\functions\deprecatedClass(IEventListener::class);
 /**
- * *DEPRECATED*
  * EventListeners can be registered for a specific event in many controller objects.
  * 
- * @deprecated	2.1, use \wcf\system\event\listener\IParameterizedEventListener
  * @author	Marcel Werk
  * @copyright	2001-2018 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	WoltLabSuite\Core\System\Event
+ * @deprecated	2.1 - Use \wcf\system\event\listener\IParameterizedEventListener which takes an additional
+ * 		`array &$parameters` parameter. Note the additional `listener` in the namespace.
  */
 interface IEventListener {
 	/**
