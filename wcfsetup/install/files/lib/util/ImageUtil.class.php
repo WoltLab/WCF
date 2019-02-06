3b1bd1c357879f9cb7c12901bc97e0f6abc0ac0b
Joshua Rüsweg <josh@bastelstu.be>
2019-02-06 11:53:16 +0100

    Add method to validate uploaded images

--- wcfsetup/install/files/lib/util/ImageUtil.class.php
+++ wcfsetup/install/files/lib/util/ImageUtil.class.php
@@ -13,9 +13,16 @@ namespace wcf\util;
  */
 final class ImageUtil {
 	/**
+	 * image extensions
+	 * @var array
+	 */
+	protected static $imageExtensions = array('jpeg', 'jpg', 'png', 'gif');
+	
+	/**
 	 * Checks the content of an image for bad sections, e.g. the use of javascript
 	 * and returns false if any bad stuff was found.
 	 * 
+	 * @param	string		$file
 	 * @return	boolean
 	 */
 	public static function checkImageContent($file) {
@@ -30,6 +37,35 @@ final class ImageUtil {
 		if (strstr($content, 'script') || strstr($content, 'javascript') || strstr($content, 'expression(')) return false;
 		
 		return true;
+	}
+	
+	/**
+	 * Checks whether a given file is a valid image.
+	 *
+	 * @param       string          $location
+	 * @param       string|null     $filename
+	 * @param       bool            $handleSvgAsValidImage  flag, whether a svg file is handled as image
+	 * @return      bool
+	 */
+	public static function isImage($location, $filename = null, $handleSvgAsValidImage = false) {
+		if ($filename === null) {
+			$filename = basename($location);
+		}
+		
+		if (@getimagesize($location) !== false) {
+			$extension = pathinfo($filename, PATHINFO_EXTENSION);
+			
+			if (in_array($extension, self::$imageExtensions)) {
+				return true;
+			}
+		}
+		else if ($handleSvgAsValidImage) {
+			if (!in_array(FileUtil::getMimeType($location), array('image/svg', 'image/svg+xml')) && pathinfo($filename, PATHINFO_EXTENSION) === 'svg') {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	/**
