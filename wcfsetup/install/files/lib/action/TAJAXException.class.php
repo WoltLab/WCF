bde5e0f1ab0e7748584dee32ab046c7b73b2627c
Matthias Schmidt <gravatronics@live.com>
2019-02-16 12:21:56 +0100

    Add missing use statement
    See 40bed660f5daa1addf76ff41975fcead1f14073f

--- wcfsetup/install/files/lib/action/TAJAXException.class.php
+++ wcfsetup/install/files/lib/action/TAJAXException.class.php
@@ -7,10 +7,11 @@ use wcf\system\exception\NamedUserException;
 use wcf\system\exception\PermissionDeniedException;
 use wcf\system\exception\UserInputException;
 use wcf\system\exception\ValidateActionException;
+use wcf\system\WCF;
 
 /**
  * Default implementation for the AJAXException throw method. 
- *
+ * 
  * @author	Alexander Ebert, Joshua Ruesweg
  * @copyright	2001-2019 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
