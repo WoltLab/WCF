4e5897aacfb9a02c76f24c949e578b96dbc934ba
Marcel Werk <burntime@woltlab.com>
2019-03-18 15:30:24 +0100

    Added importer for CMS pages
    Closes #2708

---
+++ wcfsetup/install/files/lib/system/importer/PageImporter.class.php
@@ -0,0 +1,79 @@
+<?php
+namespace wcf\system\importer;
+use wcf\data\page\content\PageContentEditor;
+use wcf\data\page\Page;
+use wcf\data\page\PageEditor;
+use wcf\system\language\LanguageFactory;
+
+/**
+ * Imports cms pages.
+ *
+ * @author	Marcel Werk
+ * @copyright	2001-2019 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	WoltLabSuite\Core\System\Importer
+ * @since       5.2
+ */
+class PageImporter extends AbstractImporter {
+	/**
+	 * @inheritDoc
+	 */
+	protected $className = Page::class;
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function import($oldID, array $data, array $additionalData = []) {
+		$contents = [];
+		foreach ($additionalData['contents'] as $languageCode => $contentData) {
+			$languageID = 0;
+			if ($languageCode) {
+				if (($language = LanguageFactory::getInstance()->getLanguageByCode($languageCode)) !== null) {
+					$languageID = $language->languageID;
+				}
+				else {
+					continue;
+				}
+			}
+			
+			$contents[$languageID] = [
+				'title' => (!empty($contentData['title']) ? $contentData['title'] : ''),
+				'content' => (!empty($contentData['content']) ? $contentData['content'] : ''),
+				'metaDescription' => (!empty($contentData['metaDescription']) ? $contentData['metaDescription'] : ''),
+				'metaKeywords' => (!empty($contentData['content']) ? $contentData['content'] : ''),
+				'customURL' => (!empty($contentData['customURL']) ? $contentData['customURL'] : ''),
+				'hasEmbeddedObjects' => (!empty($contentData['hasEmbeddedObjects']) ? $contentData['hasEmbeddedObjects'] : 0)
+			];
+		}
+		if (empty($contents)) return 0;
+		if (count($contents) > 1) {
+			$data['isMultilingual'] = 1;
+		}
+		
+		// check old id
+		if (is_numeric($oldID)) {
+			$page = new Page($oldID);
+			if (!$page->pageID) $data['pageID'] = $oldID;
+		}
+		
+		// save page
+		$page = PageEditor::create($data);
+		
+		// save page content
+		foreach ($contents as $languageID => $contentData) {
+			PageContentEditor::create([
+				'pageID' => $page->pageID,
+				'languageID' => $languageID ?: null,
+				'title' => $contentData['title'],
+				'content' => $contentData['content'],
+				'metaDescription' => $contentData['metaDescription'],
+				'metaKeywords' => $contentData['metaKeywords'],
+				'customURL' => $contentData['customURL'],
+				'hasEmbeddedObjects' => $contentData['hasEmbeddedObjects']
+			]);
+		}
+		
+		ImportHandler::getInstance()->saveNewID('com.woltlab.wcf.page', $oldID, $page->pageID);
+		return $page->pageID;
+	}
+}
