22ebca0be419fab3e05be40d58f54fe954b7ab71
Matthias Schmidt <gravatronics@live.com>
2019-04-14 10:27:45 +0200

    Fix merge issue
    See 5617fec4ff24c9336f14d6d1052ed6b52e8614c3

--- wcfsetup/install/files/lib/system/exception/AJAXException.class.php
+++ wcfsetup/install/files/lib/system/exception/AJAXException.class.php
@@ -8,7 +8,7 @@ use wcf\util\JSON;
  * AJAXException provides JSON-encoded exceptions.
  * 
  * @author	Alexander Ebert
- * @copyright	2001-2019 WoltLab Gmb
+ * @copyright	2001-2019 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	WoltLabSuite\Core\System\Exception
  */
