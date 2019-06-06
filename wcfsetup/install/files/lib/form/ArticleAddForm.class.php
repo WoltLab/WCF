7ecd550801ee0f072cd7a3c525dad36ad5774834
Matthias Schmidt <gravatronics@live.com>
2019-06-06 19:09:05 +0200

    Redirect user to published article created in frontend
    See #2681

--- wcfsetup/install/files/lib/form/ArticleAddForm.class.php
+++ wcfsetup/install/files/lib/form/ArticleAddForm.class.php
@@ -1,6 +1,8 @@
 <?php
 namespace wcf\form;
+use wcf\data\article\Article;
 use wcf\system\WCF;
+use wcf\util\HeaderUtil;
 
 /**
  * Shows the article add form.
@@ -19,5 +21,19 @@ class ArticleAddForm extends \wcf\acp\form\ArticleAddForm {
 		parent::assignVariables();
 		
 		WCF::getTPL()->assign(['articleIsFrontend' => true]);
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function save() {
+		parent::save();
+		
+		/** @var Article $article */
+		$article = $this->objectAction->getReturnValues()['returnValues'];
+		if ($article->publicationStatus == Article::PUBLISHED) {
+			HeaderUtil::redirect($article->getLink());
+			exit;
+		}
 	}
 }
