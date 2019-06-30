cad2e3f01ab9d1a65c7d039e212801d9a7b95e99
Joshua Rüsweg <ruesweg@woltlab.com>
2019-06-30 13:02:28 +0200

    Redirect 3rdparty login users to 3rdparty login on password reset See #2951

--- wcfsetup/install/files/lib/form/LostPasswordForm.class.php
+++ wcfsetup/install/files/lib/form/LostPasswordForm.class.php
@@ -80,9 +80,12 @@ class LostPasswordForm extends AbstractCaptchaForm {
 			}
 		}
 		
-		// check if using 3rd party @author dtdesign
+		// check if using 3rd party
 		if ($this->user->authData) {
-			throw new UserInputException('username', '3rdParty');
+			HeaderUtil::delayedRedirect(LinkHandler::getInstance()->getLink(ucfirst($this->user->getAuthProvider()) . 'Auth'), WCF::getLanguage()->getDynamicVariable('wcf.user.username.error.3rdParty.redirect', [
+				'provider' => WCF::getLanguage()->get('wcf.user.3rdparty.'. $this->user->getAuthProvider())
+			]),5, 'info');
+			exit;
 		}
 		
 		// check whether a lost password request was sent in the last 24 hours
