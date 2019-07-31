90b4ec65d2c87ecb70ff4f0c2d95da37b6318bbe
mutec <mysterycode@mysterycode.de>
2019-07-31 23:30:02 +0200

    prepare update instructions 3.1.10 pl 2 > 5.2.0

--- com.woltlab.wcf/update_5.2_pre_sql_api.sql
+++ com.woltlab.wcf/update_5.2_pre_sql_api.sql
@@ -1,2 +1,6 @@
 -- The database API requires this column in order to work properly. DO NOT MOVE THIS! 
 ALTER TABLE wcf1_package_installation_sql_log ADD COLUMN isDone TINYINT(1) NOT NULL DEFAULT 1;
+
+-- Let's do this here too, so we won't need another step for inserting two values
+INSERT INTO wcf1_style_variable (variableName, defaultValue) VALUES ('wcfEditorTableBorder', 'rgba(221, 221, 221, 1)');
+INSERT INTO wcf1_style_variable (variableName, defaultValue) VALUES ('wcfPageThemeColor', ''); -- uses `$wcfHeaderBackground` if left empty
---
+++ wcfsetup/install/files/acp/update-com.woltlab.wcf_5.2_reloadRoutingCache.php
@@ -0,0 +1,5 @@
+<?php
+namespace wcf\acp;
+use wcf\system\cache\builder\RoutingCacheBuilder;
+
+RoutingCacheBuilder::getInstance()->reset();
