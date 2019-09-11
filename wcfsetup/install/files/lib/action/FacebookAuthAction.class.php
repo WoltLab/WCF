002138ba3ea263becbc13f83c933fb514071bb16
Matthias Schmidt <gravatronics@live.com>
2019-09-11 18:17:46 +0200

    Remove birthday and location scope from FacebookAuth
    Close #2912

--- wcfsetup/install/files/lib/action/FacebookAuthAction.class.php
+++ wcfsetup/install/files/lib/action/FacebookAuthAction.class.php
@@ -147,7 +147,7 @@ class FacebookAuthAction extends AbstractAction {
 		// start auth by redirecting to facebook
 		$token = StringUtil::getRandomID();
 		WCF::getSession()->register('__facebookInit', $token);
-		HeaderUtil::redirect("https://www.facebook.com/dialog/oauth?client_id=".StringUtil::trim(FACEBOOK_PUBLIC_KEY). "&redirect_uri=".rawurlencode($callbackURL)."&state=".$token."&scope=email,user_birthday,user_location");
+		HeaderUtil::redirect("https://www.facebook.com/dialog/oauth?client_id=".StringUtil::trim(FACEBOOK_PUBLIC_KEY). "&redirect_uri=".rawurlencode($callbackURL)."&state=".$token."&scope=email");
 		$this->executed();
 		exit;
 	}
