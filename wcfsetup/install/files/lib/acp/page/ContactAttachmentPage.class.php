1cc783d7ecfc138106249a52dda96d95ea4dfbf6
Alexander Ebert <ebert@woltlab.com>
2019-09-25 12:43:03 +0200

    Missing page for viewing attachments in the admin panel that have been uploaded in the contact form

---
+++ wcfsetup/install/files/lib/acp/page/ContactAttachmentPage.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace wcf\acp\page;
+
+/**
+ * Shows an attachment.
+ * 
+ * @author      Alexander Ebert
+ * @copyright	2001-2019 WoltLab GmbH
+ * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package     WoltLabSuite\Core\Acp\Page
+ */
+class ContactAttachmentPage extends \wcf\page\ContactAttachmentPage {}
