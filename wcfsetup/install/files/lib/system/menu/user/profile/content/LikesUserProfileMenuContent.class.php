976a9791befad4573eb6fe80a97234b280074265
Alexander Ebert <ebert@woltlab.com>
2019-11-03 00:34:47 +0100

    Hide the likes tab if the user may not view likes

--- wcfsetup/install/files/lib/system/menu/user/profile/content/LikesUserProfileMenuContent.class.php
+++ wcfsetup/install/files/lib/system/menu/user/profile/content/LikesUserProfileMenuContent.class.php
@@ -36,6 +36,10 @@ class LikesUserProfileMenuContent extends SingletonFactory implements IUserProfi
 	 * @inheritDoc
 	 */
 	public function isVisible($userID) {
+		if (!WCF::getSession()->getPermission('user.like.canViewLike')) {
+			return false;
+		}
+		
 		return true;
 	}
 }
