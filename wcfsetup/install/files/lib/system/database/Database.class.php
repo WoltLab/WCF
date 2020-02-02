1a77c0ecd1642804f84fb621fe68a2cb83460332
Tim Düsterhus <duesterhus@woltlab.com>
2020-02-02 15:57:27 +0100

    Add \wcf\getRequestId() to request information in SQL queries

--- wcfsetup/install/files/lib/system/database/Database.class.php
+++ wcfsetup/install/files/lib/system/database/Database.class.php
@@ -261,7 +261,7 @@ abstract class Database {
 			//       must be absolutely lightweight.
 			$requestInformation = '';
 			if (defined('ENABLE_PRODUCTION_DEBUG_MODE') && ENABLE_PRODUCTION_DEBUG_MODE && isset($_SERVER['REQUEST_URI'])) {
-				$requestInformation = substr($_SERVER['REQUEST_URI'], 0, 90);
+				$requestInformation = substr($_SERVER['REQUEST_URI'].(\wcf\getRequestId() ? ' ('.\wcf\getRequestId().')' : ''), 0, 90);
 				if (isset($_REQUEST['className']) && isset($_REQUEST['actionName'])) {
 					$requestInformation .= ' ('.$_REQUEST['className'].':'.$_REQUEST['actionName'].')';
 				}
