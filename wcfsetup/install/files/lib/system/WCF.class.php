d9e5d932ff86c0e9c48be0d05ba3019a0453ba68
Alexander Ebert <ebert@woltlab.com>
2020-02-23 00:29:44 +0100

    Preparing the release 3.1.12

--- wcfsetup/install/files/lib/system/WCF.class.php
+++ wcfsetup/install/files/lib/system/WCF.class.php
@@ -48,7 +48,7 @@ if (!@ini_get('date.timezone')) {
 }
 
 // define current woltlab suite version
-define('WCF_VERSION', '3.1.11');
+define('WCF_VERSION', '3.1.12');
 
 // define current API version
 define('WSC_API_VERSION', 2018);
