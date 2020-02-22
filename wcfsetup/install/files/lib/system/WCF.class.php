9459345b1956ab4783d84f7385cedc29a1ee1e94
Alexander Ebert <ebert@woltlab.com>
2020-02-22 18:59:47 +0100

    Preparing the release 3.0.23

--- wcfsetup/install/files/lib/system/WCF.class.php
+++ wcfsetup/install/files/lib/system/WCF.class.php
@@ -47,7 +47,7 @@ if (!@ini_get('date.timezone')) {
 }
 
 // define current woltlab suite version
-define('WCF_VERSION', '3.0.22 pl 2');
+define('WCF_VERSION', '3.0.23');
 
 // define current unix timestamp
 define('TIME_NOW', time());
