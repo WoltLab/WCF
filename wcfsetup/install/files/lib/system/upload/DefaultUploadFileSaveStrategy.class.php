0976d7d0e4036b5eb94c911f7d9a54ab963072c0
Alexander Ebert <ebert@woltlab.com>
2020-04-28 13:33:13 +0200

    Precise check for ImageMagick
    The previous check was inaccurate, because it did not consider improperly configured environments that have selected ImageMagick, but did not provide it.

--- wcfsetup/install/files/lib/system/upload/DefaultUploadFileSaveStrategy.class.php
+++ wcfsetup/install/files/lib/system/upload/DefaultUploadFileSaveStrategy.class.php
@@ -169,7 +169,7 @@ class DefaultUploadFileSaveStrategy implements IUploadFileSaveStrategy {
 									}
 									
 									if ($newImage !== null) {
-										if (IMAGE_ADAPTER_TYPE === 'imagick') {
+										if ($newImage instanceof \Imagick) {
 											$newImage->setImageOrientation(\Imagick::ORIENTATION_TOPLEFT);
 										}
 										
