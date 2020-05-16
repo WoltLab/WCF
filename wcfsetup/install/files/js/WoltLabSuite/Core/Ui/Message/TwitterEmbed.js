4dc4ab76d84c8fb1eecfaab4aa11e263c953bd5f
Tim Düsterhus <duesterhus@woltlab.com>
2020-05-16 13:08:52 +0200

    Add fallback link to Twitter media provider
    Closes #3308

---
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Ui/Message/TwitterEmbed.js
@@ -0,0 +1,48 @@
+/**
+ * Wrapper around Twitter's createTweet API.
+ *
+ * @author	Tim Duesterhus
+ * @copyright	2001-2020 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @module	WoltLabSuite/Core/Ui/Message/TwitterEmbed
+ */
+define(['https://platform.twitter.com/widgets.js'], function(Widgets) {
+	"use strict";
+	
+	var twitterReady = new Promise(function(resolve, reject) {
+		twttr.ready(resolve);
+	});
+	
+	/**
+	 * @exports	WoltLabSuite/Core/Ui/Message/TwitterEmbed
+	 */
+	return {
+		/**
+		 * Embed the tweet identified by the given tweetId into the given container.
+		 * 
+		 * @param {HTMLElement} container
+		 * @param {string} tweetId
+		 * @param {boolean} clearContents Whether to remove existing children of the given container after embedding the tweet.
+		 * @return {HTMLElement} The Tweet element created by Twitter.
+		 */
+		embedTweet: function (container, tweetId, removeChildren) {
+			if (removeChildren === undefined) removeChildren = false;
+			
+			return twitterReady.then(function() {
+				return twttr.widgets.createTweet(tweetId, container, {
+					dnt: true,
+					lang: document.documentElement.lang,
+				});
+			}).then(function (tweet) {
+				if (removeChildren) {
+					while (container.lastChild) {
+						container.removeChild(container.lastChild);
+					}
+					container.appendChild(tweet);
+				}
+				
+				return tweet;
+			});
+		}
+	};
+});
