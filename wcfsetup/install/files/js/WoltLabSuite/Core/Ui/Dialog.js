d1ce21dc1be808b75dd2cc3884dc8c918f43e365
Alexander Ebert <ebert@woltlab.com>
2020-05-28 12:42:43 +0200

    Incorrect fix for the class access

--- wcfsetup/install/files/js/WoltLabSuite/Core/Ui/Dialog.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Ui/Dialog.js
@@ -683,7 +683,7 @@ define(
 				// 
 				// We can use `backface-visibility: hidden` to prevent the anti aliasing artifacts in
 				// WebKit/Blink, which will also prevent some weird font rendering issues when resizing.
-				if (data.content.parentNode !== null) data.content.parentNode.add('jsWebKitFractionalPixelFix');
+				data.content.parentNode.classList.add('jsWebKitFractionalPixelFix');
 			}
 			
 			var callbackObject = _dialogToObject.get(id);
