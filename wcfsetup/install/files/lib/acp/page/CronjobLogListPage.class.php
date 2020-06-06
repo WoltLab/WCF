15aa606e7a596fd3393f3255bb0f182969be2cc9
Marcel Werk <burntime@woltlab.com>
2020-06-06 19:52:58 +0200

    Filtering the cronjob log
    Closes #3359

--- wcfsetup/install/files/lib/acp/page/CronjobLogListPage.class.php
+++ wcfsetup/install/files/lib/acp/page/CronjobLogListPage.class.php
@@ -1,7 +1,9 @@
 <?php
 namespace wcf\acp\page;
+use wcf\data\cronjob\CronjobList;
 use wcf\data\cronjob\log\CronjobLogList;
 use wcf\page\SortablePage;
+use wcf\system\WCF;
 
 /**
  * Shows cronjob log information.
@@ -50,6 +52,31 @@ class CronjobLogListPage extends SortablePage {
 	public $objectListClassName = CronjobLogList::class;
 	
 	/**
+	 * @var integer
+	 */
+	public $cronjobID = 0;
+	
+	/**
+	 * @var integer
+	 */
+	public $success = -1;
+	
+	/**
+	 * @var CronjobList
+	 */
+	public $availableCronjobs;
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function readParameters() {
+		parent::readParameters();
+		
+		if (!empty($_REQUEST['cronjobID'])) $this->cronjobID = intval($_REQUEST['cronjobID']);
+		if (isset($_REQUEST['success'])) $this->success = intval($_REQUEST['success']);
+	}
+	
+	/**
 	 * @inheritDoc
 	 */
 	protected function initObjectList() {
@@ -57,6 +84,13 @@ class CronjobLogListPage extends SortablePage {
 		
 		$this->objectList->sqlSelects = "cronjob.*";
 		$this->objectList->sqlJoins = "LEFT JOIN wcf".WCF_N."_cronjob cronjob ON (cronjob.cronjobID = cronjob_log.cronjobID)";
+		
+		if ($this->cronjobID) {
+			$this->objectList->getConditionBuilder()->add('cronjob_log.cronjobID = ?', [$this->cronjobID]);
+		}
+		if ($this->success != -1) {
+			$this->objectList->getConditionBuilder()->add('cronjob_log.success = ?', [$this->success]);
+		}
 	}
 	
 	/**
@@ -66,5 +100,29 @@ class CronjobLogListPage extends SortablePage {
 		$this->sqlOrderBy = (($this->sortField == 'className' || $this->sortField == 'description') ? 'cronjob.' : 'cronjob_log.').$this->sortField." ".$this->sortOrder;
 		
 		parent::readObjects();
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function readData() {
+		parent::readData();
+		
+		$this->availableCronjobs = new CronjobList();
+		$this->availableCronjobs->sqlOrderBy = 'cronjobID';
+		$this->availableCronjobs->readObjects();
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function assignVariables() {
+		parent::assignVariables();
+		
+		WCF::getTPL()->assign([
+			'cronjobID' => $this->cronjobID,
+			'success' => $this->success,
+			'availableCronjobs' => $this->availableCronjobs,
+		]);
 	}
 }
