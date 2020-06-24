82a3a227af1b669a8d427575e31f989764c8c4f7
Matthias Schmidt <gravatronics@live.com>
2020-06-24 17:21:02 +0200

    Check for missing length of varchar columns

--- wcfsetup/install/files/lib/system/database/table/column/VarcharDatabaseTableColumn.class.php
+++ wcfsetup/install/files/lib/system/database/table/column/VarcharDatabaseTableColumn.class.php
@@ -3,20 +3,33 @@ namespace wcf\system\database\table\column;
 
 /**
  * Represents a `varchar` database table column.
- *
+ * 
  * @author	Matthias Schmidt
- * @copyright	2001-2019 WoltLab GmbH
+ * @copyright	2001-2020 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	WoltLabSuite\Core\System\Database\Table\Column
  * @since	5.2
  */
 class VarcharDatabaseTableColumn extends AbstractDatabaseTableColumn implements ILengthDatabaseTableColumn {
-	use TLengthDatabaseTableColumn;
+	use TLengthDatabaseTableColumn {
+		getLength as protected traitGetLength;
+	}
 	
 	/**
 	 * @inheritDoc
 	 */
 	protected $type = 'varchar';
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function getLength() {
+		if ($this->length === null) {
+			throw new \LogicException('The length of varchar fields must be explicitly set.');
+		}
+		
+		return $this->traitGetLength();
+	}
 	
 	/**
 	 * @inheritDoc
