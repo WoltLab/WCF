fe00e13d3c0c492fd8983f04aca3d3fc40758f8f
Alexander Ebert <ebert@woltlab.com>
2020-06-28 19:42:14 +0200

    New version number: 5.3.0 Alpha 1

--- wcfsetup/install/files/lib/system/WCF.class.php
+++ wcfsetup/install/files/lib/system/WCF.class.php
@@ -49,7 +49,7 @@ if (!@ini_get('date.timezone')) {
 }
 
 // define current woltlab suite version
-define('WCF_VERSION', '5.2.7');
+define('WCF_VERSION', '5.3.0 Alpha 1');
 
 // define current API version
 // @deprecated 5.2
