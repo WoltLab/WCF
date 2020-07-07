WoltLab/WCF

commit 485f8e1888824b862823de983e019afcb9bca7ce

Fix detection of stuck cronjobs
Consider the following scenario:

A cronjob is scheduled to run `0 * * * *` (i.e. every hour). The cronjob
successfully executes it's 23:00 execution belated on 23:30. Then the
nextExec will be 00:00. afterNextExec will be 01:00.

During night time nothing happens within the community, until at 05:xx when
the first user wakes up and opens their web browser. It restores two tabs
with our community and fetches them. Now the following will happen:

1. Request 1: Notices that the cronjob is due (00:00 < 05:xx). Setting
              the cronjob's state to pending and starting execution.
2. Request 2: Notices that the cronjob is overdue (01:00 < 05:xx,
              state = pending). Logging an error about a stuck job and
              setting the cronjob's state to ready.
3. Request 1: Finishes executing the cronjob and updates nextExec = 06:00,
              afterNextExec = 07:00, also setting the cronjob's state to
              ready.

Fix this issue by updating the times for the next executions together with
the update of the state in a single database query / transaction. This will
prevent other requests from seeing the cronjob with state = pending or
state = executing and an way outdated date.

A side effect of this change is that cronjobs will match the intended
schedule better.

Consider the following:

A cronjob running every minute, taking 5 seconds to execute. The execution
is triggered at 00:00:58. It will finish executing 00:01:03. Previously the
nextExec would be set to 00:02:00, now it will be 00:01:00. Thus the first
request after 00:01:03 (i.e. once the state is set back to ready) will
trigger execution, more closely matching the intended schedule.

wcfsetup/install/files/lib/system/cronjob/CronjobScheduler.class.php
@@ -86,16 +86,9 @@ class CronjobScheduler extends SingletonFactory {
 				$this->logResult($logEditor);
 			}
 			
-			// get time of next execution
-			$nextExec = $cronjobEditor->getNextExec();
-			$afterNextExec = $cronjobEditor->getNextExec($nextExec + 120);
-			
 			// mark cronjob as done
 			$cronjobEditor->update([
-				'lastExec' => TIME_NOW,
-				'afterNextExec' => $afterNextExec,
 				'failCount' => 0,
-				'nextExec' => $nextExec,
 				'state' => Cronjob::READY
 			]);
 		}
@@ -206,11 +199,23 @@ class CronjobScheduler extends SingletonFactory {
 				Cronjob::READY,
 				TIME_NOW
 			]);
+			/** @var Cronjob $cronjob */
 			while ($cronjob = $statement->fetchObject(Cronjob::class)) {
 				$cronjobEditor = new CronjobEditor($cronjob);
 				
 				// Mark the cronjob as pending to prevent concurrent requests from executing it.
-				$cronjobEditor->update(['state' => Cronjob::PENDING]);
+				$data = ['state' => Cronjob::PENDING];
+				
+				// Update next execution time.
+				// This needs to be done before executing the job to allow for a proper detection of
+				// stuck jobs. If the timestamps are updated after executing then concurrent requests
+				// might believe that a cronjob is stuck, despite the cronjob just having started just
+				// a few milliseconds before.
+				$data['nextExec'] = $cronjob->getNextExec(TIME_NOW);
+				$data['afterNextExec'] = $cronjob->getNextExec($data['nextExec'] + 120);
+				$data['lastExec'] = TIME_NOW;
+				
+				$cronjobEditor->update($data);
 				
 				$this->cronjobEditors[] = $cronjobEditor;
 			}

commit ce16b422386fcafb0516396018aa713167b3602e

Fix calculation of nextExec of cronjobs
To properly calculate nextExec we must not specify the TIME_NOW parameter,
because if the cronjob is run on the scheduled time the nextExec() will
return the current time.

Not passing the TIME_NOW parameter adds at least 120 seconds of delay to
prevent this issue.

This bug was introduced in commit 485f8e1888824b862823de983e019afcb9bca7ce.
When moving the update of the execution time the calls were not correctly
moved and the explicit passing of TIME_NOW was added.

wcfsetup/install/files/lib/system/cronjob/CronjobScheduler.class.php
@@ -207,7 +207,7 @@ class CronjobScheduler extends SingletonFactory {
 				// stuck jobs. If the timestamps are updated after executing then concurrent requests
 				// might believe that a cronjob is stuck, despite the cronjob just having started just
 				// a few milliseconds before.
-				$data['nextExec'] = $cronjob->getNextExec(TIME_NOW);
+				$data['nextExec'] = $cronjob->getNextExec();
 				$data['afterNextExec'] = $cronjob->getNextExec($data['nextExec'] + 120);
 				$data['lastExec'] = TIME_NOW;
 				