78ee08ed3cc4c7f8f1e5c34a0731a6e0ef3d517f
Alexander Ebert <ebert@woltlab.com>
2020-08-29 18:49:48 +0200

    Set up the proper default package servers

--- com.woltlab.wcf/update_5.3.sql
+++ com.woltlab.wcf/update_5.3.sql
@@ -1,3 +1,7 @@
 DELETE FROM wcf1_style_variable WHERE variableName = 'useGoogleFont';
 
-DELETE FROM wcf1_package_update_server WHERE (serverURL LIKE '%//update.woltlab.com%' OR serverURL LIKE '%//store.woltlab.com%') AND serverURL NOT LIKE '%/5.3%';
+-- Purge the existing official package servers to clean up any mess.
+DELETE FROM wcf1_package_update_server WHERE LOWER(serverURL) REGEXP 'https?://(?:store|update)\.woltlab\.com/.*';
+
+-- Insert the default official package servers that will be dynamically adjusted.
+INSERT INTO wcf1_package_update_server (serverURL) VALUES ('http://update.woltlab.com/5.3/'), ('http://store.woltlab.com/5.3/');
