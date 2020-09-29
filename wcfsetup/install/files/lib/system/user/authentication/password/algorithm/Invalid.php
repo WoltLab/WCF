2d3e478feb48274474e44dccbf0414e25619f8a1
Tim Düsterhus <duesterhus@woltlab.com>
2020-09-29 10:38:19 +0200

    Add 'Invalid' password algorithm

---
+++ wcfsetup/install/files/lib/system/user/authentication/password/algorithm/Invalid.php
@@ -0,0 +1,35 @@
+<?php
+namespace wcf\system\user\authentication\password\algorithm;
+use wcf\system\user\authentication\password\IPasswordAlgorithm;
+
+/**
+ * Always indicates that the password is invalid.
+ * 
+ * @author	Tim Duesterhus
+ * @copyright	2001-2020 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	WoltLabSuite\Core\System\User\Authentication\Password\Algorithm
+ * @since	5.4
+ */
+final class Invalid implements IPasswordAlgorithm {
+	/**
+	 * @inheritDoc
+	 */
+	public function verify(string $password, string $hash): bool {
+		return false;
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function hash(string $password): string {
+		return '';
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function needs_rehash(string $hash): bool {
+		return false;
+	}
+}
--- wcfsetup/install/files/lib/util/PasswordUtil.class.php
+++ wcfsetup/install/files/lib/util/PasswordUtil.class.php
@@ -705,13 +705,7 @@ final class PasswordUtil {
 	}
 	
 	/**
-	 * Returns false.
-	 * 
-	 * @param	string		$username
-	 * @param	string		$password
-	 * @param	string		$salt
-	 * @param	string		$dbHash
-	 * @return	boolean
+	 * @deprecated	5.4 - Use the new password algorithm framework in \wcf\system\user\authentication\password\*.
 	 */
 	protected static function invalid($username, $password, $salt, $dbHash) {
 		return false;
