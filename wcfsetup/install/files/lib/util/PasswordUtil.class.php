acb39e53c52578d72ca8c8f42467ed065cbfb934
joshuaruesweg <ruesweg@woltlab.com>
2020-09-29 18:40:25 +0200

    Add 'Wcf1' password algorithm

--- wcfsetup/install/files/lib/util/PasswordUtil.class.php
+++ wcfsetup/install/files/lib/util/PasswordUtil.class.php
@@ -422,13 +422,7 @@ final class PasswordUtil {
 	}
 	
 	/**
-	 * Validates the password hash for WoltLab Community Framework 1.x (wcf1).
-	 * 
-	 * @param	string		$username
-	 * @param	string		$password
-	 * @param	string		$salt
-	 * @param	string		$dbHash
-	 * @return	boolean
+	 * @deprecated	5.4 - Use the new password algorithm framework in \wcf\system\user\authentication\password\*.
 	 */
 	protected static function wcf1($username, $password, $salt, $dbHash) {
 		return \hash_equals($dbHash, sha1($salt . sha1($salt . sha1($password))));
