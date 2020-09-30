2b96c033b6f6954d20e23463d32491cd3339f5c0
joshuaruesweg <ruesweg@woltlab.com>
2020-09-30 11:59:07 +0200

    Add 'Joomla1' password algorithm

--- wcfsetup/install/files/lib/util/PasswordUtil.class.php
+++ wcfsetup/install/files/lib/util/PasswordUtil.class.php
@@ -509,13 +509,7 @@ final class PasswordUtil {
 	}
 	
 	/**
-	 * Validates the password hash for Joomla 1.x (kunea)
-	 * 
-	 * @param	string		$username
-	 * @param	string		$password
-	 * @param	string		$salt
-	 * @param	string		$dbHash
-	 * @return	boolean
+	 * @deprecated	5.4 - Use the new password algorithm framework in \wcf\system\user\authentication\password\*.
 	 */
 	protected static function joomla1($username, $password, $salt, $dbHash) {
 		if (\hash_equals($dbHash, md5($password . $salt))) {
