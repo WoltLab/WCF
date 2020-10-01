7eb4a4ba5bb9c8d323f3e1476b84154c14589f98
Alexander Ebert <ebert@woltlab.com>
2020-10-01 17:45:10 +0200

    Improved readability
    Co-authored-by: Matthias Schmidt <gravatronics@live.com>

--- wcfsetup/install/files/lib/system/cronjob/CronjobScheduler.class.php
+++ wcfsetup/install/files/lib/system/cronjob/CronjobScheduler.class.php
@@ -149,7 +149,14 @@ class CronjobScheduler extends SingletonFactory {
 						]);
 						$logEditor = new CronjobLogEditor($log);
 						
-						$errorMessage = sprintf("The cronjob '%s' (ID %d) appears to have failed. (nextExec %d, afterNextExec %d, now %d)", $cronjob->cronjobName, $cronjob->cronjobID, $cronjob->nextExec, $cronjob->afterNextExec, TIME_NOW);
+						$errorMessage = sprintf(
+							"The cronjob '%s' (ID %d) appears to have failed. (nextExec %d, afterNextExec %d, now %d)",
+							$cronjob->cronjobName,
+							$cronjob->cronjobID,
+							$cronjob->nextExec,
+							$cronjob->afterNextExec,
+							TIME_NOW
+						);
 						$this->logResult($logEditor, new \Exception($errorMessage));
 						break;
 					default:
