93b5a3b279e50f7a39b2d4efbee39d0c7fa771cf
Tim Düsterhus <duesterhus@woltlab.com>
2020-10-22 10:11:14 +0200

    Tighten up minimum version of scssphp/scssphp

--- wcfsetup/install/files/lib/system/api/composer/autoload_files.php
+++ wcfsetup/install/files/lib/system/api/composer/autoload_files.php
@@ -10,6 +10,6 @@ return array(
     'c964ee0ededf28c96ebd9db5099ef910' => $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
     'a0edc8309cc5e1d60e3047b5df6b7052' => $vendorDir . '/guzzlehttp/psr7/src/functions_include.php',
     '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
-    '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
     '2cffec82183ee1cea088009cef9a6fc3' => $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
+    '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
 );
