b99b3b2e7bc921bb0fb3337afa41e349db39bc21
Tim Düsterhus <duesterhus@woltlab.com>
2020-10-23 09:38:57 +0200

    Remove `Pragma: no-cache` response header
    `Pragma: no-cache` is HTTP/1.0 specific. Anything accessing WoltLab Suite
    should be able to at least speak HTTP/1.1. HTTP/1.0 does not even define the
    `Host` header for hosting multiple sites on a single IP address!

--- wcfsetup/install/files/lib/util/HeaderUtil.class.php
+++ wcfsetup/install/files/lib/util/HeaderUtil.class.php
@@ -92,7 +92,6 @@ final class HeaderUtil {
 		@header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 		@header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
 		@header('Cache-Control: max-age=0, no-cache, no-store, must-revalidate');
-		@header('Pragma: no-cache');
 	}
 	
 	/**
