0b5079c3580de45fdf474ade0e5d79e62ff74e54
Alexander Ebert <ebert@woltlab.com>
2020-10-25 00:07:30 +0200

    Convert `Ui/Dropdown/Reusable` to TypeScript

--- wcfsetup/install/files/js/WoltLabSuite/Core/Ui/Dropdown/Reusable.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Ui/Dropdown/Reusable.js
@@ -1,73 +1,63 @@
 /**
  * Simple interface to work with reusable dropdowns that are not bound to a specific item.
  *
- * @author	Alexander Ebert
- * @copyright	2001-2019 WoltLab GmbH
- * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @module	Ui/ReusableDropdown (alias)
- * @module	WoltLabSuite/Core/Ui/Dropdown/Reusable
+ * @author  Alexander Ebert
+ * @copyright  2001-2019 WoltLab GmbH
+ * @license  GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @module  Ui/ReusableDropdown (alias)
+ * @module  WoltLabSuite/Core/Ui/Dropdown/Reusable
  */
-define(['Dictionary', 'Ui/SimpleDropdown'], function (Dictionary, UiSimpleDropdown) {
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+define(["require", "exports", "./Simple"], function (require, exports, Simple_1) {
     "use strict";
-    var _dropdowns = new Dictionary();
-    var _ghostElementId = 0;
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.toggleDropdown = exports.registerCallback = exports.getDropdownMenu = exports.init = void 0;
+    Simple_1 = __importDefault(Simple_1);
+    const _dropdowns = new Map();
+    let _ghostElementId = 0;
     /**
      * Returns dropdown name by internal identifier.
-     *
-     * @param       {string}        identifier      internal identifier
-     * @returns     {string}        dropdown name
      */
-    function _getDropdownName(identifier) {
+    function getDropdownName(identifier) {
         if (!_dropdowns.has(identifier)) {
             throw new Error("Unknown dropdown identifier '" + identifier + "'");
         }
         return _dropdowns.get(identifier);
     }
     /**
-     * @exports     WoltLabSuite/Core/Ui/Dropdown/Reusable
+     * Initializes a new reusable dropdown.
      */
-    return {
-        /**
-         * Initializes a new reusable dropdown.
-         *
-         * @param       {string}        identifier      internal identifier
-         * @param       {Element}       menu            dropdown menu element
-         */
-        init: function (identifier, menu) {
-            if (_dropdowns.has(identifier)) {
-                return;
-            }
-            var ghostElement = elCreate('div');
-            ghostElement.id = 'reusableDropdownGhost' + _ghostElementId++;
-            UiSimpleDropdown.initFragment(ghostElement, menu);
-            _dropdowns.set(identifier, ghostElement.id);
-        },
-        /**
-         * Returns the dropdown menu element.
-         *
-         * @param       {string}        identifier      internal identifier
-         * @returns     {Element}       dropdown menu element
-         */
-        getDropdownMenu: function (identifier) {
-            return UiSimpleDropdown.getDropdownMenu(_getDropdownName(identifier));
-        },
-        /**
-         * Registers a callback invoked upon open and close.
-         *
-         * @param       {string}        identifier      internal identifier
-         * @param       {function}      callback        callback function
-         */
-        registerCallback: function (identifier, callback) {
-            UiSimpleDropdown.registerCallback(_getDropdownName(identifier), callback);
-        },
-        /**
-         * Toggles a dropdown.
-         *
-         * @param       {string}        identifier              internal identifier
-         * @param       {Element}       referenceElement        reference element used for alignment
-         */
-        toggleDropdown: function (identifier, referenceElement) {
-            UiSimpleDropdown.toggleDropdown(_getDropdownName(identifier), referenceElement);
+    function init(identifier, menu) {
+        if (_dropdowns.has(identifier)) {
+            return;
         }
-    };
+        const ghostElement = document.createElement('div');
+        ghostElement.id = 'reusableDropdownGhost' + _ghostElementId++;
+        Simple_1.default.initFragment(ghostElement, menu);
+        _dropdowns.set(identifier, ghostElement.id);
+    }
+    exports.init = init;
+    /**
+     * Returns the dropdown menu element.
+     */
+    function getDropdownMenu(identifier) {
+        return Simple_1.default.getDropdownMenu(getDropdownName(identifier));
+    }
+    exports.getDropdownMenu = getDropdownMenu;
+    /**
+     * Registers a callback invoked upon open and close.
+     */
+    function registerCallback(identifier, callback) {
+        Simple_1.default.registerCallback(getDropdownName(identifier), callback);
+    }
+    exports.registerCallback = registerCallback;
+    /**
+     * Toggles a dropdown.
+     */
+    function toggleDropdown(identifier, referenceElement) {
+        Simple_1.default.toggleDropdown(getDropdownName(identifier), referenceElement);
+    }
+    exports.toggleDropdown = toggleDropdown;
 });
