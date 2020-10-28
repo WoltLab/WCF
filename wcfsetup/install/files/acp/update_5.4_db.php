a5bb7021c8bdb7d4b971e685bed3547a992a3a69
Matthias Schmidt <gravatronics@live.com>
2020-10-28 13:29:23 +0100

    Add database update for flood control API
    Close #3653

--- wcfsetup/install/files/acp/update_5.4_db.php
+++ wcfsetup/install/files/acp/update_5.4_db.php
@@ -1,10 +1,37 @@
 <?php
+use wcf\system\database\table\column\BigintDatabaseTableColumn;
+use wcf\system\database\table\column\BinaryDatabaseTableColumn;
+use wcf\system\database\table\column\NotNullInt10DatabaseTableColumn;
 use wcf\system\database\table\column\TextDatabaseTableColumn;
+use wcf\system\database\table\DatabaseTable;
 use wcf\system\database\table\DatabaseTableChangeProcessor;
+use wcf\system\database\table\index\DatabaseTableIndex;
+use wcf\system\database\table\index\DatabaseTablePrimaryIndex;
 use wcf\system\database\table\PartialDatabaseTable;
 use wcf\system\package\plugin\ScriptPackageInstallationPlugin;
 
 $tables = [
+	DatabaseTable::create('wcf1_flood_control')
+		->columns([
+			BigintDatabaseTableColumn::create('logID')
+				->length(20)
+				->notNull()
+				->autoIncrement(),
+			NotNullInt10DatabaseTableColumn::create('objectTypeID'),
+			BinaryDatabaseTableColumn::create('identifier')
+				->length(16)
+				->notNull(),
+			NotNullInt10DatabaseTableColumn::create('time'),
+		])
+		->indices([
+			DatabaseTablePrimaryIndex::create()
+				->columns(['logID']),
+			DatabaseTableIndex::create()
+				->columns(['identifier']),
+			DatabaseTableIndex::create()
+				->columns(['time']),
+		]),
+	
 	PartialDatabaseTable::create('wcf1_page_content')
 		->columns([
 			TextDatabaseTableColumn::create('metaKeywords')
