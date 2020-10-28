9f839e16e858121434e6a05fee5dcbdfc46f687d
Matthias Schmidt <gravatronics@live.com>
2020-10-28 13:20:33 +0100

    Add script to remove files during update to 5.4 (#3643)
    * Add script to remove files during update to 5.4
    
    Close #3632
    
    * Check if files deleted during updates belong to Core
    
    * Fix condition in remove files script and add it to package.xml
    
    Co-authored-by: Tim Düsterhus <duesterhus@woltlab.com>

---
+++ wcfsetup/install/files/acp/update_5.4_removeFiles.php
@@ -0,0 +1,36 @@
+<?php
+use wcf\data\package\PackageCache;
+use wcf\system\WCF;
+
+$files = [
+	'lib/system/database/table/DatabaseTableUtil.class.php',
+];
+
+$sql = "SELECT  packageID
+	FROM    wcf" . WCF_N . "_package_installation_file_log
+	WHERE   filename = ?";
+$searchStatement = WCF::getDB()->prepareStatement($sql);
+
+$sql = "DELETE FROM     wcf" . WCF_N . "_package_installation_file_log
+	WHERE           packageID = ?
+	                AND filename = ?";
+$deletionStatement = WCF::getDB()->prepareStatement($sql);
+
+$packageID = $this->installation->getPackageID();
+
+foreach ($files as $file) {
+	$searchStatement->execute([$file]);
+	$filePackageID = $searchStatement->fetchSingleColumn();
+	if ($filePackageID !== null && $filePackageID != $packageID) {
+		throw new \UnexpectedValueException("File '{$file}' does not belong to package '{$this->installation->getPackage()->package}' but to package '" . PackageCache::getInstance()->getPackage($filePackageID)->package . "'.");
+	}
+	
+	if (file_exists(WCF_DIR . $file)) {
+		unlink(WCF_DIR . $file);
+	}
+	
+	$deletionStatement->execute([
+		$packageID,
+		$file,
+	]);
+}
