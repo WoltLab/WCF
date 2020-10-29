bdcfed34d78c0b977ff1e10ace7fbc14a4f1bbb5
Alexander Ebert <ebert@woltlab.com>
2020-10-29 18:25:51 +0100

    Disable the `no-non-null-assertion` rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,19 @@
 module.exports = {
-	root: true,
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: ['./tsconfig.json']
-	},
-	plugins: ["@typescript-eslint"],
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:@typescript-eslint/recommended-requiring-type-checking",
-		"prettier",
-		"prettier/@typescript-eslint",
-	],
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"]
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+    "prettier/@typescript-eslint"
+  ],
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": 0
+  }
 };
