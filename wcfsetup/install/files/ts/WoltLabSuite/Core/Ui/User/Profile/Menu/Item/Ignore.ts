273714002e6c80422bca27eac67fe7f6a037929b
Alexander Ebert <ebert@woltlab.com>
2020-10-31 13:12:38 +0100

    Convert `Ui/User/Profile/Menu/Item/Ignore` to TypeScript

---
+++ wcfsetup/install/files/ts/WoltLabSuite/Core/Ui/User/Profile/Menu/Item/Ignore.ts
@@ -0,0 +1,41 @@
+import * as Language from "../../../../../Language";
+import { RequestOptions, ResponseData } from "../../../../../Ajax/Data";
+import * as UiNotification from "../../../../Notification";
+import UiUserProfileMenuItemAbstract from "./Abstract";
+
+interface AjaxResponse extends ResponseData {
+  returnValues: {
+    isIgnoredUser: 1 | 0;
+  };
+}
+
+class UiUserProfileMenuItemIgnore extends UiUserProfileMenuItemAbstract {
+  constructor(userId: number, isActive: boolean) {
+    super(userId, isActive);
+  }
+
+  _getLabel(): string {
+    return Language.get("wcf.user.button." + (this._isActive ? "un" : "") + "ignore");
+  }
+
+  _getAjaxActionName(): string {
+    return this._isActive ? "unignore" : "ignore";
+  }
+
+  _ajaxSuccess(data: AjaxResponse): void {
+    this._isActive = !!data.returnValues.isIgnoredUser;
+    this._updateButton();
+
+    UiNotification.show();
+  }
+
+  _ajaxSetup(): RequestOptions {
+    return {
+      data: {
+        className: "wcf\\data\\user\\ignore\\UserIgnoreAction",
+      },
+    };
+  }
+}
+
+export = UiUserProfileMenuItemIgnore;
