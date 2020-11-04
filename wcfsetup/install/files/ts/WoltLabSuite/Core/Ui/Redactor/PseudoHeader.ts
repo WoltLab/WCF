ead855219a0d30033747adedda2674f6f7fc0ce8
Alexander Ebert <ebert@woltlab.com>
2020-11-04 10:42:56 +0100

    Convert `Ui/Redactor/PseudoHeader` to TypeScript

---
+++ wcfsetup/install/files/ts/WoltLabSuite/Core/Ui/Redactor/PseudoHeader.ts
@@ -0,0 +1,38 @@
+/**
+ * Helper class to deal with clickable block headers using the pseudo
+ * `::before` element.
+ *
+ * @author      Alexander Ebert
+ * @copyright  2001-2019 WoltLab GmbH
+ * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @module      WoltLabSuite/Core/Ui/Redactor/PseudoHeader
+ */
+
+/**
+ * Returns the height within a click should be treated as a click
+ * within the block element's title. This method expects that the
+ * `::before` element is used and that removing the attribute
+ * `data-title` does cause the title to collapse.
+ */
+export function getHeight(element: HTMLElement): number {
+  let height = ~~window.getComputedStyle(element).paddingTop.replace(/px$/, "");
+
+  const styles = window.getComputedStyle(element, "::before");
+  height += ~~styles.paddingTop.replace(/px$/, "");
+  height += ~~styles.paddingBottom.replace(/px$/, "");
+
+  let titleHeight = ~~styles.height.replace(/px$/, "");
+  if (titleHeight === 0) {
+    // firefox returns garbage for pseudo element height
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=925694
+
+    titleHeight = element.scrollHeight;
+    element.classList.add("redactorCalcHeight");
+    titleHeight -= element.scrollHeight;
+    element.classList.remove("redactorCalcHeight");
+  }
+
+  height += titleHeight;
+
+  return height;
+}
