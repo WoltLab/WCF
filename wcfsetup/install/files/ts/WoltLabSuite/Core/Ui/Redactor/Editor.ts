4dd8e8cfda3083e230a64835bdfc0ce0d7cd3f01
Alexander Ebert <ebert@woltlab.com>
2020-11-04 13:06:47 +0100

    Convert `Ui/Redactor/Code` to TypeScript

--- wcfsetup/install/files/ts/WoltLabSuite/Core/Ui/Redactor/Editor.ts
+++ wcfsetup/install/files/ts/WoltLabSuite/Core/Ui/Redactor/Editor.ts
@@ -1,26 +1,47 @@
 export interface RedactorEditor {
   $editor: JQuery;
+  $element: JQuery;
+
+  opts: {
+    [key: string]: any;
+  };
 
   buffer: {
-    set: () => void;
+    set(): void;
+  };
+  button: {
+    toggle(event: MouseEvent | object, btnName: string, type: string, callback: string, args?: object): void;
+  };
+  caret: {
+    after(node: Node): void;
+    end(node: Node): void;
   };
   clean: {
-    onSync: (html: string) => string;
+    onSync(html: string): string;
   };
   code: {
-    get: () => string;
-    start: (html: string) => void;
+    get(): string;
+    set(html: string): void;
+    start(html: string): void;
   };
   core: {
-    box: () => JQuery;
-    editor: () => JQuery;
-    element: () => JQuery;
-    textarea: () => JQuery;
+    box(): JQuery;
+    editor(): JQuery;
+    element(): JQuery;
+    textarea(): JQuery;
+  };
+  focus: {
+    end(): void;
   };
   insert: {
-    text: (text: string) => void;
+    text(text: string): void;
+  };
+  selection: {
+    block(): HTMLElement | false;
+    restore(): void;
+    save(): void;
   };
   utils: {
-    isEmpty: (html: string) => boolean;
+    isEmpty(html: string): boolean;
   };
 }
