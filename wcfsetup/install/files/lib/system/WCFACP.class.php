cd57635b0c621d0e244f60e33f28bb54f906cff4
Alexander Ebert <ebert@woltlab.com>
2020-11-08 19:19:17 +0100

    Obsolete exclusion of the old ACP catpcha
    This route does not exist anymore since WoltLab Suite 3.0.

--- wcfsetup/install/files/lib/system/WCFACP.class.php
+++ wcfsetup/install/files/lib/system/WCFACP.class.php
@@ -138,7 +138,7 @@ class WCFACP extends WCF {
 				exit;
 			}
 		}
-		else if (empty($pathInfo) || !preg_match('~^/?(acp-?captcha|login|logout)/~i', $pathInfo)) {
+		else if (empty($pathInfo) || !preg_match('~^/?(login|logout)/~i', $pathInfo)) {
 			if (WCF::getUser()->userID == 0) {
 				// work-around for AJAX-requests within ACP
 				if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
