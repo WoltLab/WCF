8fc2f54d60132f1ca6ee5552ada8c965a6674b85
Matthias Schmidt <gravatronics@live.com>
2020-12-09 17:34:00 +0100

    Convert `Form/Builder/Field/Field` to TypeScript

--- wcfsetup/install/files/js/WoltLabSuite/Core/Form/Builder/Field/Field.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Form/Builder/Field/Field.js
@@ -1,73 +1,65 @@
 /**
  * Data handler for a form builder field in an Ajax form.
  *
- * @author	Matthias Schmidt
- * @copyright	2001-2019 WoltLab GmbH
- * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @module	WoltLabSuite/Core/Form/Builder/Field/Field
- * @since	5.2
+ * @author  Matthias Schmidt
+ * @copyright 2001-2020 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @module  WoltLabSuite/Core/Form/Builder/Field/Field
+ * @since 5.2
  */
-define([], function () {
+define(["require", "exports", "tslib", "../../../Core"], function (require, exports, tslib_1, Core) {
     "use strict";
-    /**
-     * @constructor
-     */
-    function FormBuilderField(fieldId) {
-        this.init(fieldId);
-    }
-    ;
-    FormBuilderField.prototype = {
+    Core = tslib_1.__importStar(Core);
+    class Field {
+        constructor(fieldId) {
+            this.init(fieldId);
+        }
         /**
-         * Initializes the form field.
-         *
-         * @param	{string}	fieldId		id of the relevant form builder field
+         * Initializes the field.
          */
-        init: function (fieldId) {
+        init(fieldId) {
             this._fieldId = fieldId;
             this._readField();
-        },
+        }
         /**
          * Returns the current data of the field or a promise returning the current data
          * of the field.
          *
          * @return	{Promise|data}
          */
-        _getData: function () {
+        _getData() {
             throw new Error("Missing implementation of WoltLabSuite/Core/Form/Builder/Field/Field._getData!");
-        },
+        }
         /**
-         * Reads the field HTML element.
+         * Reads the field's HTML element.
          */
-        _readField: function () {
-            this._field = elById(this._fieldId);
+        _readField() {
+            this._field = document.getElementById(this._fieldId);
             if (this._field === null) {
                 throw new Error("Unknown field with id '" + this._fieldId + "'.");
             }
-        },
+        }
         /**
          * Destroys the field.
          *
          * This function is useful for remove registered elements from other APIs like dialogs.
          */
-        destroy: function () {
-            // does nothing
-        },
+        destroy() {
+            // does nothinbg
+        }
         /**
-         * Returns a promise returning the current data of the field.
-         *
-         * @return	{Promise}
+         * Returns a promise providing the current data of the field.
          */
-        getData: function () {
+        getData() {
             return Promise.resolve(this._getData());
-        },
+        }
         /**
          * Returns the id of the field.
-         *
-         * @return	{string}
          */
-        getId: function () {
+        getId() {
             return this._fieldId;
         }
-    };
-    return FormBuilderField;
+    }
+    Core.enableLegacyInheritance(Field);
+    return Field;
 });
