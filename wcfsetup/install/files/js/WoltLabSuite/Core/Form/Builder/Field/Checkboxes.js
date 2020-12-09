22a52612aa23a5f15db025993ac4bb8d6dfa3ad3
Matthias Schmidt <gravatronics@live.com>
2020-12-09 17:33:30 +0100

    Convert `Form/Builder/Field/Checkboxes` to TypeScript

--- wcfsetup/install/files/js/WoltLabSuite/Core/Form/Builder/Field/Checkboxes.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Form/Builder/Field/Checkboxes.js
@@ -1,41 +1,34 @@
 /**
  * Data handler for a form builder field in an Ajax form represented by checkboxes.
  *
- * @author	Matthias Schmidt
- * @copyright	2001-2019 WoltLab GmbH
- * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @module	WoltLabSuite/Core/Form/Builder/Field/Checkboxes
- * @since	5.2
+ * @author  Matthias Schmidt
+ * @copyright 2001-2020 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @module  WoltLabSuite/Core/Form/Builder/Field/Checkboxes
+ * @since 5.2
  */
-define(['Core', './Field'], function (Core, FormBuilderField) {
+define(["require", "exports", "tslib", "./Field", "../../../Core"], function (require, exports, tslib_1, Field_1, Core) {
     "use strict";
-    /**
-     * @constructor
-     */
-    function FormBuilderFieldCheckboxes(fieldId) {
-        this.init(fieldId);
-    }
-    ;
-    Core.inherit(FormBuilderFieldCheckboxes, FormBuilderField, {
-        /**
-         * @see	WoltLabSuite/Core/Form/Builder/Field/Field#_getData
-         */
-        _getData: function () {
-            var data = {};
-            data[this._fieldId] = [];
-            for (var i = 0, length = this._fields.length; i < length; i++) {
-                if (this._fields[i].checked) {
-                    data[this._fieldId].push(this._fields[i].value);
+    Field_1 = tslib_1.__importDefault(Field_1);
+    Core = tslib_1.__importStar(Core);
+    class Checkboxes extends Field_1.default {
+        _getData() {
+            const values = this._fields
+                .map((input) => {
+                if (input.checked) {
+                    return input.value;
                 }
-            }
-            return data;
-        },
-        /**
-         * @see	WoltLabSuite/Core/Form/Builder/Field/Field#_readField
-         */
-        _readField: function () {
-            this._fields = elBySelAll('input[name="' + this._fieldId + '[]"]');
+                return null;
+            })
+                .filter((v) => v !== null);
+            return {
+                [this._fieldId]: values,
+            };
         }
-    });
-    return FormBuilderFieldCheckboxes;
+        _readField() {
+            this._fields = Array.from(document.querySelectorAll("input[name=" + this._fieldId + "]"));
+        }
+    }
+    Core.enableLegacyInheritance(Checkboxes);
+    return Checkboxes;
 });
