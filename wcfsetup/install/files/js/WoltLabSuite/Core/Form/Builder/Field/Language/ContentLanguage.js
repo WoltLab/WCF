5076f3f8715f8c9aed20918c3c7ef79de756ae47
Matthias Schmidt <gravatronics@live.com>
2020-12-12 15:36:03 +0100

    Convert `Form/Builder/Field/Language/ContentLanguage` to TypeScript

--- wcfsetup/install/files/js/WoltLabSuite/Core/Form/Builder/Field/Language/ContentLanguage.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Form/Builder/Field/Language/ContentLanguage.js
@@ -1,28 +1,22 @@
 /**
  * Data handler for a content language form builder field in an Ajax form.
  *
- * @author	Matthias Schmidt
- * @copyright	2001-2019 WoltLab GmbH
- * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @module	WoltLabSuite/Core/Form/Builder/Field/Language/ContentLanguage
- * @since	5.2
+ * @author  Matthias Schmidt
+ * @copyright 2001-2020 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @module  WoltLabSuite/Core/Form/Builder/Field/Language/ContentLanguage
+ * @since 5.2
  */
-define(['Core', 'WoltLabSuite/Core/Language/Chooser', '../Value'], function (Core, LanguageChooser, FormBuilderFieldValue) {
+define(["require", "exports", "tslib", "../Value", "../../../../Language/Chooser", "../../../../Core"], function (require, exports, tslib_1, Value_1, LanguageChooser, Core) {
     "use strict";
-    /**
-     * @constructor
-     */
-    function FormBuilderFieldContentLanguage(fieldId) {
-        this.init(fieldId);
-    }
-    ;
-    Core.inherit(FormBuilderFieldContentLanguage, FormBuilderFieldValue, {
-        /**
-         * @see	WoltLabSuite/Core/Form/Builder/Field/Field#destroy
-         */
-        destroy: function () {
+    Value_1 = tslib_1.__importDefault(Value_1);
+    LanguageChooser = tslib_1.__importStar(LanguageChooser);
+    Core = tslib_1.__importStar(Core);
+    class ContentLanguage extends Value_1.default {
+        destroy() {
             LanguageChooser.removeChooser(this._fieldId);
         }
-    });
-    return FormBuilderFieldContentLanguage;
+    }
+    Core.enableLegacyInheritance(ContentLanguage);
+    return ContentLanguage;
 });
