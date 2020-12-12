68f339261e2198a1a2358a0a2f16f8b0b54c3504
Matthias Schmidt <gravatronics@live.com>
2020-12-12 16:03:25 +0100

    Convert `Form/Builder/Field/Dependency/Container/Abstract` to TypeScript

---
+++ wcfsetup/install/files/ts/WoltLabSuite/Core/Form/Builder/Field/Dependency/Container/Abstract.ts
@@ -0,0 +1,50 @@
+/**
+ * Abstract implementation of a handler for the visibility of container due the dependencies
+ * of its children.
+ *
+ * @author  Matthias Schmidt
+ * @copyright 2001-2020 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @module  WoltLabSuite/Core/Form/Builder/Field/Dependency/Container/Abstract
+ * @since 5.2
+ */
+
+import DependencyManager from "../Manager";
+import * as Core from "../../../../../Core";
+
+class Abstract {
+  protected _container: HTMLElement;
+
+  constructor(containerId: string) {
+    this.init(containerId);
+  }
+
+  /**
+   * Returns `true` if the dependency is met and thus if the container should be shown.
+   */
+  public checkContainer(): void {
+    throw new Error(
+      "Missing implementation of WoltLabSuite/Core/Form/Builder/Field/Dependency/Container.checkContainer!",
+    );
+  }
+
+  /**
+   * Initializes a new container dependency handler for the container with the given id.
+   */
+  protected init(containerId: string): void {
+    if (typeof containerId !== "string") {
+      throw new TypeError("Container id has to be a string.");
+    }
+
+    this._container = document.getElementById(containerId)!;
+    if (this._container === null) {
+      throw new Error("Unknown container with id '" + containerId + "'.");
+    }
+
+    DependencyManager.addContainerCheckCallback(() => this.checkContainer());
+  }
+}
+
+Core.enableLegacyInheritance(Abstract);
+
+export = Abstract;
