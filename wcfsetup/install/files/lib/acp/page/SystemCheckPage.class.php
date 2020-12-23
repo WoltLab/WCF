8e01769e38ab684406021468679739c8f0e00741
Tim Düsterhus <duesterhus@woltlab.com>
2020-12-23 12:25:37 +0100

    Remove search table engine check from SystemCheckPage
    As of c36b70769fd366597d463064b29afc87fb2bcefe both engines are supported just
    fine.
    
    This is related to #3404.

--- wcfsetup/install/files/lib/acp/page/SystemCheckPage.class.php
+++ wcfsetup/install/files/lib/acp/page/SystemCheckPage.class.php
@@ -1,11 +1,9 @@
 <?php
 namespace wcf\acp\page;
 use wcf\data\application\Application;
-use wcf\data\object\type\ObjectTypeCache;
 use wcf\page\AbstractPage;
 use wcf\system\database\util\PreparedStatementConditionBuilder;
 use wcf\system\exception\SystemException;
-use wcf\system\search\SearchIndexManager;
 use wcf\system\WCF;
 use wcf\util\FileUtil;
 
@@ -124,10 +122,6 @@ class SystemCheckPage extends AbstractPage {
 			'result' => false,
 			'version' => '0.0.0',
 			'foreignKeys' => false,
-			'searchEngine' => [
-				'result' => false, 
-				'incorrectTables' => [],
-			],
 		],
 		'php' => [
 			'gd' => [
@@ -257,31 +251,7 @@ class SystemCheckPage extends AbstractPage {
 		
 		$this->results['mysql']['foreignKeys'] = $statement->fetchSingleColumn() == $expectedForeignKeyCount;
 		
-		// check search engine tables
-		$objectTypes = ObjectTypeCache::getInstance()->getObjectTypes('com.woltlab.wcf.searchableObjectType');
-		$tableNames = [];
-		foreach ($objectTypes as $objectType) {
-			$tableNames[] = SearchIndexManager::getTableName($objectType->objectType);
-		}
-		$conditionBuilder = new PreparedStatementConditionBuilder(true);
-		$conditionBuilder->add('TABLE_NAME IN (?)', [$tableNames]);
-		$conditionBuilder->add('TABLE_SCHEMA = ?', [WCF::getDB()->getDatabaseName()]);
-		
-		$sql = "SELECT          TABLE_NAME, ENGINE
-			FROM            INFORMATION_SCHEMA.TABLES
-			". $conditionBuilder;
-		$statement = WCF::getDB()->prepareStatement($sql);
-		$statement->execute($conditionBuilder->getParameters());
-		
-		while ($row = $statement->fetchArray()) {
-			if ($row['ENGINE'] !== 'MyISAM') {
-				$this->results['mysql']['searchEngine']['incorrectTables'][$row['TABLE_NAME']] = $row['ENGINE'];
-			}
-		}
-		
-		$this->results['mysql']['searchEngine']['result'] = empty($this->results['mysql']['searchEngine']['incorrectTables']);
-		
-		if ($this->results['mysql']['result'] && $this->results['mysql']['innodb'] && $this->results['mysql']['foreignKeys'] && $this->results['mysql']['searchEngine']['result']) {
+		if ($this->results['mysql']['result'] && $this->results['mysql']['innodb'] && $this->results['mysql']['foreignKeys']) {
 			$this->results['status']['mysql'] = true;
 		}
 	}
