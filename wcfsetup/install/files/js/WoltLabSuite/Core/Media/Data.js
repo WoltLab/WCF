68be723833a2416f558052bef9da7a37d5279593
Matthias Schmidt <gravatronics@live.com>
2021-01-05 18:43:55 +0100

    Add media-related TypeScript interfaces

---
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Media/Data.js
@@ -0,0 +1,10 @@
+/**
+ * @author  Matthias Schmidt
+ * @copyright 2001-2021 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @module  WoltLabSuite/Core/Media/Data
+ */
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+});
