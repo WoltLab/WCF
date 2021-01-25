db579a51ea85b2e496b7c9d7059b0baf385c17d1
Tim Düsterhus <duesterhus@woltlab.com>
2021-01-25 11:45:11 +0100

    Deprecate ACPSession DBO

--- wcfsetup/install/files/lib/data/acp/session/ACPSessionList.class.php
+++ wcfsetup/install/files/lib/data/acp/session/ACPSessionList.class.php
@@ -16,6 +16,7 @@ use wcf\data\DatabaseObjectList;
  * @method  ACPSession[]        getObjects()
  * @method  ACPSession|null     search($objectID)
  * @property    ACPSession[] $objects
+ * @deprecated  5.4 Distinct ACP sessions have been removed. This class is preserved due to its use in legacy sessions.
  */
 class ACPSessionList extends DatabaseObjectList
 {
