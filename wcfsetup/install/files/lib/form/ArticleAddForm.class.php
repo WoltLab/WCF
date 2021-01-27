6a945cb2ad4c1806dd9a2c394c427a2d926b2eae
Tim Düsterhus <duesterhus@woltlab.com>
2021-01-27 17:28:25 +0100

    Fix bad merge
    see 521f18fb9505d68091945d6ee484277dac89645c
    see fa8aae2ad5f9299fdbce5feed23fe03a4fb335d4

--- wcfsetup/install/files/lib/form/ArticleAddForm.class.php
+++ wcfsetup/install/files/lib/form/ArticleAddForm.class.php
@@ -16,34 +16,39 @@ use wcf\util\HeaderUtil;
  * @package     WoltLabSuite\Core\Form
  * @since       5.2
  */
-class ArticleAddForm extends \wcf\acp\form\ArticleAddForm {
-	/**
-	 * @inheritDoc
-	 */
-	public function assignVariables() {
-		parent::assignVariables();
-		
-		WCF::getTPL()->assign(['articleIsFrontend' => true]);
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function save() {
-		parent::save();
-		
-		/** @var Article $article */
-		$article = $this->objectAction->getReturnValues()['returnValues'];
-		if ($article->publicationStatus == Article::PUBLISHED) {
-			HeaderUtil::redirect($article->getLink());
-			
-			exit;
-		}
-		else {
-			WCF::getTPL()->assign([
-				// We need to reassign the link here because otherwise it will lead to the admin panel.
-				'objectEditLink' => LinkHandler::getInstance()->getControllerLink(ArticleEditForm::class, ['id' => $article->getObjectID()]),
-			]);
-		}
-	}
+class ArticleAddForm extends \wcf\acp\form\ArticleAddForm
+{
+    /**
+     * @inheritDoc
+     */
+    public function assignVariables()
+    {
+        parent::assignVariables();
+
+        WCF::getTPL()->assign(['articleIsFrontend' => true]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function save()
+    {
+        parent::save();
+
+        /** @var Article $article */
+        $article = $this->objectAction->getReturnValues()['returnValues'];
+        if ($article->publicationStatus == Article::PUBLISHED) {
+            HeaderUtil::redirect($article->getLink());
+
+            exit;
+        } else {
+            WCF::getTPL()->assign([
+                // We need to reassign the link here because otherwise it will lead to the admin panel.
+                'objectEditLink' => LinkHandler::getInstance()->getControllerLink(
+                    ArticleEditForm::class,
+                    ['id' => $article->getObjectID()]
+                ),
+            ]);
+        }
+    }
 }
