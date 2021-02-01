a352774a31fdd854eb5f315fc7a3104b8740c27b
Alexander Ebert <ebert@woltlab.com>
2021-02-01 12:47:09 +0100

    Missing check for accessible user groups

--- wcfsetup/install/files/lib/acp/form/UserMergeForm.class.php
+++ wcfsetup/install/files/lib/acp/form/UserMergeForm.class.php
@@ -7,6 +7,7 @@ use wcf\form\AbstractForm;
 use wcf\system\clipboard\ClipboardHandler;
 use wcf\system\database\util\PreparedStatementConditionBuilder;
 use wcf\system\exception\IllegalLinkException;
+use wcf\system\exception\PermissionDeniedException;
 use wcf\system\exception\UserInputException;
 use wcf\system\session\SessionHandler;
 use wcf\system\WCF;
@@ -72,6 +73,11 @@ class UserMergeForm extends AbstractForm {
 		$this->users = ClipboardHandler::getInstance()->getMarkedItems($this->objectTypeID);
 		if (empty($this->users) || count($this->users) < 2) {
 			throw new IllegalLinkException();
+		}
+		foreach ($this->users as $user) {
+			if (!$user->canEdit()) {
+				throw new PermissionDeniedException();
+			}
 		}
 		$this->userIDs = array_keys($this->users);
 	}
