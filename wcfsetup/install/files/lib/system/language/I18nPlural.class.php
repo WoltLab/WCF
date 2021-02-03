fb169a27de86ea06194e1edeece6f5a4e0859eb4
Tim Düsterhus <duesterhus@woltlab.com>
2021-02-03 10:57:00 +0100

    Fix reference URL for Unicode plural rules

--- wcfsetup/install/files/lib/system/language/I18nPlural.class.php
+++ wcfsetup/install/files/lib/system/language/I18nPlural.class.php
@@ -14,7 +14,7 @@ use wcf\system\WCF;
  * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package WoltLabSuite\Core\System\Language
  * @since   5.3
- * @see https://unicode.org/cldr/charts/latest/supplemental/language_plural_rules.html
+ * @see https://unicode-org.github.io/cldr-staging/charts/latest/supplemental/language_plural_rules.html
  */
 class I18nPlural
 {
