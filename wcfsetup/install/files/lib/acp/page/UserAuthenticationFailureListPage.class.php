2bb077e8b9a0db85b8b4f3a584a1667b513cb223
Matthias Schmidt <gravatronics@live.com>
2021-03-10 11:06:09 +0100

    Support filtering the list of user authentication failures
    Filtering by IP address is not supported due storing IPv4 addresses in IPv6 format but displaying them in as IPv4 so that (partial) IPv4 addresses cannot be (easily) searched for.
    
    See #3395

--- wcfsetup/install/files/lib/acp/page/UserAuthenticationFailureListPage.class.php
+++ wcfsetup/install/files/lib/acp/page/UserAuthenticationFailureListPage.class.php
@@ -4,6 +4,7 @@ namespace wcf\acp\page;
 
 use wcf\data\user\authentication\failure\UserAuthenticationFailureList;
 use wcf\page\SortablePage;
+use wcf\system\WCF;
 
 /**
  * Shows a list of user authentication failures.
@@ -51,4 +52,99 @@ class UserAuthenticationFailureListPage extends SortablePage
      * @inheritDoc
      */
     public $objectListClassName = UserAuthenticationFailureList::class;
+
+    /**
+     * @var string[]
+     * @since   5.4
+     */
+    public $filter = [
+        'environment' => '',
+        'endDate' => '',
+        'startDate' => '',
+        'username' => '',
+        'userAgent' => '',
+        'validationError' => '',
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function readParameters()
+    {
+        parent::readParameters();
+
+        if (isset($_REQUEST['filter']) && \is_array($_REQUEST['filter'])) {
+            foreach ($_REQUEST['filter'] as $key => $value) {
+                if (\array_key_exists($key, $this->filter)) {
+                    $this->filter[$key] = $value;
+                }
+            }
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function initObjectList()
+    {
+        parent::initObjectList();
+
+        if ($this->filter['environment'] !== '') {
+            $this->objectList->getConditionBuilder()->add(
+                'user_authentication_failure.environment = ?',
+                [$this->filter['environment']]
+            );
+        }
+        if ($this->filter['endDate'] !== '') {
+            $endDate = @\strtotime($this->filter['endDate']);
+            if ($endDate > 0) {
+                $this->objectList->getConditionBuilder()->add(
+                    'user_authentication_failure.time <= ?',
+                    [$endDate]
+                );
+            }
+        }
+        if ($this->filter['startDate'] !== '') {
+            $startDate = @\strtotime($this->filter['startDate']);
+            if ($startDate > 0) {
+                $this->objectList->getConditionBuilder()->add(
+                    'user_authentication_failure.time >= ?',
+                    [$startDate]
+                );
+            }
+        }
+        if ($this->filter['username'] !== '') {
+            $this->objectList->getConditionBuilder()->add(
+                'user_authentication_failure.username LIKE ?',
+                ['%' . \addcslashes($this->filter['username'], '_%') . '%']
+            );
+        }
+        if ($this->filter['userAgent'] !== '') {
+            $this->objectList->getConditionBuilder()->add(
+                'user_authentication_failure.userAgent LIKE ?',
+                ['%' . \addcslashes($this->filter['userAgent'], '_%') . '%']
+            );
+        }
+        if ($this->filter['validationError'] !== '') {
+            $this->objectList->getConditionBuilder()->add(
+                'user_authentication_failure.validationError = ?',
+                [$this->filter['validationError']]
+            );
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function assignVariables()
+    {
+        parent::assignVariables();
+
+        $filterLinkParameters = \http_build_query(['filter' => \array_filter($this->filter)], '', '&');
+
+        WCF::getTPL()->assign([
+            'filter' => $this->filter,
+            'filterLinkParameters' => $filterLinkParameters ? '&' . $filterLinkParameters : '',
+        ]);
+    }
 }
