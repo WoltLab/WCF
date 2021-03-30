5d07b90bbc141bd4ce9786ad951806e87f81d1fc
Marcel Werk <burntime@woltlab.com>
2021-03-30 15:24:21 +0200

    Always fetch package information using the default language

--- wcfsetup/install/files/lib/system/cronjob/GetUpdateInfoCronjob.class.php
+++ wcfsetup/install/files/lib/system/cronjob/GetUpdateInfoCronjob.class.php
@@ -1,7 +1,9 @@
 <?php
 namespace wcf\system\cronjob;
 use wcf\data\cronjob\Cronjob;
+use wcf\system\language\LanguageFactory;
 use wcf\system\package\PackageUpdateDispatcher;
+use wcf\system\WCF;
 
 /**
  * Fetches update package information.
@@ -19,7 +21,20 @@ class GetUpdateInfoCronjob extends AbstractCronjob {
 		parent::execute($cronjob);
 		
 		if (!ENABLE_BENCHMARK) {
-			PackageUpdateDispatcher::getInstance()->refreshPackageDatabase([], true);
+			try {
+				$currentLanguage = WCF::getLanguage();
+				// Always fetch package information using the default language.
+				if ($currentLanguage->languageID !== LanguageFactory::getInstance()->getDefaultLanguage()->languageID) {
+					WCF::setLanguage(LanguageFactory::getInstance()->getDefaultLanguage());
+				}
+				
+				PackageUpdateDispatcher::getInstance()->refreshPackageDatabase([], true);
+			}
+			finally {
+				if ($currentLanguage->languageID !== LanguageFactory::getInstance()->getDefaultLanguage()->languageID) {
+					WCF::setLanguage($currentLanguage);
+				}
+			}
 		}
 	}
 }
