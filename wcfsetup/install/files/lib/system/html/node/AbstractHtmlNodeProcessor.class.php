76f66297178f7f7630a7655bfe2f99b052423f49
Alexander Ebert <ebert@woltlab.com>
2021-04-12 21:42:29 +0200

    `XPath::query()` is much more efficient than `getElementByTagName()`
    The test document with ~8k elements took ~0.6s using `getElementsByTagName(*)`, but only 0.03s using `query("//*")`

--- wcfsetup/install/files/lib/system/html/node/AbstractHtmlNodeProcessor.class.php
+++ wcfsetup/install/files/lib/system/html/node/AbstractHtmlNodeProcessor.class.php
@@ -304,7 +304,7 @@ abstract class AbstractHtmlNodeProcessor implements IHtmlNodeProcessor {
 		
 		$tags = [];
 		/** @var \DOMElement $tag */
-		foreach ($this->getDocument()->getElementsByTagName('*') as $tag) {
+		foreach ($this->getXPath()->query("//*") as $tag) {
 			$tagName = $tag->nodeName;
 			if (!isset($tags[$tagName])) $tags[$tagName] = $tagName;
 		}
