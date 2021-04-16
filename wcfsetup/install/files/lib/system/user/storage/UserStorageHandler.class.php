fcec0f82bd1807903b5dde1bfb4f4247e2485835
joshuaruesweg <ruesweg@woltlab.com>
2021-04-16 12:38:56 +0200

    Fix requesting multiple times the UserStorage tables if no cache exists

--- wcfsetup/install/files/lib/system/user/storage/UserStorageHandler.class.php
+++ wcfsetup/install/files/lib/system/user/storage/UserStorageHandler.class.php
@@ -55,7 +55,10 @@ class UserStorageHandler extends SingletonFactory {
 		
 		$tmp = [];
 		foreach ($userIDs as $userID) {
-			if (!isset($this->cache[$userID])) $tmp[] = $userID;
+			if (!isset($this->cache[$userID])) {
+				$tmp[] = $userID;
+				$this->cache[$userID] = [];
+			}
 		}
 		
 		// ignore users whose storage data is already loaded
@@ -70,10 +73,6 @@ class UserStorageHandler extends SingletonFactory {
 		$statement = WCF::getDB()->prepareStatement($sql);
 		$statement->execute($conditions->getParameters());
 		while ($row = $statement->fetchArray()) {
-			if (!isset($this->cache[$row['userID']])) {
-				$this->cache[$row['userID']] = [];
-			}
-			
 			if (isset($this->log[$row['userID']])) {
 				if (array_key_exists($row['field'], $this->log[$row['userID']])) {
 					$logged = $this->log[$row['userID']][$row['field']];
