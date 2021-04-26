de2accce654a210a89a8fe43e8a2c33b828b8cca
Matthias Schmidt <gravatronics@live.com>
2021-04-26 10:12:05 +0200

    Add `PasswordFormField` (#4148)
    * Add `PasswordFormField`
    
    * Fix typo in documentation of `PasswordFormField` constructor
    
    Co-authored-by: Joshua Rüsweg <ruesweg@woltlab.com>
    
    Co-authored-by: Joshua Rüsweg <ruesweg@woltlab.com>

---
+++ wcfsetup/install/files/lib/system/form/builder/field/PasswordFormField.class.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace wcf\system\form\builder\field;
+
+use wcf\system\form\builder\field\validation\FormFieldValidationError;
+
+/**
+ * Implementation of a form field for a password.
+ *
+ * @author  Matthias Schmidt
+ * @copyright   2001-2021 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package WoltLabSuite\Core\System\Form\Builder\Field
+ * @since   5.4
+ */
+class PasswordFormField extends AbstractFormField implements
+    IAttributeFormField,
+    IAutoCompleteFormField,
+    IAutoFocusFormField,
+    ICssClassFormField,
+    IImmutableFormField,
+    IInputModeFormField,
+    IMaximumLengthFormField,
+    IMinimumLengthFormField,
+    IPatternFormField,
+    IPlaceholderFormField
+{
+    use TInputAttributeFormField;
+    use TTextAutoCompleteFormField;
+    use TAutoFocusFormField;
+    use TCssClassFormField;
+    use TDefaultIdFormField;
+    use TImmutableFormField;
+    use TInputModeFormField;
+    use TMaximumLengthFormField;
+    use TMinimumLengthFormField;
+    use TPatternFormField;
+    use TPlaceholderFormField;
+
+    /**
+     * @inheritDoc
+     */
+    protected $javaScriptDataHandlerModule = 'WoltLabSuite/Core/Form/Builder/Field/Value';
+
+    /**
+     * @inheritDoc
+     */
+    protected $templateName = '__passwordFormField';
+
+    /**
+     * Creates a new instance of `PasswordFormField`.
+     */
+    public function __construct()
+    {
+        $this->label('wcf.user.password');
+        $this->addFieldClass('long');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getValidInputModes(): array
+    {
+        return [
+            'text',
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function readValue()
+    {
+        if ($this->getDocument()->hasRequestData($this->getPrefixedId())) {
+            $this->value = $this->getDocument()->getRequestData($this->getPrefixedId());
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function validate()
+    {
+        $value = $this->getValue();
+        $hasValue = $this->getValue() !== null && $this->getValue() !== '';
+
+        if ($this->isRequired() && !$hasValue) {
+            $this->addValidationError(new FormFieldValidationError('empty'));
+        } elseif ($hasValue) {
+            $this->validateMinimumLength($value);
+            $this->validateMaximumLength($value);
+        }
+
+        parent::validate();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected static function getDefaultId()
+    {
+        return 'password';
+    }
+}
