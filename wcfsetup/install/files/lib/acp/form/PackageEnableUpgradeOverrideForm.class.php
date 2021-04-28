7e683a6975edcf512107208aa68fba5c390a33b7
Tim Düsterhus <duesterhus@woltlab.com>
2021-04-28 15:56:13 +0200

    Add version checks from update_com.woltlab.wcf_5.4_checkSystemRequirements.php to PackageEnableUpgradeOverrideForm

--- wcfsetup/install/files/lib/acp/form/PackageEnableUpgradeOverrideForm.class.php
+++ wcfsetup/install/files/lib/acp/form/PackageEnableUpgradeOverrideForm.class.php
@@ -11,6 +11,7 @@ use wcf\system\form\builder\field\validation\FormFieldValidationError;
 use wcf\system\form\builder\field\validation\FormFieldValidator;
 use wcf\system\form\builder\TemplateFormNode;
 use wcf\system\registry\RegistryHandler;
+use wcf\system\WCF;
 
 /**
  * Allows enabling the package upgrade override.
@@ -87,7 +88,48 @@ final class PackageEnableUpgradeOverrideForm extends AbstractFormBuilderForm {
 	}
 
 	private function getIssuesPreventingUpgrade() {
-		return ['foo'];
+		$issues = [];
+
+		$phpVersion = \PHP_VERSION;
+		$neededPhpVersion = '7.2.24';
+		if (!\version_compare($phpVersion, $neededPhpVersion, '>=')) {
+			if (WCF::getLanguage()->getFixedLanguageCode() === 'de') {
+				$message = "Ihre PHP-Version '{$phpVersion}' ist unzureichend f&uuml;r die Installation dieser Software. PHP-Version {$neededPhpVersion} oder h&ouml;her wird ben&ouml;tigt.";
+			}
+			else {
+				$message = "Your PHP version '{$phpVersion}' is insufficient for installation of this software. PHP version {$neededPhpVersion} or greater is required.";
+			}
+
+			$issues[] = $message;
+		}
+
+		$sqlVersion = WCF::getDB()->getVersion();
+		$compareSQLVersion = \preg_replace('/^(\d+\.\d+\.\d+).*$/', '\\1', $sqlVersion);
+		if (\stripos($sqlVersion, 'MariaDB') !== false) {
+			$neededSqlVersion = '10.1.44';
+			$sqlFork = 'MariaDB';
+		}
+		else {
+			$sqlFork = 'MySQL';
+			if ($compareSQLVersion[0] === '5') {
+				$neededSqlVersion = '5.7.31';
+			}
+			else {
+				$neededSqlVersion = '8.0.19';
+			}
+		}
+
+		if (!\version_compare($compareSQLVersion, $neededSqlVersion, '>=')) {
+			if (WCF::getLanguage()->getFixedLanguageCode() === 'de') {
+				$message = "Ihre {$sqlFork}-Version '{$sqlVersion}' ist unzureichend f&uuml;r die Installation dieser Software. {$sqlFork}-Version {$neededSqlVersion} oder h&ouml;her wird ben&ouml;tigt.";
+			} else {
+				$message = "Your {$sqlFork} version '{$sqlVersion}' is insufficient for installation of this software. {$sqlFork} version {$neededSqlVersion} or greater is required.";
+			}
+
+			$issues[] = $message;
+		}
+
+		return $issues;
 	}
 
 	/**
