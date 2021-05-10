25f41e0c5948b18aec26022dd12ca89871f6f084
Alexander Ebert <ebert@woltlab.com>
2021-05-10 19:08:01 +0200

    Generate WebP variants of users' cover photos
    See #4187

---
+++ wcfsetup/install/files/lib/data/user/cover/photo/IWebpUserCoverPhoto.class.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace wcf\data\user\cover\photo;
+
+/**
+ * Any displayable cover photo type should implement this class.
+ * 
+ * @author Alexander Ebert
+ * @copyright 2001-2021 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package WoltLabSuite\Core\Data\User\Cover\Photo
+ * @since 5.4
+ */
+interface IWebpUserCoverPhoto extends IUserCoverPhoto
+{
+    /**
+     * @return null|bool
+     */
+    public function createWebpVariant();
+}
