88dffc6bece3a9c780ba20961c8192636b1bd4e0
Tim Düsterhus <duesterhus@woltlab.com>
2021-05-19 12:10:48 +0200

    Exclude `Devtools/Style/LiveReload` from all JS bundles

--- wcfsetup/install/files/js/WoltLabSuite/Core/Devtools/Style/LiveReload.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Devtools/Style/LiveReload.js
@@ -5,6 +5,7 @@
  * @copyright  2001-2021 WoltLab GmbH
  * @license  GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @module  WoltLabSuite/Core/Devtools/Style/LiveReload
+ * @woltlabExcludeBundle=all
  */
 define(["require", "exports"], function (require, exports) {
     "use strict";
