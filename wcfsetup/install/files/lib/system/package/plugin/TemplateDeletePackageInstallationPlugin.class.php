a0952923f1782cba8bb9582cf5ea1460fcfa3791
Matthias Schmidt <gravatronics@live.com>
2021-06-08 13:48:03 +0200

    Add `TemplateDeletePackageInstallationPlugin`

---
+++ wcfsetup/install/files/lib/system/package/plugin/TemplateDeletePackageInstallationPlugin.class.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace wcf\system\package\plugin;
+
+use wcf\data\application\Application;
+
+/**
+ * Deletes templates installed with the `template` package installation plugin.
+ *
+ * @author  Matthias Schmidt
+ * @copyright   2001-2021 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package WoltLabSuite\Core\System\Package\Plugin
+ * @since   5.5
+ */
+final class TemplateDeletePackageInstallationPlugin extends AbstractTemplateDeletePackageInstallationPlugin
+{
+    /**
+     * @inheritDoc
+     */
+    protected function getLogTableName(): string
+    {
+        return 'wcf1_template';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getFilePath(string $filename, string $application): string
+    {
+        return \sprintf(
+            '%s/templates/%s.tpl',
+            Application::getDirectory($application),
+            $filename
+        );
+    }
+}
