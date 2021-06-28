ef430bd32dc8b39f3d98004f81d5a3ca3135201a
Alexander Ebert <ebert@woltlab.com>
2021-06-28 16:27:32 +0200

    Removed the upgrade notice from the package server list

--- wcfsetup/install/files/lib/acp/page/PackageUpdateServerListPage.class.php
+++ wcfsetup/install/files/lib/acp/page/PackageUpdateServerListPage.class.php
@@ -1,9 +1,7 @@
 <?php
 namespace wcf\acp\page;
-use wcf\data\package\update\server\PackageUpdateServer;
 use wcf\data\package\update\server\PackageUpdateServerList;
 use wcf\page\SortablePage;
-use wcf\system\WCF;
 
 /**
  * Shows information about available update package servers.
@@ -48,17 +46,5 @@ class PackageUpdateServerListPage extends SortablePage {
 		$this->sqlOrderBy = ($this->sortField != 'packages' ? 'package_update_server.' : '') . $this->sortField.' '.$this->sortOrder;
 		
 		parent::readObjects();
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function assignVariables() {
-		parent::assignVariables();
-		
-		WCF::getTPL()->assign([
-			'availableUpgradeVersion' => WCF::AVAILABLE_UPGRADE_VERSION,
-			'upgradeOverrideEnabled' => PackageUpdateServer::isUpgradeOverrideEnabled(),
-		]);
 	}
 }
