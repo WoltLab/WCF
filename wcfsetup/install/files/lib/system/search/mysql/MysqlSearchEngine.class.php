3872a0383dc77ec47d965a2e8770c016904abeda
Tim Düsterhus <duesterhus@woltlab.com>
2021-08-27 12:03:15 +0200

    Make use of the `SearchFailed` exception in MysqlSearchEngine

--- wcfsetup/install/files/lib/system/search/mysql/MysqlSearchEngine.class.php
+++ wcfsetup/install/files/lib/system/search/mysql/MysqlSearchEngine.class.php
@@ -2,10 +2,12 @@
 
 namespace wcf\system\search\mysql;
 
-use wcf\system\database\DatabaseException;
+use wcf\system\database\DatabaseException as LegacyDatabaseException;
+use wcf\system\database\exception\DatabaseException;
 use wcf\system\database\util\PreparedStatementConditionBuilder;
 use wcf\system\exception\SystemException;
 use wcf\system\search\AbstractSearchEngine;
+use wcf\system\search\exception\SearchFailed;
 use wcf\system\search\SearchEngine;
 use wcf\system\search\SearchIndexManager;
 use wcf\system\WCF;
@@ -103,15 +105,18 @@ class MysqlSearchEngine extends AbstractSearchEngine
             $sql .= " ORDER BY " . $orderBy;
         }
 
-        // send search query
-        $messages = [];
-        $statement = WCF::getDB()->prepareStatement($sql, $limit);
-        $statement->execute($parameters);
-        while ($row = $statement->fetchArray()) {
-            $messages[] = [
-                'objectID' => $row['objectID'],
-                'objectType' => $row['objectType'],
-            ];
+        try {
+            $messages = [];
+            $statement = WCF::getDB()->prepareStatement($sql, $limit);
+            $statement->execute($parameters);
+            while ($row = $statement->fetchArray()) {
+                $messages[] = [
+                    'objectID' => $row['objectID'],
+                    'objectType' => $row['objectType'],
+                ];
+            }
+        } catch (DatabaseException $e) {
+            throw new SearchFailed('MySQL search query failed.', $e);
         }
 
         return $messages;
@@ -467,7 +472,7 @@ class MysqlSearchEngine extends AbstractSearchEngine
                 $statement = WCF::getDB()->prepareStatement($sql);
                 $statement->execute();
                 $row = $statement->fetchArray();
-            } catch (DatabaseException $e) {
+            } catch (LegacyDatabaseException $e) {
                 // fallback if user is disallowed to issue 'SHOW VARIABLES'
                 $row = ['Value' => 3];
             }
