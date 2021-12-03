344e02fcfb86cf0c45cc553f7cd2b62fed4de52d
Tim Düsterhus <duesterhus@woltlab.com>
2021-12-03 14:11:31 +0100

    Deprecate IExtendedMessageQuickReplyAction
    see #4575

--- wcfsetup/install/files/lib/data/IExtendedMessageQuickReplyAction.class.php
+++ wcfsetup/install/files/lib/data/IExtendedMessageQuickReplyAction.class.php
@@ -3,12 +3,7 @@
 namespace wcf\data;
 
 /**
- * Default interface for actions implementing quick reply with extended mode.
- *
- * @author  Alexander Ebert
- * @copyright   2001-2019 WoltLab GmbH
- * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package WoltLabSuite\Core\Data
+ * @deprecated 5.5 The concept of starting a message in a simple editor and then migrating to an extended editor no longer exists.
  */
 interface IExtendedMessageQuickReplyAction extends IMessageQuickReplyAction
 {
