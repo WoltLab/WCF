860e98cff580e299cbbd8cdb7eb50d0113b938cc
Tim Düsterhus <duesterhus@woltlab.com>
2022-01-07 09:50:28 +0100

    Revert "Fix PHP 8.1 compatibility when saving I18n options"
    During the discussion within the PR is was decided to opt for a different, less
    invasive, fix, because the impact of this change is not really clear.
    
    This reverts commit b46c272b28ba84892534b31c641a6dd412bb0a1e.

--- wcfsetup/install/files/lib/system/option/OptionHandler.class.php
+++ wcfsetup/install/files/lib/system/option/OptionHandler.class.php
@@ -393,22 +393,18 @@ class OptionHandler implements IOptionHandler
         // get new value
         $newValue = $this->rawValues[$option->optionName] ?? null;
 
-        // Skip validation for I18n options. They are special cased and are not provided
-        // within the `rawValues`, thus causing validation to be effectively useless.
-        if (!$this->supportI18n || !$option->supportI18n) {
-            // get save value
-            $this->optionValues[$option->optionName] = $typeObj->getData($option, $newValue);
+        // get save value
+        $this->optionValues[$option->optionName] = $typeObj->getData($option, $newValue);
 
-            // validate with pattern
-            if ($option->validationPattern) {
-                if (
-                    !\preg_match(
-                        '~' . \str_replace('~', '\~', $option->validationPattern) . '~',
-                        $this->optionValues[$option->optionName]
-                    )
-                ) {
-                    throw new UserInputException($option->optionName, 'validationFailed');
-                }
+        // validate with pattern
+        if ($option->validationPattern) {
+            if (
+                !\preg_match(
+                    '~' . \str_replace('~', '\~', $option->validationPattern) . '~',
+                    $this->optionValues[$option->optionName]
+                )
+            ) {
+                throw new UserInputException($option->optionName, 'validationFailed');
             }
         }
 
