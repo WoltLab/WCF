WoltLab/WCF

commit c8abd7df4a2c0e434964c66ef86d236613e11fe4

Stop using getAnchor() with dynamic targets
see #4580

wcfsetup/install/files/acp/templates/articleAdd.tpl
@@ -389,8 +389,7 @@
 			<nav class="tabMenu">
 				<ul>
 					{foreach from=$availableLanguages item=availableLanguage}
-						{assign var='containerID' value='language'|concat:$availableLanguage->languageID}
-						<li><a href="{@$__wcf->getAnchor($containerID)}">{$availableLanguage->languageName}</a></li>
+						<li><a href="#{$availableLanguage->languageID}">{$availableLanguage->languageName}</a></li>
 					{/foreach}
 				</ul>
 			</nav>

com.woltlab.wcf/templates/articleAdd.tpl
@@ -396,8 +396,7 @@
 			<nav class="tabMenu">
 				<ul>
 					{foreach from=$availableLanguages item=availableLanguage}
-						{assign var='containerID' value='language'|concat:$availableLanguage->languageID}
-						<li><a href="{@$__wcf->getAnchor($containerID)}">{$availableLanguage->languageName}</a></li>
+						<li><a href="#language{$availableLanguage->languageID}">{$availableLanguage->languageName}</a></li>
 					{/foreach}
 				</ul>
 			</nav>

com.woltlab.wcf/templates/__tabTabMenuFormContainer.tpl
@@ -6,8 +6,7 @@
 		<ul>
 			{foreach from=$container item='child'}
 				{if $child->isAvailable()}
-					{assign var='__tabMenuFormContainerChildId' value=$child->getPrefixedId()|concat:'Container'}
-					<li{if !$child->checkDependencies()} style="display: none;"{/if}><a{if $container->usesAnchors()} href="{@$__wcf->getAnchor($__tabMenuFormContainerChildId)}"{/if}>{@$child->getLabel()}</a></li>
+					<li{if !$child->checkDependencies()} style="display: none;"{/if}><a{if $container->usesAnchors()} href="#{$child->getPrefixedId()|rawurlencode}Container"{/if}>{@$child->getLabel()}</a></li>
 				{/if}
 			{/foreach}
 		</ul>

com.woltlab.wcf/templates/codeMetaCode.tpl
@@ -9,8 +9,7 @@
 	
 	{assign var='lineNumber' value=$startLineNumber}
 	<pre class="codeBoxCode collapsibleBbcodeOverflow"><code{if $language} class="language-{$language}"{/if}>{foreach from=$content item=line}{*
-		*}{assign var='codeLineID' value='codeLine_'|concat:$lineNumber:'_':$codeID}{*
-		*}<span class="codeBoxLine" id="{$codeLineID}"><a href="{@$__wcf->getAnchor($codeLineID)}" class="lineAnchor" title="{@$lineNumber}" tabindex="-1" aria-hidden="true"></a><span>{$line}</span></span>{*
+		*}<span class="codeBoxLine" id="#codeLine_{$lineNumber}_{$codeID}"><a href="#codeLine_{$lineNumber}_{$codeID|rawurlencode}" class="lineAnchor" title="{@$lineNumber}" tabindex="-1" aria-hidden="true"></a><span>{$line}</span></span>{*
 		*}{assign var='lineNumber' value=$lineNumber+1}{*
 	*}{/foreach}</code></pre>
 	

com.woltlab.wcf/templates/user.tpl
@@ -332,7 +332,7 @@
 			<ul>
 				{foreach from=$__wcf->getUserProfileMenu()->getMenuItems() item=menuItem}
 					{if $menuItem->getContentManager()->isVisible($userID)}
-						<li><a href="{@$__wcf->getAnchor($menuItem->getIdentifier())}">{$menuItem}</a></li>
+						<li><a href="#{$menuItem->getIdentifier()|rawurlencode}">{$menuItem}</a></li>
 					{/if}
 				{/foreach}
 			</ul>

com.woltlab.wcf/templates/userConditions.tpl
@@ -4,8 +4,7 @@
 	<nav class="tabMenu">
 		<ul>
 			{foreach from=$groupedObjectTypes key='conditionGroup' item='conditionObjectTypes'}
-				{assign var='__anchor' value='user_'|concat:$conditionGroup}
-				<li><a href="{@$__wcf->getAnchor($__anchor)}">{lang}wcf.user.condition.conditionGroup.{$conditionGroup}{/lang}</a></li>
+				<li><a href="#user_{$conditionGroup|rawurlencode}">{lang}wcf.user.condition.conditionGroup.{$conditionGroup}{/lang}</a></li>
 			{/foreach}
 		</ul>
 	</nav>

wcfsetup/install/files/acp/templates/__tabMenuFormContainer.tpl
@@ -7,8 +7,7 @@
 		<ul>
 			{foreach from=$container item='child'}
 				{if $child->isAvailable()}
-					{assign var='__tabMenuFormContainerChildId' value=$child->getPrefixedId()|concat:'Container'}
-					<li{if !$child->checkDependencies()} style="display: none;"{/if}><a{if $container->usesAnchors()} href="{@$__wcf->getAnchor($__tabMenuFormContainerChildId)}"{/if}>{@$child->getLabel()}</a></li>
+					<li{if !$child->checkDependencies()} style="display: none;"{/if}><a{if $container->usesAnchors()} href="#{$child->getPrefixedId()|rawurlencode}Container"{/if}>{@$child->getLabel()}</a></li>
 				{/if}
 			{/foreach}
 		</ul>

wcfsetup/install/files/acp/templates/__tabTabMenuFormContainer.tpl
@@ -6,8 +6,7 @@
 		<ul>
 			{foreach from=$container item='child'}
 				{if $child->isAvailable()}
-					{assign var='__tabMenuFormContainerChildId' value=$child->getPrefixedId()|concat:'Container'}
-					<li{if !$child->checkDependencies()} style="display: none;"{/if}><a{if $container->usesAnchors()} href="{@$__wcf->getAnchor($__tabMenuFormContainerChildId)}"{/if}>{@$child->getLabel()}</a></li>
+					<li{if !$child->checkDependencies()} style="display: none;"{/if}><a{if $container->usesAnchors()} href="#{$child->getPrefixedId()|rawurlencode}Container"{/if}>{@$child->getLabel()}</a></li>
 				{/if}
 			{/foreach}
 		</ul>

commit 1fc12992981ea82997c44c8b9106abf568e2c834

Fix anchor link in i18n article add template
This was broken in c8abd7df4a2c0e434964c66ef86d236613e11fe4.

Closes #4670

[Tim: Adjusted commit message to reference the original commit]

wcfsetup/install/files/acp/templates/articleAdd.tpl
@@ -389,7 +389,7 @@
 			<nav class="tabMenu">
 				<ul>
 					{foreach from=$availableLanguages item=availableLanguage}
-						<li><a href="#{$availableLanguage->languageID}">{$availableLanguage->languageName}</a></li>
+						<li><a href="#language{$availableLanguage->languageID}">{$availableLanguage->languageName}</a></li>
 					{/foreach}
 				</ul>
 			</nav>