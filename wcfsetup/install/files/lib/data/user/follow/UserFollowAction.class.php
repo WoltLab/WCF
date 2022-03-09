806b0d61f2d385b576e4ae1c474032395cab962b
Tim Düsterhus <duesterhus@woltlab.com>
2022-03-09 13:47:01 +0100

    Validate the `pageNo` in UserFollowAction::validateGetGroupedUserList()

--- wcfsetup/install/files/lib/data/user/follow/UserFollowAction.class.php
+++ wcfsetup/install/files/lib/data/user/follow/UserFollowAction.class.php
@@ -183,6 +183,10 @@ class UserFollowAction extends AbstractDatabaseObjectAction implements IGroupedU
 		if ($this->userProfile->isProtected()) {
 			throw new PermissionDeniedException();
 		}
+
+		if ($this->parameters['pageNo'] < 1) {
+			throw new UserInputException('pageNo');
+		}
 	}
 	
 	/**
