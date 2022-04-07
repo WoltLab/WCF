c6b3ed7abd80a6a53bf9e1a0d66740dd436eff66
Tim Düsterhus <duesterhus@woltlab.com>
2022-04-07 10:57:11 +0200

    Update composer dependencies

--- wcfsetup/install/files/lib/system/api/composer/installed.php
+++ wcfsetup/install/files/lib/system/api/composer/installed.php
@@ -245,12 +245,12 @@
             'dev_requirement' => false,
         ),
         'symfony/deprecation-contracts' => array(
-            'pretty_version' => 'v2.5.0',
-            'version' => '2.5.0.0',
+            'pretty_version' => 'v2.5.1',
+            'version' => '2.5.1.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../symfony/deprecation-contracts',
             'aliases' => array(),
-            'reference' => '6f981ee24cf69ee7ce9736146d1c57c2780598a8',
+            'reference' => 'e8b495ea28c1d97b5e0c121748d6f9b53d075c66',
             'dev_requirement' => false,
         ),
         'symfony/polyfill-mbstring' => array(
