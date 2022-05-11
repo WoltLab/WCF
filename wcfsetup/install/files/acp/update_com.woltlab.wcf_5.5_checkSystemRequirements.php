ad01a4736689a08626bb3be7a621007c617b4f31
Tim Düsterhus <duesterhus@woltlab.com>
2022-05-11 14:02:15 +0200

    Add update_com.woltlab.wcf_5.5_checkSystemRequirements.php
    see 48b47a4a8ba0260d52226c80063ebac081fa719b

---
+++ wcfsetup/install/files/acp/update_com.woltlab.wcf_5.5_checkSystemRequirements.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Checks the increased system requirements.
+ *
+ * @author  Tim Duesterhus
+ * @copyright   2001-2022 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package WoltLabSuite\Core
+ */
+
+use wcf\system\WCF;
+
+$phpVersion = \PHP_VERSION;
+$neededPhpVersion = '8.1.2';
+if (!\version_compare($phpVersion, $neededPhpVersion, '>=')) {
+    if (WCF::getLanguage()->getFixedLanguageCode() === 'de') {
+        $message = "Ihre PHP-Version '{$phpVersion}' ist unzureichend f&uuml;r die Installation dieser Software. PHP-Version {$neededPhpVersion} oder h&ouml;her wird ben&ouml;tigt.";
+    } else {
+        $message = "Your PHP version '{$phpVersion}' is insufficient for installation of this software. PHP version {$neededPhpVersion} or greater is required.";
+    }
+
+    throw new \RuntimeException($message);
+}
