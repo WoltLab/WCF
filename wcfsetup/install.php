e41dfd007b12baed65ab7679fb679e53bcd2adf5
Tim Düsterhus <duesterhus@woltlab.com>
2022-05-17 17:23:55 +0200

    Always use install.php's __DIR__ as the temporary directory
    The previous implementation of `BasicFileUtil::getTempFolder()` was unstable,
    as it automatically detected `tmp/` directories in the document root. If
    WoltLab Suite Core is installed into the document root, then the extraction of
    the files will implicitly create a `tmp/` directory and thus change the temp
    folder for the remaining installation.

--- wcfsetup/install.php
+++ wcfsetup/install.php
@@ -614,63 +614,12 @@ class BasicFileUtil {
 	 * @var	int
 	 */
 	protected static $mode = null;
-	
-	/**
-	 * Tries to find the temp folder.
-	 */
-	public static function getTempFolder(): string {
-		// use tmp folder in document root by default
-		if (!empty($_SERVER['DOCUMENT_ROOT'])) {
-			if (strpos($_SERVER['DOCUMENT_ROOT'], 'strato') !== false) {
-				// strato bugfix
-				// create tmp folder in document root automatically
-				if (!@file_exists($_SERVER['DOCUMENT_ROOT'].'/tmp')) {
-					@mkdir($_SERVER['DOCUMENT_ROOT'].'/tmp/', 0777);
-					try {
-						self::makeWritable($_SERVER['DOCUMENT_ROOT'].'/tmp/');
-					}
-					catch (\Exception $e) {}
-				}
-			}
-			if (@file_exists($_SERVER['DOCUMENT_ROOT'].'/tmp') && @is_writable($_SERVER['DOCUMENT_ROOT'].'/tmp')) {
-				return $_SERVER['DOCUMENT_ROOT'].'/tmp/';
-			}
-		}
-		
-		if (isset($_ENV['TMP']) && @is_writable($_ENV['TMP'])) {
-			return $_ENV['TMP'] . '/';
-		}
-		if (isset($_ENV['TEMP']) && @is_writable($_ENV['TEMP'])) {
-			return $_ENV['TEMP'] . '/';
-		}
-		if (isset($_ENV['TMPDIR']) && @is_writable($_ENV['TMPDIR'])) {
-			return $_ENV['TMPDIR'] . '/';
-		}
-		
-		if (($path = ini_get('upload_tmp_dir')) && @is_writable($path)) {
-			return $path . '/';
-		}
-		if (@file_exists('/tmp/') && @is_writable('/tmp/')) {
-			return '/tmp/';
-		}
-		if (function_exists('session_save_path') && ($path = session_save_path()) && @is_writable($path)) {
-			return $path . '/';
-		}
-		
-		$path = INSTALL_SCRIPT_DIR.'tmp/';
-		if (@file_exists($path) && @is_writable($path)) {
-			return $path;
-		}
-		else {
-			throw new \Exception('There is no access to the system temporary folder due to an unknown reason and no user specific temporary folder exists in '.INSTALL_SCRIPT_DIR.'! This is a misconfiguration of your webserver software! Please create a folder called '.$path.' using your favorite ftp program, make it writable and then retry this installation.');
-		}
-	}
-	
+
 	/**
 	 * Returns the temp folder for the installation.
 	 */
 	public static function getInstallTempFolder(): string {
-		$dir = self::getTempFolder() . TMP_FILE_PREFIX . '/';
+		$dir = __DIR__ . '/WCFSetup-' . TMP_FILE_PREFIX . '/';
 		@mkdir($dir);
 		self::makeWritable($dir);
 		
