9769dffe2242c09e64d547203cc4ee4176125e36
Tim Düsterhus <duesterhus@woltlab.com>
2022-05-27 15:09:54 +0200

    Deprecate RequestHandler::inRescueMode()
    This method was unable to return anything except true.

--- wcfsetup/install/files/lib/system/request/RequestHandler.class.php
+++ wcfsetup/install/files/lib/system/request/RequestHandler.class.php
@@ -39,12 +39,6 @@ final class RequestHandler extends SingletonFactory
     protected $activeRequest;
 
     /**
-     * true, if current domain mismatch any known domain
-     * @var bool
-     */
-    protected $inRescueMode = false;
-
-    /**
      * indicates if the request is an acp request
      * @var bool
      */
@@ -327,12 +321,10 @@ final class RequestHandler extends SingletonFactory
     }
 
     /**
-     * Returns true, if current host mismatches any known domain.
-     *
-     * @return  bool
+     * @deprecated 5.6 - This method always returns false.
      */
     public function inRescueMode()
     {
-        return $this->inRescueMode;
+        return false;
     }
 }
