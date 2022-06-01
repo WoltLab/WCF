69d0b76213c1cf6fdf595b44564ccb0ecc4d7349
Tim Düsterhus <duesterhus@woltlab.com>
2022-06-01 15:32:00 +0200

    Update composer dependencies

--- wcfsetup/install/files/lib/system/api/composer/installed.php
+++ wcfsetup/install/files/lib/system/api/composer/installed.php
@@ -227,12 +227,12 @@
             'dev_requirement' => false,
         ),
         'scssphp/scssphp' => array(
-            'pretty_version' => 'v1.10.2',
-            'version' => '1.10.2.0',
+            'pretty_version' => 'v1.10.3',
+            'version' => '1.10.3.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../scssphp/scssphp',
             'aliases' => array(),
-            'reference' => '387f4f4abf5d99f16be16314c5ab856f81c82f46',
+            'reference' => '0f1e1516ed2412ad43e42a6a319e77624ba1f713',
             'dev_requirement' => false,
         ),
         'symfony/css-selector' => array(
