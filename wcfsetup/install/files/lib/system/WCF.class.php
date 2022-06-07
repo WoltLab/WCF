7685f6dd144baeacd03928dad0046948312e4f55
Tim Düsterhus <duesterhus@woltlab.com>
2022-06-07 12:01:01 +0200

    Clear `AVAILABLE_UPGRADE_VERSION` for 5.5
    see 15933d22a95b67d57ae794a88471098326911aba

--- wcfsetup/install/files/lib/system/WCF.class.php
+++ wcfsetup/install/files/lib/system/WCF.class.php
@@ -80,7 +80,7 @@ class WCF
      * @var ?string
      * @since 5.3
      */
-    public const AVAILABLE_UPGRADE_VERSION = '5.5';
+    public const AVAILABLE_UPGRADE_VERSION = null;
 
     /**
      * list of supported legacy API versions
