08f9e2fe8249017823185f7c1a851a9eeffbc56e
Tim Düsterhus <duesterhus@woltlab.com>
2022-06-08 11:41:51 +0200

    Create the `com.woltlab.wcf` package early during WCFSetup
    This allows us to properly specify FOREIGN KEYs for `packageID` columns.

--- wcfsetup/install/files/acp/database/update_com.woltlab.wcf_5.6.php
+++ wcfsetup/install/files/acp/database/update_com.woltlab.wcf_5.6.php
@@ -9,13 +9,38 @@
  * @package WoltLabSuite\Core
  */
 
+use wcf\system\database\table\column\NotNullInt10DatabaseTableColumn;
 use wcf\system\database\table\column\TinyintDatabaseTableColumn;
 use wcf\system\database\table\PartialDatabaseTable;
 
 return [
+    PartialDatabaseTable::create('wcf1_acp_template')
+        ->columns([
+            NotNullInt10DatabaseTableColumn::create('packageID'),
+        ]),
+    PartialDatabaseTable::create('wcf1_language_item')
+        ->columns([
+            NotNullInt10DatabaseTableColumn::create('packageID'),
+        ]),
+    PartialDatabaseTable::create('wcf1_package_installation_file_log')
+        ->columns([
+            NotNullInt10DatabaseTableColumn::create('packageID'),
+        ]),
+    PartialDatabaseTable::create('wcf1_package_installation_plugin')
+        ->columns([
+            NotNullInt10DatabaseTableColumn::create('packageID'),
+        ]),
+    PartialDatabaseTable::create('wcf1_package_installation_sql_log')
+        ->columns([
+            NotNullInt10DatabaseTableColumn::create('packageID'),
+        ]),
     PartialDatabaseTable::create('wcf1_page')
         ->columns([
             TinyintDatabaseTableColumn::create('isLandingPage')
                 ->drop(),
+        ]),
+    PartialDatabaseTable::create('wcf1_user_group_option')
+        ->columns([
+            NotNullInt10DatabaseTableColumn::create('packageID'),
         ]),
 ];
--- wcfsetup/install/files/acp/install.php
+++ wcfsetup/install/files/acp/install.php
@@ -20,41 +20,6 @@ $statement->execute([1]);
 // Clear any outdated cached data from WCFSetup.
 UserStorageHandler::getInstance()->clear();
 
-// update acp templates
-$sql = "UPDATE  wcf" . WCF_N . "_acp_template
-        SET     packageID = ?";
-$statement = WCF::getDB()->prepareStatement($sql);
-$statement->execute([1]);
-
-// update language
-$sql = "UPDATE  wcf" . WCF_N . "_language_item
-        SET     packageID = ?";
-$statement = WCF::getDB()->prepareStatement($sql);
-$statement->execute([1]);
-
-// update installation logs
-$sql = "UPDATE  wcf" . WCF_N . "_package_installation_file_log
-        SET     packageID = ?";
-$statement = WCF::getDB()->prepareStatement($sql);
-$statement->execute([1]);
-
-$sql = "UPDATE  wcf" . WCF_N . "_package_installation_sql_log
-        SET     packageID = ?";
-$statement = WCF::getDB()->prepareStatement($sql);
-$statement->execute([1]);
-
-// update pips
-$sql = "UPDATE  wcf" . WCF_N . "_package_installation_plugin
-        SET     packageID = ?";
-$statement = WCF::getDB()->prepareStatement($sql);
-$statement->execute([1]);
-
-// group options
-$sql = "UPDATE  wcf" . WCF_N . "_user_group_option
-        SET     packageID = ?";
-$statement = WCF::getDB()->prepareStatement($sql);
-$statement->execute([1]);
-
 // get server timezone
 if ($timezone = @\date_default_timezone_get()) {
     if ($timezone != 'Europe/London' && \in_array($timezone, DateUtil::getAvailableTimezones())) {
