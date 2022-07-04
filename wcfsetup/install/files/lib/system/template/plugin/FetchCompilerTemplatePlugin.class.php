2a5ce139d53d3ec7232013d4492d6e322b262cfb
Tim Düsterhus <duesterhus@woltlab.com>
2022-07-04 16:27:39 +0200

    Deprecate the `{fetch}` template plugin

--- wcfsetup/install/files/lib/system/template/plugin/FetchCompilerTemplatePlugin.class.php
+++ wcfsetup/install/files/lib/system/template/plugin/FetchCompilerTemplatePlugin.class.php
@@ -17,6 +17,7 @@ use wcf\system\template\TemplateScriptingCompiler;
  * @copyright   2001-2019 WoltLab GmbH
  * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package WoltLabSuite\Core\System\Template\Plugin
+ * @deprecated 5.4 The {fetch} plugin allows reading arbitrary files for anyone capable of editing templates.
  */
 class FetchCompilerTemplatePlugin implements ICompilerTemplatePlugin
 {
@@ -25,6 +26,12 @@ class FetchCompilerTemplatePlugin implements ICompilerTemplatePlugin
      */
     public function executeStart($tagArgs, TemplateScriptingCompiler $compiler)
     {
+        if (\ENABLE_ENTERPRISE_MODE) {
+            throw new SystemException(
+                'The {fetch} template plugin is deprecated and will be removed in a future version. It is not available in enterprise mode.'
+            );
+        }
+
         if (!isset($tagArgs['file'])) {
             throw new SystemException(
                 $compiler::formatSyntaxError(
