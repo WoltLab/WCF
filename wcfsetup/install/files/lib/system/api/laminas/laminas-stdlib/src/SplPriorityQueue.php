ed1242ea538f8159e355233c80b6bc44ae67edaf
Tim Düsterhus <duesterhus@woltlab.com>
2022-08-29 12:15:39 +0200

    Update composer dependencies

--- wcfsetup/install/files/lib/system/api/laminas/laminas-stdlib/src/SplPriorityQueue.php
+++ wcfsetup/install/files/lib/system/api/laminas/laminas-stdlib/src/SplPriorityQueue.php
@@ -24,9 +24,8 @@ use const PHP_INT_MAX;
  * Also, provides predictable heap order for datums added with the same priority
  * (i.e., they will be emitted in the same order they are enqueued).
  *
- * @psalm-type InternalPriority = array{0: mixed, 1: int}
  * @template TValue
- * @template TPriority of InternalPriority
+ * @template TPriority of int
  * @extends \SplPriorityQueue<TPriority, TValue>
  */
 class SplPriorityQueue extends \SplPriorityQueue implements Serializable
@@ -40,8 +39,8 @@ class SplPriorityQueue extends \SplPriorityQueue implements Serializable
      * Utilizes {@var $serial} to ensure that values of equal priority are
      * emitted in the same order in which they are inserted.
      *
-     * @param  TValue          $datum
-     * @param  TPriority|mixed $priority
+     * @param  TValue    $datum
+     * @param  TPriority $priority
      * @return void
      */
     public function insert($datum, $priority)
@@ -49,8 +48,6 @@ class SplPriorityQueue extends \SplPriorityQueue implements Serializable
         if (! is_array($priority)) {
             $priority = [$priority, $this->serial--];
         }
-
-        /** @psalm-var TPriority $priority */
 
         parent::insert($datum, $priority);
     }
@@ -120,7 +117,7 @@ class SplPriorityQueue extends \SplPriorityQueue implements Serializable
     /**
      * Magic method used to rebuild an instance.
      *
-     * @param array $data Data array.
+     * @param array<array-key, mixed> $data Data array.
      * @return void
      */
     public function __unserialize($data)
@@ -147,8 +144,6 @@ class SplPriorityQueue extends \SplPriorityQueue implements Serializable
             if (array_key_exists('priority', $item)) {
                 $priority = (int) $item['priority'];
             }
-
-            /** @psalm-var TValue $item['data'] */
 
             $this->insert($item['data'], $priority);
         }
