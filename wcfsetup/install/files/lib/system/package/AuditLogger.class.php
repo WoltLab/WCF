80939ae824d2a422b79dce37f8b3173baf215e0a
Tim Düsterhus <duesterhus@woltlab.com>
2022-09-20 16:00:31 +0200

    Add AuditLogger

---
+++ wcfsetup/install/files/lib/system/package/AuditLogger.class.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace wcf\system\package;
+
+use wcf\system\WCF;
+
+/**
+ * Manages audit logging (wcf1_package_audit_log) for the package system.
+ *
+ * @author  Tim Duesterhus
+ * @copyright   2001-2020 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package WoltLabSuite\Core\System\Package
+ * @since   6.0
+ */
+final class AuditLogger
+{
+    /**
+     * Logs the given $payload with the current time.
+     */
+    public function log(string $payload): void
+    {
+        $time = new \DateTimeImmutable('now', new \DateTimeZone('UTC'));
+
+        $sql = "INSERT INTO wcf1_package_audit_log
+                            (time, wcfVersion, payload)
+                VALUES      (?, ?, ?)";
+        $statement = WCF::getDB()->prepare($sql);
+        $statement->execute([
+            $time->format('Y-m-d\TH:i:s.uP'),
+            \WCF_VERSION,
+            $payload,
+        ]);
+    }
+}
