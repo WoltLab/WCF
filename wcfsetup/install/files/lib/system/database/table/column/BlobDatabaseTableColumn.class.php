5c602cad539f431de25d2e5edb43fe9c6f5cdee4
Tim Düsterhus <duesterhus@woltlab.com>
2022-09-20 16:44:49 +0200

    Stop using TUnsupportedDefaultValue

--- wcfsetup/install/files/lib/system/database/table/column/BlobDatabaseTableColumn.class.php
+++ wcfsetup/install/files/lib/system/database/table/column/BlobDatabaseTableColumn.class.php
@@ -13,8 +13,6 @@ namespace wcf\system\database\table\column;
  */
 class BlobDatabaseTableColumn extends AbstractDatabaseTableColumn
 {
-    use TUnsupportedDefaultValue;
-
     /**
      * @inheritDoc
      */
