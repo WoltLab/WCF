f681051378de466118dcc116d9f58f95f9cc547e
Alexander Ebert <ebert@woltlab.com>
2022-09-30 17:19:18 +0200

    Add a delete confirmation with custom form elements

--- ts/WoltLabSuite/Core/Component/Confirmation/Delete.ts
+++ ts/WoltLabSuite/Core/Component/Confirmation/Delete.ts
@@ -1,5 +1,12 @@
 import { dialogFactory } from "../Dialog";
 import * as Language from "../../Language";
+import WoltlabCoreDialogElement from "../../Element/woltlab-core-dialog";
+
+type CallbackWithFormElements = (dialog: WoltlabCoreDialogElement) => void;
+type ResultWithFormElements = {
+  result: boolean;
+  dialog: WoltlabCoreDialogElement;
+};
 
 export class ConfirmationDelete {
   readonly #question: string;
@@ -15,15 +22,51 @@ export class ConfirmationDelete {
   }
 
   async message(message: string): Promise<boolean> {
+    if (message.trim() === "") {
+      throw new Error(
+        "An empty message for the delete confirmation was provided. Please use `defaultMessage()` if you do not want to provide a  custom message.",
+      );
+    }
+
     const dialog = dialogFactory()
       .fromHtml(`<p>${message}</p>`)
-      .asConfirmation({ primary: Language.get("wcf.dialog.button.primary.delete") });
+      .asConfirmation({
+        primary: Language.get("wcf.dialog.button.primary.delete"),
+      });
 
     dialog.show(this.#question);
 
     return new Promise<boolean>((resolve) => {
       dialog.addEventListener("primary", () => resolve(true));
       dialog.addEventListener("cancel", () => resolve(false));
+    });
+  }
+
+  async withFormElements(callback: CallbackWithFormElements): Promise<ResultWithFormElements> {
+    const dialog = dialogFactory()
+      .withoutContent()
+      .asConfirmation({
+        primary: Language.get("wcf.dialog.button.primary.delete"),
+      });
+
+    callback(dialog);
+
+    dialog.show(this.#question);
+
+    return new Promise<ResultWithFormElements>((resolve) => {
+      dialog.addEventListener("primary", () => {
+        resolve({
+          result: true,
+          dialog,
+        });
+      });
+
+      dialog.addEventListener("cancel", () => {
+        resolve({
+          result: false,
+          dialog,
+        });
+      });
     });
   }
 }
--- wcfsetup/install/files/js/WoltLabSuite/Core/Component/Confirmation/Delete.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Component/Confirmation/Delete.js
@@ -13,13 +13,41 @@ define(["require", "exports", "tslib", "../Dialog", "../../Language"], function 
             return this.message(message);
         }
         async message(message) {
+            if (message.trim() === "") {
+                throw new Error("An empty message for the delete confirmation was provided. Please use `defaultMessage()` if you do not want to provide a  custom message.");
+            }
             const dialog = (0, Dialog_1.dialogFactory)()
                 .fromHtml(`<p>${message}</p>`)
-                .asConfirmation({ primary: Language.get("wcf.dialog.button.primary.delete") });
+                .asConfirmation({
+                primary: Language.get("wcf.dialog.button.primary.delete"),
+            });
             dialog.show(this.#question);
             return new Promise((resolve) => {
                 dialog.addEventListener("primary", () => resolve(true));
                 dialog.addEventListener("cancel", () => resolve(false));
+            });
+        }
+        async withFormElements(callback) {
+            const dialog = (0, Dialog_1.dialogFactory)()
+                .withoutContent()
+                .asConfirmation({
+                primary: Language.get("wcf.dialog.button.primary.delete"),
+            });
+            callback(dialog);
+            dialog.show(this.#question);
+            return new Promise((resolve) => {
+                dialog.addEventListener("primary", () => {
+                    resolve({
+                        result: true,
+                        dialog,
+                    });
+                });
+                dialog.addEventListener("cancel", () => {
+                    resolve({
+                        result: false,
+                        dialog,
+                    });
+                });
             });
         }
     }
