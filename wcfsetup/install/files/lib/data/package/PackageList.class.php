2347b63303fd564266d4096c583d4ecad2e3d33f
Tim Düsterhus <duesterhus@woltlab.com>
2022-10-19 13:58:15 +0200

    Add PackageList::getTopologicallySortedPackages()

--- wcfsetup/install/files/lib/data/package/PackageList.class.php
+++ wcfsetup/install/files/lib/data/package/PackageList.class.php
@@ -3,6 +3,7 @@
 namespace wcf\data\package;
 
 use wcf\data\DatabaseObjectList;
+use wcf\system\WCF;
 
 /**
  * Represents a list of packages.
@@ -24,4 +25,55 @@ class PackageList extends DatabaseObjectList
      * @inheritDoc
      */
     public $className = Package::class;
+
+    /**
+     * Returns a topologically list of all installed packages.
+     *
+     * Packages listed in a later "group" of the outer array depend on at least
+     * one package in an earlier group, but do not depend on any package within
+     * the same or later group.
+     *
+     * @return Package[][]
+     * @since 6.0
+     */
+    public static function getTopologicallySortedPackages(): array
+    {
+        $list = new self();
+        $list->readObjects();
+        $pending = $list->getObjects();
+
+        $sql = "SELECT  packageID, requirement
+                FROM    wcf1_package_requirement";
+        $statement = WCF::getDB()->prepare($sql);
+        $statement->execute();
+        $requirementMap = $statement->fetchMap('packageID', 'requirement', false);
+
+        $result = [];
+        $handled = [];
+
+        while ($pending !== []) {
+            $newResult = [];
+            $newPending = [];
+
+            foreach ($pending as $package) {
+                $allFulfilled = \array_diff(
+                    $requirementMap[$package->packageID] ?? [],
+                    $handled
+                ) === [];
+
+                if ($allFulfilled) {
+                    $newResult[] = $package;
+                } else {
+                    $newPending[] = $package;
+                }
+            }
+
+            \array_push($handled, ...\array_column($newResult, 'packageID'));
+
+            $pending = $newPending;
+            $result[] = $newResult;
+        }
+
+        return $result;
+    }
 }
