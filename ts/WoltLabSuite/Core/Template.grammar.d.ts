99cc897531f2b0f5b40f3940fc32efd83cb33453
Tim Düsterhus <duesterhus@woltlab.com>
2022-10-21 10:52:42 +0200

    Add missing module comments to `.d.ts` files

--- ts/WoltLabSuite/Core/Template.grammar.d.ts
+++ ts/WoltLabSuite/Core/Template.grammar.d.ts
@@ -1,1 +1,10 @@
+/**
+ * TypeScript definition for the parser generated from Template.grammar.jison.
+ *
+ * @author	Tim Duesterhus
+ * @copyright	2001-2022 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @module	WoltLabSuite/Core/Template.grammar
+ */
+
 export function parse(input: string): unknown;
