WoltLab/WCF

commit 5590bc1425b03e1f8d91610b7d3c52ccccb7d338

Use `PackageArchive::getUpdateInstructionsFor()` in PackageValidationArchive
This removes the only user of `PackageArchive::isValidUpdate()` which will be
removed in a follow-up commit.

wcfsetup/install/files/lib/system/package/validation/PackageValidationArchive.class.php
@@ -233,7 +233,8 @@ final class PackageValidationArchive implements \RecursiveIterator
             }
         } else {
             // package is already installed, check update path
-            if (!$this->archive->isValidUpdate($package)) {
+            $instructions = $this->archive->getUpdateInstructionsFor($package);
+            if ($instructions === null) {
                 $deliveredPackageVersion = $this->archive->getPackageInfo('version');
 
                 // check if the package is already installed with the same exact version
@@ -252,7 +253,7 @@ final class PackageValidationArchive implements \RecursiveIterator
             }
 
             if ($validationMode === PackageValidationManager::VALIDATION_RECURSIVE) {
-                $this->validatePackageInstallationPlugins('update', $this->archive->getUpdateInstructions());
+                $this->validatePackageInstallationPlugins('update', $instructions);
             }
         }
     }

commit 3aee45351c1c5584da3864635b0cfa25225ff3dc

Fix downgrade prevention in PackageValidationArchive
This regressed in 5590bc1425b03e1f8d91610b7d3c52ccccb7d338, because
`PackageArchive::isValidUpdate()` checked not just the existence of the
instructions, but also the version numbers.

In practice this regression is only visible for wildcard update instructions,
because otherwise a valid instruction will simply not exist.

wcfsetup/install/files/lib/system/package/validation/PackageValidationArchive.class.php
@@ -233,23 +233,21 @@ final class PackageValidationArchive implements \RecursiveIterator
             }
         } else {
             // package is already installed, check update path
+            $deliveredPackageVersion = $this->archive->getPackageInfo('version');
+            if (Package::compareVersion($package->packageVersion, $deliveredPackageVersion, '>=')) {
+                throw new PackageValidationException(PackageValidationException::ALREADY_INSTALLED, [
+                    'packageName' => $package->packageName,
+                    'packageVersion' => $package->packageVersion,
+                ]);
+            }
+
             $instructions = $this->archive->getUpdateInstructionsFor($package->packageVersion);
             if ($instructions === null) {
-                $deliveredPackageVersion = $this->archive->getPackageInfo('version');
-
-                // check if the package is already installed with the same exact version
-                if ($package->packageVersion === $deliveredPackageVersion) {
-                    throw new PackageValidationException(PackageValidationException::ALREADY_INSTALLED, [
-                        'packageName' => $package->packageName,
-                        'packageVersion' => $package->packageVersion,
-                    ]);
-                } else {
-                    throw new PackageValidationException(PackageValidationException::NO_UPDATE_PATH, [
-                        'packageName' => $package->packageName,
-                        'packageVersion' => $package->packageVersion,
-                        'deliveredPackageVersion' => $deliveredPackageVersion,
-                    ]);
-                }
+                throw new PackageValidationException(PackageValidationException::NO_UPDATE_PATH, [
+                    'packageName' => $package->packageName,
+                    'packageVersion' => $package->packageVersion,
+                    'deliveredPackageVersion' => $deliveredPackageVersion,
+                ]);
             }
 
             if ($validationMode === PackageValidationManager::VALIDATION_RECURSIVE) {