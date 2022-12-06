307d9e3811df36f9de656e3d6d451b757bef2186
Marcel Werk <burntime@woltlab.com>
2022-12-06 13:42:20 +0100

    Add new language variables

--- wcfsetup/install/files/lib/system/event/listener/PreloadPhrasesCollectingListener.class.php
+++ wcfsetup/install/files/lib/system/event/listener/PreloadPhrasesCollectingListener.class.php
@@ -129,7 +129,9 @@ final class PreloadPhrasesCollectingListener
 
         $event->preload('wcf.page.jumpTo');
         $event->preload('wcf.page.jumpTo.description');
+        $event->preload('wcf.page.jumpTo.pageNo');
         $event->preload('wcf.page.pagePosition');
+        $event->preload('wcf.page.pagination');
 
         $event->preload('wcf.reactions.react');
         $event->preload('wcf.reactions.summary.listReactions');
