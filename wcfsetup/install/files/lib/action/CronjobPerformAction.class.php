04078912a33afeedfd149dc501344cc8c5532be4
Tim Düsterhus <duesterhus@woltlab.com>
2022-12-08 14:27:52 +0100

    Add CronjobPerformAction
    see #5150

---
+++ wcfsetup/install/files/lib/action/CronjobPerformAction.class.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace wcf\action;
+
+use Laminas\Diactoros\Response\EmptyResponse;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use wcf\data\user\User;
+use wcf\system\cronjob\CronjobScheduler;
+use wcf\system\WCF;
+
+/**
+ * Performs pending cronjobs.
+ *
+ * @author  Tim Duesterhus
+ * @copyright   2001-2022 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package WoltLabSuite\Core\Action
+ * @since   6.0
+ */
+final class CronjobPerformAction implements RequestHandlerInterface
+{
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        CronjobScheduler::getInstance()->executeCronjobs();
+
+        WCF::getSession()->disableUpdate();
+        WCF::getSession()->deleteIfNew();
+
+        return new EmptyResponse();
+    }
+}
