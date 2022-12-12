2f286a3f6c53a67cf1fd9750f32d7e370cc2e491
Tim Düsterhus <duesterhus@woltlab.com>
2022-12-12 14:53:03 +0100

    Stop implementing IArgumentedCLICommand in WorkerCLICommand

--- wcfsetup/install/files/lib/system/cli/command/WorkerCLICommand.class.php
+++ wcfsetup/install/files/lib/system/cli/command/WorkerCLICommand.class.php
@@ -24,7 +24,7 @@ use Zend\Console\Getopt as ArgvParser;
  * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package WoltLabSuite\Core\System\Cli\Command
  */
-class WorkerCLICommand implements IArgumentedCLICommand
+class WorkerCLICommand implements ICLICommand
 {
     /**
      * arguments parser
