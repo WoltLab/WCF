cb100fec5b63213bdd61ea714622360b573f5ea6
Tim Düsterhus <duesterhus@woltlab.com>
2022-12-15 10:40:36 +0100

    Replace `empty()` check by check against `''` in DynamicRequestRoute::buildRoute()

--- wcfsetup/install/files/lib/system/request/route/DynamicRequestRoute.class.php
+++ wcfsetup/install/files/lib/system/request/route/DynamicRequestRoute.class.php
@@ -220,13 +220,13 @@ class DynamicRequestRoute implements IRequestRoute
                 }
             }
 
-            if (!empty($link) && $lastSeparator !== null) {
+            if ($link !== '' && $lastSeparator !== null) {
                 $link .= $lastSeparator;
             }
         }
 
         if ($this->isACP() || !URL_OMIT_INDEX_PHP) {
-            if (!empty($link)) {
+            if ($link !== '') {
                 $link = 'index.php?' . $link;
             }
         }
