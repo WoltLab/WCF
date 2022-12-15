9fd5b3f8dda54cb011cdf758507180eca4487fd4
Tim Düsterhus <duesterhus@woltlab.com>
2022-12-15 14:27:38 +0100

    Recreate the wcf1_like.reactionTypeID foreign key
    Resolves #5019
    Depends on #5190

---
+++ wcfsetup/install/files/acp/database/update_com.woltlab.wcf_6.0_reaction_fk_step1.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Drops the reactionTypeID foreign key to later recreate it with the correct name.
+ *
+ * @author Tim Duesterhus
+ * @copyright 2001-2022 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package WoltLabSuite\Core
+ */
+
+use wcf\system\database\table\index\DatabaseTableForeignKey;
+use wcf\system\database\table\PartialDatabaseTable;
+
+return [
+    PartialDatabaseTable::create('wcf1_like')
+        ->foreignKeys([
+            DatabaseTableForeignKey::create()
+                ->columns(['reactionTypeID'])
+                ->referencedTable('wcf1_reaction_type')
+                ->referencedColumns(['reactionTypeID'])
+                ->onDelete('CASCADE')
+                ->drop(),
+        ]),
+];
