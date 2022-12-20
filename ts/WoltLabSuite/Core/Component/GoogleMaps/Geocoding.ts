f48d7e14ea56e1764a785d7504e760ea29f008c9
Marcel Werk <burntime@woltlab.com>
2022-12-20 18:32:39 +0100

    Add handling for moved markers and clicked suggestions

--- ts/WoltLabSuite/Core/Component/GoogleMaps/Geocoding.ts
+++ ts/WoltLabSuite/Core/Component/GoogleMaps/Geocoding.ts
@@ -15,10 +15,33 @@ export function setup(): void {
       void geocoder.geocode({ location: marker.getPosition() }, (results, status) => {
         if (status === google.maps.GeocoderStatus.OK) {
           element.value = results![0].formatted_address;
+          setLocation(results![0].geometry.location.lat(), results![0].geometry.location.lng());
         }
       });
     });
 
-    setupSuggestion(element, geocoder);
+    function moveMarker(address: string): void {
+      void geocoder.geocode({ address }, (results, status) => {
+        if (status === google.maps.GeocoderStatus.OK) {
+          marker.setPosition(results![0].geometry.location);
+          (marker.getMap() as google.maps.Map).setCenter(marker.getPosition()!);
+          setLocation(results![0].geometry.location.lat(), results![0].geometry.location.lng());
+        }
+      });
+    }
+
+    function setLocation(lat: number, lng: number): void {
+      element.dataset.googleMapsLat = lat.toString();
+      element.dataset.googleMapsLng = lng.toString();
+    }
+
+    if (element.value) {
+      moveMarker(element.value);
+    }
+
+    setupSuggestion(element, geocoder, (item: HTMLElement) => {
+      moveMarker(item.dataset.label!);
+      return true;
+    });
   });
 }
--- wcfsetup/install/files/js/WoltLabSuite/Core/Component/GoogleMaps/Geocoding.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Component/GoogleMaps/Geocoding.js
@@ -11,10 +11,30 @@ define(["require", "exports", "../../Helper/Selector", "./Geocoding/Suggestion",
                 void geocoder.geocode({ location: marker.getPosition() }, (results, status) => {
                     if (status === google.maps.GeocoderStatus.OK) {
                         element.value = results[0].formatted_address;
+                        setLocation(results[0].geometry.location.lat(), results[0].geometry.location.lng());
                     }
                 });
             });
-            (0, Suggestion_1.setup)(element, geocoder);
+            function moveMarker(address) {
+                void geocoder.geocode({ address }, (results, status) => {
+                    if (status === google.maps.GeocoderStatus.OK) {
+                        marker.setPosition(results[0].geometry.location);
+                        marker.getMap().setCenter(marker.getPosition());
+                        setLocation(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+                    }
+                });
+            }
+            function setLocation(lat, lng) {
+                element.dataset.googleMapsLat = lat.toString();
+                element.dataset.googleMapsLng = lng.toString();
+            }
+            if (element.value) {
+                moveMarker(element.value);
+            }
+            (0, Suggestion_1.setup)(element, geocoder, (item) => {
+                moveMarker(item.dataset.label);
+                return true;
+            });
         });
     }
     exports.setup = setup;
