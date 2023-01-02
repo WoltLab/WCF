e33358308f882f87ea5d580eb011e71ce5b3bb87
Marcel Werk <burntime@woltlab.com>
2023-01-02 17:49:17 +0100

    Add component for guest dialog handling

---
+++ ts/WoltLabSuite/Core/Component/Comment/GuestDialog.ts
@@ -0,0 +1,55 @@
+import ControllerCaptcha from "../../Controller/Captcha";
+import { getPhrase } from "../../Language";
+import { dialogFactory } from "../Dialog";
+
+export function showGuestDialog(template: string): Promise<Record<string, unknown> | undefined> {
+  const captchaId = "commentAdd";
+  const dialog = dialogFactory().fromHtml(template).asPrompt();
+  dialog.show(getPhrase("wcf.global.confirmation.title"));
+
+  const usernameInput = dialog.content.querySelector("input[name=username]") as HTMLInputElement;
+  dialog.incomplete = usernameInput.value.trim() === "";
+  usernameInput.addEventListener("input", () => {
+    dialog.incomplete = usernameInput.value.trim() === "";
+  });
+
+  dialog.addEventListener("afterClose", () => {
+    if (ControllerCaptcha.has(captchaId)) {
+      ControllerCaptcha.delete(captchaId);
+    }
+  });
+
+  return new Promise((resolve) => {
+    dialog.addEventListener("primary", () => {
+      const parameters = {
+        data: {
+          username: usernameInput.value,
+        },
+      };
+
+      if (ControllerCaptcha.has(captchaId)) {
+        const data = ControllerCaptcha.getData(captchaId);
+        ControllerCaptcha.delete(captchaId);
+        if (data instanceof Promise) {
+          void data.then((data) => {
+            resolve({
+              ...parameters,
+              ...data,
+            });
+          });
+        } else {
+          resolve({
+            ...parameters,
+            ...(data as ArbitraryObject),
+          });
+        }
+      } else {
+        resolve(parameters);
+      }
+    });
+
+    dialog.addEventListener("cancel", () => {
+      resolve(undefined);
+    });
+  });
+}
---
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Component/Comment/GuestDialog.js
@@ -0,0 +1,55 @@
+define(["require", "exports", "tslib", "../../Controller/Captcha", "../../Language", "../Dialog"], function (require, exports, tslib_1, Captcha_1, Language_1, Dialog_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.showGuestDialog = void 0;
+    Captcha_1 = tslib_1.__importDefault(Captcha_1);
+    function showGuestDialog(template) {
+        const captchaId = "commentAdd";
+        const dialog = (0, Dialog_1.dialogFactory)().fromHtml(template).asPrompt();
+        dialog.show((0, Language_1.getPhrase)("wcf.global.confirmation.title"));
+        const usernameInput = dialog.content.querySelector("input[name=username]");
+        dialog.incomplete = usernameInput.value.trim() === "";
+        usernameInput.addEventListener("input", () => {
+            dialog.incomplete = usernameInput.value.trim() === "";
+        });
+        dialog.addEventListener("afterClose", () => {
+            if (Captcha_1.default.has(captchaId)) {
+                Captcha_1.default.delete(captchaId);
+            }
+        });
+        return new Promise((resolve) => {
+            dialog.addEventListener("primary", () => {
+                const parameters = {
+                    data: {
+                        username: usernameInput.value,
+                    },
+                };
+                if (Captcha_1.default.has(captchaId)) {
+                    const data = Captcha_1.default.getData(captchaId);
+                    Captcha_1.default.delete(captchaId);
+                    if (data instanceof Promise) {
+                        void data.then((data) => {
+                            resolve({
+                                ...parameters,
+                                ...data,
+                            });
+                        });
+                    }
+                    else {
+                        resolve({
+                            ...parameters,
+                            ...data,
+                        });
+                    }
+                }
+                else {
+                    resolve(parameters);
+                }
+            });
+            dialog.addEventListener("cancel", () => {
+                resolve(undefined);
+            });
+        });
+    }
+    exports.showGuestDialog = showGuestDialog;
+});
