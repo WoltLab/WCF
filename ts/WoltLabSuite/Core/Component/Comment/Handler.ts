9a1627235ec9dd026727506338b21e9b64eb8503
Marcel Werk <burntime@woltlab.com>
2023-01-02 18:28:30 +0100

    Add handling for loading more comments

--- ts/WoltLabSuite/Core/Component/Comment/Handler.ts
+++ ts/WoltLabSuite/Core/Component/Comment/Handler.ts
@@ -1,5 +1,12 @@
 import { dboAction } from "../../Ajax";
+import DomUtil from "../../Dom/Util";
+import { getPhrase } from "../../Language";
 import { CommentAdd } from "./Add";
+
+type ResponseLoadComments = {
+  lastCommentTime: number;
+  template: string;
+};
 
 class CommentHandler {
   readonly #container: HTMLElement;
@@ -8,6 +15,7 @@ class CommentHandler {
     this.#container = container;
 
     this.#initComments();
+    this.#initLoadNextComments();
     this.#initCommentAdd();
   }
 
@@ -18,6 +26,54 @@ class CommentHandler {
   }
 
   #initComments(): void {}
+
+  #initLoadNextComments(): void {
+    if (this.#displayedComments < this.#totalComments) {
+      if (!this.#container.querySelector(".commentLoadNext")) {
+        const li = document.createElement("li");
+        li.classList.add("commentLoadNext", "showMore");
+        this.#container.append(li);
+
+        const button = document.createElement("button");
+        button.type = "button";
+        button.classList.add("button", "small", "commentLoadNext__button");
+        button.textContent = getPhrase("wcf.comment.more");
+        li.append(button);
+
+        button.addEventListener("click", () => {
+          void this.#loadNextComments();
+        });
+      }
+    }
+  }
+
+  async #loadNextComments(): Promise<void> {
+    const button = this.#container.querySelector<HTMLButtonElement>(".commentLoadNext__button")!;
+    button.disabled = true;
+
+    const response = (await dboAction("loadComments", "wcf\\data\\comment\\CommentAction")
+      .payload({
+        data: {
+          objectID: this.#container.dataset.objectId,
+          objectTypeID: this.#container.dataset.objectTypeId,
+          lastCommentTime: this.#container.dataset.lastCommentTime,
+        },
+      })
+      .dispatch()) as ResponseLoadComments;
+
+    const fragment = DomUtil.createFragmentFromHtml(response.template);
+    this.#container.insertBefore(fragment, this.#container.querySelector(".commentLoadNext"));
+
+    this.#container.dataset.lastCommentTime = response.lastCommentTime.toString();
+
+    if (this.#displayedComments < this.#totalComments) {
+      button.disabled = false;
+    } else {
+      this.#container.querySelector<HTMLElement>(".commentLoadNext")!.hidden = true;
+    }
+
+    this.#initComments();
+  }
 
   #initComment(commentId: number, comment: HTMLElement) {
     /*if (this._container.data('canAdd')) {
@@ -39,7 +95,7 @@ class CommentHandler {
       });
     }
 
-    const replyButton = comment.querySelector('.jsCommentReplyButton');
+    const replyButton = comment.querySelector(".jsCommentReplyButton");
     if (replyButton) {
       replyButton.addEventListener("click", (event) => {
         //this._showAddResponse();
@@ -55,6 +111,14 @@ class CommentHandler {
     comment.dataset.isDisabled = "";
     comment.querySelector<HTMLElement>(".jsIconDisabled")!.hidden = true;
     comment.querySelector<HTMLElement>(".jsCommentEnableButton")!.hidden = true;
+  }
+
+  get #displayedComments(): number {
+    return this.#container.querySelectorAll(".comment").length;
+  }
+
+  get #totalComments(): number {
+    return parseInt(this.#container.dataset.comments!);
   }
 }
 
--- wcfsetup/install/files/js/WoltLabSuite/Core/Component/Comment/Handler.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Component/Comment/Handler.js
@@ -1,12 +1,14 @@
-define(["require", "exports", "../../Ajax", "./Add"], function (require, exports, Ajax_1, Add_1) {
+define(["require", "exports", "tslib", "../../Ajax", "../../Dom/Util", "../../Language", "./Add"], function (require, exports, tslib_1, Ajax_1, Util_1, Language_1, Add_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.setup = void 0;
+    Util_1 = tslib_1.__importDefault(Util_1);
     class CommentHandler {
         #container;
         constructor(container) {
             this.#container = container;
             this.#initComments();
+            this.#initLoadNextComments();
             this.#initCommentAdd();
         }
         #initCommentAdd() {
@@ -15,6 +17,46 @@ define(["require", "exports", "../../Ajax", "./Add"], function (require, exports
             }
         }
         #initComments() { }
+        #initLoadNextComments() {
+            if (this.#displayedComments < this.#totalComments) {
+                if (!this.#container.querySelector(".commentLoadNext")) {
+                    const li = document.createElement("li");
+                    li.classList.add("commentLoadNext", "showMore");
+                    this.#container.append(li);
+                    const button = document.createElement("button");
+                    button.type = "button";
+                    button.classList.add("button", "small", "commentLoadNext__button");
+                    button.textContent = (0, Language_1.getPhrase)("wcf.comment.more");
+                    li.append(button);
+                    button.addEventListener("click", () => {
+                        void this.#loadNextComments();
+                    });
+                }
+            }
+        }
+        async #loadNextComments() {
+            const button = this.#container.querySelector(".commentLoadNext__button");
+            button.disabled = true;
+            const response = (await (0, Ajax_1.dboAction)("loadComments", "wcf\\data\\comment\\CommentAction")
+                .payload({
+                data: {
+                    objectID: this.#container.dataset.objectId,
+                    objectTypeID: this.#container.dataset.objectTypeId,
+                    lastCommentTime: this.#container.dataset.lastCommentTime,
+                },
+            })
+                .dispatch());
+            const fragment = Util_1.default.createFragmentFromHtml(response.template);
+            this.#container.insertBefore(fragment, this.#container.querySelector(".commentLoadNext"));
+            this.#container.dataset.lastCommentTime = response.lastCommentTime.toString();
+            if (this.#displayedComments < this.#totalComments) {
+                button.disabled = false;
+            }
+            else {
+                this.#container.querySelector(".commentLoadNext").hidden = true;
+            }
+            this.#initComments();
+        }
         #initComment(commentId, comment) {
             /*if (this._container.data('canAdd')) {
                     this._initAddResponse(commentID, comment);
@@ -32,7 +74,7 @@ define(["require", "exports", "../../Ajax", "./Add"], function (require, exports
                     event.preventDefault();
                 });
             }
-            const replyButton = comment.querySelector('.jsCommentReplyButton');
+            const replyButton = comment.querySelector(".jsCommentReplyButton");
             if (replyButton) {
                 replyButton.addEventListener("click", (event) => {
                     //this._showAddResponse();
@@ -46,6 +88,12 @@ define(["require", "exports", "../../Ajax", "./Add"], function (require, exports
             comment.dataset.isDisabled = "";
             comment.querySelector(".jsIconDisabled").hidden = true;
             comment.querySelector(".jsCommentEnableButton").hidden = true;
+        }
+        get #displayedComments() {
+            return this.#container.querySelectorAll(".comment").length;
+        }
+        get #totalComments() {
+            return parseInt(this.#container.dataset.comments);
         }
     }
     function setup(elementId) {
