550ca130744aed3e2d84e9903517a269be577152
Alexander Ebert <ebert@woltlab.com>
2023-01-03 15:53:08 +0100

    Migrate from `Language.get()` to `getPhrase()`

--- ts/WoltLabSuite/Core/Component/Confirmation.ts
+++ ts/WoltLabSuite/Core/Component/Confirmation.ts
@@ -24,7 +24,7 @@
  */
 
 import { dialogFactory } from "./Dialog";
-import * as Language from "../Language";
+import { getPhrase } from "../Language";
 import * as DomUtil from "../Dom/Util";
 import { ConfirmationCustom } from "./Confirmation/Custom";
 
@@ -39,14 +39,14 @@ class ConfirmationPrefab {
   }
 
   async delete(title: string): Promise<boolean> {
-    const html = `<p>${Language.get("wcf.dialog.confirmation.cannotBeUndone")}</p>`;
+    const html = `<p>${getPhrase("wcf.dialog.confirmation.cannotBeUndone")}</p>`;
     const dialog = dialogFactory()
       .fromHtml(html)
       .asConfirmation({
-        primary: Language.get("wcf.dialog.button.primary.delete"),
+        primary: getPhrase("wcf.dialog.button.primary.delete"),
       });
 
-    const question = Language.get("wcf.dialog.confirmation.delete", { title });
+    const question = getPhrase("wcf.dialog.confirmation.delete", { title });
     dialog.show(question);
 
     return new Promise<boolean>((resolve) => {
@@ -58,7 +58,7 @@ class ConfirmationPrefab {
   async restore(title: string): Promise<boolean> {
     const dialog = dialogFactory().withoutContent().asConfirmation();
 
-    const question = Language.get("wcf.dialog.confirmation.restore", { title });
+    const question = getPhrase("wcf.dialog.confirmation.restore", { title });
     dialog.show(question);
 
     return new Promise<boolean>((resolve) => {
@@ -73,7 +73,7 @@ class ConfirmationPrefab {
     let reason: HTMLTextAreaElement | undefined = undefined;
     if (askForReason) {
       const id = DomUtil.getUniqueId();
-      const label = Language.get("wcf.dialog.confirmation.softDelete.reason");
+      const label = getPhrase("wcf.dialog.confirmation.softDelete.reason");
 
       const dl = document.createElement("dl");
       dl.innerHTML = `
@@ -85,7 +85,7 @@ class ConfirmationPrefab {
       dialog.content.append(dl);
     }
 
-    const question = Language.get("wcf.dialog.confirmation.softDelete", { title });
+    const question = getPhrase("wcf.dialog.confirmation.softDelete", { title });
     dialog.show(question);
 
     return new Promise<ResultSoftDelete>((resolve) => {
