d9c06ec192f27a80313e66e47da00962dd3f8b12
Alexander Ebert <ebert@woltlab.com>
2023-01-03 15:55:18 +0100

    Rebuild the JS artifacts

--- wcfsetup/install/files/js/WoltLabSuite/Core/Component/Confirmation.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Component/Confirmation.js
@@ -22,24 +22,23 @@
  * @module WoltLabSuite/Core/Component/Confirmation
  * @since 6.0
  */
-define(["require", "exports", "tslib", "./Dialog", "../Language", "../Dom/Util", "./Confirmation/Custom"], function (require, exports, tslib_1, Dialog_1, Language, DomUtil, Custom_1) {
+define(["require", "exports", "tslib", "./Dialog", "../Language", "../Dom/Util", "./Confirmation/Custom"], function (require, exports, tslib_1, Dialog_1, Language_1, DomUtil, Custom_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.confirmationFactory = void 0;
-    Language = tslib_1.__importStar(Language);
     DomUtil = tslib_1.__importStar(DomUtil);
     class ConfirmationPrefab {
         custom(question) {
             return new Custom_1.ConfirmationCustom(question);
         }
         async delete(title) {
-            const html = `<p>${Language.get("wcf.dialog.confirmation.cannotBeUndone")}</p>`;
+            const html = `<p>${(0, Language_1.getPhrase)("wcf.dialog.confirmation.cannotBeUndone")}</p>`;
             const dialog = (0, Dialog_1.dialogFactory)()
                 .fromHtml(html)
                 .asConfirmation({
-                primary: Language.get("wcf.dialog.button.primary.delete"),
+                primary: (0, Language_1.getPhrase)("wcf.dialog.button.primary.delete"),
             });
-            const question = Language.get("wcf.dialog.confirmation.delete", { title });
+            const question = (0, Language_1.getPhrase)("wcf.dialog.confirmation.delete", { title });
             dialog.show(question);
             return new Promise((resolve) => {
                 dialog.addEventListener("primary", () => resolve(true));
@@ -48,7 +47,7 @@ define(["require", "exports", "tslib", "./Dialog", "../Language", "../Dom/Util",
         }
         async restore(title) {
             const dialog = (0, Dialog_1.dialogFactory)().withoutContent().asConfirmation();
-            const question = Language.get("wcf.dialog.confirmation.restore", { title });
+            const question = (0, Language_1.getPhrase)("wcf.dialog.confirmation.restore", { title });
             dialog.show(question);
             return new Promise((resolve) => {
                 dialog.addEventListener("primary", () => resolve(true));
@@ -60,7 +59,7 @@ define(["require", "exports", "tslib", "./Dialog", "../Language", "../Dom/Util",
             let reason = undefined;
             if (askForReason) {
                 const id = DomUtil.getUniqueId();
-                const label = Language.get("wcf.dialog.confirmation.softDelete.reason");
+                const label = (0, Language_1.getPhrase)("wcf.dialog.confirmation.softDelete.reason");
                 const dl = document.createElement("dl");
                 dl.innerHTML = `
         <dt><label for="${id}">${label}</label></dt>
@@ -69,7 +68,7 @@ define(["require", "exports", "tslib", "./Dialog", "../Language", "../Dom/Util",
                 reason = dl.querySelector("textarea");
                 dialog.content.append(dl);
             }
-            const question = Language.get("wcf.dialog.confirmation.softDelete", { title });
+            const question = (0, Language_1.getPhrase)("wcf.dialog.confirmation.softDelete", { title });
             dialog.show(question);
             return new Promise((resolve) => {
                 dialog.addEventListener("primary", () => {
