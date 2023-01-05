2ab4f68748237ca190819f14d282ec6046eda720
Alexander Ebert <ebert@woltlab.com>
2023-01-05 19:04:08 +0100

    Add the `<woltlab-core-loading-indicator>` component

--- ts/WoltLabSuite/WebComponent/index.ts
+++ ts/WoltLabSuite/WebComponent/index.ts
@@ -8,5 +8,6 @@ import "./fa-metadata.js";
 import "./fa-brand.ts";
 import "./fa-icon.ts";
 import "./woltlab-core-date-time.ts";
+import "./woltlab-core-loading-indicator";
 import "./woltlab-core-pagination.ts";
 import "./woltlab-core-reaction-summary.ts";
--- wcfsetup/install/files/js/WoltLabSuite/WebComponent.js
+++ wcfsetup/install/files/js/WoltLabSuite/WebComponent.js
@@ -1261,6 +1261,79 @@
     });
   }
 
+  // ts/WoltLabSuite/WebComponent/woltlab-core-loading-indicator.ts
+  {
+    const DefaultIconSize = 24;
+    const ValidIconSizes = [24, 48, 96];
+    class WoltlabCoreLoadingIndicatorElement extends HTMLElement {
+      #icon;
+      #text;
+      connectedCallback() {
+        if (this.#icon === void 0) {
+          this.#createDom();
+        }
+      }
+      attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "size") {
+          const newSize = parseInt(newValue || "");
+          if (!ValidIconSizes.includes(newSize)) {
+            let newValue2 = parseInt(oldValue || "");
+            if (!ValidIconSizes.includes(newValue2)) {
+              newValue2 = DefaultIconSize;
+            }
+            this.setAttribute(name, newValue2.toString());
+          }
+        }
+      }
+      #createDom() {
+        this.classList.add("loading-indicator");
+        if (!this.hasAttribute("size")) {
+          this.setAttribute("size", DefaultIconSize.toString());
+        }
+        this.#icon = document.createElement("fa-icon");
+        this.#icon.size = this.size;
+        this.#icon.setIcon("spinner");
+        this.#text = document.createElement("span");
+        this.#text.classList.add("loading-indicator__text");
+        this.#text.textContent = window.WoltLabLanguage.getPhrase("wcf.global.loading");
+        this.#text.hidden = this.hideText;
+        const container = document.createElement("div");
+        container.classList.add("loading-indicator__wrapper");
+        container.append(this.#icon, this.#text);
+        this.append(container);
+      }
+      get size() {
+        return parseInt(this.getAttribute("size"));
+      }
+      set size(size) {
+        if (!ValidIconSizes.includes(size)) {
+          throw new TypeError(`The size ${size} is unrecognized, permitted values are ${ValidIconSizes.join(", ")}.`);
+        }
+        this.setAttribute("size", size.toString());
+        if (this.#icon) {
+          this.#icon.size = size;
+        }
+      }
+      get hideText() {
+        return this.hasAttribute("hide-text");
+      }
+      set hideText(hideText) {
+        if (hideText) {
+          this.setAttribute("hide-text", "");
+        } else {
+          this.removeAttribute("hide-text");
+        }
+        if (this.#text) {
+          this.#text.hidden = hideText;
+        }
+      }
+      static get observedAttributes() {
+        return ["size"];
+      }
+    }
+    window.customElements.define("woltlab-core-loading-indicator", WoltlabCoreLoadingIndicatorElement);
+  }
+
   // ts/WoltLabSuite/WebComponent/woltlab-core-pagination.ts
   {
     let mediaQuery;
@@ -1570,6 +1643,15 @@
  *
  * @author Alexander Ebert
  * @copyright 2001-2022 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @woltlabExcludeBundle all
+ */
+/**
+ * The `<woltlab-core-loading-indicator>` provides a ready-to-use
+ * widget to indicate the loading status of a component to the user.
+ *
+ * @author Alexander Ebert
+ * @copyright 2001-2023 WoltLab GmbH
  * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @woltlabExcludeBundle all
  */
