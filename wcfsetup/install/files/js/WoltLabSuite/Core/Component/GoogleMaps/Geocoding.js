d9aec613e92155a193e398420149a9da0c4122ce
Tim Düsterhus <duesterhus@woltlab.com>
2023-01-12 14:10:32 +0100

    Fix TypeScript code style

--- wcfsetup/install/files/js/WoltLabSuite/Core/Component/GoogleMaps/Geocoding.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Component/GoogleMaps/Geocoding.js
@@ -54,7 +54,7 @@ define(["require", "exports", "../../Helper/Selector", "./Geocoding/Suggestion",
             this.#element.dataset.googleMapsLat = lat.toString();
             this.#element.dataset.googleMapsLng = lng.toString();
             if (this.#element.hasAttribute("data-google-maps-geocoding-store") && this.#element.form) {
-                this.#store(this.#element.dataset.googleMapsGeocdingStore || '', lat, lng);
+                this.#store(this.#element.dataset.googleMapsGeocdingStore || "", lat, lng);
             }
         }
         #store(prefix, lat, lng) {
