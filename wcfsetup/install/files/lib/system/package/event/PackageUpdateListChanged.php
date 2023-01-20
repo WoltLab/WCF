c4a0547d8486e200b0ff549c3d5b42e3bc1afdc6
mutec <mysterycode@mysterycode.de>
2023-01-20 16:28:52 +0100

    implement `PackageUpdateListChanged`

---
+++ wcfsetup/install/files/lib/system/package/event/PackageUpdateListChanged.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace wcf\system\package\event;
+
+use wcf\data\package\update\server\PackageUpdateServer;
+use wcf\system\event\IEvent;
+
+/**
+ * Indicates that the there have been changes to the
+ * package update list.
+ *
+ * @author Florian Gail
+ * @copyright 2001-2022 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since 6.0
+ */
+final class PackageUpdateListChanged implements IEvent
+{
+    public function __construct(
+        public readonly PackageUpdateServer $updateServer,
+    ) {
+    }
+}
