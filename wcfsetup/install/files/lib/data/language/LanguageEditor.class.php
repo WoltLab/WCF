f855889a66f7b7a37e1444d210af0fcfeb657a23
mutec <mysterycode@mysterycode.de>
2023-01-31 20:55:26 +0100

    Implement `LanguageContentCopying` event
    This event is meant to give 3rd-party developers the possibility to copy i18n
    contents in every case contents should be copied to another language.
    
    [Tim: Renamed the event to match the event naming scheme]
    
    see #5252

--- wcfsetup/install/files/lib/data/language/LanguageEditor.class.php
+++ wcfsetup/install/files/lib/data/language/LanguageEditor.class.php
@@ -11,8 +11,10 @@ use wcf\data\language\item\LanguageItemList;
 use wcf\data\page\PageEditor;
 use wcf\system\cache\builder\LanguageCacheBuilder;
 use wcf\system\database\util\PreparedStatementConditionBuilder;
+use wcf\system\event\EventHandler;
 use wcf\system\exception\SystemException;
 use wcf\system\io\AtomicWriter;
+use wcf\system\language\event\LanguageContentCopying;
 use wcf\system\language\LanguageFactory;
 use wcf\system\Regex;
 use wcf\system\WCF;
@@ -1041,5 +1043,10 @@ class LanguageEditor extends DatabaseObjectEditor implements IEditableCachedObje
         }
 
         PageEditor::resetCache();
+
+        EventHandler::getInstance()->fire(new LanguageContentCopying(
+            new Language($sourceLanguageID),
+            new Language($destinationLanguageID)
+        ));
     }
 }
---
+++ wcfsetup/install/files/lib/system/language/event/LanguageContentCopying.class.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace wcf\system\language\event;
+
+use wcf\data\language\Language;
+use wcf\system\event\IEvent;
+
+/**
+ * Indicates that the contents of a language should be copied to another one.
+ *
+ * @author Florian Gail
+ * @copyright 2001-2023 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since 6.0
+ */
+final class LanguageContentCopying implements IEvent
+{
+    public function __construct(
+        public readonly Language $sourceLanguage,
+        public readonly Language $targetLanguage,
+    ) {
+    }
+}
