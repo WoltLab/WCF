8503c8946fc81038eb9ef4051166f5febfeba267
Tim Düsterhus <duesterhus@woltlab.com>
2023-02-16 11:22:44 +0100

    Mark CaptchaQuestionHandler as final

--- wcfsetup/install/files/lib/system/captcha/CaptchaQuestionHandler.class.php
+++ wcfsetup/install/files/lib/system/captcha/CaptchaQuestionHandler.class.php
@@ -12,11 +12,11 @@ use wcf\util\StringUtil;
 /**
  * Captcha handler for captcha questions.
  *
- * @author  Matthias Schmidt
- * @copyright   2001-2019 WoltLab GmbH
+ * @author  Tim Duesterhus, Matthias Schmidt
+ * @copyright   2001-2023 WoltLab GmbH
  * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
-class CaptchaQuestionHandler implements ICaptchaHandler
+final class CaptchaQuestionHandler implements ICaptchaHandler
 {
     /**
      * answer to the captcha question
