41162ef49c51c83ee1356a968f40c8fd4172a404
Alexander Ebert <ebert@woltlab.com>
2023-02-17 17:48:48 +0100

    Move the configuration generation into a class
    This is a “one-off” class, but allows us to structure the toolbar generation a bit while also preventing passing around multiple extra variables per function.

--- ts/WoltLabSuite/Core/Component/Ckeditor/Configuration.ts
+++ ts/WoltLabSuite/Core/Component/Ckeditor/Configuration.ts
@@ -6,6 +6,224 @@ import type { EditorConfig } from "@ckeditor/ckeditor5-core/src/editor/editorcon
 type ToolbarItem = string | { label: string; icon?: string; items: string[] };
 type ToolbarConfig = ToolbarItem[];
 
+class ConfigurationBuilder {
+  readonly #features: Features;
+  readonly #divider = "|";
+  readonly #removePlugins: string[] = [];
+  readonly #toolbar: ToolbarConfig = [];
+  readonly #toolbarGroups: Record<string, { icon: string; label: string }> = {};
+
+  constructor(features: Features) {
+    this.#features = features;
+  }
+
+  #setupHeading(): void {
+    if (this.#features.heading) {
+      this.#toolbar.push("heading");
+    } else {
+      this.#removePlugins.push("Heading");
+    }
+  }
+
+  #setupBasicFormat(): void {
+    this.#toolbar.push("bold", "italic");
+  }
+
+  #setupTextFormat(): void {
+    const items: string[] = [];
+    if (this.#features.underline) {
+      items.push("underline");
+    } else {
+      this.#removePlugins.push("Underline");
+    }
+
+    if (this.#features.strikethrough) {
+      items.push("strikethrough");
+    } else {
+      this.#removePlugins.push("Strikethrough");
+    }
+
+    if (this.#features.subscript) {
+      items.push("subscript");
+    } else {
+      this.#removePlugins.push("Subscript");
+    }
+
+    if (this.#features.superscript) {
+      items.push("superscript");
+    } else {
+      this.#removePlugins.push("Superscript");
+    }
+
+    if (this.#features.code) {
+      items.push("code");
+    } else {
+      this.#removePlugins.push("Code");
+    }
+
+    if (items.length > 0) {
+      this.#toolbar.push({
+        label: "woltlabToolbarGroup_format",
+        items,
+      });
+
+      this.#toolbarGroups["format"] = {
+        icon: "ellipsis;false",
+        label: getPhrase("wcf.editor.button.group.format"),
+      };
+    }
+  }
+
+  #setupList(): void {
+    if (this.#features.list) {
+      this.#toolbar.push({
+        label: "woltlabToolbarGroup_list",
+        items: ["bulletedList", "numberedList"],
+      });
+
+      this.#toolbarGroups["list"] = {
+        icon: "list;false",
+        label: getPhrase("wcf.editor.button.group.list"),
+      };
+    } else {
+      this.#removePlugins.push("List");
+    }
+  }
+
+  #setupAlignment(): void {
+    if (this.#features.alignment) {
+      this.#toolbar.push("alignment");
+    } else {
+      this.#removePlugins.push("Alignment");
+    }
+  }
+
+  #setupLink(): void {
+    if (this.#features.link) {
+      this.#toolbar.push("link");
+    } else {
+      this.#removePlugins.push("Link", "LinkImage");
+    }
+  }
+
+  #setupImage(): void {
+    if (this.#features.image) {
+      this.#toolbar.push("insertImage");
+    } else {
+      this.#removePlugins.push("Image", "ImageInsertUI", "ImageToolbar", "ImageStyle", "ImageUpload", "ImageUploadUI");
+
+      if (this.#features.link) {
+        this.#removePlugins.push("LinkImage");
+      }
+    }
+  }
+
+  #setupBlocks(): void {
+    const items: string[] = [];
+
+    if (this.#features.table) {
+      items.push("insertTable");
+    } else {
+      this.#removePlugins.push("Table", "TableToolbar");
+    }
+
+    if (this.#features.quoteBlock) {
+      items.push("blockQuote");
+    } else {
+      this.#removePlugins.push("BlockQuote", "WoltlabBlockQuote");
+    }
+
+    if (this.#features.codeBlock) {
+      items.push("codeBlock");
+    } else {
+      this.#removePlugins.push("CodeBlock", "WoltlabCodeBlock");
+    }
+
+    if (this.#features.spoiler) {
+      items.push("spoiler");
+    } else {
+      this.#removePlugins.push("WoltlabSpoiler");
+    }
+
+    if (this.#features.html) {
+      items.push("htmlEmbed");
+    } else {
+      this.#removePlugins.push("HtmlEmbed");
+    }
+
+    if (this.#features.media) {
+      items.push("woltlabBbcode_media");
+    } else {
+      this.#removePlugins.push("WoltlabMedia");
+    }
+
+    if (items.length > 0) {
+      this.#toolbar.push({
+        label: getPhrase("wcf.editor.button.group.block"),
+        icon: "plus",
+        items,
+      });
+    }
+  }
+
+  #insertDivider(): void {
+    this.#toolbar.push(this.#divider);
+  }
+
+  #getToolbar(): ToolbarConfig {
+    let allowDivider = false;
+    const toolbar = this.#toolbar.filter((item) => {
+      if (typeof item === "string" && item === this.#divider) {
+        if (!allowDivider) {
+          return false;
+        }
+
+        allowDivider = false;
+
+        return true;
+      }
+
+      allowDivider = true;
+
+      return true;
+    });
+
+    return toolbar;
+  }
+
+  build(): void {
+    if (this.#removePlugins.length > 0 || this.#toolbar.length > 0) {
+      throw new Error("Cannot build the configuration twice.");
+    }
+
+    this.#setupHeading();
+
+    this.#insertDivider();
+
+    this.#setupBasicFormat();
+    this.#setupTextFormat();
+
+    this.#insertDivider();
+
+    this.#setupList();
+    this.#setupAlignment();
+    this.#setupLink();
+    this.#setupImage();
+    this.#setupBlocks();
+
+    this.#insertDivider();
+  }
+
+  toConfig(): EditorConfig {
+    // TODO: The typings are both incompleted and outdated.
+    return {
+      removePlugins: this.#removePlugins,
+      toolbar: this.#getToolbar(),
+      woltlabToolbarGroup: this.#toolbarGroups,
+    } as any;
+  }
+}
+
 export type Features = {
   alignment: boolean;
   attachment: boolean;
@@ -29,172 +247,8 @@ export type Features = {
 };
 
 export function createConfigurationFor(features: Features): EditorConfig {
-  const Divider = "|";
+  const configuration = new ConfigurationBuilder(features);
+  configuration.build();
 
-  // TODO: This works, but is pretty much unreadable.
-  const removePlugins: string[] = [];
-  let toolbar: ToolbarConfig = [];
-
-  if (features.heading) {
-    toolbar.push("heading");
-  } else {
-    removePlugins.push("Heading");
-  }
-
-  toolbar.push(Divider);
-
-  toolbar.push("bold", "italic");
-
-  let items: string[] = [];
-  if (features.underline) {
-    items.push("underline");
-  } else {
-    removePlugins.push("Underline");
-  }
-
-  if (features.strikethrough) {
-    items.push("strikethrough");
-  } else {
-    removePlugins.push("Strikethrough");
-  }
-
-  if (features.subscript) {
-    items.push("subscript");
-  } else {
-    removePlugins.push("Subscript");
-  }
-
-  if (features.superscript) {
-    items.push("superscript");
-  } else {
-    removePlugins.push("Superscript");
-  }
-
-  if (features.code) {
-    items.push("code");
-  } else {
-    removePlugins.push("Code");
-  }
-
-  if (items.length > 0) {
-    toolbar.push({
-      label: "woltlabToolbarGroup_format",
-      items,
-    });
-  }
-
-  toolbar.push(Divider);
-
-  if (features.list) {
-    toolbar.push({
-      label: "woltlabToolbarGroup_list",
-      items: ["bulletedList", "numberedList"],
-    });
-  } else {
-    removePlugins.push("List");
-  }
-
-  if (features.alignment) {
-    toolbar.push("alignment");
-  } else {
-    removePlugins.push("Alignment");
-  }
-
-  if (features.link) {
-    toolbar.push("link");
-  } else {
-    removePlugins.push("Link", "LinkImage");
-  }
-
-  if (features.image) {
-    toolbar.push("insertImage");
-  } else {
-    removePlugins.push("Image", "ImageInsertUI", "ImageToolbar", "ImageStyle", "ImageUpload", "ImageUploadUI");
-
-    if (features.link) {
-      removePlugins.push("LinkImage");
-    }
-  }
-
-  items = [];
-
-  if (features.table) {
-    items.push("insertTable");
-  } else {
-    removePlugins.push("Table", "TableToolbar");
-  }
-
-  if (features.quoteBlock) {
-    items.push("blockQuote");
-  } else {
-    removePlugins.push("BlockQuote", "WoltlabBlockQuote");
-  }
-
-  if (features.codeBlock) {
-    items.push("codeBlock");
-  } else {
-    removePlugins.push("CodeBlock", "WoltlabCodeBlock");
-  }
-
-  if (features.spoiler) {
-    items.push("spoiler");
-  } else {
-    removePlugins.push("WoltlabSpoiler");
-  }
-
-  if (features.html) {
-    items.push("htmlEmbed");
-  } else {
-    removePlugins.push("HtmlEmbed");
-  }
-
-  if (features.media) {
-    items.push("woltlabBbcode_media");
-  } else {
-    removePlugins.push("WoltlabMedia");
-  }
-
-  if (items.length > 0) {
-    toolbar.push({
-      label: getPhrase("wcf.editor.button.group.block"),
-      icon: "plus",
-      items,
-    });
-  }
-
-  let allowDivider = false;
-  toolbar = toolbar.filter((item) => {
-    if (typeof item === "string" && item === Divider) {
-      if (!allowDivider) {
-        return false;
-      }
-
-      allowDivider = false;
-
-      return true;
-    }
-
-    allowDivider = true;
-
-    return true;
-  });
-
-  const woltlabToolbarGroup = {
-    format: {
-      icon: "ellipsis;false",
-      label: getPhrase("wcf.editor.button.group.format"),
-    },
-    list: {
-      icon: "list;false",
-      label: getPhrase("wcf.editor.button.group.list"),
-    },
-  };
-
-  // TODO: The typings are both outdated and incomplete.
-  const config: Record<string, unknown> = {
-    toolbar,
-    woltlabToolbarGroup,
-  };
-
-  return config as EditorConfig;
+  return configuration.toConfig();
 }
--- wcfsetup/install/files/js/WoltLabSuite/Core/Component/Ckeditor/Configuration.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Component/Ckeditor/Configuration.js
@@ -2,159 +2,204 @@ define(["require", "exports", "../../Language"], function (require, exports, Lan
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.createConfigurationFor = void 0;
-    function createConfigurationFor(features) {
-        const Divider = "|";
-        // TODO: This works, but is pretty much unreadable.
-        const removePlugins = [];
-        let toolbar = [];
-        if (features.heading) {
-            toolbar.push("heading");
+    class ConfigurationBuilder {
+        #features;
+        #divider = "|";
+        #removePlugins = [];
+        #toolbar = [];
+        #toolbarGroups = {};
+        constructor(features) {
+            this.#features = features;
         }
-        else {
-            removePlugins.push("Heading");
-        }
-        toolbar.push(Divider);
-        toolbar.push("bold", "italic");
-        let items = [];
-        if (features.underline) {
-            items.push("underline");
-        }
-        else {
-            removePlugins.push("Underline");
-        }
-        if (features.strikethrough) {
-            items.push("strikethrough");
-        }
-        else {
-            removePlugins.push("Strikethrough");
-        }
-        if (features.subscript) {
-            items.push("subscript");
-        }
-        else {
-            removePlugins.push("Subscript");
-        }
-        if (features.superscript) {
-            items.push("superscript");
-        }
-        else {
-            removePlugins.push("Superscript");
-        }
-        if (features.code) {
-            items.push("code");
-        }
-        else {
-            removePlugins.push("Code");
-        }
-        if (items.length > 0) {
-            toolbar.push({
-                label: "woltlabToolbarGroup_format",
-                items,
-            });
-        }
-        toolbar.push(Divider);
-        if (features.list) {
-            toolbar.push({
-                label: "woltlabToolbarGroup_list",
-                items: ["bulletedList", "numberedList"],
-            });
-        }
-        else {
-            removePlugins.push("List");
-        }
-        if (features.alignment) {
-            toolbar.push("alignment");
-        }
-        else {
-            removePlugins.push("Alignment");
-        }
-        if (features.link) {
-            toolbar.push("link");
-        }
-        else {
-            removePlugins.push("Link", "LinkImage");
-        }
-        if (features.image) {
-            toolbar.push("insertImage");
-        }
-        else {
-            removePlugins.push("Image", "ImageInsertUI", "ImageToolbar", "ImageStyle", "ImageUpload", "ImageUploadUI");
-            if (features.link) {
-                removePlugins.push("LinkImage");
+        #setupHeading() {
+            if (this.#features.heading) {
+                this.#toolbar.push("heading");
+            }
+            else {
+                this.#removePlugins.push("Heading");
             }
         }
-        items = [];
-        if (features.table) {
-            items.push("insertTable");
+        #setupBasicFormat() {
+            this.#toolbar.push("bold", "italic");
         }
-        else {
-            removePlugins.push("Table", "TableToolbar");
+        #setupTextFormat() {
+            const items = [];
+            if (this.#features.underline) {
+                items.push("underline");
+            }
+            else {
+                this.#removePlugins.push("Underline");
+            }
+            if (this.#features.strikethrough) {
+                items.push("strikethrough");
+            }
+            else {
+                this.#removePlugins.push("Strikethrough");
+            }
+            if (this.#features.subscript) {
+                items.push("subscript");
+            }
+            else {
+                this.#removePlugins.push("Subscript");
+            }
+            if (this.#features.superscript) {
+                items.push("superscript");
+            }
+            else {
+                this.#removePlugins.push("Superscript");
+            }
+            if (this.#features.code) {
+                items.push("code");
+            }
+            else {
+                this.#removePlugins.push("Code");
+            }
+            if (items.length > 0) {
+                this.#toolbar.push({
+                    label: "woltlabToolbarGroup_format",
+                    items,
+                });
+                this.#toolbarGroups["format"] = {
+                    icon: "ellipsis;false",
+                    label: (0, Language_1.getPhrase)("wcf.editor.button.group.format"),
+                };
+            }
         }
-        if (features.quoteBlock) {
-            items.push("blockQuote");
+        #setupList() {
+            if (this.#features.list) {
+                this.#toolbar.push({
+                    label: "woltlabToolbarGroup_list",
+                    items: ["bulletedList", "numberedList"],
+                });
+                this.#toolbarGroups["list"] = {
+                    icon: "list;false",
+                    label: (0, Language_1.getPhrase)("wcf.editor.button.group.list"),
+                };
+            }
+            else {
+                this.#removePlugins.push("List");
+            }
         }
-        else {
-            removePlugins.push("BlockQuote", "WoltlabBlockQuote");
+        #setupAlignment() {
+            if (this.#features.alignment) {
+                this.#toolbar.push("alignment");
+            }
+            else {
+                this.#removePlugins.push("Alignment");
+            }
         }
-        if (features.codeBlock) {
-            items.push("codeBlock");
+        #setupLink() {
+            if (this.#features.link) {
+                this.#toolbar.push("link");
+            }
+            else {
+                this.#removePlugins.push("Link", "LinkImage");
+            }
         }
-        else {
-            removePlugins.push("CodeBlock", "WoltlabCodeBlock");
-        }
-        if (features.spoiler) {
-            items.push("spoiler");
-        }
-        else {
-            removePlugins.push("WoltlabSpoiler");
-        }
-        if (features.html) {
-            items.push("htmlEmbed");
-        }
-        else {
-            removePlugins.push("HtmlEmbed");
-        }
-        if (features.media) {
-            items.push("woltlabBbcode_media");
-        }
-        else {
-            removePlugins.push("WoltlabMedia");
-        }
-        if (items.length > 0) {
-            toolbar.push({
-                label: (0, Language_1.getPhrase)("wcf.editor.button.group.block"),
-                icon: "plus",
-                items,
-            });
-        }
-        let allowDivider = false;
-        toolbar = toolbar.filter((item) => {
-            if (typeof item === "string" && item === Divider) {
-                if (!allowDivider) {
-                    return false;
+        #setupImage() {
+            if (this.#features.image) {
+                this.#toolbar.push("insertImage");
+            }
+            else {
+                this.#removePlugins.push("Image", "ImageInsertUI", "ImageToolbar", "ImageStyle", "ImageUpload", "ImageUploadUI");
+                if (this.#features.link) {
+                    this.#removePlugins.push("LinkImage");
                 }
-                allowDivider = false;
-                return true;
             }
-            allowDivider = true;
-            return true;
-        });
-        const woltlabToolbarGroup = {
-            format: {
-                icon: "ellipsis;false",
-                label: (0, Language_1.getPhrase)("wcf.editor.button.group.format"),
-            },
-            list: {
-                icon: "list;false",
-                label: (0, Language_1.getPhrase)("wcf.editor.button.group.list"),
-            },
-        };
-        // TODO: The typings are both outdated and incomplete.
-        const config = {
-            toolbar,
-            woltlabToolbarGroup,
-        };
-        return config;
+        }
+        #setupBlocks() {
+            const items = [];
+            if (this.#features.table) {
+                items.push("insertTable");
+            }
+            else {
+                this.#removePlugins.push("Table", "TableToolbar");
+            }
+            if (this.#features.quoteBlock) {
+                items.push("blockQuote");
+            }
+            else {
+                this.#removePlugins.push("BlockQuote", "WoltlabBlockQuote");
+            }
+            if (this.#features.codeBlock) {
+                items.push("codeBlock");
+            }
+            else {
+                this.#removePlugins.push("CodeBlock", "WoltlabCodeBlock");
+            }
+            if (this.#features.spoiler) {
+                items.push("spoiler");
+            }
+            else {
+                this.#removePlugins.push("WoltlabSpoiler");
+            }
+            if (this.#features.html) {
+                items.push("htmlEmbed");
+            }
+            else {
+                this.#removePlugins.push("HtmlEmbed");
+            }
+            if (this.#features.media) {
+                items.push("woltlabBbcode_media");
+            }
+            else {
+                this.#removePlugins.push("WoltlabMedia");
+            }
+            if (items.length > 0) {
+                this.#toolbar.push({
+                    label: (0, Language_1.getPhrase)("wcf.editor.button.group.block"),
+                    icon: "plus",
+                    items,
+                });
+            }
+        }
+        #insertDivider() {
+            this.#toolbar.push(this.#divider);
+        }
+        #getToolbar() {
+            let allowDivider = false;
+            const toolbar = this.#toolbar.filter((item) => {
+                if (typeof item === "string" && item === this.#divider) {
+                    if (!allowDivider) {
+                        return false;
+                    }
+                    allowDivider = false;
+                    return true;
+                }
+                allowDivider = true;
+                return true;
+            });
+            return toolbar;
+        }
+        build() {
+            if (this.#removePlugins.length > 0 || this.#toolbar.length > 0) {
+                throw new Error("Cannot build the configuration twice.");
+            }
+            this.#setupHeading();
+            this.#insertDivider();
+            this.#setupBasicFormat();
+            this.#setupTextFormat();
+            this.#insertDivider();
+            this.#setupList();
+            this.#setupAlignment();
+            this.#setupLink();
+            this.#setupImage();
+            this.#setupBlocks();
+            this.#insertDivider();
+        }
+        toConfig() {
+            // TODO: The typings are both incompleted and outdated.
+            return {
+                removePlugins: this.#removePlugins,
+                toolbar: this.#getToolbar(),
+                woltlabToolbarGroup: this.#toolbarGroups,
+            };
+        }
+    }
+    function createConfigurationFor(features) {
+        const configuration = new ConfigurationBuilder(features);
+        configuration.build();
+        return configuration.toConfig();
     }
     exports.createConfigurationFor = createConfigurationFor;
 });
