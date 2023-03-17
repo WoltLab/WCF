89dbcf60bdd65c424fca6e934c51931d1a4c81e2
Tim Düsterhus <duesterhus@woltlab.com>
2023-03-17 15:12:53 +0100

    Reset the CSS file in AddDarkMode command
    see 3fe37449ca14b1b8f924ca6b6f98cb5d20135988
    Fixes #5367

--- wcfsetup/install/files/lib/system/style/command/AddDarkMode.class.php
+++ wcfsetup/install/files/lib/system/style/command/AddDarkMode.class.php
@@ -5,12 +5,13 @@ namespace wcf\system\style\command;
 use wcf\data\style\Style;
 use wcf\data\style\StyleEditor;
 use wcf\system\cache\builder\StyleCacheBuilder;
+use wcf\system\style\StyleHandler;
 
 /**
  * Adds the dark color scheme to a style.
  *
  * @author Alexander Ebert
- * @copyright 2001-2022 WoltLab GmbH
+ * @copyright 2001-2023 WoltLab GmbH
  * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package WoltLabSuite\Core\System\Style\Command
  * @since 6.0
@@ -19,9 +20,13 @@ final class AddDarkMode
 {
     private readonly int $styleID;
 
+    private readonly StyleHandler $styleHandler;
+
     public function __construct(Style $style)
     {
         $this->styleID = $style->styleID;
+
+        $this->styleHandler = StyleHandler::getInstance();
     }
 
     public function __invoke(): void
@@ -31,6 +36,9 @@ final class AddDarkMode
             'hasDarkMode' => 1,
         ]);
 
+        $style = new Style($this->styleID);
+
         StyleCacheBuilder::getInstance()->reset();
+        $this->styleHandler->resetStylesheet($style);
     }
 }
