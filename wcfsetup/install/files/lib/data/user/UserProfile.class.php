WoltLab/WCF

commit 48aad6ed4800f20d3e12fcf7e9a351d8791cd90d

Replace `DateTime` by `DateTimeImmutable` for purely local variables

wcfsetup/install/files/lib/data/user/UserProfile.class.php
@@ -836,7 +836,7 @@ class UserProfile extends DatabaseObjectDecorator implements ITitledLinkObject
 
         $showYear = $this->birthdayShowYear || WCF::getSession()->getPermission('admin.general.canViewPrivateUserOptions');
 
-        $d = new \DateTime();
+        $d = new \DateTimeImmutable();
         $d->setTimezone(WCF::getUser()->getTimeZone());
         $d->setDate($birthdayYear, $month, $day);
         $dateFormat = (($showYear && $birthdayYear) ? WCF::getLanguage()->get(DateUtil::DATE_FORMAT) : \str_replace(

wcfsetup/install/files/lib/system/condition/UserBirthdayCondition.class.php
@@ -89,7 +89,7 @@ HTML;
             return false;
         }
 
-        $dateTime = new \DateTime();
+        $dateTime = new \DateTimeImmutable();
         $dateTime->setTimezone(WCF::getUser()->getTimeZone());
 
         $userIDs = UserBirthdayCache::getInstance()->getBirthdays($dateTime->format('n'), $dateTime->format('j'));

wcfsetup/install/files/lib/system/condition/TimeCondition.class.php
@@ -180,7 +180,7 @@ HTML;
 
         $startDateTime = $endDateTime = null;
         if ($this->startTime) {
-            $startDateTime = \DateTime::createFromFormat('H:i', $this->startTime);
+            $startDateTime = \DateTimeImmutable::createFromFormat('H:i', $this->startTime);
             if ($startDateTime === false) {
                 $this->errorMessages['time'] = 'wcf.date.startTime.error.invalid';
 
@@ -188,7 +188,7 @@ HTML;
             }
         }
         if ($this->endTime) {
-            $endDateTime = \DateTime::createFromFormat('H:i', $this->endTime);
+            $endDateTime = \DateTimeImmutable::createFromFormat('H:i', $this->endTime);
             if ($endDateTime === false) {
                 $this->errorMessages['time'] = 'wcf.date.endTime.error.invalid';
 
@@ -226,7 +226,7 @@ HTML;
         /** @noinspection PhpUndefinedFieldInspection */
         $startTime = $condition->startTime;
         if ($startTime) {
-            $dateTime = \DateTime::createFromFormat('H:i', $startTime, $timezone);
+            $dateTime = \DateTimeImmutable::createFromFormat('H:i', $startTime, $timezone);
             if ($dateTime->getTimestamp() > TIME_NOW) {
                 return false;
             }
@@ -235,7 +235,7 @@ HTML;
         /** @noinspection PhpUndefinedFieldInspection */
         $endTime = $condition->endTime;
         if ($endTime) {
-            $dateTime = \DateTime::createFromFormat('H:i', $endTime, $timezone);
+            $dateTime = \DateTimeImmutable::createFromFormat('H:i', $endTime, $timezone);
             if ($dateTime->getTimestamp() < TIME_NOW) {
                 return false;
             }

wcfsetup/install/files/lib/system/poll/PollManager.class.php
@@ -175,7 +175,7 @@ class PollManager extends SingletonFactory
 
         // poll data
         if (isset($postData['pollEndTime'])) {
-            $d = \DateTime::createFromFormat('Y-m-d\TH:i:sP', $postData['pollEndTime']);
+            $d = \DateTimeImmutable::createFromFormat('Y-m-d\TH:i:sP', $postData['pollEndTime']);
             $this->pollData['endTime'] = ($d !== false) ? $d->getTimestamp() : 0;
         }
 

commit 0c63a7c26150afc0a1ed33278179639b586b2d52

Fix incorrect migration to DateTimeImmutable
This was broken in 48aad6ed4800f20d3e12fcf7e9a351d8791cd90d.

Fixes #5391

wcfsetup/install/files/lib/data/user/UserProfile.class.php
@@ -836,9 +836,7 @@ class UserProfile extends DatabaseObjectDecorator implements ITitledLinkObject
 
         $showYear = $this->birthdayShowYear || WCF::getSession()->getPermission('admin.general.canViewPrivateUserOptions');
 
-        $d = new \DateTimeImmutable();
-        $d->setTimezone(WCF::getUser()->getTimeZone());
-        $d->setDate($birthdayYear, $month, $day);
+        $d = new \DateTimeImmutable($this->birthday, WCF::getUser()->getTimeZone());
         $dateFormat = (($showYear && $birthdayYear) ? WCF::getLanguage()->get(DateUtil::DATE_FORMAT) : \str_replace(
             'Y',
             '',

wcfsetup/install/files/lib/system/condition/UserBirthdayCondition.class.php
@@ -89,8 +89,7 @@ HTML;
             return false;
         }
 
-        $dateTime = new \DateTimeImmutable();
-        $dateTime->setTimezone(WCF::getUser()->getTimeZone());
+        $dateTime = new \DateTimeImmutable('now', WCF::getUser()->getTimeZone());
 
         $userIDs = UserBirthdayCache::getInstance()->getBirthdays($dateTime->format('n'), $dateTime->format('j'));
 