b708614ffc03fd227fdd4dff6dc1f1763eba01ab
Tim Düsterhus <duesterhus@woltlab.com>
2023-05-21 16:44:24 +0200

    Update composer dependencies

--- wcfsetup/install/files/lib/system/api/composer/autoload_files.php
+++ wcfsetup/install/files/lib/system/api/composer/autoload_files.php
@@ -7,7 +7,6 @@ $baseDir = $vendorDir;
 
 return array(
     '7b11c4dc42b3b3023073cb14e519683c' => $vendorDir . '/ralouphie/getallheaders/src/getallheaders.php',
-    'c964ee0ededf28c96ebd9db5099ef910' => $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
     '6e3fae29631ef280660b3cdad06f25a8' => $vendorDir . '/symfony/deprecation-contracts/function.php',
     '2cffec82183ee1cea088009cef9a6fc3' => $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
     '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
