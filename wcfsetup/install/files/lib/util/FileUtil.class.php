48860b88232c979292cd26cccb63d2c168cfa285
Tim Düsterhus <duesterhus@woltlab.com>
2023-07-28 13:57:50 +0200

    Perform a case-insensitive match in FileUtil::extensionAllowsPhpExecution()

--- wcfsetup/install/files/lib/util/FileUtil.class.php
+++ wcfsetup/install/files/lib/util/FileUtil.class.php
@@ -746,7 +746,7 @@ final class FileUtil
      */
     public function extensionAllowsPhpExecution(string $extension): bool
     {
-        return !!\preg_match('/^\.?(php[0-9]*|phtml)$/', $extension);
+        return !!\preg_match('/^\.?(php[0-9]*|phtml)$/i', $extension);
     }
 
     /**
