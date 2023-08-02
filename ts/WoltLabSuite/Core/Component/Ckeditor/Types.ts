c2961773323f0d5f3c9a2f2c7983cfec0e27e891
Tim Düsterhus <duesterhus@woltlab.com>
2023-08-02 11:19:24 +0200

    Augment the EditorConfig with `woltlabBbcode` in Ckeditor/Types

--- ts/WoltLabSuite/Core/Component/Ckeditor/Types.ts
+++ ts/WoltLabSuite/Core/Component/Ckeditor/Types.ts
@@ -10,7 +10,6 @@
 
 export type { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic";
 export type { CodeBlockConfig } from "@ckeditor/ckeditor5-code-block";
-export type { EditorConfig } from "@ckeditor/ckeditor5-core";
 export type { Element } from "@ckeditor/ckeditor5-engine";
 
 // We only want these for the configuration augmentation.
@@ -19,3 +18,19 @@ export type { Element } from "@ckeditor/ckeditor5-engine";
 // no symbols will be emitted.
 import type {} from "@ckeditor/ckeditor5-autosave";
 import type {} from "@ckeditor/ckeditor5-mention";
+
+import type { EditorConfig as UpstreamEditorConfig } from "@ckeditor/ckeditor5-core";
+
+type WoltlabBbcodeButton = {
+  icon?: string;
+  name: string;
+  label: string;
+};
+
+type WoltlabBbcodeConfig = WoltlabBbcodeButton[];
+
+interface EditorConfig extends UpstreamEditorConfig {
+  woltlabBbcode?: WoltlabBbcodeConfig;
+}
+
+export type { EditorConfig };
