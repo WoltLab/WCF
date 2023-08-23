WoltLab/WCF

commit 149cdf58f476ea36ad9757a0c28aee4b5ed7070d

Replace `mb_strpos()` usage with `str_starts_with`
See https://www.woltlab.com/community/thread/301190-mb-strpos-is-not-an-allowed-template-modifier/

wcfsetup/install/files/acp/templates/userGroupBooleanOptionType.tpl
@@ -7,7 +7,7 @@
 		<input type="radio" id="{$option->optionName}_no"{if $value == 0} checked{/if} name="values[{$option->optionName}]" value="0"{if $disableOptions || $enableOptions} class="jsEnablesOptions" data-is-boolean="true" data-disable-options="[ {@$disableOptions}]" data-enable-options="[ {@$enableOptions}]"{/if}>
 		<label for="{$option->optionName}_no" class="red">{icon name='xmark'} {lang}wcf.acp.option.type.boolean.no{/lang}</label>
 	</li>
-	{if $option->optionName|mb_strpos:'admin.' !== 0 && ($group === null || (!$group->isEveryone() && !$group->isUsers()))}
+	{if $option->optionName|str_starts_with:'admin.' && ($group === null || (!$group->isEveryone() && !$group->isUsers()))}
 		<li>
 			<input type="radio" id="{$option->optionName}_never"{if $value == -1} checked{/if} name="values[{$option->optionName}]" value="-1"{if $disableOptions || $enableOptions} class="jsEnablesOptions" data-is-boolean="true" data-disable-options="[ {@$disableOptions}]" data-enable-options="[ {@$enableOptions}]"{/if}>
 			<label for="{$option->optionName}_never" class="yellow">{icon name='ban'} {lang}wcf.acp.option.type.boolean.never{/lang}</label>

commit 559e5d836c1e58035dea0ea33ce32e581f35b4dd

Fix incorrect comparison in userGroupBooleanOptionType.tpl
This regressed in 149cdf58f476ea36ad9757a0c28aee4b5ed7070d.

see https://www.woltlab.com/community/thread/301313-benutzergruppenrechte-berechtigungsstufe-nie-wird-nicht-angezeigt/

wcfsetup/install/files/acp/templates/userGroupBooleanOptionType.tpl
@@ -7,7 +7,7 @@
 		<input type="radio" id="{$option->optionName}_no"{if $value == 0} checked{/if} name="values[{$option->optionName}]" value="0"{if $disableOptions || $enableOptions} class="jsEnablesOptions" data-is-boolean="true" data-disable-options="[ {@$disableOptions}]" data-enable-options="[ {@$enableOptions}]"{/if}>
 		<label for="{$option->optionName}_no" class="red">{icon name='xmark'} {lang}wcf.acp.option.type.boolean.no{/lang}</label>
 	</li>
-	{if $option->optionName|str_starts_with:'admin.' && ($group === null || (!$group->isEveryone() && !$group->isUsers()))}
+	{if !$option->optionName|str_starts_with:'admin.' && ($group === null || (!$group->isEveryone() && !$group->isUsers()))}
 		<li>
 			<input type="radio" id="{$option->optionName}_never"{if $value == -1} checked{/if} name="values[{$option->optionName}]" value="-1"{if $disableOptions || $enableOptions} class="jsEnablesOptions" data-is-boolean="true" data-disable-options="[ {@$disableOptions}]" data-enable-options="[ {@$enableOptions}]"{/if}>
 			<label for="{$option->optionName}_never" class="yellow">{icon name='ban'} {lang}wcf.acp.option.type.boolean.never{/lang}</label>