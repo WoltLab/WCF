WoltLab/WCF

commit 4da8c1de463ca71f09d284f3529c1d499a7ac8f8

Remove support for implicitly routing *Action requests to com.woltlab.wcf
This is no longer required since the removal of multi-domain support

see 5d1c96bc6e92b8b44170922b8abb26b898582f8c

wcfsetup/install/files/lib/system/request/ControllerMap.class.php
@@ -429,15 +429,7 @@ class ControllerMap extends SingletonFactory
     {
         $className = $application . '\\' . ($isAcpRequest ? 'acp\\' : '') . $pageType . '\\' . $controller . \ucfirst($pageType);
         if (!\class_exists($className)) {
-            // avoid CORS by allowing action classes invoked form every application domain
-            if ($pageType === 'action' && $application !== 'wcf') {
-                $className = 'wcf\\' . ($isAcpRequest ? 'acp\\' : '') . $pageType . '\\' . $controller . \ucfirst($pageType);
-                if (!\class_exists($className)) {
-                    return null;
-                }
-            } else {
-                return null;
-            }
+            return null;
         }
 
         // check for abstract classes

commit 3f3c0b170a291062adce5b8ba1bd2a636020a87b

Fix the test for the correct set-up of index.php-less URLs
This likely regressed in 4da8c1de463ca71f09d284f3529c1d499a7ac8f8.

see https://www.woltlab.com/community/thread/301353-seltsamer-verhalten-link-umschreibungen-aktivieren/

wcfsetup/install/files/lib/system/request/ControllerMap.class.php
@@ -2,6 +2,7 @@
 
 namespace wcf\system\request;
 
+use wcf\action\CoreRewriteTestAction;
 use wcf\page\CmsPage;
 use wcf\system\cache\builder\RoutingCacheBuilder;
 use wcf\system\exception\SystemException;
@@ -90,6 +91,15 @@ final class ControllerMap extends SingletonFactory
             }
             if ($classData === null) {
                 $classData = $this->getClassData($application, $controller, $isAcpRequest, 'action');
+            }
+            if ($classData === null) {
+                // The CoreRewriteTest controller is special and needs to be available in all apps.
+                if ($controller === 'CoreRewriteTest') {
+                    $classData = [
+                        'controller' => 'CoreRewriteTest',
+                        'className' => CoreRewriteTestAction::class,
+                    ];
+                }
             }
         }
 