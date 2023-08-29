ed0130aef22a67691ffb44705162e911285aa0e1
Olaf Braun <info@braun-development.de>
2023-08-29 12:02:41 +0200

    Fix min/width image check

--- wcfsetup/install/files/lib/system/form/builder/field/UploadFormField.class.php
+++ wcfsetup/install/files/lib/system/form/builder/field/UploadFormField.class.php
@@ -794,7 +794,7 @@ class UploadFormField extends AbstractFormField
             }
 
             $minimumImageHeight = $this->getMinimumImageHeight();
-            if ($minimumImageHeight !== null && $maximumImageHeight > $minimumImageHeight) {
+            if ($minimumImageHeight !== null && $minimumImageHeight > $maximumImageHeight) {
                 throw new \InvalidArgumentException(
                     "Maximum image height ({$maximumImageHeight}) cannot be smaller than minimum image height ({$minimumImageHeight}) for field '{$this->getId()}'."
                 );
