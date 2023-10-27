caf878335a06f4d4cfd0b52ffb2c8bc70320ce62
Marcel Werk <burntime@woltlab.com>
2023-10-27 16:08:27 +0200

    Add support for pbkdf2 password algorithm

---
+++ wcfsetup/install/files/lib/system/user/authentication/password/algorithm/Pbkdf2.class.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace wcf\system\user\authentication\password\algorithm;
+
+use ParagonIE\ConstantTime\Hex;
+use wcf\system\user\authentication\password\IPasswordAlgorithm;
+
+/**
+ * Implementation of the pbkdf2 password algorithm.
+ *
+ * @author      Marcel Werk
+ * @copyright   2001-2023 WoltLab GmbH
+ * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since       6.0
+ */
+final class Pbkdf2 implements IPasswordAlgorithm
+{
+    /**
+     * @inheritDoc
+     */
+    public function verify(
+        #[\SensitiveParameter]
+        string $password,
+        string $hash
+    ): bool {
+        $parts = \explode(':', $hash, 5);
+        $hash = $parts[0];
+        $salt = $parts[1];
+        $algo = $parts[2];
+        $iterations = $parts[3];
+        $length = $parts[4];
+
+        return \hash_equals($hash, \hash_pbkdf2($algo, $password, $salt, $iterations, $length));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function hash(
+        #[\SensitiveParameter]
+        string $password
+    ): string {
+        $salt = Hex::encode(\random_bytes(20));
+        $algo = 'sha256';
+        $iterations = 600000;
+        $length = 32;
+        $hash = \hash_pbkdf2($algo, $password, $salt, $iterations, $length);
+
+        return implode(':', [$hash, $salt, $algo, $iterations, $length]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function needsRehash(string $hash): bool
+    {
+        return false;
+    }
+}
