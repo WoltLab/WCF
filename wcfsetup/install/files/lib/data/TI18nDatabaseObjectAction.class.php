38b61a5a0bf5ca459df576576ef8b11f8b85829e
Olaf Braun <info@braun-development.de>
2023-11-10 09:06:43 +0100

    Provide a trait class to save i18n values from FormBuilder

---
+++ wcfsetup/install/files/lib/data/TI18nDatabaseObjectAction.class.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace wcf\data;
+
+use wcf\system\database\util\PreparedStatementConditionBuilder;
+use wcf\system\language\I18nHandler;
+use wcf\system\language\LanguageFactory;
+use wcf\system\WCF;
+
+/**
+ * Provides two methods to save or delete i18n values from FormBuilder.
+ *
+ * @author    Olaf Braun
+ * @copyright 2001-2023 WoltLab GmbH
+ * @license   GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ *
+ * @mixin AbstractDatabaseObjectAction
+ */
+trait TI18nDatabaseObjectAction
+{
+    /**
+     * Deletes old language items.
+     * This method should be called after the object is deleted.
+     */
+    protected function deleteI18nValues(): void
+    {
+        $langaugeItems = [];
+        foreach ($this->getObjects() as $object) {
+            foreach ($this->getI18nSaveTypes() as $name => $regex) {
+                if ($object->$name === \str_replace('\d+', $object->getObjectID(), $regex)) {
+                    $langaugeItems[] = $object->$name;
+                }
+            }
+        }
+        if ($langaugeItems !== []) {
+            //find language category id
+            $sql = "SELECT  languageCategoryID
+                FROM    wcf1_language_category
+                WHERE   languageCategory = ?";
+            $statement = WCF::getDB()->prepare($sql, 1);
+            $statement->execute([$this->getLanguageCategory()]);
+            $languageCategoryID = $statement->fetchSingleColumn();
+
+            $con = new PreparedStatementConditionBuilder();
+            $con->add('languageItem IN (?)', [$langaugeItems]);
+            $con->add('packageID = ?', [$this->getPackageID()]);
+            $con->add('languageCategoryID = ?', [$languageCategoryID]);
+
+            $sql = "DELETE FROM wcf1_language_item " . $con;
+            $statement = WCF::getDB()->prepare($sql);
+            $statement->execute($con->getParameters());
+
+            LanguageFactory::getInstance()->deleteLanguageCache();
+        }
+    }
+
+    /**
+     * Returns the i18n save types.
+     * The key is the name of the property and the value is the regex for the language name.
+     * `\d+` will be replaced with the object id.
+     *
+     * @example [ 'subject' => 'com.woltlab.wbb.post.\d+.subject' ]
+     *
+     * @return array<string, string>
+     */
+    abstract public function getI18nSaveTypes(): array;
+
+    /**
+     * Returns the language category under which the i18n values should be saved.
+     *
+     * @return string
+     */
+    abstract public function getLanguageCategory(): string;
+
+    /**
+     * Returns the package id under which the i18n values should be saved.
+     *
+     * @return int
+     */
+    abstract public function getPackageID(): int;
+
+    /**
+     * Saves the i18n values from FormBuilder.
+     * This method should be called after the object has been created or updated.
+     *
+     * @param DatabaseObject $object
+     */
+    protected function saveI18nValue(DatabaseObject $object): void
+    {
+        $updateData = [];
+
+        foreach ($this->getI18nSaveTypes() as $name => $regex) {
+            $languageName = \str_replace('\d+', $object->getObjectID(), $regex);
+            if (isset($this->parameters[$name . '_i18n'])) {
+                I18nHandler::getInstance()->save(
+                    $this->parameters[$name . '_i18n'],
+                    $languageName,
+                    $this->getLanguageCategory(),
+                    $this->getPackageID(),
+                );
+
+                $updateData[$name] = $languageName;
+            }
+        }
+        if (!empty($updateData)) {
+            $editor = new $this->className($object);
+            $editor->update($updateData);
+        }
+    }
+}
