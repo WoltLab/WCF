57e499179d1af43a7d905ffa8fa9063b2df4c86c
Cyperghost <olaf_schmitz_1@t-online.de>
2023-12-22 12:58:07 +0100

    Reload content in pages after save

--- wcfsetup/install/files/lib/acp/form/PageEditForm.class.php
+++ wcfsetup/install/files/lib/acp/form/PageEditForm.class.php
@@ -259,6 +259,16 @@ class PageEditForm extends PageAddForm
         // call saved event
         $this->saved();
 
+        // Ensue that the CKEditor has the correct content after save
+        if ($this->isMultilingual) {
+            foreach (LanguageFactory::getInstance()->getLanguages() as $language) {
+                $this->content[$language->languageID] = $this->htmlInputProcessors[$language->languageID] ?
+                    $this->htmlInputProcessors[$language->languageID]->getHtml() : '';
+            }
+        } else {
+            $this->content[0] = $this->htmlInputProcessors[0] ? $this->htmlInputProcessors[0]->getHtml() : '';
+        }
+
         // show success message
         WCF::getTPL()->assign('success', true);
     }
