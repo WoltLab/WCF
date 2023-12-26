cf2cec58408001708caa0dbc1d57b0feb68427ea
Alexander Ebert <ebert@woltlab.com>
2023-12-26 19:20:15 +0100

    Use buffers to write uploaded files

--- ts/WoltLabSuite/Core/Component/File/Upload.ts
+++ ts/WoltLabSuite/Core/Component/File/Upload.ts
@@ -17,13 +17,30 @@ async function upload(element: WoltlabCoreFileUploadElement, file: File): Promis
   const chunkSize = 2_000_000;
   const chunks = Math.ceil(file.size / chunkSize);
 
+  const arrayBufferToHex = (buffer: ArrayBuffer): string => {
+    return Array.from(new Uint8Array(buffer))
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
+  };
+
+  const hash = await window.crypto.subtle.digest("SHA-256", await file.arrayBuffer());
+  console.log("checksum for the entire file is:", arrayBufferToHex(hash));
+
+  const data: Blob[] = [];
   for (let i = 0; i < chunks; i++) {
     const start = i * chunkSize;
     const end = start + chunkSize;
     const chunk = file.slice(start, end);
+    data.push(chunk);
+
+    console.log("Uploading", start, "to", end, " (total: " + chunk.size + " of " + file.size + ")");
 
     await prepareRequest(endpoints[i]).post(chunk).fetchAsResponse();
   }
+
+  const uploadedChunks = new Blob(data);
+  const uploadedHash = await window.crypto.subtle.digest("SHA-256", await uploadedChunks.arrayBuffer());
+  console.log("checksum for the entire file is:", arrayBufferToHex(uploadedHash));
 }
 
 export function setup(): void {
@@ -31,5 +48,8 @@ export function setup(): void {
     element.addEventListener("upload", (event: CustomEvent<File>) => {
       void upload(element, event.detail);
     });
+
+    const file = new File(["a".repeat(4_000_001)], "test.txt");
+    void upload(element, file);
   });
 }
