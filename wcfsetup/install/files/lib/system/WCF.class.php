8f45db43d2cfef9088e7572d70721de8508d8d40
Alexander Ebert <ebert@woltlab.com>
2024-01-01 14:04:50 +0100

    Release 5.5.20

--- wcfsetup/install/files/lib/system/WCF.class.php
+++ wcfsetup/install/files/lib/system/WCF.class.php
@@ -50,7 +50,7 @@ if (!@\ini_get('date.timezone')) {
 }
 
 // define current woltlab suite version
-\define('WCF_VERSION', '5.5.19');
+\define('WCF_VERSION', '5.5.20');
 
 // define current API version
 // @deprecated 5.2
