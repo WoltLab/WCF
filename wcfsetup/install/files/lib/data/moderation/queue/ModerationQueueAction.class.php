bec9afc911127d7b53ff2bf55e3557f5bbdf34e2
Alexander Ebert <ebert@woltlab.com>
2024-01-01 13:47:42 +0100

    Encode the title of moderation queues

--- wcfsetup/install/files/lib/data/moderation/queue/ModerationQueueAction.class.php
+++ wcfsetup/install/files/lib/data/moderation/queue/ModerationQueueAction.class.php
@@ -14,6 +14,7 @@ use wcf\system\moderation\queue\ModerationQueueManager;
 use wcf\system\user\storage\UserStorageHandler;
 use wcf\system\visitTracker\VisitTracker;
 use wcf\system\WCF;
+use wcf\util\StringUtil;
 
 /**
  * Executes moderation queue-related actions.
@@ -145,7 +146,7 @@ class ModerationQueueAction extends AbstractDatabaseObjectAction
 
         $items = \array_map(static function (ViewableModerationQueue $queue) {
             return [
-                'content' => $queue->getAffectedObject()->getTitle(),
+                'content' => StringUtil::encodeHTML($queue->getAffectedObject()->getTitle()),
                 'image' => '<span class="icon icon48 ' . $queue->getIconName() . '"></span>',
                 'isUnread' => $queue->isNew(),
                 'link' => $queue->getLink(),
