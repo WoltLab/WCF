6708764e9df0368c179cfcf187b8ae08fe2638d9
Cyperghost <olaf_schmitz_1@t-online.de>
2024-01-24 11:49:20 +0100

    Create a new event to find missing PHP extensions

---
+++ wcfsetup/install/files/lib/system/acp/dashboard/box/event/PHPExtensionCollecting.class.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace wcf\system\acp\dashboard\box\event;
+
+use wcf\system\event\IEvent;
+
+/**
+ * @author Olaf Braun
+ * @copyright 2001-2024 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since 6.1
+ */
+final class PHPExtensionCollecting implements IEvent
+{
+    /**
+     * @var string[]
+     */
+    private array $extensions = [
+        'ctype',
+        'dom',
+        'exif',
+        'intl',
+        'libxml',
+        'mbstring',
+        'pdo',
+        'pdo_mysql',
+        'zlib',
+    ];
+
+    /**
+     * Registers a php extension.
+     */
+    public function register(string $extension): void
+    {
+        if (\in_array($extension, $this->extensions)) {
+            return;
+        }
+        $this->extensions[] = $extension;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getExtensions(): array
+    {
+        return $this->extensions;
+    }
+}
