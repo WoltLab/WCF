76cfef838f7cd9d69c2ef4068d02296d572e3123
Cyperghost <olaf_schmitz_1@t-online.de>
2024-01-26 12:00:26 +0100

    Add `@since 6.1`

--- wcfsetup/install/files/lib/system/template/SetupTemplateEngine.class.php
+++ wcfsetup/install/files/lib/system/template/SetupTemplateEngine.class.php
@@ -8,6 +8,7 @@ namespace wcf\system\template;
  * @author  Alexander Ebert
  * @copyright   2001-2019 WoltLab GmbH
  * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since 6.1
  */
 class SetupTemplateEngine extends TemplateEngine
 {
