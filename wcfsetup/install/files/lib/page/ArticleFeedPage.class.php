a9da8426408c70fc88c0c153eb2bcc21c8cc69ba
Marcel Werk <burntime@woltlab.com>
2024-02-04 14:48:34 +0100

    Add redirect to new rss feed page

--- wcfsetup/install/files/lib/page/ArticleFeedPage.class.php
+++ wcfsetup/install/files/lib/page/ArticleFeedPage.class.php
@@ -15,6 +15,7 @@ use wcf\system\WCF;
  * @copyright   2001-2019 WoltLab GmbH
  * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @since   3.0
+ * @deprecated 6.1 use `ArticleRssFeedPage` instead
  */
 class ArticleFeedPage extends AbstractFeedPage
 {
@@ -47,6 +48,8 @@ class ArticleFeedPage extends AbstractFeedPage
                 throw new PermissionDeniedException();
             }
         }
+
+        $this->redirectToNewPage(ArticleRssFeedPage::class);
     }
 
     /**
