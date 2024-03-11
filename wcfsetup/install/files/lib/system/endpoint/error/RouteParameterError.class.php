d4465bebf9a9972e1f325251c16a66f511b4b8c4
Alexander Ebert <ebert@woltlab.com>
2024-03-11 14:02:24 +0100

    Remove unused components

--- wcfsetup/install/files/lib/system/endpoint/error/RouteParameterError.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace wcf\system\endpoint\error;
-
-enum RouteParameterError: string
-{
-    case ExpectedPositiveInteger = 'expected_positive_integer';
-    case ExpectedNonEmptyString = 'expected_non_empty_string';
-}
