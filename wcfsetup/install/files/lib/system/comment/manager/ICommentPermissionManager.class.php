4585f1a3bca736b6793af71ae2204f533ca1335d
Olaf Braun <olaf_schmitz_1@t-online.de>
2024-03-25 13:21:06 +0100

    Update wcfsetup/install/files/lib/system/comment/manager/ICommentPermissionManager.class.php
    Co-authored-by: Alexander Ebert <ebert@woltlab.com>

--- wcfsetup/install/files/lib/system/comment/manager/ICommentPermissionManager.class.php
+++ wcfsetup/install/files/lib/system/comment/manager/ICommentPermissionManager.class.php
@@ -10,6 +10,7 @@ use wcf\data\user\UserProfile;
  * @author      Olaf Braun
  * @copyright   2001-2024 WoltLab GmbH
  * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since 6.1
  */
 interface ICommentPermissionManager extends ICommentManager
 {
