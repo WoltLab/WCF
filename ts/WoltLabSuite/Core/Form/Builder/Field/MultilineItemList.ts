dd0021ce0ac4c787549f25fd8344cefc4b89c089
Cyperghost <olaf_schmitz_1@t-online.de>
2024-04-25 11:24:25 +0200

    Add new form field `MultilineItemListFormField`

---
+++ ts/WoltLabSuite/Core/Form/Builder/Field/MultilineItemList.ts
@@ -0,0 +1,22 @@
+/**
+ * Data handler for an multiline item list form builder field in an Ajax form.
+ *
+ * @author      Olaf Braun
+ * @copyright   2001-2024 WoltLab GmbH
+ * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since       6.0
+ */
+
+import Field from "WoltLabSuite/Core/Form/Builder/Field/Field";
+import { FormBuilderData } from "WoltLabSuite/Core/Form/Builder/Data";
+import { getValues } from "./Controller/MultilineItemList";
+
+class MultilineItemList extends Field {
+  protected _getData(): FormBuilderData {
+    return {
+      [this._fieldId]: getValues(this._fieldId),
+    };
+  }
+}
+
+export = MultilineItemList;
---
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Form/Builder/Field/MultilineItemList.js
@@ -0,0 +1,20 @@
+/**
+ * Data handler for an multiline item list form builder field in an Ajax form.
+ *
+ * @author      Olaf Braun
+ * @copyright   2001-2024 WoltLab GmbH
+ * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since       6.0
+ */
+define(["require", "exports", "tslib", "WoltLabSuite/Core/Form/Builder/Field/Field", "./Controller/MultilineItemList"], function (require, exports, tslib_1, Field_1, MultilineItemList_1) {
+    "use strict";
+    Field_1 = tslib_1.__importDefault(Field_1);
+    class MultilineItemList extends Field_1.default {
+        _getData() {
+            return {
+                [this._fieldId]: (0, MultilineItemList_1.getValues)(this._fieldId),
+            };
+        }
+    }
+    return MultilineItemList;
+});
