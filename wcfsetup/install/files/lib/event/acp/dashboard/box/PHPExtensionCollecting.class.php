75aebdcae327c94ff84c2bb6e86de84a8ac7c2fc
Marcel Werk <burntime@woltlab.com>
2024-05-14 12:48:08 +0200

    Add missing class documentation

--- wcfsetup/install/files/lib/event/acp/dashboard/box/PHPExtensionCollecting.class.php
+++ wcfsetup/install/files/lib/event/acp/dashboard/box/PHPExtensionCollecting.class.php
@@ -5,6 +5,8 @@ namespace wcf\event\acp\dashboard\box;
 use wcf\event\IPsr14Event;
 
 /**
+ * Requests the collection of PHP extensions for the system info ACP dashboard box.
+ *
  * @author Olaf Braun
  * @copyright 2001-2024 WoltLab GmbH
  * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
