4643ea9d5663025989da7f22e0530d965e59b6e5
Alexander Ebert <ebert@woltlab.com>
2024-09-13 17:05:12 +0200

    Release 6.0.17

--- wcfsetup/install/files/lib/system/WCF.class.php
+++ wcfsetup/install/files/lib/system/WCF.class.php
@@ -81,7 +81,7 @@ if (\function_exists('mb_regex_encoding')) {
 \mb_language('uni');
 
 // define current woltlab suite version
-\define('WCF_VERSION', '6.0.16');
+\define('WCF_VERSION', '6.0.17');
 
 // define current unix timestamp
 \define('TIME_NOW', \time());
