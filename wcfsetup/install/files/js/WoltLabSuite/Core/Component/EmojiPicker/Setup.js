147c3b58ca5d21e421b512d4e28ec5b4609b55ba
Cyperghost <olaf_schmitz_1@t-online.de>
2024-09-23 11:51:50 +0200

    Import `emoji-picker-element-data`

--- wcfsetup/install/files/js/WoltLabSuite/Core/Component/EmojiPicker/Setup.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Component/EmojiPicker/Setup.js
@@ -3,6 +3,8 @@ define(["require", "exports", "WoltLabSuite/Core/Helper/Selector"], function (re
     Object.defineProperty(exports, "__esModule", { value: true });
     (0, Selector_1.wheneverFirstSeen)("emoji-picker", (emojiPicker) => {
         emojiPicker.locale = window.LANGUAGE_CODE;
+        // TODO host local emoji data
+        emojiPicker.dataSource = `https://cdn.jsdelivr.net/npm/emoji-picker-element-data@^1/${window.LANGUAGE_CODE}/cldr-native/data.json`;
         if (window.EmojiPickerLocales[window.LANGUAGE_CODE] !== undefined) {
             emojiPicker.i18n = window.EmojiPickerLocales[window.LANGUAGE_CODE];
         }
