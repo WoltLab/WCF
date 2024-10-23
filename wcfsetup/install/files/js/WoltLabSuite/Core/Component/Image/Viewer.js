6801efd00c93a868a458fddb0c6ea466cd0b25b2
Cyperghost <olaf_schmitz_1@t-online.de>
2024-10-23 08:49:07 +0200

    Run `tsc`

--- wcfsetup/install/files/js/WoltLabSuite/Core/Component/Image/Viewer.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Component/Image/Viewer.js
@@ -1,10 +1,9 @@
 define(["require", "exports", "@fancyapps/ui"], function (require, exports, ui_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.setup = void 0;
+    exports.setup = setup;
     function setup() {
         ui_1.Fancybox.bind("[data-fancybox]");
         ui_1.Fancybox.bind('[data-fancybox="attachments"]');
     }
-    exports.setup = setup;
 });
