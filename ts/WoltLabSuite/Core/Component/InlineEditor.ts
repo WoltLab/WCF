9e89babb58b30e0864f22dba832cfc28ebfd56f5
Cyperghost <olaf_schmitz_1@t-online.de>
2025-01-27 13:23:15 +0100

    Add functions to set the menu entries and get the permissions

--- ts/WoltLabSuite/Core/Component/InlineEditor.ts
+++ ts/WoltLabSuite/Core/Component/InlineEditor.ts
@@ -1,5 +1,5 @@
 /**
- * Provides an inline editor for objects with a dropdown menu.
+ * Provides an inline editor for objects using a drop-down menu.
  *
  * @author Olaf Braun
  * @copyright 2001-2025 WoltLab GmbH
@@ -7,28 +7,114 @@
  * @since 6.2
  */
 
-import { DropdownBuilderItemData, create as createDropDownMenu } from "WoltLabSuite/Core/Ui/Dropdown/Builder";
+import {
+  DropdownBuilderItemData,
+  create as createDropdownMenu,
+  attach as attachDropdownMenu,
+  setItems as setDropdownItems,
+} from "WoltLabSuite/Core/Ui/Dropdown/Builder";
 import { stringToBool } from "WoltLabSuite/Core/Core";
 
-export abstract class InlineEditor {
+export interface DropdownMenuItem {
+  visible?: () => boolean;
+  item: DropdownBuilderItemData;
+}
+
+const inlineEditors = new Map<HTMLElement, InlineEditor>();
+
+export class InlineEditor {
   protected readonly element: HTMLElement;
   protected readonly dropdownToggle: HTMLElement;
-  protected readonly dropdownMenu: HTMLUListElement | null = null;
+  protected permissions: Record<string, boolean> = {};
+  protected readonly dropdownMenu: HTMLUListElement;
+  protected readonly menuItems: DropdownMenuItem[] = [];
 
-  protected constructor(element: HTMLElement, dropdownToggleSelector: string) {
+  public constructor(element: HTMLElement, dropdownToggleSelector: string) {
     this.element = element;
     this.dropdownToggle = this.element.querySelector(dropdownToggleSelector) as HTMLElement;
-    this.dropdownMenu = createDropDownMenu(this.getDropdownOptions());
-    this.dropdownToggle.parentElement!.appendChild(this.dropdownMenu);
+    this.dropdownMenu = createDropdownMenu([]);
+    attachDropdownMenu(this.dropdownMenu, this.dropdownToggle);
+
+    inlineEditors.set(this.element, this);
   }
 
-  abstract getDropdownOptions(): DropdownBuilderItemData[];
-
-  public getPermission(permission: string): boolean {
-    if (!Object.prototype.hasOwnProperty.call(this.element.dataset, permission)) {
+  /**
+   * Gets the state of a property from the element's dataset as a boolean.
+   */
+  public getState(propertyName: string): boolean {
+    if (!Object.prototype.hasOwnProperty.call(this.element.dataset, propertyName)) {
       return false;
     }
 
-    return stringToBool(this.element.dataset[permission]!);
+    return stringToBool(this.element.dataset[propertyName]!);
   }
+
+  /**
+   * Updates the state of the element's dataset with the provided data.
+   */
+  public updateState(data: Record<string, boolean>): void {
+    Object.entries(data).forEach(([key, value]) => {
+      this.element.dataset[key] = value ? "1" : "0";
+    });
+
+    this.rebuildDropdownMenu();
+  }
+
+  /**
+   * Sets the permissions for the inline editor.
+   */
+  public setPermissions(permissions: Record<string, boolean>): void {
+    this.permissions = permissions;
+
+    this.rebuildDropdownMenu();
+  }
+
+  /**
+   * Gets the permissions for the inline editor.
+   */
+  public getPermissions(): Record<string, boolean> {
+    return this.permissions;
+  }
+
+  /**
+   * Adds a menu item to the dropdown menu and rebuilds the menu.
+   */
+  public addMenuItem(menuItem: DropdownMenuItem): void {
+    this.menuItems.push(menuItem);
+
+    this.rebuildDropdownMenu();
+  }
+
+  /**
+   * Adds multiple menu items to the dropdown menu and rebuilds the menu.
+   */
+  public addMenuItems(menuItems: DropdownMenuItem[]): void {
+    this.menuItems.push(...menuItems);
+
+    this.rebuildDropdownMenu();
+  }
+
+  /**
+   * Rebuilds the dropdown menu based on the current menu items and their visibility.
+   */
+  public rebuildDropdownMenu(): void {
+    const dropdownMenuItems = this.menuItems
+      .filter((item) => {
+        return item.visible === undefined || item.visible();
+      })
+      .map((item) => item.item);
+
+    if (dropdownMenuItems.length === 0) {
+      this.dropdownMenu.innerHTML = "";
+    } else {
+      setDropdownItems(this.dropdownMenu, dropdownMenuItems);
+    }
+  }
+}
+
+/**
+ * Gets the inline editor instance associated with the given element.
+ */
+export function getInlineEditor(element: HTMLElement): InlineEditor | undefined {
+  return inlineEditors.get(element);
 }
--- wcfsetup/install/files/js/WoltLabSuite/Core/Component/InlineEditor.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Component/InlineEditor.js
@@ -1,5 +1,5 @@
 /**
- * Provides an inline editor for objects with a dropdown menu.
+ * Provides an inline editor for objects using a drop-down menu.
  *
  * @author Olaf Braun
  * @copyright 2001-2025 WoltLab GmbH
@@ -10,22 +10,88 @@ define(["require", "exports", "WoltLabSuite/Core/Ui/Dropdown/Builder", "WoltLabS
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.InlineEditor = void 0;
+    exports.getInlineEditor = getInlineEditor;
+    const inlineEditors = new Map();
     class InlineEditor {
         element;
         dropdownToggle;
-        dropdownMenu = null;
+        permissions = {};
+        dropdownMenu;
+        menuItems = [];
         constructor(element, dropdownToggleSelector) {
             this.element = element;
             this.dropdownToggle = this.element.querySelector(dropdownToggleSelector);
-            this.dropdownMenu = (0, Builder_1.create)(this.getDropdownOptions());
-            this.dropdownToggle.parentElement.appendChild(this.dropdownMenu);
+            this.dropdownMenu = (0, Builder_1.create)([]);
+            (0, Builder_1.attach)(this.dropdownMenu, this.dropdownToggle);
+            inlineEditors.set(this.element, this);
         }
-        getPermission(permission) {
-            if (!Object.prototype.hasOwnProperty.call(this.element.dataset, permission)) {
+        /**
+         * Gets the state of a property from the element's dataset as a boolean.
+         */
+        getState(propertyName) {
+            if (!Object.prototype.hasOwnProperty.call(this.element.dataset, propertyName)) {
                 return false;
             }
-            return (0, Core_1.stringToBool)(this.element.dataset[permission]);
+            return (0, Core_1.stringToBool)(this.element.dataset[propertyName]);
+        }
+        /**
+         * Updates the state of the element's dataset with the provided data.
+         */
+        updateState(data) {
+            Object.entries(data).forEach(([key, value]) => {
+                this.element.dataset[key] = value ? "1" : "0";
+            });
+            this.rebuildDropdownMenu();
+        }
+        /**
+         * Sets the permissions for the inline editor.
+         */
+        setPermissions(permissions) {
+            this.permissions = permissions;
+            this.rebuildDropdownMenu();
+        }
+        /**
+         * Gets the permissions for the inline editor.
+         */
+        getPermissions() {
+            return this.permissions;
+        }
+        /**
+         * Adds a menu item to the dropdown menu and rebuilds the menu.
+         */
+        addMenuItem(menuItem) {
+            this.menuItems.push(menuItem);
+            this.rebuildDropdownMenu();
+        }
+        /**
+         * Adds multiple menu items to the dropdown menu and rebuilds the menu.
+         */
+        addMenuItems(menuItems) {
+            this.menuItems.push(...menuItems);
+            this.rebuildDropdownMenu();
+        }
+        /**
+         * Rebuilds the dropdown menu based on the current menu items and their visibility.
+         */
+        rebuildDropdownMenu() {
+            const dropdownMenuItems = this.menuItems
+                .filter((item) => {
+                return item.visible === undefined || item.visible();
+            })
+                .map((item) => item.item);
+            if (dropdownMenuItems.length === 0) {
+                this.dropdownMenu.innerHTML = "";
+            }
+            else {
+                (0, Builder_1.setItems)(this.dropdownMenu, dropdownMenuItems);
+            }
         }
     }
     exports.InlineEditor = InlineEditor;
+    /**
+     * Gets the inline editor instance associated with the given element.
+     */
+    function getInlineEditor(element) {
+        return inlineEditors.get(element);
+    }
 });
