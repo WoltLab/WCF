c2b7354e82275a0b00872c522b8160f4176bf8b1
Cyperghost <olaf_schmitz_1@t-online.de>
2025-06-05 13:10:12 +0200

    Add IMigrateConditionType interface and implement migration methods in user group condition types

--- wcfsetup/install/files/lib/system/condition/type/user/UserNotInGroupConditionType.class.php
+++ wcfsetup/install/files/lib/system/condition/type/user/UserNotInGroupConditionType.class.php
@@ -8,6 +8,7 @@ use wcf\data\user\User;
 use wcf\data\user\UserList;
 use wcf\system\condition\type\AbstractConditionType;
 use wcf\system\condition\type\IDatabaseObjectListConditionType;
+use wcf\system\condition\type\IMigrateConditionType;
 use wcf\system\condition\type\IObjectConditionType;
 use wcf\system\form\builder\field\SelectFormField;
 
@@ -21,7 +22,7 @@ use wcf\system\form\builder\field\SelectFormField;
  * @implements IObjectConditionType<User, string>
  * @extends AbstractConditionType<string>
  */
-final class UserNotInGroupConditionType extends AbstractConditionType implements IDatabaseObjectListConditionType, IObjectConditionType
+final class UserNotInGroupConditionType extends AbstractConditionType implements IDatabaseObjectListConditionType, IObjectConditionType, IMigrateConditionType
 {
     #[\Override]
     public function getFormField(string $id): SelectFormField
@@ -68,5 +69,25 @@ final class UserNotInGroupConditionType extends AbstractConditionType implements
     public function matches(object $object): bool
     {
         return !\in_array((int)$this->filter, $object->getGroupIDs(), true);
+    }
+
+    #[\Override]
+    public function migrateConditionData(array &$conditionData): array
+    {
+        if (!isset($conditionData['notGroupIDs'])) {
+            return [];
+        }
+
+        $result = [];
+        foreach ($conditionData['notGroupIDs'] as $groupID) {
+            $result[] = [
+                'identifier' => $this->getIdentifier(),
+                'value' => $groupID,
+            ];
+        }
+
+        unset($conditionData['notGroupIDs']);
+
+        return $result;
     }
 }
