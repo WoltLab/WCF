48c9a7b5ae41543715826c80873902c715d81b4d
Marcel Werk <burntime@woltlab.com>
2025-07-06 12:38:29 +0200

    Deprecate `AbstractCaptchaForm`

--- wcfsetup/install/files/lib/form/AbstractCaptchaForm.class.php
+++ wcfsetup/install/files/lib/form/AbstractCaptchaForm.class.php
@@ -13,6 +13,7 @@ use wcf\system\WCF;
  * @author  Matthias Schmidt
  * @copyright   2001-2019 WoltLab GmbH
  * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @deprecated 6.2 Use FormBuilder with a `CaptchaFormField` instead.
  */
 abstract class AbstractCaptchaForm extends AbstractForm
 {
