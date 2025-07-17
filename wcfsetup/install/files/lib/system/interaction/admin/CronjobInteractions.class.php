5b5fa2887af14706ac633c8633d702903bbd61a4
Marcel Werk <burntime@woltlab.com>
2025-07-17 15:24:06 +0200

    Add link from cronjob list to cronjob log
    ref https://www.woltlab.com/community/thread/312924-von-cronjob-liste-auf-cronjob-protokoll-verlinken/

--- wcfsetup/install/files/lib/system/interaction/admin/CronjobInteractions.class.php
+++ wcfsetup/install/files/lib/system/interaction/admin/CronjobInteractions.class.php
@@ -2,12 +2,18 @@
 
 namespace wcf\system\interaction\admin;
 
+use wcf\acp\page\CronjobLogListPage;
 use wcf\data\cronjob\Cronjob;
+use wcf\data\DatabaseObject;
 use wcf\event\interaction\admin\CronjobInteractionCollecting;
 use wcf\system\event\EventHandler;
+use wcf\system\interaction\AbstractInteraction;
 use wcf\system\interaction\AbstractInteractionProvider;
 use wcf\system\interaction\DeleteInteraction;
 use wcf\system\interaction\RpcInteraction;
+use wcf\system\request\LinkHandler;
+use wcf\system\WCF;
+use wcf\util\StringUtil;
 
 /**
  * Interaction provider for cronjobs.
@@ -23,7 +29,25 @@ final class CronjobInteractions extends AbstractInteractionProvider
     {
         $this->addInteractions([
             new DeleteInteraction('core/cronjobs/%s', static fn(Cronjob $cronjob) => $cronjob->isDeletable()),
-            new RpcInteraction('execute', 'core/cronjobs/%s/execute', 'wcf.acp.cronjob.execute')
+            new RpcInteraction('execute', 'core/cronjobs/%s/execute', 'wcf.acp.cronjob.execute'),
+            new class("show-log") extends AbstractInteraction {
+                #[\Override]
+                public function render(DatabaseObject $object): string
+                {
+                    \assert($object instanceof Cronjob);
+                    $href = LinkHandler::getInstance()->getControllerLink(
+                        CronjobLogListPage::class,
+                        [
+                            'filters' => [
+                                'cronjobID' => $object->getObjectID(),
+                            ],
+                        ]
+                    );
+                    $title = WCF::getLanguage()->get("wcf.acp.cronjob.showLog");
+
+                    return \sprintf('<a href="%s">%s</a>', StringUtil::encodeHTML($href), $title);
+                }
+            }
         ]);
 
         EventHandler::getInstance()->fire(
