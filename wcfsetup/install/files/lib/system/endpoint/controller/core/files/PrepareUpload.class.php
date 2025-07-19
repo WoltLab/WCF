89b1e411116f80375d4be60a412b9ada74695cc5
Alexander Ebert <ebert@woltlab.com>
2025-07-19 14:19:52 +0200

    Parse the raw EXIF bytes before storing

--- wcfsetup/install/files/lib/system/endpoint/controller/core/files/PrepareUpload.class.php
+++ wcfsetup/install/files/lib/system/endpoint/controller/core/files/PrepareUpload.class.php
@@ -16,6 +16,7 @@ use wcf\system\exception\UserInputException;
 use wcf\system\file\processor\FileProcessor;
 use wcf\system\file\processor\FileProcessorPreflightResult;
 use wcf\util\JSON;
+use WoltLab\WebpExif\Chunk\Exif;
 
 /**
  * Prepares the upload of a file.
@@ -75,6 +76,11 @@ final class PrepareUpload implements IController
         $identifier = \bin2hex(\random_bytes(20));
         $objectType = FileProcessor::getInstance()->getObjectType($parameters->objectType);
 
+        $exifData = $this->parseExifData($parameters->exifData);
+        if ($exifData !== null) {
+            $exifData = JSON::encode($exifData);
+        }
+
         $action = new FileTemporaryAction([], 'create', [
             'data' => [
                 'identifier' => $identifier,
@@ -85,11 +91,22 @@ final class PrepareUpload implements IController
                 'objectTypeID' => $objectType?->objectTypeID,
                 'context' => $parameters->context,
                 'chunks' => \str_repeat('0', $numberOfChunks),
-                'exifData' => $parameters->exifData,
+                'exifData' => $exifData,
             ],
         ]);
 
         return $action->executeAction()['returnValues'];
+    }
+
+    private function parseExifData(?string $exifData): array|null
+    {
+        if ($exifData === null) {
+            return null;
+        }
+
+        $exif = new Exif(0, $exifData);
+
+        return $exif->getParsedExif();
     }
 }
 
