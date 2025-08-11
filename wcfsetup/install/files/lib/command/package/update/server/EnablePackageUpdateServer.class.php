8232fdef21be3fb1229008e28a2e96f88a673437
Cyperghost <olaf_schmitz_1@t-online.de>
2025-08-11 11:51:37 +0200

    Migrate the function to disable/enable package update servers to a command

---
+++ wcfsetup/install/files/lib/command/package/update/server/EnablePackageUpdateServer.class.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace wcf\command\package\update\server;
+
+use wcf\data\package\update\server\PackageUpdateServer;
+use wcf\data\package\update\server\PackageUpdateServerEditor;
+use wcf\event\package\update\server\PackageUpdateServerEnabled;
+use wcf\system\event\EventHandler;
+
+/**
+ * Enables the given package update server.
+ *
+ * @author Olaf Braun
+ * @copyright 2001-2025 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since 6.3
+ */
+final class EnablePackageUpdateServer
+{
+    public function __construct(private readonly PackageUpdateServer $server) {}
+
+    public function __invoke(): void
+    {
+        if (!$this->server->canDisable()) {
+            return;
+        }
+
+        if (!$this->server->isDisabled) {
+            return;
+        }
+
+        (new PackageUpdateServerEditor($this->server))->update([
+            'isDisabled' => 0,
+        ]);
+
+        $event = new PackageUpdateServerEnabled($this->server);
+        EventHandler::getInstance()->fire($event);
+    }
+}
