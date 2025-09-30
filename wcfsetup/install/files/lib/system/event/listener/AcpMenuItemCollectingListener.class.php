4ab7fa9ef0fe08cbdb301731c59741b61ea64069
Marcel Werk <burntime@woltlab.com>
2025-09-30 17:02:54 +0200

    Code formatted more compactly

--- wcfsetup/install/files/lib/system/event/listener/AcpMenuItemCollectingListener.class.php
+++ wcfsetup/install/files/lib/system/event/listener/AcpMenuItemCollectingListener.class.php
@@ -31,12 +31,10 @@ final class AcpMenuItemCollectingListener
 
     private function addTopLevelItems(ItemCollecting $event): void
     {
-        $event->register(
-            new AcpMenuItem(
-                'wcf.acp.menu.link.configuration',
-                icon: FontAwesomeIcon::fromValues('wrench'),
-            )
-        );
+        $event->register(new AcpMenuItem(
+            'wcf.acp.menu.link.configuration',
+            icon: FontAwesomeIcon::fromValues('wrench'),
+        ));
     }
 
     private function addOptionItems(ItemCollecting $event): void
@@ -45,12 +43,10 @@ final class AcpMenuItemCollectingListener
             return;
         }
 
-        $event->register(
-            new AcpMenuItem(
-                'wcf.acp.menu.link.option',
-                parentMenuItem: 'wcf.acp.menu.link.configuration'
-            )
-        );
+        $event->register(new AcpMenuItem(
+            'wcf.acp.menu.link.option',
+            parentMenuItem: 'wcf.acp.menu.link.configuration'
+        ));
 
         foreach ((new TopOptionCategoryCache())->getCache() as $optionCategory) {
             if (!$optionCategory->validateOptions() || $optionCategory->validatePermissions()) {
@@ -72,71 +68,57 @@ final class AcpMenuItemCollectingListener
 
     private function addPackageItems(ItemCollecting $event): void
     {
-        $event->register(
-            new AcpMenuItem(
-                'wcf.acp.menu.link.package',
-                parentMenuItem: 'wcf.acp.menu.link.configuration'
-            )
-        );
+        $event->register(new AcpMenuItem(
+            'wcf.acp.menu.link.package',
+            parentMenuItem: 'wcf.acp.menu.link.configuration'
+        ));
 
         if (WCF::getSession()->getPermission('admin.configuration.canManageApplication')) {
-            $event->register(
-                new AcpMenuItem(
-                    'wcf.acp.menu.link.application.management',
-                    parentMenuItem: 'wcf.acp.menu.link.package',
-                    link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\form\ApplicationManagementForm::class)
-                )
-            );
+            $event->register(new AcpMenuItem(
+                'wcf.acp.menu.link.application.management',
+                parentMenuItem: 'wcf.acp.menu.link.package',
+                link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\form\ApplicationManagementForm::class)
+            ));
         }
 
         if (
             WCF::getSession()->getPermission('admin.configuration.package.canInstallPackage')
             || WCF::getSession()->getPermission('admin.configuration.package.canUpdatePackage')
         ) {
-            $event->register(
-                new AcpMenuItem(
-                    'wcf.acp.menu.link.package.list',
-                    parentMenuItem: 'wcf.acp.menu.link.package',
-                    link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\page\PackageListPage::class)
-                )
-            );
+            $event->register(new AcpMenuItem(
+                'wcf.acp.menu.link.package.list',
+                parentMenuItem: 'wcf.acp.menu.link.package',
+                link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\page\PackageListPage::class)
+            ));
         }
 
         if (WCF::getSession()->getPermission('admin.configuration.package.canInstallPackage')) {
-            $event->register(
-                new AcpMenuItem(
-                    'wcf.acp.menu.link.package.license',
-                    parentMenuItem: 'wcf.acp.menu.link.package.list',
-                    link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\page\LicensePage::class),
-                    icon: FontAwesomeIcon::fromValues('cart-arrow-down')
-                )
-            );
-            $event->register(
-                new AcpMenuItem(
-                    'wcf.acp.menu.link.package.install',
-                    parentMenuItem: 'wcf.acp.menu.link.package.list',
-                    link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\form\PackageStartInstallForm::class),
-                    icon: FontAwesomeIcon::fromValues('plus')
-                )
-            );
+            $event->register(new AcpMenuItem(
+                'wcf.acp.menu.link.package.license',
+                parentMenuItem: 'wcf.acp.menu.link.package.list',
+                link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\page\LicensePage::class),
+                icon: FontAwesomeIcon::fromValues('cart-arrow-down')
+            ));
+            $event->register(new AcpMenuItem(
+                'wcf.acp.menu.link.package.install',
+                parentMenuItem: 'wcf.acp.menu.link.package.list',
+                link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\form\PackageStartInstallForm::class),
+                icon: FontAwesomeIcon::fromValues('plus')
+            ));
         }
 
         if (WCF::getSession()->getPermission('admin.configuration.package.canEditServer')) {
-            $event->register(
-                new AcpMenuItem(
-                    'wcf.acp.menu.link.package.server.list',
-                    parentMenuItem: 'wcf.acp.menu.link.package',
-                    link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\page\PackageUpdateServerListPage::class),
-                )
-            );
-            $event->register(
-                new AcpMenuItem(
-                    'wcf.acp.menu.link.package.server.add',
-                    parentMenuItem: 'wcf.acp.menu.link.package.server.list',
-                    link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\form\PackageUpdateServerAddForm::class),
-                    icon: FontAwesomeIcon::fromValues('plus')
-                )
-            );
+            $event->register(new AcpMenuItem(
+                'wcf.acp.menu.link.package.server.list',
+                parentMenuItem: 'wcf.acp.menu.link.package',
+                link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\page\PackageUpdateServerListPage::class),
+            ));
+            $event->register(new AcpMenuItem(
+                'wcf.acp.menu.link.package.server.add',
+                parentMenuItem: 'wcf.acp.menu.link.package.server.list',
+                link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\form\PackageUpdateServerAddForm::class),
+                icon: FontAwesomeIcon::fromValues('plus')
+            ));
         }
     }
 
@@ -150,43 +132,33 @@ final class AcpMenuItemCollectingListener
             return;
         }
 
-        $event->register(
-            new AcpMenuItem(
-                'wcf.acp.menu.link.contact',
-                parentMenuItem: 'wcf.acp.menu.link.configuration'
-            )
-        );
-        $event->register(
-            new AcpMenuItem(
-                'wcf.acp.menu.link.contact.options',
-                parentMenuItem: 'wcf.acp.menu.link.contact',
-                link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\page\ContactOptionListPage::class),
-            )
-        );
-        $event->register(
-            new AcpMenuItem(
-                'wcf.acp.menu.link.contact.options.add',
-                WCF::getLanguage()->get('wcf.acp.contact.option.add'),
-                'wcf.acp.menu.link.contact.options',
-                LinkHandler::getInstance()->getControllerLink(\wcf\acp\form\ContactOptionAddForm::class),
-                FontAwesomeIcon::fromValues('plus'),
-            )
-        );
-        $event->register(
-            new AcpMenuItem(
-                'wcf.acp.menu.link.contact.recipients',
-                parentMenuItem: 'wcf.acp.menu.link.contact',
-                link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\page\ContactRecipientListPage::class),
-            )
-        );
-        $event->register(
-            new AcpMenuItem(
-                'wcf.acp.menu.link.contact.recipients.add',
-                WCF::getLanguage()->get('wcf.acp.contact.recipient.add'),
-                'wcf.acp.menu.link.contact.recipients',
-                LinkHandler::getInstance()->getControllerLink(\wcf\acp\form\ContactRecipientAddForm::class),
-                FontAwesomeIcon::fromValues('plus'),
-            )
-        );
+        $event->register(new AcpMenuItem(
+            'wcf.acp.menu.link.contact',
+            parentMenuItem: 'wcf.acp.menu.link.configuration'
+        ));
+        $event->register(new AcpMenuItem(
+            'wcf.acp.menu.link.contact.options',
+            parentMenuItem: 'wcf.acp.menu.link.contact',
+            link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\page\ContactOptionListPage::class),
+        ));
+        $event->register(new AcpMenuItem(
+            'wcf.acp.menu.link.contact.options.add',
+            WCF::getLanguage()->get('wcf.acp.contact.option.add'),
+            'wcf.acp.menu.link.contact.options',
+            LinkHandler::getInstance()->getControllerLink(\wcf\acp\form\ContactOptionAddForm::class),
+            FontAwesomeIcon::fromValues('plus'),
+        ));
+        $event->register(new AcpMenuItem(
+            'wcf.acp.menu.link.contact.recipients',
+            parentMenuItem: 'wcf.acp.menu.link.contact',
+            link: LinkHandler::getInstance()->getControllerLink(\wcf\acp\page\ContactRecipientListPage::class),
+        ));
+        $event->register(new AcpMenuItem(
+            'wcf.acp.menu.link.contact.recipients.add',
+            WCF::getLanguage()->get('wcf.acp.contact.recipient.add'),
+            'wcf.acp.menu.link.contact.recipients',
+            LinkHandler::getInstance()->getControllerLink(\wcf\acp\form\ContactRecipientAddForm::class),
+            FontAwesomeIcon::fromValues('plus'),
+        ));
     }
 }
