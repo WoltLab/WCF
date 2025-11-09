136b8299722233c68d9f1ff592636ef521401110
Marcel Werk <burntime@woltlab.com>
2025-11-09 17:35:34 +0100

    Truncate the title of URL form options
    ref https://www.woltlab.com/community/thread/314615-lange-url-wird-umgebrochen/

--- wcfsetup/install/files/lib/system/form/option/formatter/UrlFormatter.class.php
+++ wcfsetup/install/files/lib/system/form/option/formatter/UrlFormatter.class.php
@@ -2,6 +2,8 @@
 
 namespace wcf\system\form\option\formatter;
 
+use GuzzleHttp\Psr7\Exception\MalformedUriException;
+use GuzzleHttp\Psr7\Uri;
 use wcf\util\StringUtil;
 
 /**
@@ -17,6 +19,39 @@ final class UrlFormatter implements IFormOptionFormatter
     #[\Override]
     public function format(string $value, int $languageID, array $configuration): string
     {
-        return StringUtil::getAnchorTag($value, $value, true, true);
+        return StringUtil::getAnchorTag($value, $this->getTruncatedTitle($value), true, true);
+    }
+
+    private function getTruncatedTitle(string $href): string
+    {
+        if (\mb_strlen($href) <= 60) {
+            return $href;
+        }
+
+        try {
+            $uri = new Uri($href);
+        } catch (MalformedUriException) {
+            return $href;
+        }
+
+        $schemeHost = Uri::composeComponents(
+            $uri->getScheme(),
+            $uri->getAuthority(),
+            '',
+            null,
+            null,
+        );
+        $pathQueryFragment = Uri::composeComponents(
+            null,
+            null,
+            $uri->getPath(),
+            $uri->getQuery(),
+            $uri->getFragment(),
+        );
+        if (\mb_strlen($pathQueryFragment) > 35) {
+            $pathQueryFragment = \mb_substr($pathQueryFragment, 0, 15) . StringUtil::HELLIP . \mb_substr($pathQueryFragment, -15);
+        }
+
+        return $schemeHost . $pathQueryFragment;
     }
 }
