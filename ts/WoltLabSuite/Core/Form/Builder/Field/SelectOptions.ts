411442ec6032849b8da6964eb55c05975af72d03
Alexander Ebert <ebert@woltlab.com>
2025-11-29 18:37:22 +0100

    Refactor the UI for the select options
    See https://www.woltlab.com/community/thread/314959/
    
    The entire component was rewritten to solve multiple issues:
    * The UI took too much space on mobile, causing it to be unusable.
    * Creating the DOM entirely in JS was cumbersome, using a `<template>` made it easier to make adjustments.
    * The component relied on `_languages` as a global state which could be mutated with every invocation of `setup()`.

--- ts/WoltLabSuite/Core/Form/Builder/Field/SelectOptions.ts
+++ ts/WoltLabSuite/Core/Form/Builder/Field/SelectOptions.ts
@@ -10,165 +10,139 @@ type Data = {
 
 type Languages = Record<string, string>;
 
-let _languages: Languages;
+class SelectOptions {
+  readonly #languages: Languages;
+  readonly #list: HTMLUListElement;
+  readonly #template: HTMLTemplateElement;
+
+  constructor(formField: HTMLInputElement, languages: Languages) {
+    if (formField.form === null) {
+      throw new Error("Cannot create select options for an form field that is not part of a form.", {
+        cause: {
+          formField,
+        },
+      });
+    }
+
+    this.#languages = languages;
+    this.#template = document.getElementById(`${formField.id}_template`) as HTMLTemplateElement;
+
+    this.#list = this.#createUi(formField);
+    this.#createListItems(formField);
+    this.#initializeSorting();
+
+    formField.form.addEventListener("submit", () => {
+      this.#setHiddenValue(formField);
+    });
+  }
+
+  #createUi(formField: HTMLInputElement): HTMLUListElement {
+    const ul = document.createElement("ul");
+    ul.classList.add("selectOptionsList");
+    formField.insertAdjacentElement("afterend", ul);
+
+    const addButton = this.#createAddButton();
+    addButton.addEventListener("click", () => {
+      this.#createRow(undefined, true);
+    });
+    ul.insertAdjacentElement("afterend", addButton);
+
+    return ul;
+  }
+
+  #createListItems(formField: HTMLInputElement): void {
+    if (formField.value) {
+      const data = JSON.parse(formField.value) as Data[];
+      data.forEach((option) => {
+        this.#createRow(option);
+      });
+    } else {
+      this.#createRow();
+    }
+  }
+
+  #createAddButton(): HTMLButtonElement {
+    const button = document.createElement("button");
+    button.type = "button";
+    button.classList.add("button", "small", "selectOptionsListItem__addItem");
+    button.textContent = getPhrase("wcf.form.selectOptions.addItem");
+
+    return button;
+  }
+
+  #createRow(option?: Data, autoFocus: boolean = false): void {
+    const li = document.createElement("li");
+    li.classList.add("selectOptionsListItem");
+    li.append(this.#template.content.cloneNode(true));
+
+    this.#list.append(li);
+
+    const removeButton = li.querySelector(".selectOptionsListItem__remove") as HTMLButtonElement;
+    removeButton.addEventListener("click", () => {
+      li.remove();
+
+      if (!this.#list.childElementCount) {
+        this.#createRow();
+      }
+    });
+
+    const keyInput = li.querySelector(".selectOptionsListItem__key") as HTMLInputElement;
+    keyInput.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        this.#createRow(undefined, true);
+      }
+    });
+    keyInput.value = option ? option.key : "";
+
+    const valueInput = li.querySelector(".selectOptionsListItem__value") as HTMLInputElement;
+    valueInput.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        this.#createRow(undefined, true);
+      }
+    });
+
+    const hasI18nValues = option && !Object.hasOwn(option.value, 0);
+
+    initI18n(identify(valueInput), hasI18nValues ? option.value : {}, this.#languages, false);
+
+    if (!hasI18nValues) {
+      valueInput.value = option?.value[0] ?? "";
+    }
+
+    if (autoFocus) {
+      keyInput.focus();
+    }
+  }
+
+  #initializeSorting(): void {
+    new Sortable(this.#list, {
+      direction: "vertical",
+      animation: 150,
+      fallbackOnBody: true,
+      draggable: "li",
+      handle: ".selectOptionsListItem__handle",
+    });
+  }
+
+  #setHiddenValue(formField: HTMLInputElement): void {
+    const data: Data[] = [];
+
+    this.#list.querySelectorAll(".selectOptionsListItem").forEach((li) => {
+      const key = li.querySelector<HTMLInputElement>(".selectOptionsListItem__key")!.value;
+      const valueInput = li.querySelector<HTMLInputElement>(".selectOptionsListItem__value")!;
+
+      data.push({
+        key,
+        value: Object.fromEntries(getValues(valueInput.id)),
+      });
+    });
+
+    formField.value = JSON.stringify(data);
+  }
+}
 
 export function setup(formField: HTMLInputElement, languages: Languages): void {
-  _languages = languages;
-
-  const ul = createUi(formField);
-
-  formField.form?.addEventListener("submit", () => {
-    setHiddenValue(formField);
-  });
-
-  new Sortable(ul, {
-    direction: "vertical",
-    animation: 150,
-    fallbackOnBody: true,
-    draggable: "li",
-    handle: ".selectOptionsListItem__handle",
-  });
-}
-
-function createUi(formField: HTMLInputElement): HTMLUListElement {
-  const ul = document.createElement("ul");
-  ul.classList.add("selectOptionsList");
-  formField.parentElement?.append(ul);
-
-  if (formField.value) {
-    const data = JSON.parse(formField.value) as Data[];
-    data.forEach((option) => {
-      createRow(ul, option);
-    });
-  } else {
-    createRow(ul);
-  }
-
-  return ul;
-}
-
-function createRow(ul: HTMLUListElement, option?: Data, autoFocus: boolean = false): void {
-  const li = document.createElement("li");
-  li.classList.add("selectOptionsListItem");
-  ul.append(li);
-
-  const addButton = getAddButton();
-  addButton.addEventListener("click", () => {
-    createRow(ul, undefined, true);
-  });
-
-  const deleteButton = getDeleteButton();
-  deleteButton.addEventListener("click", () => {
-    li.remove();
-
-    if (!ul.childElementCount) {
-      createRow(ul);
-    }
-  });
-
-  const keyInput = getKeyInput();
-  keyInput.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      createRow(ul, undefined, true);
-    }
-  });
-  keyInput.value = option ? option.key : "";
-
-  const equalsIcon = document.createElement("fa-icon");
-  equalsIcon.setIcon("equals");
-
-  const valueInput = getValueInput();
-  valueInput.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      createRow(ul, undefined, true);
-    }
-  });
-
-  li.append(getSortableHandle(), addButton, deleteButton, keyInput, equalsIcon, valueInput);
-
-  const hasI18nValues = option && !Object.hasOwn(option.value, 0);
-
-  initI18n(identify(valueInput), hasI18nValues ? option.value : {}, _languages, false);
-
-  if (!hasI18nValues) {
-    valueInput.value = option?.value[0] ?? "";
-  }
-
-  if (autoFocus) {
-    keyInput.focus();
-  }
-}
-
-function getAddButton(): HTMLButtonElement {
-  const addIcon = document.createElement("fa-icon");
-  addIcon.setIcon("plus");
-
-  const addButton = document.createElement("button");
-  addButton.type = "button";
-  addButton.append(addIcon);
-  addButton.classList.add("jsTooltip");
-  addButton.title = getPhrase("wcf.global.button.add");
-
-  return addButton;
-}
-
-function getDeleteButton(): HTMLButtonElement {
-  const deleteIcon = document.createElement("fa-icon");
-  deleteIcon.setIcon("xmark");
-
-  const deleteButton = document.createElement("button");
-  deleteButton.type = "button";
-  deleteButton.append(deleteIcon);
-  deleteButton.classList.add("jsTooltip");
-  deleteButton.title = getPhrase("wcf.global.button.delete");
-
-  return deleteButton;
-}
-
-function getKeyInput(): HTMLInputElement {
-  const keyInput = document.createElement("input");
-  keyInput.classList.add("selectOptionsListItem__key");
-  keyInput.placeholder = getPhrase("wcf.form.selectOptions.key");
-  keyInput.type = "text";
-  keyInput.required = true;
-
-  return keyInput;
-}
-
-function getValueInput(): HTMLInputElement {
-  const valueInput = document.createElement("input");
-  valueInput.classList.add("selectOptionsListItem__value");
-  valueInput.placeholder = getPhrase("wcf.form.selectOptions.value");
-  valueInput.type = "text";
-  valueInput.required = true;
-
-  return valueInput;
-}
-
-function getSortableHandle(): HTMLElement {
-  const icon = document.createElement("fa-icon");
-  icon.setIcon("up-down");
-  const handle = document.createElement("span");
-  handle.append(icon);
-  handle.classList.add("selectOptionsListItem__handle");
-
-  return handle;
-}
-
-function setHiddenValue(formField: HTMLInputElement): void {
-  const data: Data[] = [];
-
-  formField.parentElement?.querySelectorAll(".selectOptionsListItem").forEach((li) => {
-    const key = li.querySelector<HTMLInputElement>(".selectOptionsListItem__key")!.value;
-    const valueInput = li.querySelector<HTMLInputElement>(".selectOptionsListItem__value")!;
-
-    data.push({
-      key,
-      value: Object.fromEntries(getValues(valueInput.id)),
-    });
-  });
-
-  formField.value = JSON.stringify(data);
+  new SelectOptions(formField, languages);
 }
