480d012a32aa332b824fbff4c720d9428e66b739
Alexander Ebert <ebert@woltlab.com>
2026-01-07 17:56:26 +0100

    Align the avatar management with the cover photo

--- wcfsetup/install/files/js/WoltLabSuite/Core/Component/User/Avatar.js
+++ wcfsetup/install/files/js/WoltLabSuite/Core/Component/User/Avatar.js
@@ -7,27 +7,22 @@
  * @since     6.2
  * @woltlabExcludeBundle all
  */
-define(["require", "exports", "WoltLabSuite/Core/Helper/PromiseMutex", "WoltLabSuite/Core/Helper/Selector", "WoltLabSuite/Core/Component/Dialog", "WoltLabSuite/Core/Form/Builder/Field/Controller/FileProcessor", "../Snackbar"], function (require, exports, PromiseMutex_1, Selector_1, Dialog_1, FileProcessor_1, Snackbar_1) {
+define(["require", "exports", "tslib", "WoltLabSuite/Core/Helper/PromiseMutex", "WoltLabSuite/Core/Helper/Selector", "WoltLabSuite/Core/Component/Dialog", "WoltLabSuite/Core/Form/Builder/Field/Controller/FileProcessor", "../Snackbar", "WoltLabSuite/Core/Ajax/Backend", "WoltLabSuite/Core/Form/Builder/Manager"], function (require, exports, tslib_1, PromiseMutex_1, Selector_1, Dialog_1, FileProcessor_1, Snackbar_1, Backend_1, FormBuilderManager) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.setup = setup;
+    FormBuilderManager = tslib_1.__importStar(FormBuilderManager);
+    let defaultAvatar = "";
     async function editAvatar(button) {
-        const { ok, result } = await (0, Dialog_1.dialogFactory)().usingFormBuilder().fromEndpoint(button.dataset.editAvatar);
-        if (ok) {
-            const avatarForm = document.getElementById("avatarForm");
-            if (avatarForm) {
-                const img = avatarForm.querySelector("img.userAvatarImage");
-                if (img.src === result.avatar) {
-                    return;
-                }
-                // In the ACP, the form should not be reloaded after changing the avatar.
-                img.src = result.avatar;
-                (0, Snackbar_1.showDefaultSuccessSnackbar)();
+        defaultAvatar = button.dataset.defaultAvatar || "";
+        const json = (await (0, Backend_1.prepareRequest)(button.dataset.editAvatar).get().fetchAsJson());
+        const dialog = (0, Dialog_1.dialogFactory)().fromHtml(json.dialog).withoutControls();
+        dialog.addEventListener("afterClose", () => {
+            if (FormBuilderManager.hasForm(json.formId)) {
+                FormBuilderManager.unregisterForm(json.formId);
             }
-            else {
-                window.location.reload();
-            }
-        }
+        });
+        dialog.show(json.title);
     }
     function setup() {
         (0, Selector_1.wheneverFirstSeen)(`#wcf\\\\action\\\\UserAvatarAction_avatarFileIDContainer woltlab-core-file img`, (img) => {
@@ -37,24 +32,26 @@ define(["require", "exports", "WoltLabSuite/Core/Helper/PromiseMutex", "WoltLabS
         const avatarForm = document.getElementById("avatarForm");
         if (avatarForm) {
             (0, FileProcessor_1.registerCallback)("wcf\\action\\UserAvatarAction_avatarFileID", (fileId) => {
-                if (!fileId) {
-                    return;
+                let link = defaultAvatar;
+                if (fileId !== undefined) {
+                    const file = document.querySelector(`#wcf\\\\action\\\\UserAvatarAction_avatarFileIDContainer woltlab-core-file[file-id="${fileId}"]`);
+                    link = file.link;
                 }
-                const file = document.querySelector(`#wcf\\\\action\\\\UserAvatarAction_avatarFileIDContainer woltlab-core-file[file-id="${fileId}"]`);
-                avatarForm.querySelector("img.userAvatarImage").src = file.link;
+                avatarForm.querySelector("img.userAvatarImage").src = link;
+                document
+                    .getElementById("wcf\\action\\UserAvatarAction_avatarFileIDContainer")
+                    ?.closest("woltlab-core-dialog")
+                    ?.close();
                 (0, Snackbar_1.showDefaultSuccessSnackbar)();
             });
         }
         else {
-            (0, FileProcessor_1.registerCallback)("wcf\\action\\UserAvatarAction_avatarFileID", (fileId) => {
-                if (fileId === undefined) {
-                    return;
-                }
+            (0, FileProcessor_1.registerCallback)("wcf\\action\\UserAvatarAction_avatarFileID", () => {
                 document
                     .getElementById("wcf\\action\\UserAvatarAction_avatarFileIDContainer")
                     ?.closest("woltlab-core-dialog")
-                    ?.querySelector(".dialog__control__button--primary")
-                    ?.click();
+                    ?.close();
+                window.location.reload();
             });
         }
         (0, Selector_1.wheneverFirstSeen)("[data-edit-avatar]", (button) => {
