86964a59190fbba742bbf1998717bf874d594e0a
Alexander Ebert <ebert@woltlab.com>
2026-01-20 14:22:31 +0100

    Migrate discussions when transitioning article to i18n
    See https://www.woltlab.com/community/thread/315856/

--- wcfsetup/install/files/lib/command/article/EnableI18n.class.php
+++ wcfsetup/install/files/lib/command/article/EnableI18n.class.php
@@ -4,9 +4,14 @@ namespace wcf\command\article;
 
 use wcf\data\article\Article;
 use wcf\data\article\ArticleAction;
+use wcf\data\article\content\ArticleContent;
 use wcf\data\article\content\ArticleContentAction;
+use wcf\data\article\content\ArticleContentEditor;
+use wcf\data\object\type\ObjectTypeCache;
+use wcf\system\article\discussion\IArticleDiscussionProvider;
 use wcf\system\language\LanguageFactory;
 use wcf\system\version\VersionTracker;
+use wcf\system\WCF;
 
 /**
  * Converts a monolingual article to a multilingual.
@@ -36,13 +41,18 @@ final class EnableI18n
             ];
         }
 
+        $discussionProvider = $this->article->getDiscussionProvider();
+
         $action = new ArticleAction([$this->article], 'update', [
             'content' => $data,
             'data' => [
                 'isMultilingual' => 1,
             ],
+            'migrateDiscussions' => true,
         ]);
         $action->executeAction();
+
+        $this->migrateDiscussions($discussionProvider, $this->article, $articleContent);
 
         $action = new ArticleContentAction([$articleContent], 'delete');
         $action->executeAction();
@@ -51,5 +61,23 @@ final class EnableI18n
             'com.woltlab.wcf.article',
             $this->article->articleID
         );
+    }
+
+    /**
+     * Preserves the comments by associating the existing comments with the
+     * content matching the site’s default language id.
+     */
+    private function migrateDiscussions(IArticleDiscussionProvider $discussionProvider, Article $article, ArticleContent $oldContent): void
+    {
+        $article = new Article($article->articleID);
+        $newContent = $article->getArticleContents()[LanguageFactory::getInstance()->getDefaultLanguageID()] ?? null;
+        if ($newContent === null) {
+            // This shouldn’t be possible but throwing an exception here would
+            // yield a malformed article that cannot be fixed except through
+            // manual editing of the database.
+            return;
+        }
+
+        $discussionProvider->migrateDiscussions($oldContent, $newContent);
     }
 }
