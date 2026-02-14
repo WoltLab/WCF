909f4231d5f9c0ae36f92eef0fac4217ba54c190
Marcel Werk <burntime@woltlab.com>
2026-02-14 17:15:51 +0100

    Add missing column for the subscription to paid subscription log list

--- wcfsetup/install/files/lib/system/gridView/admin/PaidSubscriptionTransactionLogGridView.class.php
+++ wcfsetup/install/files/lib/system/gridView/admin/PaidSubscriptionTransactionLogGridView.class.php
@@ -89,6 +89,24 @@ final class PaidSubscriptionTransactionLogGridView extends AbstractGridView
                     }
                 )
                 ->filter(UserFilter::class),
+            GridViewColumn::for('subscriptionID')
+                ->label('wcf.acp.paidSubscription.subscription')
+                ->renderer(
+                    new class extends DefaultColumnRenderer {
+                        #[\Override]
+                        public function render(mixed $value, DatabaseObject $row): string
+                        {
+                            \assert($row instanceof PaidSubscriptionTransactionLog);
+
+                            if (!$row->subscriptionID) {
+                                return '';
+                            }
+
+                            $subscription = PaidSubscriptionCacheBuilder::getInstance()->getData()[$row->subscriptionID];
+                            return StringUtil::encodeHTML($subscription->getTitle());
+                        }
+                    }
+                ),
             GridViewColumn::for('paymentMethodObjectTypeID')
                 ->label('wcf.acp.paidSubscription.transactionLog.paymentMethod')
                 ->filter(new SelectFilter(
