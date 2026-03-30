98895058bcd43ffdb2b7c092e330bc060c3e9a2e
Alexander Ebert <ebert@woltlab.com>
2026-03-30 17:33:00 +0200

    Register object filters through object types, serialization support

--- wcfsetup/install/files/lib/system/form/builder/field/ObjectFilterFormField.class.php
+++ wcfsetup/install/files/lib/system/form/builder/field/ObjectFilterFormField.class.php
@@ -2,11 +2,18 @@
 
 namespace wcf\system\form\builder\field;
 
+use CuyZ\Valinor\Mapper\MappingError;
+use CuyZ\Valinor\Mapper\Source\Source;
+use CuyZ\Valinor\MapperBuilder;
 use Override;
 use wcf\system\form\builder\field\validation\FormFieldValidationError;
+use wcf\system\form\builder\TObjectTypeFormNode;
+use wcf\system\object\filter\builder\IObjectFilterBuilder;
 
 final class ObjectFilterFormField extends AbstractFormField
 {
+    use TObjectTypeFormNode;
+
     protected $templateName = 'shared_objectFilterFormField';
 
     #[Override]
@@ -22,9 +29,92 @@ final class ObjectFilterFormField extends AbstractFormField
     #[Override]
     public function validate(): void
     {
-        // TODO: This error only exists for development purposes!
+        try {
+            $values = $this->unserializeFilters($this->getValue());
+        } catch (MappingError) {
+            $this->addValidationError(
+                new FormFieldValidationError('malformedJson')
+            );
+
+            return;
+        }
+
+        if ($this->isRequired() && $values === []) {
+            $this->addValidationError(
+                new FormFieldValidationError('empty')
+            );
+        }
+
+        // TODO: debug only
         $this->addValidationError(
             new FormFieldValidationError('empty')
+        );
+    }
+
+    #[Override]
+    public function getObjectTypeDefinition(): string
+    {
+        return 'com.woltlab.wcf.objectFilter';
+    }
+
+    public function toJson(): string
+    {
+        return \json_encode(
+            $this->unserializeFilters($this->getValue()),
+            \JSON_THROW_ON_ERROR,
+        );
+    }
+
+    /**
+     * @return list<array{
+     *  identifier: string,
+     *  summary: string,
+     *  value: string,
+     * }>
+     * @throws MappingError
+     */
+    private function unserializeFilters(?string $json): array
+    {
+        if ($json === null) {
+            return [];
+        }
+
+        /** @var list<array{0: string, 1: string}> $values */
+        $values = (new MapperBuilder())->mapper()->map(
+            <<<'EOT'
+                list<array{0: string, 1: string}>
+                EOT,
+            Source::json($json)
+        );
+
+        if ($values === []) {
+            return $values;
+        }
+
+        /** @var IObjectFilterBuilder $builder */
+        $builder = $this->getObjectType()->getProcessor();
+        $filters = [];
+        foreach ($builder->getFilters() as $filter) {
+            $filters[$filter->getIdentifier()] = $filter;
+        }
+
+        return \array_map(
+            function (array $value) use ($filters): array {
+                [$identifier, $serializedValue] = $value;
+                $filter = $filters[$identifier];
+
+                return [
+                    'identifier' => $identifier,
+                    'summary' => $filter->summarizeValue(
+                        $filter->unserializeValue($serializedValue),
+                    ),
+                    'value' => $serializedValue,
+                ];
+            },
+            \array_filter(
+                $values,
+                static fn($value) => isset($filters[$value[0]]),
+            ),
         );
     }
 }
