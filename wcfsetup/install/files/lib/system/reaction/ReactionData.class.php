d389cc2388e3bde51b7d0853f8210ad1f1748dcb
Marcel Werk <burntime@woltlab.com>
2026-03-31 17:27:58 +0200

    Change default value of `reactionsJson` to valid JSON

--- wcfsetup/install/files/lib/system/reaction/ReactionData.class.php
+++ wcfsetup/install/files/lib/system/reaction/ReactionData.class.php
@@ -2,6 +2,14 @@
 
 namespace wcf\system\reaction;
 
+/**
+ * Data transfer object for an object that has reactions.
+ *
+ * @author      Marcel Werk
+ * @copyright   2001-2026 WoltLab GmbH
+ * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since       6.3
+ */
 final class ReactionData
 {
     public function __construct(
@@ -9,6 +17,6 @@ final class ReactionData
         public readonly int $objectID,
         public readonly int $reactionTypeID = 0,
         public readonly ?string $cachedReactions = null,
-        public readonly ?string $reactionsJson = null,
+        public readonly string $reactionsJson = '[]',
     ) {}
 }
