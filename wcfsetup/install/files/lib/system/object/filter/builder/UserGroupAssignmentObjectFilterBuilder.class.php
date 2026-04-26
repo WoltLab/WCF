95c05386af27ad19ce07f3f492d0a01f034a1640
Alexander Ebert <ebert@woltlab.com>
2026-04-26 13:03:44 +0200

    Simplify the calls to apply the conditions

--- wcfsetup/install/files/lib/system/object/filter/builder/UserGroupAssignmentObjectFilterBuilder.class.php
+++ wcfsetup/install/files/lib/system/object/filter/builder/UserGroupAssignmentObjectFilterBuilder.class.php
@@ -3,7 +3,11 @@
 namespace wcf\system\object\filter\builder;
 
 use Override;
+use wcf\data\user\group\assignment\UserGroupAssignment;
+use wcf\data\user\User;
+use wcf\system\database\util\PreparedStatementConditionBuilder;
 use wcf\system\object\filter\IObjectFilter;
+use wcf\system\object\filter\ObjectFilterHandler;
 use wcf\system\object\filter\user\UserAvatarObjectFilter;
 use wcf\system\object\filter\user\UserLanguageObjectFilter;
 
@@ -13,6 +17,8 @@ final class UserGroupAssignmentObjectFilterBuilder implements IObjectFilterBuild
      * @var list<IObjectFilter<mixed>>
      */
     private readonly array $filters;
+
+    private ObjectFilterHandler $handler;
 
     public function __construct()
     {
@@ -26,5 +32,38 @@ final class UserGroupAssignmentObjectFilterBuilder implements IObjectFilterBuild
     public function getFilters(): array
     {
         return $this->filters;
+    }
+
+    #[Override]
+    public function getObjectTypeName(): string
+    {
+        return 'com.woltlab.wcf.userGroupAssignment';
+    }
+
+    public function applyFilters(UserGroupAssignment $assignment, PreparedStatementConditionBuilder $conditions): void
+    {
+        $this->getHandler()->applyFilters(
+            $conditions,
+            $this->getObjectTypeName(),
+            $assignment->conditions,
+        );
+    }
+
+    public function testUser(UserGroupAssignment $assignment, User $user): bool
+    {
+        return $this->getHandler()->testUser(
+            $user,
+            $this->getObjectTypeName(),
+            $assignment->conditions,
+        );
+    }
+
+    private function getHandler(): ObjectFilterHandler
+    {
+        if (!isset($this->handler)) {
+            $this->handler = new ObjectFilterHandler($this->getFilters());
+        }
+
+        return $this->handler;
     }
 }
