01e46a915f057d34756257c297d1d925fea04d59
Alexander Ebert <ebert@woltlab.com>
2026-07-31 23:16:15 +0200

    Improve the type safety
    An array passes the `isset()` check, yielding an error message when trying to pass that to `resolveActiveApplication()`.

--- wcfsetup/install/files/lib/system/WCF.class.php
+++ wcfsetup/install/files/lib/system/WCF.class.php
@@ -527,7 +527,7 @@ class WCF
      */
     protected function resolveActiveApplication(): void
     {
-        if (!isset($_GET['__rewrittenPath']) || \defined('PACKAGE_ID')) {
+        if (!isset($_GET['__rewrittenPath']) || !\is_string($_GET['__rewrittenPath']) || \defined('PACKAGE_ID')) {
             if (!\defined('PACKAGE_ID')) {
                 \define('PACKAGE_ID', 1);
             }
