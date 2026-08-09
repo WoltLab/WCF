b7a0c8796ae950e7128f05d7dc423117a4c9dc18
Alexander Ebert <ebert@woltlab.com>
2026-08-09 16:30:20 +0200

    Prevent disclosure of inaccessible categories via CategoryFilter

--- wcfsetup/install/files/lib/system/view/filter/CategoryFilter.class.php
+++ wcfsetup/install/files/lib/system/view/filter/CategoryFilter.class.php
@@ -2,6 +2,7 @@
 
 namespace wcf\system\view\filter;
 
+use wcf\data\category\Category;
 use wcf\data\DatabaseObjectList;
 use wcf\system\category\CategoryHandler;
 use wcf\system\form\builder\field\AbstractFormField;
@@ -18,6 +19,11 @@ use wcf\system\view\filter\exception\InvalidFilterValue;
  */
 class CategoryFilter extends AbstractFilter
 {
+    /**
+     * @var list<int>|null
+     */
+    private ?array $availableCategoryIDs = null;
+
     /**
      * @param \Traversable<mixed> $options
      */
@@ -42,19 +48,49 @@ class CategoryFilter extends AbstractFilter
     #[\Override]
     public function applyFilter(DatabaseObjectList $list, string $value): void
     {
-        $category = CategoryHandler::getInstance()->getCategory((int)$value);
+        $category = $this->getCategory($value);
         if ($category === null) {
             throw new InvalidFilterValue("Invalid value '{$value}' for filter '{$this->id}' given.");
         }
 
         $columnName = $this->getDatabaseColumnName($list);
 
-        $list->getConditionBuilder()->add("{$columnName} = ?", [$value]);
+        $list->getConditionBuilder()->add("{$columnName} = ?", [$category->categoryID]);
     }
 
     #[\Override]
     public function renderValue(string $value): string
     {
-        return CategoryHandler::getInstance()->getCategory((int)$value)->getTitle();
+        return $this->getCategory($value)?->getTitle() ?? '';
+    }
+
+    /**
+     * Returns the category for the given value, but only if it is part of the
+     * categories offered by this filter. `CategoryHandler` holds the categories
+     * of every object type, therefore looking up the value there would accept
+     * arbitrary categories, including inaccessible ones.
+     */
+    private function getCategory(string $value): ?Category
+    {
+        if (!\in_array((int)$value, $this->getAvailableCategoryIDs(), true)) {
+            return null;
+        }
+
+        return CategoryHandler::getInstance()->getCategory((int)$value);
+    }
+
+    /**
+     * @return list<int>
+     */
+    private function getAvailableCategoryIDs(): array
+    {
+        if ($this->availableCategoryIDs === null) {
+            $this->availableCategoryIDs = \array_map(
+                static fn($option) => (int)$option->getObjectID(),
+                \iterator_to_array($this->options, false)
+            );
+        }
+
+        return $this->availableCategoryIDs;
     }
 }
