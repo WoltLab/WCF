ca0d1df70d4ed66392d48d900c02eeb668a95b8c
Alexander Ebert <ebert@woltlab.com>
2026-08-11 11:30:31 +0200

    Prevent XSS via unvalidated ACL values in permission JavaScript

--- wcfsetup/install/files/lib/system/acl/ACLHandler.class.php
+++ wcfsetup/install/files/lib/system/acl/ACLHandler.class.php
@@ -92,20 +92,34 @@ class ACLHandler extends SingletonFactory
                 ];
 
                 foreach ($optionData as $typeID => $optionValues) {
+                    // The ids originate from the request and may reference
+                    // objects that do not exist.
+                    $userGroup = null;
+                    if ($type === 'group') {
+                        $userGroup = UserGroup::getGroupByID($typeID);
+                        if ($userGroup === null) {
+                            continue;
+                        }
+                    } elseif (!isset($users[$typeID])) {
+                        continue;
+                    }
+
                     foreach ($optionValues as $optionID => $optionValue) {
-                        if (!isset($data['options'][$optionID])) {
+                        if (isset($data['options'][$optionID])) {
+                            $optionValues[$optionID] = (int)$optionValue;
+                        } else {
                             unset($optionValues[$optionID]);
                         }
                     }
 
-                    if (empty($optionValues)) {
+                    if ($optionValues === []) {
                         continue;
                     }
 
                     $values[$type]['option'][$typeID] = $optionValues;
 
                     if ($type === 'group') {
-                        $values[$type]['label'][$typeID] = UserGroup::getGroupByID($typeID)->getName();
+                        $values[$type]['label'][$typeID] = $userGroup->getName();
                     } else {
                         $values[$type]['label'][$typeID] = $users[$typeID]->username;
                     }
@@ -172,7 +186,7 @@ class ACLHandler extends SingletonFactory
 
                         foreach ($optionData as $optionID => $optionValue) {
                             if (isset($options[$optionID])) {
-                                $this->__readValues[$objectTypeID][$type][$typeID][$optionID] = $optionValue;
+                                $this->__readValues[$objectTypeID][$type][$typeID][$optionID] = (int)$optionValue;
                             }
                         }
                     }
@@ -304,7 +318,7 @@ class ACLHandler extends SingletonFactory
                     $optionID,
                     $objectID,
                     $typeID,
-                    $optionValue,
+                    (int)$optionValue,
                 ]);
             }
         }
