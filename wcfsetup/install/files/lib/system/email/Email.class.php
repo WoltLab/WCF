7f81562592dc747b5c0e9526f18ddcb0b2f38f4a
Alexander Ebert <ebert@woltlab.com>
2026-08-16 13:41:24 +0200

    Deep clone the email body to prevent leaking recipient data across recipients
    (cherry picked from commit 6909c89e8fcae0c57b0ad38bf0ff7bc24a7ed037)

--- wcfsetup/install/files/lib/system/email/Email.class.php
+++ wcfsetup/install/files/lib/system/email/Email.class.php
@@ -610,6 +610,17 @@ class Email
     }
 
     /**
+     * Deep clones the body, otherwise every clone of this email would share a
+     * single body and thus a single recipient (see getJobs()).
+     */
+    public function __clone(): void
+    {
+        if ($this->body !== null) {
+            $this->body = clone $this->body;
+        }
+    }
+
+    /**
      * Sets the body of this email.
      *
      * @param AbstractMimePart $body
@@ -680,8 +691,9 @@ class Email
                 $mail->addHeader('X-WoltLab-Suite-Recipient', $recipient['mailbox']->getUser()->username);
             }
 
-            if ($this->body instanceof IRecipientAwareMimePart) {
-                $this->body->setRecipient($recipient['mailbox']);
+            $body = $mail->getBody();
+            if ($body instanceof IRecipientAwareMimePart) {
+                $body->setRecipient($recipient['mailbox']);
             }
 
             $data = [
