2011bb8f0d92ad8b57b9faeddba2f9c363ebcf00
Alexander Ebert <ebert@woltlab.com>
2026-08-17 10:40:53 +0200

    Restrict the article feeds to an enabled MODULE_ARTICLE

--- wcfsetup/install/files/lib/page/ArticleFeedPage.class.php
+++ wcfsetup/install/files/lib/page/ArticleFeedPage.class.php
@@ -22,6 +22,11 @@ use wcf\system\WCF;
 class ArticleFeedPage extends AbstractFeedPage
 {
     /**
+     * @inheritDoc
+     */
+    public $neededModules = ['MODULE_ARTICLE'];
+
+    /**
      * category the listed articles belong to
      * @var ArticleCategory
      */
