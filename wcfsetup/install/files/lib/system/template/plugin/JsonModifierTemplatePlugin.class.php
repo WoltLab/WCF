e19e5815173de3b101358240d94e653a1d46d882
Alexander Ebert <ebert@woltlab.com>
2026-08-27 19:52:03 +0200

    Prevent XSS via `|json` output escaping the surrounding `<script>` element

--- wcfsetup/install/files/lib/system/template/plugin/JsonModifierTemplatePlugin.class.php
+++ wcfsetup/install/files/lib/system/template/plugin/JsonModifierTemplatePlugin.class.php
@@ -3,7 +3,6 @@
 namespace wcf\system\template\plugin;
 
 use wcf\system\template\TemplateEngine;
-use wcf\util\JSON;
 
 /**
  * JSON encodes the given value.
@@ -11,10 +10,10 @@ use wcf\util\JSON;
  * Usage:
  *  { "title": {$foo->getTitle()|json} }
  *
- * Depending on the location you might need to either HTML-encode the resulting JSON string
- * or not. Within a `<script>` tag, additional HTML-encoding usually is an error, as HTML is
- * not interpreted within there, thus `{@$var|json}` with the additional `@` will need to be
- * used.
+ * Inside a `<script>` element `{unsafe:$var|json}` must be used, the result contains no
+ * verbatim `<` and therefore cannot terminate the element. Within an HTML attribute
+ * `{$var|json}` must be used, because the structural quotes of the JSON value itself are
+ * not escaped and require the HTML-encoding.
  *
  * @author  Tim Duesterhus
  * @copyright   2001-2022 WoltLab GmbH
@@ -28,6 +27,12 @@ class JsonModifierTemplatePlugin implements IModifierTemplatePlugin
      */
     public function execute($tagArgs, TemplateEngine $tplObj)
     {
-        return JSON::encode($tagArgs[0]);
+        // `<` must never appear verbatim, otherwise a value containing `<!--<script `
+        // pushes the HTML tokenizer out of the surrounding `<script>` element and the
+        // remainder of the document is swallowed as script data.
+        return \json_encode(
+            $tagArgs[0],
+            \JSON_THROW_ON_ERROR | \JSON_HEX_TAG | \JSON_HEX_AMP | \JSON_HEX_APOS | \JSON_HEX_QUOT
+        );
     }
 }
